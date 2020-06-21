aaeaf02b5085edb82f031c633a25a691f4ba0375
Tiago Gimenes <tlgimenes@gmail.com>
2020-06-21 16:31:55 -0300

    adds back seo component

--- packages/gatsby-vtex/src/pages/index.tsx
+++ packages/gatsby-vtex/src/pages/index.tsx
@@ -1,16 +1,14 @@
-import React, { StrictMode } from 'react'
 import { Router } from '@reach/router'
+import React from 'react'
 
 import Home from '../components/Home'
 import Product from '../templates/product'
 
 export default function IndexPage() {
   return (
-    <StrictMode>
-      <Router>
-        <Home path="/" />
-        <Product path="/:slug/p" />
-      </Router>
-    </StrictMode>
+    <Router>
+      <Home path="/" />
+      <Product path="/:slug/p" />
+    </Router>
   )
 }
