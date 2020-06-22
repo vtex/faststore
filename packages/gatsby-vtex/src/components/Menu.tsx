4b35bbfd0d410a05ae1b35b9dd6c0bb6d76c01b7
Tiago Gimenes <tlgimenes@gmail.com>
2020-06-22 18:24:02 -0300

    adds store-theme like hard-coded menu

---
+++ packages/gatsby-vtex/src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'gatsby'
+import React, { FC } from 'react'
+
+const Menu: FC = () => (
+  <>
+    <Link to="/apparel---accessories">Apparel & Accessories</Link>
+    <Link to="/home---decor">Home & Decor</Link>
+    <Link to="/about">About us</Link>
+  </>
+)
+
+export default Menu
