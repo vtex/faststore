e9b5dcf860b475d62e623286bb8c6656219455d2
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-06-23 22:45:45 -0300

    get products by category

--- packages/gatsby-source-vtex/src/fetch.ts
+++ packages/gatsby-source-vtex/src/fetch.ts
@@ -2,5 +2,7 @@ import { Product, Category } from './types'
 import { getCatalogSystem } from './utils'
 
 export const ProductSearch = getCatalogSystem<Product>('products/search')
-
 export const CategoryTree = getCatalogSystem<Category>('category/tree/1')
+export const ProductsByCategory = getCatalogSystem<Product>(
+  ({ categoryId }) => `products/search?fq=C:${categoryId}&map=c&_from=0&_to=9`
+)
--- packages/gatsby-source-vtex/src/gatsby-node.ts
+++ packages/gatsby-source-vtex/src/gatsby-node.ts
@@ -2,53 +2,28 @@ import './setup'
 
 import { GatsbyNode, PluginOptions, SourceNodesArgs } from 'gatsby'
 
-import { ProductSearch, CategoryTree } from './fetch'
+import { ProductSearch, CategoryTree, ProductsByCategory } from './fetch'
+import { createProductNode, createCategoryNode } from './utils'
 
 export const sourceNodes: GatsbyNode['sourceNodes'] = async (
-  {
-    actions: { createNode },
-    createNodeId,
-    createContentDigest,
-  }: SourceNodesArgs,
+  args: SourceNodesArgs,
   options: PluginOptions
 ) => {
   // PRODUCT
-  const PRODUCT_NODE_TYPE = 'Product'
   const productData = await ProductSearch(options)
-  productData.forEach((product) => {
-    const data = {
-      ...product,
-      slug: `${product.linkText}/p`,
-    }
-    createNode({
-      ...data,
-      id: createNodeId(`${PRODUCT_NODE_TYPE}-${data.productId}`),
-      internal: {
-        type: PRODUCT_NODE_TYPE,
-        content: JSON.stringify(data),
-        contentDigest: createContentDigest(data),
-      },
-    })
-  })
+  productData.forEach((product) => createProductNode(args, product))
+
   // CATEGORY
-  const CATEGORY_NODE_TYPE = 'Category'
   const categoryData = await CategoryTree(options)
-  categoryData.forEach(({ id, children, ...category }) => {
-    const urlSplited = category.url.split('/')
-    const slug = urlSplited[urlSplited.length - 1]
-    const data = {
-      ...category,
-      slug,
-      categoryId: id,
-    }
-    createNode({
-      ...data,
-      id: createNodeId(`${CATEGORY_NODE_TYPE}-${id}`),
-      internal: {
-        type: CATEGORY_NODE_TYPE,
-        content: JSON.stringify(data),
-        contentDigest: createContentDigest(data),
-      },
+  categoryData.forEach(async (category) => {
+    createCategoryNode(args, category)
+
+    // GET THE FIRST 10 PRODUCTS BY THIS CATEGORY
+    const productsByCategory = await ProductsByCategory({
+      ...options,
+      categoryId: category.id,
     })
+
+    productsByCategory.forEach((product) => createProductNode(args, product))
   })
 }
