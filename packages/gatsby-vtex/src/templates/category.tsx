28358cae998ed54a6fae987f69b152ffc14d8f8d
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-06-23 22:46:02 -0300

    list products in category page

--- packages/gatsby-vtex/src/templates/category.tsx
+++ packages/gatsby-vtex/src/templates/category.tsx
@@ -1,21 +1,38 @@
-import { Category } from '@vtex/gatsby-source-vtex'
+import { Category, Product } from '@vtex/gatsby-source-vtex'
 import { graphql } from 'gatsby'
 import React, { FC } from 'react'
 import { Flex, Heading } from 'theme-ui'
 
 import Layout from '../components/Layout'
 import SEO from '../components/Seo'
+import { ProductList } from '../components/ProductList'
 
 export const staticQuery = graphql`
-  query($id: String!) {
+  query($id: String!, $categoryId: String!) {
     category(id: { eq: $id }) {
       name
+    }
+    allProduct(filter: { categoryId: { eq: $categoryId } }) {
+      nodes {
+        id
+        productName
+        slug
+        productName
+        items {
+          images {
+            imageUrl
+          }
+        }
+      }
     }
   }
 `
 
 interface Props {
   data: {
+    allProduct: {
+      nodes: Product[]
+    }
     category: Category
   }
 }
@@ -26,9 +43,7 @@ const CategoryTemplate: FC<Props> = ({ data }) => {
       <SEO title={data.category.name} />
       <Flex sx={{ flexDirection: 'column' }} my={4}>
         <Heading as="h1">{data.category.name}</Heading>
-        <Flex sx={{ flexWrap: 'wrap' }} mt={4}>
-          products
-        </Flex>
+        <ProductList data={data.allProduct.nodes} />
       </Flex>
     </Layout>
   )
