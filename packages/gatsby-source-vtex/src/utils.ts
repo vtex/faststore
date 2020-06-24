6d57d17b706a67590097f3609a5598b16835b645
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-06-23 22:45:45 -0300

    get products by category

--- packages/gatsby-source-vtex/src/utils.ts
+++ packages/gatsby-source-vtex/src/utils.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { PluginOptions } from 'gatsby'
+import { PluginOptions, SourceNodesArgs } from 'gatsby'
+
+import { Product, Category } from './types'
 
 interface ApiUrl {
   tenant: unknown
@@ -7,14 +9,80 @@ interface ApiUrl {
   sufix: string
 }
 
+type SufixVoid = (options: PluginOptions) => string
+type Sufix = string | SufixVoid
+
 const getApiUrl = ({ tenant, environment, sufix }: ApiUrl): string =>
   `https://${tenant}.${environment}.com.br/api/catalog_system/pub/${sufix}`
 
-export const getCatalogSystem = <T>(sufix: string) => async ({
+export const getCatalogSystem = <T>(sufix: Sufix) => async ({
   tenant,
   environment,
+  ...options
 }: PluginOptions): Promise<T[]> => {
-  const url = getApiUrl({ tenant, environment, sufix })
-  const { data } = await axios.get(url)
+  const url = getApiUrl({
+    tenant,
+    environment,
+    sufix: typeof sufix === 'string' ? sufix : sufix(options),
+  })
+  const { data } = await axios.get(url, {
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+    },
+  })
   return data
+}
+
+export const createProductNode = (
+  {
+    actions: { createNode },
+    createNodeId,
+    createContentDigest,
+  }: SourceNodesArgs,
+  product: Product
+) => {
+  const NODE_TYPE = 'Product'
+  const data = {
+    ...product,
+    slug: `/${product.linkText}/p`,
+  }
+
+  createNode({
+    ...data,
+    id: createNodeId(`${NODE_TYPE}-${data.productId}`),
+    internal: {
+      type: NODE_TYPE,
+      content: JSON.stringify(data),
+      contentDigest: createContentDigest(data),
+    },
+  })
+}
+
+export const createCategoryNode = (
+  {
+    actions: { createNode },
+    createNodeId,
+    createContentDigest,
+  }: SourceNodesArgs,
+  { id, children, ...category }: Category
+) => {
+  const NODE_TYPE = 'Category'
+  const urlSplited = category.url.split('/')
+  const slug = urlSplited[urlSplited.length - 1]
+  const data = {
+    ...category,
+    slug,
+    categoryId: id,
+  }
+
+  createNode({
+    ...data,
+    id: createNodeId(`${NODE_TYPE}-${data.categoryId}`),
+    internal: {
+      type: NODE_TYPE,
+      content: JSON.stringify(data),
+      contentDigest: createContentDigest(data),
+    },
+  })
 }
