7aa2b75c2fc94c50d56028d45492155e5ee6d335
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-06-24 15:36:34 -0300

    add paginate in product search

--- packages/gatsby-source-vtex/src/fetch.ts
+++ packages/gatsby-source-vtex/src/fetch.ts
@@ -1,7 +1,9 @@
 import { Product, Category } from './types'
 import { getCatalogSystem } from './utils'
 
-export const ProductSearch = getCatalogSystem<Product>('products/search')
+export const ProductSearch = getCatalogSystem<Product>(
+  'products/search?_from=0&_to=49'
+)
 export const CategoryTree = getCatalogSystem<Category>('category/tree/1')
 export const ProductsByCategory = getCatalogSystem<Product>(
   ({ categoryId }) => `products/search?fq=C:${categoryId}&map=c&_from=0&_to=9`
