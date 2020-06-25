91c61b75277a1dc7943d06daf1b17975a7bf394f
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-06-25 16:07:32 -0300

    add sufix an

--- packages/gatsby-source-vtex/src/fetch.ts
+++ packages/gatsby-source-vtex/src/fetch.ts
@@ -2,6 +2,8 @@ import { Product, Category } from './types'
 import { getCatalogSystem } from './utils'
 
 export const ProductSearch = getCatalogSystem<Product>(
-  'products/search?_from=0&_to=9'
+  ({ tenant }) => `products/search?_from=0&_to=9&an${tenant}`
 )
-export const CategoryTree = getCatalogSystem<Category>('category/tree/1')
+export const CategoryTree = getCatalogSystem<Category>(
+  ({ tenant }) => `category/tree/1?an=${tenant}`
+)
