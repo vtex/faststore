b7a792dfb623879f1bb4b615b94dcbaa786ad6d2
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-06-25 16:17:39 -0300

    remove an

--- packages/gatsby-source-vtex/src/fetch.ts
+++ packages/gatsby-source-vtex/src/fetch.ts
@@ -2,8 +2,6 @@ import { Product, Category } from './types'
 import { getCatalogSystem } from './utils'
 
 export const ProductSearch = getCatalogSystem<Product>(
-  ({ tenant }) => `products/search?_from=0&_to=9&an${tenant}`
+  'products/search?_from=0&_to=9'
 )
-export const CategoryTree = getCatalogSystem<Category>(
-  ({ tenant }) => `category/tree/1?an=${tenant}`
-)
+export const CategoryTree = getCatalogSystem<Category>('category/tree/1')
