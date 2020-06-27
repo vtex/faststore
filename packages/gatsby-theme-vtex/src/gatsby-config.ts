3669dc227d4e925918d25ea57956db4f5fa1be2a
Tiago Gimenes <tlgimenes@gmail.com>
2020-06-26 23:51:08 -0300

    adds plugins and sources

---
+++ packages/gatsby-theme-vtex/src/gatsby-config.ts
@@ -0,0 +1,45 @@
+interface Options {
+  title: string
+  description: string
+  tenant: string
+  environment: 'vtexcommercestable' | 'vtexcommercebeta'
+}
+
+module.exports = ({ title, description, tenant, environment }: Options) => ({
+  siteMetadata: {
+    title,
+    description,
+    vtex: {
+      tenant,
+    },
+  },
+  plugins: [
+    {
+      resolve: require.resolve('gatsby-plugin-react-helmet'),
+    },
+    {
+      resolve: require.resolve('gatsby-plugin-theme-ui'),
+    },
+    {
+      // Transform cms's json files into .tsx nodes so other
+      // scripts can use it to generate files or templates
+      resolve: require.resolve('@vtex/gatsby-transformer-vtex-cms'),
+    },
+    {
+      // Adds search info into the Gatsby's source graph. This is
+      // the plugin responsible for adding Product/Category/Brand
+      // info into the gatsby's source graph
+      resolve: require.resolve('@vtex/gatsby-source-vtex'),
+      options: {
+        tenant,
+        environment,
+      },
+    },
+  ],
+  proxy: [
+    {
+      prefix: '/api',
+      url: `https://${tenant}.${environment}.com.br`,
+    },
+  ],
+})
---
+++ packages/gatsby-theme-vtex/src/gatsby-node.ts
@@ -0,0 +1,118 @@
+import './setup'
+
+import { join } from 'path'
+
+import { ensureDir, outputFile } from 'fs-extra'
+import { CreatePagesArgs } from 'gatsby'
+
+const root = process.cwd()
+
+export const createPages = async ({
+  actions: { createPage, createRedirect },
+  graphql,
+}: CreatePagesArgs) => {
+  createRedirect({
+    fromPath: '/api/*',
+    toPath: `https://${process.env.GATSBY_VTEX_TENANT}.${process.env.GATSBY_VTEX_ENVIRONMENT}.com.br/api/:splat`,
+    statusCode: 200,
+  })
+
+  const { data, errors } = await graphql<any>(`
+    query {
+      allProduct {
+        nodes {
+          id
+          slug
+        }
+      }
+      allCategory {
+        nodes {
+          id
+          slug
+          categoryId
+        }
+      }
+      allCmsPage {
+        nodes {
+          name
+          slug
+          src
+        }
+      }
+    }
+  `)
+
+  if (errors) {
+    console.error(errors)
+    return
+  }
+
+  const { allProduct, allCmsPage, allCategory } = data
+
+  // Product Pages
+
+  // Pre generated product pages
+  allProduct.nodes.forEach((product: any) => {
+    createPage({
+      path: product.slug,
+      component: join(root, `./src/templates/product/server.tsx`),
+      context: {
+        id: product.id,
+      },
+    })
+  })
+
+  // Client-side rendered product pages
+  createPage({
+    path: '/:slug/p',
+    matchPath: '/:slug/p',
+    component: join(root, `./src/templates/product/client.tsx`),
+    context: {},
+  })
+
+  // Category Pages
+
+  allCategory.nodes.forEach((category: any) => {
+    createPage({
+      path: category.slug,
+      component: join(root, `./src/templates/category.tsx`),
+      context: {
+        id: category.id,
+      },
+    })
+  })
+
+  // CMS Pages
+
+  // ensure dist folder
+  const cmsRoot = join(root, '.cache/vtex-cms')
+
+  await ensureDir(cmsRoot)
+
+  // Create page .tsx files as well as gatsby's node pages
+  const cmsPages = allCmsPage.nodes.map(async (page: any) => {
+    const { src, slug, name } = page
+    const filepath = join(cmsRoot, `${name}.tsx`)
+
+    await outputFile(filepath, src)
+
+    createPage({
+      path: slug,
+      component: filepath,
+      context: {},
+    })
+  })
+
+  await Promise.all(cmsPages)
+}
+
+// export const setWebpackConfig = () => ({
+//   resolve: {
+//     alias: {
+//       react: require.resolve('react'),
+//       'react-dom': require.resolve('react-dom'),
+//       '@loadable/component': require.resolve('@loadable/component'),
+//       '@loadable/server': require.resolve('@loadable/server'),
+//     },
+//   },
+// })
