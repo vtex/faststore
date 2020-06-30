957900dbdb0b6e51a0c18dc94f392fa11cc93b89
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-06-30 17:01:16 -0300

    organize api and fetch

--- packages/gatsby-source-vtex/src/fetch.ts
+++ packages/gatsby-source-vtex/src/fetch.ts
@@ -1,7 +1,5 @@
 import { Product, Category } from './types'
-import { getCatalogSystem } from './utils'
+import { getVtex } from './api'
 
-export const ProductSearch = getCatalogSystem<Product>(
-  'products/search?_from=0&_to=9'
-)
-export const CategoryTree = getCatalogSystem<Category>('category/tree/1')
+export const ProductSearch = getVtex<Product>('products/search?_from=0&_to=9')
+export const CategoryTree = getVtex<Category>('category/tree/1')
--- packages/gatsby-source-vtex/src/utils.ts
+++ packages/gatsby-source-vtex/src/utils.ts
@@ -1,36 +1,6 @@
-import axios from 'axios'
-import { PluginOptions, SourceNodesArgs } from 'gatsby'
+import { SourceNodesArgs } from 'gatsby'
 
-import { Product, Category } from './types'
-
-interface ApiUrl {
-  tenant: unknown
-  environment: unknown
-  sufix: string
-}
-
-type SufixVoid = (options: PluginOptions) => string
-type Sufix = string | SufixVoid
-
-const getApiUrl = ({ tenant, environment, sufix }: ApiUrl): string =>
-  `https://${tenant}.${environment}.com.br/api/catalog_system/pub/${sufix}`
-
-export const getCatalogSystem = <T>(sufix: Sufix) => async (
-  options: PluginOptions
-): Promise<T[]> => {
-  const url = getApiUrl({
-    tenant: options.tenant,
-    environment: options.environment,
-    sufix: typeof sufix === 'string' ? sufix : sufix(options),
-  })
-  const { data } = await axios.get(url, {
-    headers: {
-      'content-type': 'application/json',
-      accept: 'application/json',
-    },
-  })
-  return data
-}
+import { Category, Product } from './types'
 
 export const createProductNode = (
   {
