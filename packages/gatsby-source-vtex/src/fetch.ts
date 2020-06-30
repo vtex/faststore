4894cb4a2051e76d9cf7946b5b71b295314d8932
Tiago Gimenes <tlgimenes@gmail.com>
2020-06-30 18:54:52 -0300

    apply requested changes

--- packages/gatsby-source-vtex/src/fetch.ts
+++ packages/gatsby-source-vtex/src/fetch.ts
@@ -1,14 +1,7 @@
-import { PluginOptions } from 'gatsby'
-
 import { Category, Product } from './types'
 import { getCatalogSystem } from './utils'
 
 export const ProductSearch = getCatalogSystem<Product>(
   'products/search?_from=0&_to=9'
 )
-export const CategoryTree = async (
-  options: PluginOptions
-): Promise<Category[]> =>
-  getCatalogSystem<Category>('category/tree/1')(options).then((categories) =>
-    categories.filter((c) => !c.name.includes('[Inactive]'))
-  )
+export const CategoryTree = getCatalogSystem<Category>('category/tree/1')
