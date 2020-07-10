f87b933cb8ad6b8b78a8af0c9577f29a74aa8aec
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-07-10 13:54:32 -0300

    create Container component

---
+++ packages/gatsby-theme-vtex/src/components/Container.tsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { FC } from 'react'
+import { Box, jsx } from 'theme-ui'
+
+const Container: FC = ({ children }) => (
+  <Box
+    px={[2, 3, 4]}
+    sx={{
+      margin: '0 auto',
+      width: '100%',
+      maxWidth: ['100%', '100%', '96rem'],
+    }}
+  >
+    <main>{children}</main>
+  </Box>
+)
+
+export default Container
