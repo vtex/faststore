0a2d7e5e59078e0ead2772f000644acf27e98d8f
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-10 15:51:54 -0300

    adding files into the category

--- packages/gatsby-source-vtex/src/utils.ts
+++ packages/gatsby-source-vtex/src/utils.ts
@@ -82,35 +82,3 @@ export const createChannelNode = (
     },
   })
 }
-
-export const createCategorySearchResultNode = (
-  {
-    actions: { createNode },
-    createNodeId,
-    createContentDigest,
-  }: SourceNodesArgs,
-  category: Category,
-  products: Product[]
-) => {
-  const NODE_TYPE = 'CategorySearchResult'
-  const urlSplited = category.url.split('/')
-  const slug = urlSplited[urlSplited.length - 1]
-  const data = {
-    slug,
-    categoryId: Number(category.id),
-    products: products.map(parseProductToDataNode).map((product) => ({
-      ...product,
-      id: createNodeId(`${NODE_TYPE}-${product.productId}`),
-    })),
-  }
-
-  createNode({
-    ...data,
-    id: createNodeId(`${NODE_TYPE}-${data.categoryId}`),
-    internal: {
-      type: NODE_TYPE,
-      content: JSON.stringify(data),
-      contentDigest: createContentDigest(data),
-    },
-  })
-}
