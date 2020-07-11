60734612e1ecdf911d465623d3f49d4cacc5de48
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-10 21:14:42 -0300

    add carousel

--- packages/gatsby-theme-vtex/src/components/Layout.tsx
+++ packages/gatsby-theme-vtex/src/components/Layout.tsx
@@ -1,23 +1,14 @@
 /** @jsx jsx */
 import { FC, Fragment } from 'react'
-import { Box, jsx } from 'theme-ui'
+import { jsx } from 'theme-ui'
 
 import Header from './Header'
 
 const Layout: FC = ({ children }) => (
   <Fragment>
     <Header />
-    <Box
-      px={[2, 3, 4]}
-      sx={{
-        margin: '0 auto',
-        width: '100%',
-        maxWidth: ['100%', '100%', '96rem'],
-      }}
-    >
-      <main>{children}</main>
-      <footer>© {new Date().getFullYear()}, Built with Gatsby and VTEX</footer>
-    </Box>
+    {children}
+    <footer>© {new Date().getFullYear()}, Built with Gatsby and VTEX</footer>
   </Fragment>
 )
 
