f8e6e4403d5ed3fee0c5ca1ebed1483cac1bedd2
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-10 21:13:06 -0300

    Make minicart similar to store-frameworks one

--- packages/gatsby-theme-vtex/src/components/Minicart/ItemCount.tsx
+++ packages/gatsby-theme-vtex/src/components/Minicart/ItemCount.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react'
+import { Box } from 'theme-ui'
 
-import { useOrderForm } from '../providers/OrderForm/manager'
+import { useOrderForm } from '../../providers/OrderForm/controler'
 
 const ItemCount: FC = () => {
-  const { orderForm } = useOrderForm()
-  return <>{orderForm.items.length}</>
+  const { value: orderForm } = useOrderForm()
+  const count = orderForm?.items.length ?? 0
+  return <Box variant="minicart-badge">{count}</Box>
 }
 
 export default ItemCount
