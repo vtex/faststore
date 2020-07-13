315104790c3ad61912e0304b2f01f498e3273558
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-07-13 15:14:31 -0300

    add header search

--- packages/gatsby-theme-vtex/src/components/Search.tsx
+++ packages/gatsby-theme-vtex/src/components/Search.tsx
@@ -3,12 +3,7 @@ import { FC } from 'react'
 import { Input, jsx } from 'theme-ui'
 
 const Search: FC = () => (
-  <Input
-    id="searchbar"
-    placeholder="Search"
-    sx={{ maxWidth: 250, marginTop: [3, 0, 0] }}
-    aria-label="Search"
-  />
+  <Input variant="header-search" placeholder="Search" aria-label="Search" />
 )
 
 export default Search
