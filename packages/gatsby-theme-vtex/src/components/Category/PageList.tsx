38ed2140b1b6ac0af7f80ccc4f75fcec80a56b30
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-14 14:52:28 -0300

    load once when idle

--- packages/gatsby-theme-vtex/src/components/Category/PageList.tsx
+++ packages/gatsby-theme-vtex/src/components/Category/PageList.tsx
@@ -14,31 +14,13 @@ const loadAsyncPageList = () => import('./AsyncPageList')
 
 const AsyncPageList = lazy(loadAsyncPageList)
 
+// load AsyncPageList when idle
+;(window as any).requestIdleCallback?.(loadAsyncPageList)
+
 const List: FC<Props> = ({ category: { products, categoryId } }) => {
   const [renderAsyncList, setRenderAsyncList] = useState(products.length === 0)
   const SyncPage = products.length > 0 ? <Page products={products} /> : null
   const offset = SyncPage ? 1 : 0
-
-  // Preload when idle
-  useEffect(() => {
-    const idleCallback = (window as any).requestIdleCallback
-
-    if (typeof idleCallback !== 'function') {
-      return
-    }
-
-    idleCallback(async () => {
-      if (renderAsyncList) {
-        return // PageList was already loaded
-      }
-
-      const lib = await loadAsyncPageList()
-
-      if (typeof lib?.prefetchPageData === 'function') {
-        lib.prefetchPageData(offset, categoryId)
-      }
-    })
-  }, [categoryId, offset, renderAsyncList])
 
   return (
     <Fragment>
