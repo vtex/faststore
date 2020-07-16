633d0d1a291ebe5d858b90a40d6f0da0f2080680
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-15 22:31:13 -0300

    add retry

--- packages/gatsby-source-vtex/src/fetch.ts
+++ packages/gatsby-source-vtex/src/fetch.ts
@@ -10,22 +10,43 @@ const headers = {
   accept: 'application/json',
 }
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+async function fetchRetry<T>(
+  path: string,
+  init: RequestInit,
+  retryCount = 3
+): Promise<T> {
+  try {
+    const response = await fetch(path, init)
+
+    return await response.json()
+  } catch (err) {
+    if (retryCount === 1) {
+      throw err
+    }
+
+    await delay(300) // wait for 300ms
+
+    return fetchRetry(path, init, retryCount - 1)
+  }
+}
+
 export const fetchVTEX = async <T>(
   path: string,
   options: VTEXOptions,
   init?: RequestInit
 ): Promise<T> => {
-  const url = `https://${options.tenant}.${options.environment}.com.br${path}`
-  const response = await fetch(url, {
-    ...init,
-    headers: {
-      ...headers,
-      ...init?.headers,
-    },
-  })
-
   try {
-    return await response.json()
+    const url = `https://${options.tenant}.${options.environment}.com.br${path}`
+
+    return await fetchRetry(url, {
+      ...init,
+      headers: {
+        ...headers,
+        ...init?.headers,
+      },
+    })
   } catch (err) {
     console.error(err)
     throw err
