61381a4677c969602087cb6052a83d7b08610483
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-07-16 16:16:40 -0300

    add carousel with background image

--- packages/gatsby-theme-vtex/src/components/Carousel.tsx
+++ packages/gatsby-theme-vtex/src/components/Carousel.tsx
@@ -1,11 +1,14 @@
 /** @jsx jsx */
 import { FC } from 'react'
 import { Link } from 'gatsby'
-import { jsx, Button } from 'theme-ui'
+import { jsx, Button, Box } from 'theme-ui'
 import NukaCarousel, { CarouselProps } from 'nuka-carousel'
 
 interface Item {
-  src: string
+  src: {
+    desktop: string
+    mobile: string
+  }
   altText: string
   href?: string
 }
@@ -17,16 +20,27 @@ interface Props {
 
 const defaultOptions: CarouselProps = {
   dragging: false,
-  initialSlideHeight: 593,
   heightMode: 'first',
+  initialSlideHeight: 300,
 }
 
-const Image: FC<Item> = ({ src, altText, href }) => {
+const Image: FC<Item> = ({ src, href }) => {
   if (href) {
     return <Link to={href}>Hello</Link>
   }
 
-  return <img src={src} alt={altText} />
+  return (
+    <Box
+      sx={{
+        backgroundImage: [`url(${src.mobile})`, `url(${src.desktop})`],
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        width: '100%',
+        height: [300, 400, 500],
+      }}
+    />
+  )
 }
 
 const Carousel: FC<Props> = ({ items, options = defaultOptions }) => (
