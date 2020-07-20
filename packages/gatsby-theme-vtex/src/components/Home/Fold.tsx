52d7ff8b719d8ccbd8ea3512d5199aa4dddd25ba
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-20 19:22:04 -0300

    improves fold component

--- packages/gatsby-theme-vtex/src/components/Home/Fold.tsx
+++ packages/gatsby-theme-vtex/src/components/Home/Fold.tsx
@@ -1,37 +1,43 @@
 /** @jsx jsx */
-import { FC, Fragment } from 'react'
+import { FC, Fragment, lazy, Suspense } from 'react'
 import { jsx } from 'theme-ui'
 
-import Action from '../InfoCard/ButtonAction'
-import InfoCard from '../InfoCard'
-import RichText from '../RichText'
+const Action = lazy(() => import('../InfoCard/ButtonAction'))
+const InfoCard = lazy(() => import('../InfoCard'))
+const RichText = lazy(() => import('../RichText'))
 
 const Space: FC = ({ children }) => <div sx={{ my: 5 }}>{children}</div>
 
 const Fold: FC = () => (
   <Fragment>
-    <Space>
-      <InfoCard headline="New Promotion!" to="/vintage-phone/p">
-        <div sx={{ my: 3 }}>
-          <Action to="/vintage-phone/p" label="BUY NOW" />
+    <Suspense fallback={null}>
+      <Space>
+        <InfoCard headline="New Promotion!" to="/vintage-phone/p">
+          <div sx={{ my: 3 }}>
+            <Suspense fallback={null}>
+              <Action to="/vintage-phone/p" label="BUY NOW" />
+            </Suspense>
+          </div>
+        </InfoCard>
+      </Space>
+    </Suspense>
+    <Suspense fallback={null}>
+      <Space>
+        <RichText
+          text="This is an example store built using the VTEX platform."
+          variant="question"
+        />
+        <RichText text="Want to know more?" variant="question" />
+      </Space>
+      <Space>
+        <div sx={{ mb: 3 }}>
+          <RichText text="Reach us at" />
         </div>
-      </InfoCard>
-    </Space>
-    <Space>
-      <RichText
-        text="This is an example store built using the VTEX platform."
-        variant="question"
-      />
-      <RichText text="Want to know more?" variant="question" />
-    </Space>
-    <Space>
-      <div sx={{ mb: 3 }}>
-        <RichText text="Reach us at" />
-      </div>
-      <a href="www.vtex.com.br">
-        <RichText text="www.vtex.com.br" variant="link" />
-      </a>
-    </Space>
+        <a href="www.vtex.com.br">
+          <RichText text="www.vtex.com.br" variant="link" />
+        </a>
+      </Space>
+    </Suspense>
   </Fragment>
 )
 
