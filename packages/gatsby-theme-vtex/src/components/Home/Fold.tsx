3a8efbec032e01e5797e2574ceb27eced6fce789
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-20 21:51:37 -0300

    update theme

--- packages/gatsby-theme-vtex/src/components/Home/Fold.tsx
+++ packages/gatsby-theme-vtex/src/components/Home/Fold.tsx
@@ -1,58 +1,44 @@
 /** @jsx jsx */
-import { FC, Fragment, lazy, Suspense } from 'react'
+import { FC, Fragment } from 'react'
 import { jsx } from 'theme-ui'
 
 import Action from '../InfoCard/ButtonAction'
+import RichText from '../RichText'
+import InfoCard from '../InfoCard'
 
-const RichText = lazy(() => import('../RichText'))
-const InfoCard = lazy(() => import('../InfoCard'))
-
-const Space: FC = ({ children }) => <div sx={{ my: 5 }}>{children}</div>
-
-const SuspenseNull: FC = ({ children }) => (
-  <Suspense fallback={null}>{children}</Suspense>
-)
+const Block: FC = ({ children }) => <div sx={{ my: 5 }}>{children}</div>
 
 const Fold: FC = () => (
   <Fragment>
-    <Space>
-      <SuspenseNull>
-        <InfoCard
-          to="/vintage-phone/p"
-          headline="New Promotion!"
-          imgSrc="https://storecomponents.vtexassets.com/arquivos/banner-infocard2.png"
-          imgAlt="infocard-banner"
-        >
-          <div sx={{ my: 3 }}>
-            <Action to="/vintage-phone/p" label="BUY NOW" />
-          </div>
-        </InfoCard>
-      </SuspenseNull>
-    </Space>
+    <Block>
+      <InfoCard
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/arquivos/banner-infocard2.png"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard>
+    </Block>
 
-    <Space>
-      <SuspenseNull>
-        <RichText
-          text="This is an example store built using the VTEX platform."
-          variant="question"
-        />
-      </SuspenseNull>
-      <SuspenseNull>
-        <RichText text="Want to know more?" variant="question" />
-      </SuspenseNull>
-    </Space>
-    <Space>
+    <Block>
+      <RichText
+        text="This is an example store built using the VTEX platform."
+        variant="question"
+      />
+      <RichText text="Want to know more?" variant="question" />
+    </Block>
+
+    <Block>
       <div sx={{ mb: 3 }}>
-        <SuspenseNull>
-          <RichText text="Reach us at" />
-        </SuspenseNull>
+        <RichText text="Reach us at" />
       </div>
       <a href="www.vtex.com.br">
-        <SuspenseNull>
-          <RichText text="www.vtex.com.br" variant="link" />
-        </SuspenseNull>
+        <RichText text="www.vtex.com.br" variant="link" />
       </a>
-    </Space>
+    </Block>
   </Fragment>
 )
 
