744fe6d015ab31924d6614dd7b34c56765779eb7
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-24 19:03:44 -0300

    always render the list price

--- packages/gatsby-theme-vtex/src/components/ProductSummary.tsx
+++ packages/gatsby-theme-vtex/src/components/ProductSummary.tsx
@@ -44,11 +44,7 @@ export const ProductSummary: FC<Props> = ({ syncProduct }) => {
           alt={imageText}
           loading="lazy" // lazy load images
         />
-        <Heading
-          variant="shellProductName"
-          as="h3"
-          sx={{ marginY: ['0.5rem', '1rem'] }}
-        >
+        <Heading variant="summary-name" as="h3">
           {syncProduct.productName.slice(0, 12)}
         </Heading>
         {!offer ? (
