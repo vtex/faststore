9f145d2804c2f64a391020e5035cc6926273f6ab
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-25 00:44:37 -0300

    wip

--- packages/gatsby-theme-vtex/src/components/Offer/Preview.tsx
+++ packages/gatsby-theme-vtex/src/components/Offer/Preview.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from 'react'
 import Skeleton from 'react-loading-skeleton'
 
-const OfferPreview: FC = () => <Skeleton height={79} />
+const OfferPreview: FC = () => (
+  <>
+    <Skeleton height={21} />
+    <Skeleton height={28} />
+    <Skeleton height={21} />
+  </>
+)
 
 export default OfferPreview
