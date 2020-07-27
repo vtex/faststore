e0245b5850b855ff3853f3024a5e0861c6513702
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-27 16:59:16 -0300

    long page with different components

--- packages/gatsby-theme-vtex/src/components/Home/Fold.tsx
+++ packages/gatsby-theme-vtex/src/components/Home/Fold.tsx
@@ -2,9 +2,28 @@
 import { FC, Fragment } from 'react'
 import { jsx } from 'theme-ui'
 
-import Action from '../InfoCard/ButtonAction'
 import RichText from '../RichText'
-import InfoCard from '../InfoCard'
+import Action from '../InfoCard01/ButtonAction'
+import InfoCard01 from '../InfoCard01'
+import InfoCard02 from '../InfoCard02'
+import InfoCard03 from '../InfoCard03'
+import InfoCard04 from '../InfoCard04'
+import InfoCard05 from '../InfoCard05'
+import InfoCard06 from '../InfoCard06'
+import InfoCard07 from '../InfoCard07'
+import InfoCard08 from '../InfoCard08'
+import InfoCard09 from '../InfoCard09'
+import InfoCard10 from '../InfoCard10'
+import InfoCard11 from '../InfoCard11'
+import InfoCard12 from '../InfoCard12'
+import InfoCard13 from '../InfoCard13'
+import InfoCard14 from '../InfoCard14'
+import InfoCard15 from '../InfoCard15'
+import InfoCard16 from '../InfoCard16'
+import InfoCard17 from '../InfoCard17'
+import InfoCard18 from '../InfoCard18'
+import InfoCard19 from '../InfoCard19'
+import InfoCard20 from '../InfoCard20'
 
 const Block: FC = ({ children }) => <div sx={{ my: 5 }}>{children}</div>
 
@@ -12,20 +31,265 @@ const size = 20
 
 const Fold: FC = () => (
   <Fragment>
-    {new Array(size).fill(null, 0, size).map((x, index) => (
-      <Block key={`block-${index}`}>
-        <InfoCard
-          to="/vintage-phone/p"
-          headline="New Promotion!"
-          imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
-          imgAlt="infocard-banner"
-        >
-          <div sx={{ my: 3 }}>
-            <Action to="/vintage-phone/p" label="BUY NOW" />
-          </div>
-        </InfoCard>
-      </Block>
-    ))}
+    <Block>
+      <InfoCard01
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard01>
+    </Block>
+
+    <Block>
+      <InfoCard02
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard02>
+    </Block>
+
+    <Block>
+      <InfoCard03
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard03>
+    </Block>
+
+    <Block>
+      <InfoCard04
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard04>
+    </Block>
+
+    <Block>
+      <InfoCard05
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard05>
+    </Block>
+
+    <Block>
+      <InfoCard06
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard06>
+    </Block>
+
+    <Block>
+      <InfoCard07
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard07>
+    </Block>
+
+    <Block>
+      <InfoCard08
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard08>
+    </Block>
+
+    <Block>
+      <InfoCard09
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard09>
+    </Block>
+
+    <Block>
+      <InfoCard10
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard10>
+    </Block>
+
+    <Block>
+      <InfoCard11
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard11>
+    </Block>
+
+    <Block>
+      <InfoCard12
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard12>
+    </Block>
+
+    <Block>
+      <InfoCard13
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard13>
+    </Block>
+
+    <Block>
+      <InfoCard14
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard14>
+    </Block>
+
+    <Block>
+      <InfoCard15
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard15>
+    </Block>
+
+    <Block>
+      <InfoCard16
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard16>
+    </Block>
+
+    <Block>
+      <InfoCard17
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard17>
+    </Block>
+
+    <Block>
+      <InfoCard18
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard18>
+    </Block>
+
+    <Block>
+      <InfoCard19
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard19>
+    </Block>
+
+    <Block>
+      <InfoCard20
+        to="/vintage-phone/p"
+        headline="New Promotion!"
+        imgSrc="https://storecomponents.vtexassets.com/assets/faststore/images/banner-infocard2___3f284742ba9ede3826bc0721f0789694.png?height=300&aspect=true"
+        imgAlt="infocard-banner"
+      >
+        <div sx={{ my: 3 }}>
+          <Action to="/vintage-phone/p" label="BUY NOW" />
+        </div>
+      </InfoCard20>
+    </Block>
 
     <Block>
       <RichText
---
+++ packages/gatsby-theme-vtex/src/components/InfoCard19/index.tsx
@@ -0,0 +1,56 @@
+import { Link } from 'gatsby'
+import React, { FC } from 'react'
+import { Box, Image } from 'theme-ui'
+
+interface Props {
+  to: string
+  headline?: string
+  subhead?: string
+  imgSrc: string
+  imgAlt: string
+  variant?: string
+}
+
+const InfoCard: FC<Props> = ({
+  headline,
+  subhead,
+  children,
+  to,
+  imgSrc,
+  imgAlt,
+  variant,
+}) => {
+  return (
+    <Box variant={`infocard${variant ? `.${variant}` : ''}`}>
+      <Box sx={{ width: ['100%', '30%'] }}>
+        {headline && (
+          <div
+            sx={{
+              fontSize: 5,
+              fontWeight: 'bold',
+            }}
+          >
+            {headline}
+          </div>
+        )}
+        {subhead && <div>{subhead}</div>}
+        {children}
+      </Box>
+      <Box sx={{ width: ['0%', '70%'], display: ['none', 'inline'] }}>
+        <Link to={to}>
+          <Image
+            sx={{
+              maxHeight: '540px',
+              width: '100%',
+            }}
+            alt={imgAlt}
+            src={imgSrc}
+            loading="lazy"
+          />
+        </Link>
+      </Box>
+    </Box>
+  )
+}
+
+export default InfoCard
