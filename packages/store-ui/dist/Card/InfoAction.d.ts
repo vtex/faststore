485ef22ecdceba32cc0807e020390e0493eff9fb
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-07-27 20:05:48 -0300

    remove dist

--- packages/store-ui/dist/Card/InfoAction.d.ts
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-interface Props {
-    href: string;
-    label: string;
-}
-declare const CardInfoAction: FC<Props>;
-export default CardInfoAction;
--- packages/store-ui/dist/Card/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-interface Props {
-    variant?: string;
-}
-declare const Card: FC<Props>;
-export default Card;
