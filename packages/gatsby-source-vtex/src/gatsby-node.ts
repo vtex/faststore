f0a7638f1584faffb81145d7763d4e2febee1ff7
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-28 12:26:20 -0300

    remove the need for category

--- packages/gatsby-source-vtex/src/gatsby-node.ts
+++ packages/gatsby-source-vtex/src/gatsby-node.ts
@@ -2,8 +2,8 @@ import { GatsbyNode, PluginOptions, SourceNodesArgs } from 'gatsby'
 
 import { api } from './api'
 import { fetchVTEX, VTEXOptions } from './fetch'
-import { Category, PageType, Product, RawFacets, Tenant } from './types'
-import { createCategoryNode, createChannelNode } from './utils'
+import { Tenant } from './types'
+import { createChannelNode } from './utils'
 
 interface Options extends PluginOptions, VTEXOptions {
   getStaticPaths?: () => Promise<string[]>
@@ -13,11 +13,7 @@ export const sourceNodes: GatsbyNode['sourceNodes'] = async (
   args: SourceNodesArgs,
   options: Options
 ) => {
-  const { tenant, getStaticPaths } = options
-  const staticPaths =
-    typeof getStaticPaths === 'function' ? await getStaticPaths() : []
-
-  const staticPathsSet = new Set(staticPaths)
+  const { tenant } = options
 
   // VTEX Context
   const { bindings } = await fetchVTEX<Tenant>(
@@ -26,66 +22,4 @@ export const sourceNodes: GatsbyNode['sourceNodes'] = async (
   )
 
   bindings.forEach((binding) => createChannelNode(args, binding))
-
-  // CATEGORY
-  const categoryData = await fetchVTEX<Category[]>(
-    api.catalog.category.tree(1),
-    options
-  )
-
-  const activesCategories = categoryData.filter(
-    (category) => !category.name.includes('[Inactive]')
-  )
-
-  const categoriesWithProducts = await Promise.all(
-    activesCategories.map(async (category) => {
-      const id = category.id.toString()
-      const url = new URL(category.url)
-
-      // Fetch Products in the category
-      let products: Product[] = []
-
-      if (staticPathsSet.has(url.pathname)) {
-        products = await fetchVTEX<Product[]>(
-          api.search({ from: 0, to: 7, categoryIds: [id] }),
-          options
-        )
-      }
-
-      // Fatch department's facets
-      const rawFacets = await fetchVTEX<RawFacets>(
-        api.facets({ department: url.pathname.slice(1, url.pathname.length) }),
-        options
-      )
-
-      // Fix facet's brands
-      const brands = await Promise.all(
-        rawFacets.Brands?.map(async ({ Value, Name, Quantity }) => {
-          const { id: brandId } = await fetchVTEX<PageType>(
-            api.pageType(Value),
-            options
-          )
-
-          return {
-            name: Name,
-            id: Number(brandId),
-            quantity: Quantity,
-          }
-        }) ?? []
-      )
-
-      return {
-        ...category,
-        products,
-        facets: {
-          ...rawFacets,
-          brands,
-        },
-      }
-    })
-  )
-
-  categoriesWithProducts.forEach((category) =>
-    createCategoryNode(args, category)
-  )
 }
