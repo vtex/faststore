21adf288fc43101aaa89c49abf9382a86508e81a
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-28 14:52:59 -0300

    graphql client-side

--- packages/gatsby-theme-vtex/src/components/Search/fetcher.tsx
+++
@@ -1,9 +0,0 @@
-import { api, FilterOptions, Product } from '@vtex/gatsby-source-vtex'
-
-import { jsonFetcher } from '../../utils/fetcher'
-
-export const fetcher = (filters: FilterOptions) => {
-  const url = api.search(filters)
-
-  return jsonFetcher<Product[]>(url)
-}
