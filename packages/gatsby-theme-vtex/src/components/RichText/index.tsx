f3c9820389a366a7a75125ac84bfcbece1bb2de4
Estácio Pereira <estacio.pereira@gmail.com>
2020-07-29 11:45:00 -0300

    Remove theme-ui from RichText

--- packages/gatsby-theme-vtex/src/components/RichText/index.tsx
+++ packages/gatsby-theme-vtex/src/components/RichText/index.tsx
@@ -1,12 +1,44 @@
-import React, { FC } from 'react'
+import React, { FC, CSSProperties } from 'react'
 
 interface Props {
   text: string
   variant?: string
 }
 
-const RichText: FC<Props> = ({ variant = 'default', text }) => (
-  <div sx={{ variant: `rich-text.${variant}` }}>{text}</div>
-)
+const RichText: FC<Props> = ({ variant = 'default', text }) => {
+  let style: CSSProperties = {}
+
+  switch (variant) {
+    case 'question':
+      style = {
+        color: 'textBold',
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      }
+      break
+
+    case 'link':
+      style = {
+        color: 'textBold',
+        fontSize: 24,
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        textDecoration: 'none',
+      }
+      break
+
+    default:
+      style = {
+        color: 'textBold',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+      }
+  }
+
+  return <div style={style}>{text}</div>
+}
 
 export default RichText
