4fb93150227aecf5f5439c1bdc965a02695d396e
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-29 15:55:39 -0300

    wip

--- packages/gatsby-source-vtex/src/gatsby-node.ts
+++ packages/gatsby-source-vtex/src/gatsby-node.ts
@@ -1,4 +1,6 @@
 import { GatsbyNode, PluginOptions, SourceNodesArgs } from 'gatsby'
+import { introspectSchema, wrapSchema } from '@graphql-tools/wrap'
+import { AsyncExecutor } from '@graphql-tools/delegate'
 
 import { api } from './api'
 import { fetchVTEX, VTEXOptions } from './fetch'
@@ -14,6 +16,7 @@ export const sourceNodes: GatsbyNode['sourceNodes'] = async (
   options: Options
 ) => {
   const { tenant } = options
+  const { addThirdPartySchema } = args
 
   // VTEX Context
   const { bindings } = await fetchVTEX<Tenant>(
@@ -22,4 +25,44 @@ export const sourceNodes: GatsbyNode['sourceNodes'] = async (
   )
 
   bindings.forEach((binding) => createChannelNode(args, binding))
+
+  // const executor = (app: string): AsyncExecutor => async ({
+  //   document,
+  //   variables,
+  //   context,
+  // }) => {
+  //   const {
+  //     vtex: { account, workspace, authToken },
+  //   } = (context as unknown) as Context
+
+  //   const { name, version } = parseAppId(app)
+  //   const major = versionToMajor(version)
+  //   const query = print(document)
+
+  //   const fetchResult = await fetch(
+  //     `http://app.io.vtex.com/${name}/v${major}/${account}/${workspace}/_v/graphql`,
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: authToken,
+  //         'x-vtex-locale': 'en-US',
+  //         'x-vtex-tenant': 'en-US',
+  //       },
+  //       body: JSON.stringify({ query, variables }),
+  //     }
+  //   )
+
+  //   return fetchResult.json()
+  // }
+
+  // const schema = wrapSchema(
+  //   {
+  //     schema: introspectionSchema,
+  //     executor: linkToExecutor(link),
+  //   },
+  //   [new StripNonQueryTransform()]
+  // )
+
+  // addThirdPartySchema({ schema })
 }
