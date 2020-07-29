18ff75b43d40d992438441ad245cff300f137ec9
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-07-29 14:31:49 -0300

    remove lint config from store-ui

--- packages/gatsby-theme-vtex/src/components/Shelf/index.tsx
+++ packages/gatsby-theme-vtex/src/components/Shelf/index.tsx
@@ -53,7 +53,7 @@ const Shelf: FC<Props> = ({ syncProducts }) => {
             <Button
               backgroundColor="transparent"
               onClick={() => setPage(page - 1)}
-              aria-label={'See shelf previous page'}
+              aria-label="See shelf previous page"
             >
               <ArrowLeft size={arrowSize} />
             </Button>
@@ -79,7 +79,7 @@ const Shelf: FC<Props> = ({ syncProducts }) => {
                   onClick={() => {
                     setPage(page + 1)
                   }}
-                  aria-label={'See shelf next page'}
+                  aria-label="See shelf next page"
                 >
                   <ArrowRight size={arrowSize} />
                 </Button>
