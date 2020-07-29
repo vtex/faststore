af55e3f778c78635fb5aa9f33817d6db18bba830
klynger <rafael.klynger@gmail.com>
2020-07-29 19:58:02 -0300

    Remove riple

--- packages/gatsby-theme-vtex/src/components/material-ui-components/Button/Button.tsx
+++ packages/gatsby-theme-vtex/src/components/material-ui-components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import Button, { ButtonProps } from '@material-ui/core/Button'
 
 const CustomButton: FC<ButtonProps> = (props) => {
-  return <Button {...props} />
+  return <Button disableRipple disableTouchRipple {...props} />
 }
 
 export default CustomButton
