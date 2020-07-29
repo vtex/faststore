b161183d3871f0b959dc8aebca9041735ff80fb7
klynger <rafael.klynger@gmail.com>
2020-07-29 19:26:42 -0300

    Fix layout gallery

--- packages/gatsby-theme-vtex/src/components/Category/Page.tsx
+++ packages/gatsby-theme-vtex/src/components/Category/Page.tsx
@@ -1,5 +1,7 @@
-import { Product } from '@vtex/gatsby-source-vtex'
 import React, { FC } from 'react'
+import type { Theme } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import { Product } from '@vtex/gatsby-source-vtex'
 
 import Grid from '../material-ui-components/Grid'
 import { ProductSummary } from '../ProductSummary'
@@ -8,14 +10,32 @@ interface Props {
   products: Product[]
 }
 
-const Page: FC<Props> = ({ products }) => (
-  <Grid item container>
-    {products.map((product) => (
-      <Grid key={product.productId} xs={12} sm={6} md={4} lg={3}>
-        <ProductSummary syncProduct={product} />
-      </Grid>
-    ))}
-  </Grid>
-)
+const useStyles = makeStyles((theme: Theme) => ({
+  summary: {
+    padding: theme.spacing(1),
+  }
+}))
+
+const Page: FC<Props> = ({ products }) => {
+  const classes = useStyles()
+
+  return (
+    <Grid item container>
+      {products.map((product) => (
+        <Grid
+          className={classes.summary}
+          style={{ padding: '8px' }}
+          key={product.productId}
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+        >
+          <ProductSummary syncProduct={product} />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
 
 export default Page
