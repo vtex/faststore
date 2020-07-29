7c6405a92a868d9103c7e8e34b7f5e442a68b5c2
Estácio Pereira <estacio.pereira@gmail.com>
2020-07-29 19:31:16 -0300

    Add tracing to SuspenseSSR

--- packages/gatsby-theme-vtex/src/components/SuspenseSSR.tsx
+++ packages/gatsby-theme-vtex/src/components/SuspenseSSR.tsx
@@ -3,6 +3,7 @@ import React, { FC, Suspense, SuspenseProps } from 'react'
 import { isServer } from '../utils/env'
 
 const SuspenseSSR: FC<SuspenseProps> = ({ fallback, children }) => {
+  console.log('SuspenseSSR', { isServer })
   if (isServer) {
     return fallback as any
   }
