beb054e6af2d398e584196fb6f69eeb34db83ebf
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-30 19:33:16 -0300

    make search filters to work

--- packages/gatsby-theme-vtex/src/components/Search/Facets/Brands.tsx
+++ packages/gatsby-theme-vtex/src/components/Search/Facets/Brands.tsx
@@ -2,65 +2,52 @@
 import { FC, Fragment } from 'react'
 import { Checkbox, Label, jsx } from 'theme-ui'
 
-import {
-  useSearchFilters,
-  SearchOptions,
-} from '../../../providers/SearchFilter'
+import { useSearchFilters, SearchOptions } from '../../../providers/Search'
 
 interface BrandFacet {
-  id: number
   name: string
+  value: string
+  quantity: number
 }
 
 interface Props {
   brands: BrandFacet[]
 }
 
-const toggleFilter = (id: number, filters: SearchOptions) => {
-  // // First time adding a brand, just append it
-  // if (!filters.brandIds) {
-  //   return {
-  //     ...filters,
-  //     brandIds: [id],
-  //   }
-  // }
+const toggleFilter = (value: string, filters: SearchOptions) => {
+  const { query, map } = filters
+  const lowered = value.toLowerCase()
+  const withSlash = lowered
 
-  // const { brandIds } = filters
+  if (query?.includes(withSlash) && map?.includes(',b')) {
+    return {
+      ...filters,
+      query: query.replace(withSlash, ''),
+      map: map.replace(',b', ''),
+    }
+  }
 
-  // const index = brandIds.findIndex((i) => i === id)
-
-  // // item already filtering result, let's remove it
-  // if (index > -1) {
-  //   brandIds.splice(index, 1)
-
-  //   return {
-  //     ...filters,
-  //     brandIds: brandIds.length === 0 ? undefined : brandIds,
-  //   }
-  // }
-
-  // // filter not found on array, let's add it
-  // return {
-  //   ...filters,
-  //   brandIds: [...brandIds, id],
-  // }
-  return filters
+  return {
+    ...filters,
+    query: `${query}/${lowered}`,
+    map: `${map},b`,
+  }
 }
 
 const BrandSelector: FC<Props> = ({ brands }) => {
-  const [, setFilters] = useSearchFilters()
+  const { setFilters } = useSearchFilters()
 
   return (
     <Fragment>
       <div>Brands</div>
 
       <ul sx={{ listStyleType: 'none', mx: 0, px: 0 }}>
-        {brands.map(({ name, id }, index) => (
+        {brands.map(({ name, value }, index) => (
           <li key={`brands-selector-${index}`}>
             <Label>
               <Checkbox
                 onClick={() =>
-                  setFilters((filters) => toggleFilter(id, filters))
+                  setFilters((filters) => toggleFilter(value, filters))
                 }
               />
               {name}
