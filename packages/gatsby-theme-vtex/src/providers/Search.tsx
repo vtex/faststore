614d86b6dc2b5f0bf6fd2e49aa5ec5a4614e84da
Tiago Gimenes <tlgimenes@gmail.com>
2020-08-10 12:46:36 -0300

    make theme-ui shadowing to work when linked

--- packages/gatsby-theme-vtex/src/providers/Search.tsx
+++ packages/gatsby-theme-vtex/src/providers/Search.tsx
@@ -18,11 +18,19 @@ interface Props {
   filters?: SearchOptions
 }
 
+// Creates a string with as many `c,c` as pathname has
+// segments.
+// For instance: cozinha/faqueiro-e-talheres would
+// generate the string c,c
+// TODO: this function may have to change in the future
+const createMap = (pathname: string) =>
+  new Array(pathname.split('/').length).fill('c').join(',')
+
 const SearchProvider: FC<Props> = ({ children, filters }) => {
   const { params, pathname } = useLocationSearch()
   const value = {
     query: filters?.query ?? pathname.slice(1, pathname.length),
-    map: filters?.map ?? params.get('map'),
+    map: filters?.map ?? params.get('map') ?? createMap(pathname),
   }
 
   return (
