0ccdfa4adf8204dfd50a80978c4c4203bd9cc0a1
Luciano Júnior <luciannojunior@gmail.com>
2020-08-13 13:52:57 -0300

    Adding Breadcrumb components and stories

---
+++ packages/store-ui/src/Breadcrumb/index.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { ThemeProvider } from 'theme-ui'
+
+import baseTheme from '../theme'
+import Breadcrumb from './index'
+
+export default {
+  title: 'product|Breadcrumb',
+  component: Breadcrumb,
+}
+
+export const ProductBreadcrumb = () => (
+  <ThemeProvider theme={{ ...baseTheme }}>
+    <Breadcrumb
+      categoryTree={[
+        {
+          href: '/Cubas-e-Tanques',
+          name: 'Cubas e Tanques',
+        },
+        {
+          href: '/Cubas-e-Tanques/Cubas',
+          name: 'Cubas',
+        },
+      ]}
+    />
+  </ThemeProvider>
+)
+
+export const SearchBreadcrumb = () => (
+  <ThemeProvider theme={{ ...baseTheme }}>
+    <Breadcrumb
+      breadcrumb={[
+        {
+          name: 'Apparel & Accessories',
+          href: '/apparel---accessories',
+        },
+        {
+          name: 'Clothing',
+          href: '/apparel---accessories/clothing',
+        },
+      ]}
+    />
+  </ThemeProvider>
+)
