64b1877d2eb5d86e63dcc6a6952e4d72c6c96fd6
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-08-12 21:03:03 -0300

    remove badge from minicart button

---
+++ packages/gatsby-theme-vtex/src/components/Minicart/Badge.tsx
@@ -0,0 +1,13 @@
+import { MinicartBadge } from '@vtex/store-ui'
+import React, { FC } from 'react'
+
+import { useOrderForm } from '../../hooks/useOrderForm'
+
+const CustomMinicartBadge: FC<{ variant?: string }> = ({ variant }) => {
+  const orderForm = useOrderForm()
+  const count = orderForm?.value?.items.length ?? 0
+
+  return <MinicartBadge variant={variant} value={count} />
+}
+
+export default CustomMinicartBadge
