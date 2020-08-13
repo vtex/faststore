1e8ad85deeb9e7ed1b99e9501692374a01d2eb04
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-08-13 16:53:52 -0300

    overwrite target array on theme merge

--- packages/store-ui/src/createTheme.ts
+++ packages/store-ui/src/createTheme.ts
@@ -1,3 +1,6 @@
 import merge from 'deepmerge'
 
-export const createTheme = (...args: any) => merge.all(args)
+const overwriteMerge = (_: any, sourceArray: any, __: any) => sourceArray
+
+export const createTheme = (...args: any) =>
+  merge.all(args, { arrayMerge: overwriteMerge })
