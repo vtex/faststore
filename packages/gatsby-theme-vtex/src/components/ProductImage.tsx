3f4b4808ce2bb527214b9dcb5546996be1bfc702
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-08-17 17:27:28 -0300

    add progressive image in store ui and using in product image

--- packages/gatsby-theme-vtex/src/components/ProductImage.tsx
+++ packages/gatsby-theme-vtex/src/components/ProductImage.tsx
@@ -1,6 +1,7 @@
 import { Image } from '@vtex/gatsby-source-vtex'
+import { ProgressiveImage } from '@vtex/store-ui'
 import React, { FC } from 'react'
-import { AspectImage } from '@vtex/store-ui'
+import { AspectImage } from 'theme-ui'
 
 import { IMAGE_DEFAULT, scaleImage } from '../utils/img'
 
@@ -12,17 +13,21 @@ interface Props {
   loading?: 'lazy' | 'eager'
 }
 
+const SIZE = 96
+
 const ProductImage: FC<Props> = ({
-  width = 300,
-  height = 300,
+  width = SIZE,
+  height = SIZE,
   src = IMAGE_DEFAULT,
   alt = 'Product Image',
   loading = 'lazy',
 }) => (
-  <AspectImage
+  <ProgressiveImage
+    as={AspectImage}
+    placeholder={scaleImage(src, SIZE, SIZE)}
+    src={scaleImage(src, width, height)}
     ratio={1}
     loading={loading}
-    src={scaleImage(src, width, height)}
     alt={alt}
     width={`${width}px`}
     height={`${height}px`}
---
+++ packages/store-ui/src/ProgressiveImage/index.tsx
@@ -0,0 +1,32 @@
+import { createElement, ElementType, memo, useEffect, useState } from 'react'
+import { AspectImageProps, ImageProps } from 'theme-ui'
+
+export interface ProgressiveImageProps extends ImageProps, AspectImageProps {
+  src: string
+  placeholder: string
+  as?: ElementType
+}
+
+export const ProgressiveImage = memo<ProgressiveImageProps>(
+  ({ src, placeholder, as = 'img', ...props }) => {
+    const [currentSrc, updateSrc] = useState(placeholder)
+
+    useEffect(() => {
+      const imageToLoad = new Image()
+
+      imageToLoad.src = src
+      imageToLoad.onload = () => {
+        updateSrc(src)
+      }
+    }, [src])
+
+    const elementProps = {
+      ...props,
+      src: currentSrc,
+    }
+
+    return createElement(as, elementProps)
+  }
+)
+
+ProgressiveImage.displayName = 'ProgressiveImage'
