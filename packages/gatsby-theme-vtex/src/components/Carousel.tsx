12759f9e06968d31b60af1ea6bd30f1ffb120a5a
Luciano Júnior <luciannojunior@gmail.com>
2020-08-18 17:02:19 -0300

    Adding HomeBlocks

--- packages/gatsby-theme-vtex/src/components/Carousel.tsx
+++ packages/gatsby-theme-vtex/src/components/Carousel.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { FC, useState } from 'react'
-import { Box, Button, jsx, Image } from '@vtex/store-ui'
+import { Box, Button, jsx, Image, PaginationDots } from '@vtex/store-ui'
 import { useIntl } from '@vtex/gatsby-plugin-i18n'
 
 interface Item {
@@ -12,10 +12,12 @@ interface Item {
 }
 
 interface Props {
-  items?: Item[]
+  items: Item[]
+  showArrows?: boolean
+  showDots?: boolean
 }
 
-const Carousel: FC<Props> = ({ items = [] }) => {
+const Carousel: FC<Props> = ({ items, showArrows = true, showDots = true }) => {
   const [index, setIndex] = useState(0)
   const lastIndex = items.length - 1
   const height = 450
@@ -28,18 +30,22 @@ const Carousel: FC<Props> = ({ items = [] }) => {
           key={item.altText}
           sx={{ display: index === i ? 'block' : 'none', height }}
         >
-          <Button
-            onClick={() => setIndex(i === 0 ? lastIndex : i - 1)}
-            sx={{ position: 'absolute', top: '50%', left: 0 }}
-          >
-            {formatMessage({ id: 'carousel.previous' })}
-          </Button>
-          <Button
-            onClick={() => setIndex(i === lastIndex ? 0 : i + 1)}
-            sx={{ position: 'absolute', top: '50%', right: 0 }}
-          >
-            {formatMessage({ id: 'carousel.next' })}
-          </Button>
+          {showArrows && (
+            <Button
+              onClick={() => setIndex(i === 0 ? lastIndex : i - 1)}
+              sx={{ position: 'absolute', top: '50%', left: 0 }}
+            >
+              {formatMessage({ id: 'carousel.previous' })}
+            </Button>
+          )}
+          {showArrows && (
+            <Button
+              onClick={() => setIndex(i === lastIndex ? 0 : i + 1)}
+              sx={{ position: 'absolute', top: '50%', right: 0 }}
+            >
+              {formatMessage({ id: 'carousel.next' })}
+            </Button>
+          )}
           <Image
             src={item.src}
             width={item.width}
@@ -50,6 +56,14 @@ const Carousel: FC<Props> = ({ items = [] }) => {
           />
         </Box>
       ))}
+      {showDots && (
+        <PaginationDots
+          variant="carousel"
+          totalItems={items.length}
+          selectedIndex={index}
+          onSelect={setIndex}
+        />
+      )}
     </Box>
   )
 }
---
+++ packages/store-ui/src/Slider/PaginationDots.tsx
@@ -0,0 +1,73 @@
+/** @jsx jsx */
+import { FC, useMemo } from 'react'
+import { jsx, Box } from 'theme-ui'
+
+const DOTS_DEFAULT_SIZE = 0.625
+
+interface Props {
+  totalItems: number
+  selectedIndex: number
+  onSelect: (index: number) => void
+  variant?: string
+}
+
+export const PaginationDots: FC<Props> = ({
+  variant,
+  selectedIndex,
+  totalItems,
+  onSelect,
+}) => {
+  const indexes = useMemo(() => Array.from(Array(totalItems).keys()), [
+    totalItems,
+  ])
+
+  return (
+    <Box
+      variant={`${variant}.paginationDots.container`}
+      sx={{
+        marginTop: '0.5rem',
+        marginBottom: '0.5rem',
+        position: 'absolute',
+        justifyContent: 'center',
+        display: 'flex',
+        left: 0,
+        bottom: 0,
+        right: 0,
+        margin: 0,
+        padding: 0,
+      }}
+      role="group"
+      aria-label="Slider pagination dots"
+    >
+      {indexes.map((index) => {
+        const isActive = index === selectedIndex
+
+        return (
+          <Box
+            variant={`${variant}.paginationDots.${
+              isActive ? 'activeDot' : 'dot'
+            }`}
+            sx={{
+              cursor: 'pointer',
+              display: 'inline-block',
+              borderRadius: '100%',
+              margin: '0 0.25rem',
+              padding: '0.25rem',
+              borderWidth: 0,
+              outline: '0',
+              height: `${DOTS_DEFAULT_SIZE}rem`,
+              width: `${DOTS_DEFAULT_SIZE}rem`,
+            }}
+            key={index}
+            tabIndex={index}
+            onKeyDown={() => onSelect(index)}
+            onClick={() => onSelect(index)}
+            role="button"
+            aria-label={`Dot ${index + 1} of ${totalItems}`}
+            data-testid="paginationDot"
+          />
+        )
+      })}
+    </Box>
+  )
+}
