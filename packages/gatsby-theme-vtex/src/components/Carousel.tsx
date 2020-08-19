ed391ebdcdcd2c426d178655bdc6abb73ce4d518
Luciano Júnior <luciannojunior@gmail.com>
2020-08-19 17:22:04 -0300

    Adding Banner and autoplayProps to Carousel

--- packages/gatsby-theme-vtex/src/components/Carousel.tsx
+++ packages/gatsby-theme-vtex/src/components/Carousel.tsx
@@ -1,7 +1,14 @@
 /** @jsx jsx */
 import { FC, useState } from 'react'
-import { Box, Button, jsx, Image, PaginationDots } from '@vtex/store-ui'
 import { useIntl } from '@vtex/gatsby-plugin-i18n'
+import {
+  Box,
+  Button,
+  jsx,
+  Image,
+  PaginationDots,
+  useInterval,
+} from '@vtex/store-ui'
 
 interface Item {
   src: string
@@ -15,20 +22,37 @@ interface Props {
   items: Item[]
   showArrows?: boolean
   showDots?: boolean
+  autoplay?: boolean
+  autoplayTimeout?: number
 }
 
-const Carousel: FC<Props> = ({ items, showArrows = true, showDots = true }) => {
+const Carousel: FC<Props> = ({
+  items,
+  showArrows = true,
+  showDots = true,
+  autoplayTimeout = 1000,
+  autoplay = false,
+}) => {
+  const { formatMessage } = useIntl()
   const [index, setIndex] = useState(0)
   const lastIndex = items.length - 1
-  const height = 450
-  const { formatMessage } = useIntl()
+
+  useInterval(() => {
+    if (!autoplay) {
+      return
+    }
+
+    setIndex((currentIndex) =>
+      currentIndex === lastIndex ? 0 : currentIndex + 1
+    )
+  }, autoplayTimeout)
 
   return (
     <Box sx={{ position: 'relative' }}>
       {items.map((item, i) => (
         <Box
           key={item.altText}
-          sx={{ display: index === i ? 'block' : 'none', height }}
+          sx={{ display: index === i ? 'block' : 'none' }}
         >
           {showArrows && (
             <Button
@@ -52,7 +76,7 @@ const Carousel: FC<Props> = ({ items, showArrows = true, showDots = true }) => {
             height={item.height}
             alt={item.altText}
             loading={i === 0 ? 'eager' : 'lazy'}
-            sx={{ height, width: '100%', objectFit: 'cover' }}
+            sx={{ width: '100%', objectFit: 'cover' }}
           />
         </Box>
       ))}
