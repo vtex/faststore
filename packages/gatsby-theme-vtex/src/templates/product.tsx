9a1e44af7bb2221ac3654860e50e51d50c0ebfd4
Luciano Júnior <luciannojunior@gmail.com>
2020-08-20 12:30:51 -0300

    Realli fixing type errors

--- packages/gatsby-theme-vtex/src/templates/product.tsx
+++ packages/gatsby-theme-vtex/src/templates/product.tsx
@@ -1,5 +1,6 @@
 import { graphql, PageProps } from 'gatsby'
-import React, { FC } from 'react'
+import React, { FC, useMemo } from 'react'
+import { Breadcrumb, Container, NavigationItem } from '@vtex/store-ui'
 
 import ErrorBoundary from '../components/ErrorBoundary'
 import HybridWrapper from '../components/HybridWrapper'
@@ -37,11 +38,21 @@ const ProductPage: FC<Props> = ({
     initialData: staticPath ? initialData : undefined,
   })
 
+  const categoryTree = useMemo(
+    () => (data?.vtex?.product?.categoryTree ?? []) as NavigationItem[],
+    [data]
+  )
+
   if (!data?.vtex.product) {
     return <div>Product Not Found</div>
   }
 
-  return <ProductDetails product={data.vtex.product} />
+  return (
+    <Container>
+      <Breadcrumb categoryTree={categoryTree} />
+      <ProductDetails product={data.vtex.product} />
+    </Container>
+  )
 }
 
 const ProductPageContainer: FC<Props> = (props) => {
