118f4e803b7244fd3c5d3e8c9893f0d03e60cc63
Tiago Gimenes <tlgimenes@gmail.com>
2020-08-24 17:20:25 -0300

    images

--- packages/gatsby-theme-vtex/src/pages/index.tsx
+++ packages/gatsby-theme-vtex/src/pages/index.tsx
@@ -16,13 +16,13 @@ const Fold = lazy(() => import('../components/Home/Fold'))
 const itemsCarousel = [
   {
     src:
-      'https://storecomponents.vtexassets.com/assets/faststore/images/banner___febafa22a7ffc9a7f2fd049f416e7c7b.webp?aspect=true&height=450',
-    altText: 'Slide 2',
+      'https://storecomponents.vtexassets.com/assets/faststore/images/banner___febafa22a7ffc9a7f2fd049f416e7c7b.webp?aspect=true&width=360',
+    altText: 'Slide 1',
   },
   {
     src:
       'https://storecomponents.vtexassets.com/assets/vtex.file-manager-graphql/images/main___59700c0e5c56dcd769179d434f514892.webp?aspect=true&height=450',
-    altText: 'Slide 1',
+    altText: 'Slide 2',
   },
 ]
 
