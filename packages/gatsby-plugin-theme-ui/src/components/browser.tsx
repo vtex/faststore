66c43d3314bf41a115c3d68ad189de8de1d159ba
Tiago Gimenes <tlgimenes@gmail.com>
2020-08-25 21:31:32 -0300

    fallback to normal provider

--- packages/gatsby-plugin-theme-ui/src/components/browser.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, lazy, Suspense } from 'react'
-
-const BaseProvider = lazy(() => import('./provider'))
-
-export const ThemeProvider: FC = ({ children }) => (
-  <Suspense fallback={null}>
-    <BaseProvider>{children}</BaseProvider>
-  </Suspense>
-)
