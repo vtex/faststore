2ee5bb5ea06920275dfbc3f9af943f462cd995da
Tiago Gimenes <tlgimenes@gmail.com>
2020-08-25 22:32:07 -0300

    fallback to normal provider

--- packages/gatsby-plugin-theme-ui/src/gatsby-browser.ts
+++ packages/gatsby-plugin-theme-ui/src/gatsby-browser.ts
@@ -1,7 +1,7 @@
 import { createElement } from 'react'
 import { WrapRootElementBrowserArgs } from 'gatsby'
 
-const { ThemeProvider } = require('./src/components/browser')
+const { ThemeProvider } = require('./src/components/provider')
 
 export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) =>
   createElement(ThemeProvider, { children: element })
