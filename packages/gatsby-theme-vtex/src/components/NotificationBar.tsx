e49c27d4455def44160d6bb77c91d3e854aa3bdb
Joao Fidelis <joaogfidelis@gmail.com>
2020-08-26 16:22:23 -0300

    not bar

--- packages/gatsby-theme-vtex/src/components/NotificationBar.tsx
+++ packages/gatsby-theme-vtex/src/components/NotificationBar.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from 'react'
 import { Box } from '@vtex/store-ui'
+import { useLocalizationIntl } from '@vtex/gatsby-plugin-i18n'
 
 interface Props {
-  text: string
+  // text: string
   variant?: string
 }
 
-const StoreHeaderNotificationBar: FC<Props> = ({ text, variant }) => (
-  <Box variant={variant}>{text}</Box>
-)
+const StoreHeaderNotificationBar: FC<Props> = ({ variant }) => {
+  const { formatMessage } = useLocalizationIntl()
+  return (
+    <Box variant={variant}>{formatMessage({ id: 'signin' })}</Box>
+  )
+}
 
 export default StoreHeaderNotificationBar
