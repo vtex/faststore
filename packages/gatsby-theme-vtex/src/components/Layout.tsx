8b9f6a220c623f604393179b892a9a987b01d825
Tiago Gimenes <tlgimenes@gmail.com>
2020-08-27 15:13:16 -0300

    fix footer

--- packages/gatsby-theme-vtex/src/components/Layout.tsx
+++ packages/gatsby-theme-vtex/src/components/Layout.tsx
@@ -3,18 +3,17 @@ import { jsx } from '@vtex/store-ui'
 import { FC, Fragment, lazy } from 'react'
 
 import Header from './Header'
-import Footer from './Footer'
-// import SuspenseDelay from './SuspenseDelay'
+import SuspenseDelay from './SuspenseDelay'
 
-// const Footer = lazy(() => import('./Footer'))
+const Footer = lazy(() => import('./Footer'))
 
 const Layout: FC = ({ children }) => (
   <Fragment>
     <Header />
     {children}
-    {/* <SuspenseDelay fallback={null}> */}
+    <SuspenseDelay fallback={null}>
       <Footer />
-    {/* </SuspenseDelay> */}
+    </SuspenseDelay>
   </Fragment>
 )
 
