2003e792cb2b4d8753642a94ba3756ff03b49c86
Luciano Júnior <luciannojunior@gmail.com>
2020-08-31 14:21:08 -0300

    Adding width and height

--- packages/gatsby-theme-vtex/src/components/Home/index.tsx
+++ packages/gatsby-theme-vtex/src/components/Home/index.tsx
@@ -12,11 +12,15 @@ const itemsCarousel = [
     src:
       'https://storecomponents.vtexassets.com/assets/faststore/images/banner___febafa22a7ffc9a7f2fd049f416e7c7b.webp?aspect=true&height=450',
     altText: 'Slide 2',
+    width: 1024,
+    height: 400,
   },
   {
     src:
       'https://storecomponents.vtexassets.com/assets/vtex.file-manager-graphql/images/main___59700c0e5c56dcd769179d434f514892.webp?aspect=true&height=450',
     altText: 'Slide 1',
+    width: 1024,
+    height: 400,
   },
 ]
 
