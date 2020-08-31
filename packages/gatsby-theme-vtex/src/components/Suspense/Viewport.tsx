81b425b51a1a1c9fef182202a51fd674b47a2c5e
Tiago Gimenes <tlgimenes@gmail.com>
2020-08-31 09:34:45 -0300

    suspense inview

--- packages/gatsby-theme-vtex/src/components/Suspense/Viewport.tsx
+++ packages/gatsby-theme-vtex/src/components/Suspense/Viewport.tsx
@@ -3,13 +3,21 @@ import { useInView } from 'react-hook-inview'
 
 interface Props extends SuspenseProps {
   preloader: () => Promise<any>
+  rootMargin?: string // '0px' or '0px 0px 0px 0px', also accepts '%' unit
+  threshold?: number // A threshold of 1.0 means that when 100% of the target is visible within the element specified by the root option, the callback is invoked.
 }
 
-const SuspenseViewport: FC<Props> = ({ fallback, children, preloader }) => {
+const SuspenseViewport: FC<Props> = ({
+  fallback,
+  children,
+  preloader,
+  rootMargin = '50px',
+  threshold = 0,
+}) => {
   const [ref, isInView] = useInView({
     unobserveOnEnter: true, // Set 'true' to run only once
-    rootMargin: '50px', // '0px' or '0px 0px 0px 0px', also accepts '%' unit
-    threshold: 0, // A threshold of 1.0 means that when 100% of the target is visible within the element specified by the root option, the callback is invoked.
+    rootMargin,
+    threshold,
   })
 
   // useEffect(() => {
