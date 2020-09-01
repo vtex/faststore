a64da48d958b426d0ad6825ab517de6b6a95e51c
Luciano Júnior <luciannojunior@gmail.com>
2020-09-01 13:45:56 -0300

    Improvements on Banner

--- packages/store-ui/src/Banner/index.tsx
+++ packages/store-ui/src/Banner/index.tsx
@@ -1,17 +1,33 @@
 import React, { FC } from 'react'
 
+import LocalizedLink from '../LocalizedLink'
+
 type Props = {
   imgSrc: string
+  loading?: 'eager' | 'lazy'
   alt: string
   href: string
   width?: number | string
   height?: number | string
 }
 
-export const Banner: FC<Props> = ({ imgSrc, href, alt, width, height }) => {
+export const Banner: FC<Props> = ({
+  imgSrc,
+  href,
+  alt,
+  width,
+  height,
+  loading,
+}) => {
   return (
-    <a href={href}>
-      <img src={imgSrc} alt={alt} width={width} height={height} />
-    </a>
+    <LocalizedLink to={href}>
+      <img
+        src={imgSrc}
+        alt={alt}
+        width={width}
+        height={height}
+        loading={loading}
+      />
+    </LocalizedLink>
   )
 }
