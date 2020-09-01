de963f703ea4ee2002cf4223632fa26f8ee60bea
Tiago Gimenes <tlgimenes@gmail.com>
2020-09-01 11:12:50 -0300

    uses true global variable

--- packages/gatsby-theme-vtex/src/components/HybridWrapper.tsx
+++ packages/gatsby-theme-vtex/src/components/HybridWrapper.tsx
@@ -6,15 +6,15 @@ interface Props extends SuspenseProps {
   isPrerendered: boolean
 }
 
-;(window as any).__REACT_HYDRATED__ = false
+;(globalThis as any).__REACT_HYDRATED__ = false
 
 const HybridWrapper: FC<Props> = ({ fallback, isPrerendered, children }) => {
   const [isHydrated, setIsHydrated] = useState(
-    (window as any).__REACT_HYDRATED__
+    (globalThis as any).__REACT_HYDRATED__
   )
 
   useEffect(() => {
-    ;(window as any).__REACT_HYDRATED__ = true
+    ;(globalThis as any).__REACT_HYDRATED__ = true
     setIsHydrated(true)
   }, [])
 
