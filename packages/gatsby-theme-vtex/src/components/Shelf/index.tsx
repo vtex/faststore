3f775556543a7d9640f5b3ae40c2d9eb70049496
Tiago Gimenes <tlgimenes@gmail.com>
2020-09-07 13:59:18 -0300

    use slider in shelf

--- packages/gatsby-theme-vtex/src/components/Shelf/index.tsx
+++ packages/gatsby-theme-vtex/src/components/Shelf/index.tsx
@@ -1,68 +1,63 @@
-import React, { FC, useState } from 'react'
-import { Flex, Grid, Button, Box, Heading } from '@vtex/store-ui'
+/** @jsx jsx */
+import { FC, Fragment } from 'react'
+import { Flex, Grid, Box, Heading, useSlider, jsx } from '@vtex/store-ui'
 import { useResponsiveValue } from '@theme-ui/match-media'
 
 import { ProductSummary } from '../ProductSummary'
 import ArrowLeft from './ArrowLeft'
 import ArrowRight from './ArrowRight'
 import { ProductSummary_SyncProductFragment } from '../__generated__/ProductSummary_syncProduct.graphql'
+import PaginationDots from './PaginationDots'
 
 interface Props {
   products: Array<ProductSummary_SyncProductFragment | undefined | null>
+  showArrows?: boolean
+  showDots?: boolean
+  autoplay?: boolean
+  autoplayTimeout?: number
 }
 
-const ARROW_SIZES = [25, 50]
-const MAX_ITEMS = [1, 4]
-
-const hasPrevArrow = (page: number) => page > 0
-
-const hasNextArrow = (
-  page: number,
-  itemCount: number,
-  itemsPerPage: number
-) => {
-  const lastItemIndex = (page + 1) * itemsPerPage - 1
-
-  return lastItemIndex < itemCount - 1
-}
+const PAGE_SIZES = [1, 4]
 
 const FullWidthContainer: FC = ({ children }) => (
   <Box sx={{ width: '100%' }}>{children}</Box>
 )
 
-const Shelf: FC<Props> = ({ products }) => {
-  const [page, setPage] = useState(0)
-  const maxItems = useResponsiveValue(MAX_ITEMS)
-  const arrowSize = useResponsiveValue(ARROW_SIZES)
-
-  const items =
-    maxItems > 1
-      ? products.slice(page * maxItems, (page + 1) * maxItems)
-      : [products[page]]
+const Shelf: FC<Props> = ({
+  products,
+  showArrows = true,
+  showDots = true,
+  autoplay = false,
+  autoplayTimeout = 1e3,
+}) => {
+  const pageSize = useResponsiveValue(PAGE_SIZES, { defaultIndex: 1 })
+  const {
+    page,
+    items,
+    totalPages,
+    setPage,
+    setNextPage,
+    setPreviousPage,
+  } = useSlider({
+    allItems: products,
+    autoplayTimeout,
+    autoplay,
+    pageSize,
+  })
 
   return (
-    <Box>
+    <Fragment>
       <Flex p={2} sx={{ justifyContent: 'center' }} marginY={[16, 30]}>
         <Heading variant="shelfTitle" as="h2">
           summer
         </Heading>
       </Flex>
       <Flex>
-        <Flex sx={{ alignItems: 'center' }}>
-          {hasPrevArrow(page) && (
-            <Button
-              backgroundColor="transparent"
-              onClick={() => setPage(page - 1)}
-              aria-label="See shelf previous page"
-            >
-              <ArrowLeft size={arrowSize} />
-            </Button>
-          )}
-        </Flex>
+        {showArrows && <ArrowLeft onClick={() => setPreviousPage()} />}
         <FullWidthContainer>
           <Flex sx={{ justifyContent: 'center' }}>
             <FullWidthContainer>
-              <Grid gap={2} columns={MAX_ITEMS}>
+              <Grid gap={2} columns={PAGE_SIZES}>
                 {items.map((item) => {
                   return (
                     <Flex key={item!.productId!} sx={{ flexGrow: 1 }}>
@@ -72,23 +67,19 @@ const Shelf: FC<Props> = ({ products }) => {
                 })}
               </Grid>
             </FullWidthContainer>
-            <Flex sx={{ alignItems: 'center' }}>
-              {hasNextArrow(page, products.length, maxItems) && (
-                <Button
-                  backgroundColor="transparent"
-                  onClick={() => {
-                    setPage(page + 1)
-                  }}
-                  aria-label="See shelf next page"
-                >
-                  <ArrowRight size={arrowSize} />
-                </Button>
-              )}
-            </Flex>
           </Flex>
         </FullWidthContainer>
+        {showArrows && <ArrowRight onClick={() => setNextPage()} />}
       </Flex>
-    </Box>
+      {showDots && (
+        <PaginationDots
+          variant="shelf"
+          onSelect={setPage}
+          selectedPage={page}
+          totalPages={totalPages}
+        />
+      )}
+    </Fragment>
   )
 }
 
