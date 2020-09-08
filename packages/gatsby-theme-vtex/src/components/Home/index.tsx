5970abf6c98de9930ef0547d116280b40ec5da9a
Tiago Gimenes <tlgimenes@gmail.com>
2020-09-08 10:18:34 -0300

    internationalize

--- packages/gatsby-theme-vtex/src/components/Home/index.tsx
+++ packages/gatsby-theme-vtex/src/components/Home/index.tsx
@@ -1,30 +1,33 @@
+import { useIntl } from '@vtex/gatsby-plugin-i18n'
 import React, { FC, Fragment } from 'react'
 
+import { HomePageQueryQuery } from '../../__generated__/HomePageQuery.graphql'
 import Carousel from '../Carousel'
 import Container from '../Container'
 import Shelf from '../Shelf'
-import { HomePageQueryQuery } from '../../__generated__/HomePageQuery.graphql'
 import CAROUSEL_ITEMS from './carousel.json'
 
 interface Props {
   data: HomePageQueryQuery
 }
 
-const HomeBlocks: FC<Props> = ({ data }) => (
+const HomeBlocks: FC<Props> = ({ data }) => {
+  const { formatMessage } = useIntl()
+
+  return (
   <Fragment>
     <Carousel allItems={CAROUSEL_ITEMS} autoplayTimeout={5e3} autoplay />
     <Container>
       <Shelf
         products={data.vtex.productSearch!.products!}
-        title="New Offers"
+        title={formatMessage({ id: 'shelf.title.0' })}
         showArrows
         showDots
         autoplay={false}
         pageSizes={[1, 3, 4, 5]}
-        autoplayTimeout={8e3}
       />
     </Container>
   </Fragment>
-)
+)}
 
 export default HomeBlocks
