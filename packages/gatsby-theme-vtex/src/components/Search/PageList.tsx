fa50a9f9be8f139c1fb299c298bc88b51d18e384
Tiago Gimenes <tlgimenes@gmail.com>
2020-09-08 14:46:18 -0300

    adds search hook

--- packages/gatsby-theme-vtex/src/components/Search/PageList.tsx
+++ packages/gatsby-theme-vtex/src/components/Search/PageList.tsx
@@ -1,75 +1,25 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { gql } from '@vtex/gatsby-plugin-graphql'
 import { Button, Grid } from '@vtex/store-ui'
-import React, { FC, Fragment, useCallback } from 'react'
+import React, { FC, Fragment } from 'react'
 
-import { useQueryInfinite } from '../../sdk/graphql/useQueryInfinite'
-import { useFilters } from '../../sdk/search/useFilters'
 import {
   SearchQuery,
   SearchQueryQuery,
-  SearchQueryQueryVariables,
 } from './__generated__/SearchQuery.graphql'
 import OverlaySpinner from './OverlaySpinner'
 import Page from './Page'
-
-const PAGE_SIZE = 10
+import { useSearch } from '../../sdk/search/useSearch'
 
 interface Props {
   initialData: SearchQueryQuery | undefined
 }
 
 const List: FC<Props> = ({ initialData }) => {
-  const filters = useFilters()
-  const { data, error, size, setSize } = useQueryInfinite<
-    SearchQueryQuery,
-    SearchQueryQueryVariables
-  >(
-    SearchQuery,
-    (page, previousPageData) => {
-      if (
-        page !== 0 &&
-        previousPageData?.vtex.productSearch?.products?.length === 0
-      ) {
-        return null
-      }
-
-      const from = page * PAGE_SIZE
-      const to = (page + 1) * PAGE_SIZE - 1
-
-      return {
-        ...filters,
-        from,
-        to,
-      }
-    },
-    {
-      revalidateOnMount: true,
-      initialData: initialData && [initialData],
-      initialSize: 2, // 2 will always prefetch the next page
-    }
-  )
-
-  const fetchMore = useCallback(() => setSize!((s) => s + 1), [setSize])
-
-  // Since we prefetch the next page, we always render one page less from
-  // what we have in memory
-  const viewSize = Math.max(1, size! - 1)
-
-  // The code below was copied from SWR's own repo example
-  // https://codesandbox.io/s/swr-infinite-z6r0r?file=/src/App.js
-  const isLoadingInitialData = !data && !error
-  const isLoadingMore =
-    isLoadingInitialData ||
-    !!(data && typeof data[viewSize - 1] === 'undefined')
-
-  const isEmpty = data?.[0]?.vtex.productSearch!.products!.length === 0
-  const isReachingEnd =
-    isEmpty ||
-    !!(
-      data &&
-      data[viewSize - 1]?.vtex.productSearch!.products!.length < PAGE_SIZE
-    )
+  const { data, fetchMore, isLoadingMore, isReachingEnd } = useSearch({
+    query: SearchQuery,
+    initialData,
+  })
 
   if (!data) {
     return <OverlaySpinner />
@@ -78,25 +28,23 @@ const List: FC<Props> = ({ initialData }) => {
   return (
     <Fragment>
       <Grid my={4} gap={3} columns={[2, 2, 3, 5]}>
-        {data.slice(0, viewSize).map((searchQuery, index) => (
+        {data.map((searchQuery, index) => (
           <Page
             key={`summary-page-${index}`}
-            products={searchQuery.vtex.productSearch!.products!}
+            products={searchQuery!.vtex.productSearch!.products!}
           />
         ))}
       </Grid>
-      {isReachingEnd ? null : (
-        <Button
-          variant="loadMore"
-          onClick={(e) => {
-            ;(e.target as any).blur?.()
-            fetchMore()
-          }}
-          disabled={isReachingEnd || isLoadingMore}
-        >
-          {isLoadingMore ? 'Loading...' : 'More'}
-        </Button>
-      )}
+      <Button
+        variant="loadMore"
+        onClick={(e) => {
+          ;(e.target as any).blur?.()
+          fetchMore()
+        }}
+        disabled={isReachingEnd || isLoadingMore}
+      >
+        {isLoadingMore ? 'Loading...' : 'More'}
+      </Button>
     </Fragment>
   )
 }
