e990416b202bb551a6b23467a21bafb8db9ec8c0
Larícia Mota <laricia.mota@vtex.com.br>
2020-09-24 10:38:07 -0300

    Remove marin brasil specificities

--- packages/store-ui/src/FloatingActionButton/theme.ts
+++ packages/store-ui/src/FloatingActionButton/theme.ts
@@ -2,12 +2,10 @@ import { SxStyleProp } from 'theme-ui'
 
 const floatingActionButtonTheme: SxStyleProp = {
   floatingActionButton: {
-    div: {
-      position: 'fixed',
-      bottom: '25px',
-      right: '25px',
-      zIndex: 999,
-    },
+    position: 'fixed',
+    bottom: '25px',
+    right: '25px',
+    zIndex: 999,
     a: {
       display: 'flex',
       justifyContent: 'center',
@@ -15,36 +13,9 @@ const floatingActionButtonTheme: SxStyleProp = {
       width: '67px',
       height: '67px',
       borderRadius: '50%',
-      backgroundColor: '#5ece61',
+      backgroundColor: 'primary',
       boxShadow: '2px 2px 15px #999',
       padding: '1rem',
-      '&:hover div': {
-        opacity: 1,
-        transition: '.5s ease-in-out',
-        width: '182px',
-      },
-      div: {
-        position: 'absolute',
-        zIndex: -1,
-        opacity: 0,
-        color: '#fff',
-        background: '#5ece61',
-        fontSize: '.9rem',
-        textAlign: 'center',
-        width: '150px',
-        height: '67px',
-        borderRadius: '500px 0 0 500px',
-        right: '35px',
-        bottom: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        span: {
-          display: 'block',
-          fontSize: '1.2rem',
-          fontWeight: '900',
-        },
-      },
     },
   },
 }
