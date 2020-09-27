0fa51de98148eb82023eda54cdd35f8591807c9b
Tiago Gimenes <tlgimenes@gmail.com>
2020-09-26 22:07:45 -0300

    improve docs for search page

--- packages/gatsby-theme-vtex/src/components/SearchPage/AboveTheFold.tsx
+++ packages/gatsby-theme-vtex/src/components/SearchPage/AboveTheFold.tsx
@@ -3,50 +3,48 @@ import { Text, Center, jsx } from '@vtex/store-ui'
 import { FC, Fragment } from 'react'
 
 import { Props } from '../../templates/search'
+import {
+  SearchTemplateAside,
+  SearchTemplateContainer,
+  SearchTemplateMain,
+} from '../Search/SearchTemplate'
+import SuspenseDevice from '../Suspense/Device'
 
-const format = (x: string) =>
-  x.length > 100 ? `${x.slice(0, 100)} ...Truncated` : x
-
-const AboveTheFold: FC<Props> = (props) => (
+const AboveTheFold: FC<Props> = () => (
   <Fragment>
     <Center height="150px">
       <Text sx={{ width: '50%' }}>
         This is the Above the fold part of your search template. All sync items
         should be rendered in here. Thus, make sure all data rendered in this
         part is fetched during Server Side Rendering and revalidated on the
-        client if necessary
+        client if necessary. All data is available via props
       </Text>
     </Center>
 
-    <Center height="150px">
-      <Text sx={{ width: '50%' }}>
-        Product info will be available via the
-        &quot;data.vtex.productSearch&quot; prop. Also, if you want to fetch
-        more fields, you can add these fields in the
-        &quot;ProductSummary_syncProduct&quot; fragment. These fields will be
-        available in the same prop as above.
-      </Text>
-    </Center>
+    <SearchTemplateContainer>
+      <SearchTemplateAside>
+        <SuspenseDevice device="desktop" fallback={null}>
+          <Center height="150px">
+            <Text sx={{ width: '50%' }}>
+              This is where the desktop filters should fit. Note that Desktop
+              filters are lazy loaded and not SSR, thus this component should be
+              dynamically imported via React.lazy
+            </Text>
+          </Center>
+        </SuspenseDevice>
+      </SearchTemplateAside>
 
-    <Center>
-      <Text sx={{ width: '50%' }}>
-        Currently, this page&apos;s props are:
-        <ul>
-          {Object.keys(props)
-            .filter(
-              (k) =>
-                typeof (props as any)[k] === 'object' ||
-                typeof (props as any)[k] === 'string' ||
-                typeof (props as any)[k] === 'number'
-            )
-            .map((key) => (
-              <li key={key}>
-                {key}: {format(JSON.stringify((props as any)[key]))}
-              </li>
-            ))}
-        </ul>
-      </Text>
-    </Center>
+      <SearchTemplateMain>
+        <Center height="150px">
+          <Text sx={{ width: '50%' }}>
+            This is where the product list should be loaded. Note that a huge
+            number of products delay the first paint, thus harming hugely the
+            performance mainly in mobile devices. A good number varies from 8 to
+            16 depending on your case
+          </Text>
+        </Center>
+      </SearchTemplateMain>
+    </SearchTemplateContainer>
   </Fragment>
 )
 
