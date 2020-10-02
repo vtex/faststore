581205da4783af98b61b14954fadfa4c0a74180a
Luciano Júnior <luciannojunior@gmail.com>
2020-10-02 17:01:22 -0300

    Hooking the props
    :q
    
    :q

--- packages/gatsby-theme-vtex/src/templates/__generated__/ProductPageQuery.graphql.ts
+++ packages/gatsby-theme-vtex/src/templates/__generated__/ProductPageQuery.graphql.ts
@@ -13,7 +13,6 @@ type Scalars = {
   String: string
   Float: number
   Int: number
-  ID: string
 }
 
 // Operation related types
@@ -23,41 +22,14 @@ export type ProductPageQueryQueryVariables = Exact<{
 }>;
 
 
-export type ProductPageQueryQuery = {
-  vtex: {
-    product: Maybe<{
-      productReference: Maybe<string>
-      productName: Maybe<string>
-      linkText: Maybe<string>
-      productId: Maybe<string>
-      description: Maybe<string>
-      items: Maybe<
-        Array<
-          Maybe<{
-            itemId: Maybe<string>
-            images: Maybe<
-              Array<
-                Maybe<{ imageUrl: Maybe<string>; imageText: Maybe<string> }>
-              >
-            >
-          }>
-        >
-      >
-      productClusters: Maybe<Array<Maybe<{ name: Maybe<string> }>>>
-      categoryTree: Maybe<
-        Array<Maybe<{ name: Maybe<string>; href: Maybe<string> }>>
-      >
-    }>
-  }
-}
+export type ProductPageQueryQuery = { vtex: { product: Maybe<{ productReference: Maybe<string>, productName: Maybe<string>, linkText: Maybe<string>, productId: Maybe<string>, description: Maybe<string>, items: Maybe<Array<Maybe<{ itemId: Maybe<string>, images: Maybe<Array<Maybe<{ imageUrl: Maybe<string>, imageText: Maybe<string> }>>> }>>>, productClusters: Maybe<Array<Maybe<{ name: Maybe<string> }>>>, categoryTree: Maybe<Array<Maybe<{ name: Maybe<string>, href: Maybe<string> }>>> }> } };
+
 
 // Query Related Code
 
 export const ProductPageQuery = {
-  query:
-    'query ProductPageQuery($slug: String, $staticPath: Boolean!) {\n  vtex {\n    product(slug: $slug) @include(if: $staticPath) {\n      productReference\n      productName\n      linkText\n      items {\n        images {\n          imageUrl\n          imageText\n        }\n        itemId\n      }\n      productClusters {\n        name\n      }\n      productId\n      description\n      categoryTree {\n        name\n        href\n      }\n    }\n  }\n}\n',
-  sha256Hash:
-    '3f01910dd0ff42573bc8ff83b031ccccadd2bda796e9b34e2109045c98a06c25',
-  operationName: 'ProductPageQuery',
+  query: "query ProductPageQuery($slug: String, $staticPath: Boolean!) {\n  vtex {\n    product(slug: $slug) @include(if: $staticPath) {\n      productReference\n      productName\n      linkText\n      items {\n        images {\n          imageUrl\n          imageText\n        }\n        itemId\n      }\n      productClusters {\n        name\n      }\n      productId\n      description\n      categoryTree {\n        name\n        href\n      }\n    }\n  }\n}\n",
+  sha256Hash: "3f01910dd0ff42573bc8ff83b031ccccadd2bda796e9b34e2109045c98a06c25",
+  operationName: "ProductPageQuery",
 }
 
