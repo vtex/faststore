e224a50b32ef50b89c424e96ed38c927aed355d7
Luciano Júnior <luciannojunior@gmail.com>
2020-10-02 14:55:00 -0300

    Adding simulate query
    :q

--- packages/gatsby-theme-vtex/src/components/ShippingSimulator/ShippingTable.tsx
+++ packages/gatsby-theme-vtex/src/components/ShippingSimulator/ShippingTable.tsx
@@ -1,8 +1,17 @@
 import React, { FC } from 'react'
+import { Box } from '@vtex/store-ui'
 
-type Props = { shipping: any }
-const ShippingTable: FC<Props> = ({ shipping }) => {
-  return <div>{JSON.stringify(shipping, null, 2)}</div>
+type Props = {
+  shipping: any
+  variant: string
+}
+
+const ShippingTable: FC<Props> = ({ shipping, variant }) => {
+  return (
+    <Box variant={`${variant}.shippingTable`}>
+      {JSON.stringify(shipping, null, 2)}
+    </Box>
+  )
 }
 
 export default ShippingTable
---
+++ packages/gatsby-theme-vtex/src/sdk/graphql/useLazyQuery.ts
@@ -0,0 +1,35 @@
+import { request, RequestOptions } from '@vtex/gatsby-plugin-graphql'
+import useSWR, { ConfigInterface } from 'swr'
+
+type QueryOptions = ConfigInterface & RequestOptions
+
+// TODO: Change here
+const getKey = (options: QueryOptions) =>
+  `${options.sha256Hash}::${JSON.stringify(options.variables)}`
+
+export const useLazyQuery = <
+  Query extends any = any,
+  Variables extends any = any
+>(
+  options: QueryOptions
+) => {
+  const response = useSWR<Query | null, any[]>(getKey(options), () => null)
+
+  const execute = async (variables: Variables) => {
+    const { data } = await request<Variables, Query>('/graphql/', {
+      ...options,
+      variables,
+      fetchOptions: {
+        ...options.fetchOptions,
+        headers: {
+          'x-vtex-graphql-referer': window.location.host,
+          ...options.fetchOptions?.headers,
+        },
+      },
+    })
+
+    response.mutate(data, false)
+  }
+
+  return [execute, response]
+}
