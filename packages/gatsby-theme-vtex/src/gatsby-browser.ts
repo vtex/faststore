008273caf379c756fb20e06536043724a03c63a2
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-03 11:58:38 -0300

    preact

--- packages/gatsby-theme-vtex/src/gatsby-browser.ts
+++ packages/gatsby-theme-vtex/src/gatsby-browser.ts
@@ -10,25 +10,25 @@ const {
   onRouteUpdate: progressOnRouteUpdate,
 } = require('./src/sdk/progress')
 
-export const replaceHydrateFunction = () => (
-  element: ElementType,
-  container: Element,
-  callback: any
-) => {
-  const development = (process.env.GATSBY_BUILD_STAGE as any).includes(
-    'develop'
-  )
+// export const replaceHydrateFunction = () => (
+//   element: ElementType,
+//   container: Element,
+//   callback: any
+// ) => {
+//   const development = (process.env.GATSBY_BUILD_STAGE as any).includes(
+//     'develop'
+//   )
 
-  const { unstable_createRoot: createRoot }: any = ReactDOM
-  const root = createRoot(container, {
-    hydrate: !development,
-    hydrationOptions: {
-      onHydrated: callback,
-    },
-  })
+//   const { unstable_createRoot: createRoot }: any = ReactDOM
+//   const root = createRoot(container, {
+//     hydrate: !development,
+//     hydrationOptions: {
+//       onHydrated: callback,
+//     },
+//   })
 
-  root.render(element)
-}
+//   root.render(element)
+// }
 
 export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) =>
   createElement(StrictMode, {
