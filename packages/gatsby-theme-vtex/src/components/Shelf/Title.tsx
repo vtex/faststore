1f36bd7037e6a1177e7fc18cf27395ff8ade39be
Larícia Mota <laricia.mota@vtex.com.br>
2020-10-07 08:48:16 -0300

    Add variant to title

--- packages/gatsby-theme-vtex/src/components/Shelf/Title.tsx
+++ packages/gatsby-theme-vtex/src/components/Shelf/Title.tsx
@@ -4,11 +4,12 @@ import { FC } from 'react'
 
 interface Props {
   title: string
+  variant?: string
 }
 
-const ShelfTitle: FC<Props> = ({ title }) => (
+const ShelfTitle: FC<Props> = ({ title, variant = 'shelfTitle' }) => (
   <Flex p={2} sx={{ justifyContent: 'center' }} marginY={[16, 30]}>
-    <Heading variant="shelfTitle" as="h2">
+    <Heading variant={variant} as="h2">
       {title}
     </Heading>
   </Flex>
