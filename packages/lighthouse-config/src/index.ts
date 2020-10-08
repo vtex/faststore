65f9f7299047da089d08e9a3181c81b1860513bd
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-08 13:53:38 -0300

    replace url pattern

--- packages/lighthouse-config/src/index.ts
+++ packages/lighthouse-config/src/index.ts
@@ -15,28 +15,42 @@ const lhConfig = ({ urls, server }: Params) => {
       assert: {
         preset: 'lighthouse:no-pwa',
         assertions: {
+          // We still don't have a pwa capable app
+          'categories:pwa': 'off',
+
+          // Preview pages are not crawlable
+          'is-crawlable': 'off',
+
+          // Somehow lighthouse ci can't find the dns-prefetch tags we are adding
+          'uses-rel-preconnect': 'warn',
+
+          // Some images break this even after 4days of cache time
+          'uses-long-cache-ttl': 'off',
+
+          bypass: 'off',
+
+          // Minimal scope for categories
           'categories:accessibility': ['error', { minScore: 0.95 }],
           'categories:best-practices': ['error', { minScore: 0.95 }],
           'categories:performance': ['error', { minScore: 0.95 }],
-          'categories:pwa': 'off',
           'categories:seo': ['error', { minScore: 0.9 }],
+
+          // Max values for metrics
           'cumulative-layout-shift': ['error', { maxNumericValue: 0.2 }],
           'first-contentful-paint': ['error', { maxNumericValue: 2000 }],
-          'is-crawlable': 'off', // preview pages are not crawlable
           'largest-contentful-paint': ['error', { maxNumericValue: 3500 }],
           'max-potential-fid': ['error', { maxNumericValue: 300 }],
           'total-blocking-time': ['error', { maxNumericValue: 200 }],
-          'unused-javascript': ['error', { maxLength: 10 }],
-          'uses-long-cache-ttl': 'off',
-          'uses-rel-preconnect': 'warn', // somehow lighthouse ci can't find the dns-prefetch tags we are adding
-          'uses-text-compression': ['warn', { maxLength: 1 }],
-          bypass: 'off',
           interactive: ['error', { maxNumericValue: 3000 }],
+
+          // Somehow we can't get rid of this
+          'unused-javascript': ['error', { maxLength: 10 }],
+          'uses-text-compression': ['warn', { maxLength: 1 }],
         },
       },
       upload: {
         target: 'temporary-public-storage',
-        urlReplacementPatterns: 's/.*app//',
+        urlReplacementPatterns: 's/*.vtex.io//',
       },
     },
   }
