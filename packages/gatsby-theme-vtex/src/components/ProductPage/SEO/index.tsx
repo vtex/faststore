e3bb076cfc8ce1355bcb82b0cf2f792388f2991b
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-09 14:20:44 -0300

    product page

--- packages/gatsby-theme-vtex/src/components/ProductPage/SEO/index.tsx
+++ packages/gatsby-theme-vtex/src/components/ProductPage/SEO/index.tsx
@@ -5,11 +5,15 @@ import { isBot } from '../../../utils/env'
 import SiteMetadataSEO from '../../HomePage/SEO'
 import StructuredData from './StructuredData'
 
-const SEO: FC<ProductPageProps> = (props) => (
-  <>
-    <SiteMetadataSEO {...props} />
-    {isBot && <StructuredData {...props} />}
-  </>
-)
+const SEO: FC<ProductPageProps> = (props) => {
+  console.log({ isBot, useAgent: navigator?.userAgent })
+
+  return (
+    <>
+      <SiteMetadataSEO {...props} />
+      {isBot && <StructuredData {...props} />}
+    </>
+  )
+}
 
 export default SEO
