f90f0ce148339419ea0017dff7a3993da9182fc8
Larícia Mota <laricia.mota@vtex.com.br>
2020-10-13 12:10:53 -0300

    Add default shelf variant

--- packages/gatsby-theme-vtex/src/components/Shelf/Title.tsx
+++ packages/gatsby-theme-vtex/src/components/Shelf/Title.tsx
@@ -7,9 +7,9 @@ interface Props {
   variant?: string
 }
 
-const ShelfTitle: FC<Props> = ({ title, variant = 'shelf.title' }) => (
+const ShelfTitle: FC<Props> = ({ title, variant = 'shelf.default.title' }) => (
   <Flex p={2} sx={{ justifyContent: 'center' }} marginY={[16, 30]}>
-    <Heading variant={`${variant}`} as="h2">
+    <Heading variant={variant} as="h2">
       {title}
     </Heading>
   </Flex>
