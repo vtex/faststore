6e2aa25d391981860c67a3052d467ecb61f40b89
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-14 15:13:02 -0300

    add popup

--- packages/gatsby-theme-vtex/src/sdk/auth/OAuth/paths.ts
+++ packages/gatsby-theme-vtex/src/sdk/auth/OAuth/paths.ts
@@ -18,10 +18,9 @@ export const oAuthRedirectUrl = ({
   return `${api.pub.authentication.oauth.redirect}?${search.toString()}`
 }
 
-export const oAuthCallbackUrl = new URL(
-  api.oauth.finish,
-  window.location.origin
-).href
+export const oAuthCallbackUrl = `${
+  new URL(api.oauth.finish, window.location.origin).href
+}?popup=true`
 
 export const oAuthErrorFallbackUrl = () =>
   new URL(api.oauth.error, window.location.origin).href
