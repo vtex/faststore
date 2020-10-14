99111e985f47750c71385f6c6818fda06d7967f6
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-14 19:09:54 -0300

    rollback

--- packages/gatsby-theme-vtex/src/sdk/mask/useMaskedInput.ts
+++ packages/gatsby-theme-vtex/src/sdk/mask/useMaskedInput.ts
@@ -1,8 +1,9 @@
-import { createTextMaskInputElement } from 'text-mask-core'
+import {
+  createTextMaskInputElement,
+  TextMaskInputElement,
+  CreateTextMaskConfig,
+} from 'text-mask-core'
 import { MutableRefObject, useLayoutEffect, useRef } from 'react'
-
-type TextMaskInputElement = any
-type CreateTextMaskConfig = any
 
 interface Args extends Omit<CreateTextMaskConfig, 'inputElement'> {
   value?: string
