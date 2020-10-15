9f944a596518ce4897e17f36b14b987632662bf9
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-14 22:02:20 -0300

    sleak

--- packages/gatsby-theme-vtex/src/pages/account.tsx
+++ packages/gatsby-theme-vtex/src/pages/account.tsx
@@ -3,28 +3,26 @@ import React, { FC } from 'react'
 import Container from '../components/Container'
 import Layout from '../components/Layout'
 
-const Page: FC = () => {
-  return (
-    <Layout>
-      <Container>
-        <h1>My account</h1>
-        <iframe
-          title="my-account"
-          id="my-account"
-          frameBorder={0}
-          allowFullScreen
-          src="/legacy-extensions/account"
-          style={{
-            border: 'none',
-            visibility: 'visible',
-            overflow: 'hidden',
-            height: 750,
-            width: '100%',
-          }}
-        />
-      </Container>
-    </Layout>
-  )
-}
+const Page: FC = () => (
+  <Layout>
+    <Container>
+      <h1>My account</h1>
+      <iframe
+        title="my-account"
+        id="my-account"
+        frameBorder={0}
+        allowFullScreen
+        src="/legacy-extensions/account"
+        style={{
+          border: 'none',
+          visibility: 'visible',
+          overflow: 'hidden',
+          height: 750,
+          width: '100%',
+        }}
+      />
+    </Container>
+  </Layout>
+)
 
 export default Page
