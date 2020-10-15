aeb061bc5b6ec70c76143f6a39a09d61cfe16dd6
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-15 13:52:30 -0300

    upgrade theme

--- packages/gatsby-theme-vtex/src/pages/account.tsx
+++ packages/gatsby-theme-vtex/src/pages/account.tsx
@@ -1,36 +1,28 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import Container from '../components/Container'
 import Layout from '../components/Layout'
 
-const Account = () => {
-  return (
-    <Layout>
-      <Container>
-        <h1>My account</h1>
-        <MyAccountFetcher />
-      </Container>
-    </Layout>
-  )
-}
+const Page: FC = () => (
+  <Layout>
+    <Container>
+      <h1>My account</h1>
+      <iframe
+        title="my-account"
+        id="my-account"
+        frameBorder={0}
+        allowFullScreen
+        src="/my-account"
+        style={{
+          border: 'none',
+          visibility: 'visible',
+          overflow: 'hidden',
+          height: 750,
+          width: '100%',
+        }}
+      />
+    </Container>
+  </Layout>
+)
 
-const MyAccountFetcher = () => {
-  return (
-    <iframe
-      title="my-account"
-      id="my-account"
-      frameBorder={0}
-      allowFullScreen
-      src="/my-account"
-      style={{
-        border: 'none',
-        visibility: 'visible',
-        overflow: 'hidden',
-        height: 750,
-        width: '100%',
-      }}
-    />
-  )
-}
-
-export default Account
+export default Page
