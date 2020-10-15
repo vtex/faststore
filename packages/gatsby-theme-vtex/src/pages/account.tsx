8637d264035d58bade01c821d0aaf35ffbf947af
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-15 14:19:27 -0300

    render extension loader

--- packages/gatsby-theme-vtex/src/pages/account.tsx
+++ packages/gatsby-theme-vtex/src/pages/account.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useEffect } from 'react'
+import { Center, Spinner } from '@vtex/store-ui'
+import React, { FC, useEffect, useState } from 'react'
 
 import Container from '../components/Container'
 import Layout from '../components/Layout'
-import RenderExtensionLoader from '../utils/render-extension-loader'
+import RenderExtensionLoader from '../sdk/renderExtensionLoader'
 
 const MY_ACCOUNT_PATH = '/account'
 const MY_ACCOUNT_DIV_NAME = 'my-account'
@@ -13,58 +14,56 @@ const workspace = process.env.GATSBY_VTEX_IO_WORKSPACE
 const tenant = process.env.GATSBY_VTEX_TENANT
 
 const MyAccount: FC = () => {
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    const loader = new RenderExtensionLoader({
-      account: tenant,
-      workspace,
-      path: MY_ACCOUNT_PATH,
-      locale: 'pt-BR',
-      verbose: true,
-      publicEndpoint: undefined,
-      timeout: ONE_MIN_IN_MILLI,
-    })
-
-    const myAccountDiv = document.getElementById(MY_ACCOUNT_DIV_NAME)
-
-    if (window.__RENDER_7_RUNTIME__) {
-      loader.render(MY_ACCOUNT_EXTENSION_NAME, myAccountDiv, undefined)
-
-      return
-    }
-
-    loader
-      .load()
-      .then(() => {
-        window.__RUNTIME__ = loader.render(
-          MY_ACCOUNT_EXTENSION_NAME,
-          myAccountDiv,
-          undefined
-        )
+    ;(async () => {
+      const loader = new RenderExtensionLoader({
+        account: tenant,
+        workspace,
+        path: MY_ACCOUNT_PATH,
+        locale: 'pt-BR',
+        verbose: true,
+        publicEndpoint: undefined,
+        timeout: ONE_MIN_IN_MILLI,
       })
-      .catch(() => {})
+
+      const myAccountDiv = document.getElementById(MY_ACCOUNT_DIV_NAME)
+
+      if (window.__RENDER_7_RUNTIME__) {
+        loader.render(MY_ACCOUNT_EXTENSION_NAME, myAccountDiv, undefined)
+
+        return
+      }
+
+      await loader.load()
+
+      window.__RUNTIME__ = loader.render(
+        MY_ACCOUNT_EXTENSION_NAME,
+        myAccountDiv,
+        undefined
+      )
+
+      setLoading(false)
+    })()
   }, [])
 
-  return <div id={MY_ACCOUNT_DIV_NAME} />
+  return (
+    <>
+      <div id={MY_ACCOUNT_DIV_NAME} />
+      {loading && (
+        <Center height="500px">
+          <Spinner />
+        </Center>
+      )}
+    </>
+  )
 }
 
 const Page: FC = () => (
   <Layout>
     <Container>
       <h1>My account</h1>
-      {/* <iframe
-        title="my-account"
-        id="my-account"
-        frameBorder={0}
-        allowFullScreen
-        src="/my-account"
-        style={{
-          border: 'none',
-          visibility: 'visible',
-          overflow: 'hidden',
-          height: 750,
-          width: '100%',
-        }}
-      /> */}
       <MyAccount />
     </Container>
   </Layout>
