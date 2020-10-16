9879048d559ee58d2040d02509d24c3322e2474a
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-16 14:12:20 -0300

    adding login

---
+++ packages/gatsby-theme-vtex/src/components/Login/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+
+import { useProfile } from '../../sdk/session/useProfile'
+import SuspenseSSR from '../Suspense/SSR'
+import Anonymous from './Anonymous'
+import Authenticated from './Authenticated'
+
+const Login: FC = () => {
+  const profile = useProfile()
+  const name = profile?.firstName?.value ?? profile?.email?.value
+  const isAuthenticated = profile?.isAuthenticated?.value === 'true'
+
+  return isAuthenticated ? <Authenticated name={name!} /> : <Anonymous />
+}
+
+const LoginContainer: FC = () => (
+  <SuspenseSSR fallback={<Anonymous />}>
+    <Login />
+  </SuspenseSSR>
+)
+
+export default LoginContainer
