8565eec4ff6e04556801bdb9b4dbcd9e9da87cd2
Cezar <cezar.guimaraes@vtex.com.br>
2020-10-16 12:03:08 -0300

    Handle default options

---
+++ packages/gatsby-plugin-vtex-nginx/src/pluginOptions.ts
@@ -0,0 +1,44 @@
+import {
+  LOCATIONS_ONLY_ENV_VAR,
+  FILE_SERVE_DIRECTIVE_ENV_VAR,
+} from './constants'
+
+const defaultOptions: PluginOptions = {
+  transformHeaders: undefined,
+  writeOnlyLocations: false,
+  serveFileDirective: ['try_files', '$file'],
+  plugins: [],
+}
+
+export function pluginOptions(options: Partial<PluginOptions>): PluginOptions {
+  return {
+    transformHeaders:
+      options.transformHeaders ?? defaultOptions.transformHeaders,
+    writeOnlyLocations:
+      safeTransform(process.env[LOCATIONS_ONLY_ENV_VAR], (x) =>
+        Boolean(JSON.parse(x))
+      ) ??
+      options.writeOnlyLocations ??
+      defaultOptions.writeOnlyLocations,
+    serveFileDirective:
+      safeTransform(process.env[FILE_SERVE_DIRECTIVE_ENV_VAR], JSON.parse) ??
+      options.serveFileDirective ??
+      defaultOptions.serveFileDirective,
+    plugins: options.plugins ?? defaultOptions.plugins,
+  }
+}
+
+function safeTransform<T, R>(
+  value: T | undefined,
+  f: (_: T) => R
+): R | undefined {
+  if (value === undefined) {
+    return undefined
+  }
+
+  try {
+    return f(value)
+  } catch {
+    return undefined
+  }
+}
