d140797149364fb745f20126f835e47aa350f902
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-17 16:00:49 -0300

    anonymous

---
+++ packages/gatsby-theme-vtex/src/components/Login/Anonymous.tsx
@@ -0,0 +1,23 @@
+import { useIntl } from '@vtex/gatsby-plugin-i18n'
+import { Box, LocalizedLink } from '@vtex/store-ui'
+import React, { FC } from 'react'
+
+import Logo from './Logo'
+
+const Anonymous: FC = () => {
+  const { formatMessage } = useIntl()
+
+  return (
+    <LocalizedLink to="/account">
+      <Logo />
+      <Box variant="login.button.greeting">
+        {formatMessage(
+          { id: 'login.button.greeting', defaultMessage: 'Hello {name}' },
+          { name }
+        )}
+      </Box>
+    </LocalizedLink>
+  )
+}
+
+export default Anonymous
--- packages/gatsby-theme-vtex/src/components/Login/index.tsx
+++ packages/gatsby-theme-vtex/src/components/Login/index.tsx
@@ -1,44 +1,22 @@
-/** @jsx jsx */
-import { useIntl } from '@vtex/gatsby-plugin-i18n'
-import { Box, LocalizedLink, jsx } from '@vtex/store-ui'
-import { FC } from 'react'
+import { Box } from '@vtex/store-ui'
+import React, { FC } from 'react'
 
 import { useProfile } from '../../sdk/session/useProfile'
 import SuspenseSSR from '../Suspense/SSR'
-import Logo from './Logo'
+import Anonymous from './Anonymous'
+import Authenticated from './Authenticated'
 
 const Login: FC = () => {
-  const { formatMessage } = useIntl()
   const profile = useProfile()
   const name = profile?.firstName?.value ?? profile?.email?.value
   const isAuthenticated = profile?.isAuthenticated?.value === 'true'
 
-  return isAuthenticated ? (
-    <LocalizedLink to="/account">
-      <Logo />
-      <Box variant="login.button.greeting">
-        {formatMessage(
-          { id: 'login.button.greeting', defaultMessage: 'Hello {name}' },
-          { name }
-        )}
-      </Box>
-    </LocalizedLink>
-  ) : (
-    <LocalizedLink to="/login">
-      <Logo />
-      <Box variant="login.button.greeting">
-        {formatMessage({
-          id: 'login.button.action',
-          defaultMessage: 'Sign In',
-        })}
-      </Box>
-    </LocalizedLink>
-  )
+  return isAuthenticated ? <Authenticated name={name!} /> : <Anonymous />
 }
 
 const LoginContainer: FC = () => (
   <Box variant="login.button.container">
-    <SuspenseSSR fallback={null}>
+    <SuspenseSSR fallback={<Anonymous />}>
       <Login />
     </SuspenseSSR>
   </Box>
