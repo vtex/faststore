dce3e880a89fa1631c534e3d86227b30e569d01a
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-17 14:12:29 -0300

    my-account

--- packages/gatsby-theme-vtex/src/pages/account.tsx
+++ packages/gatsby-theme-vtex/src/pages/account.tsx
@@ -1,16 +1,16 @@
+import React, { FC, useEffect } from 'react'
 import { navigate } from 'gatsby'
-import React, { FC, useEffect, useRef } from 'react'
 
 import Container from '../components/Container'
 import Layout from '../components/Layout'
 import SuspenseSSR from '../components/Suspense/SSR'
-import { useSession } from '../sdk/session/useSession'
+import { useProfile } from '../sdk/session/useProfile'
+import RenderMyAccount from './legacy-extensions/account'
 
 const Iframe: FC = () => {
-  const iframeRef = useRef<HTMLIFrameElement>(null)
-  const [session, dispatch] = useSession({ stale: false })
-  const isAuthenticated =
-    session?.namespaces.profile?.isAuthenticated?.value === 'true'
+  // const iframeRef = useRef<HTMLIFrameElement>(null)
+  const profile = useProfile({ stale: false })
+  const isAuthenticated = profile?.isAuthenticated?.value === 'true'
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -22,35 +22,25 @@ const Iframe: FC = () => {
     return null
   }
 
-  return (
-    <iframe
-      ref={iframeRef}
-      title="my-account"
-      id="my-account"
-      frameBorder={0}
-      allowFullScreen
-      src="/legacy-extensions/account"
-      onLoad={() => {
-        const pathname = iframeRef.current?.contentWindow?.location.pathname
+  return <RenderMyAccount />
 
-        if (pathname === '/legacy-extensions/account') {
-          return
-        }
-
-        // eslint-disable-next-line no-console
-        console.log({ pathname })
-        navigate('/')
-        dispatch({ type: 'clear' })
-      }}
-      style={{
-        border: 'none',
-        visibility: 'visible',
-        overflow: 'hidden',
-        height: 1000,
-        width: '100%',
-      }}
-    />
-  )
+  // return (
+  //   <iframe
+  //     ref={iframeRef}
+  //     title="my-account"
+  //     id="my-account"
+  //     frameBorder={0}
+  //     allowFullScreen
+  //     src="/legacy-extensions/account"
+  //     style={{
+  //       border: 'none',
+  //       visibility: 'visible',
+  //       overflow: 'hidden',
+  //       height: 1000,
+  //       width: '100%',
+  //     }}
+  //   />
+  // )
 }
 
 const Page: FC = () => (
