31a3c28aaca0e1563769e8c1e1fa53f9f81a4adb
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-23 17:11:29 -0300

    fix progressive and responsive image

---
+++ packages/store-ui/src/ResponsivePicture/index.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable jsx-a11y/alt-text */
+/** @jsx jsx */
+import { FC } from 'react'
+import { jsx } from 'theme-ui'
+
+interface IResponsivePictureSource {
+  media: string
+  srcSet: string
+  sizes?: string
+}
+
+interface IResponsivePicture {
+  sources: IResponsivePictureSource[]
+  src?: string
+  alt: string
+  height?: string
+  width?: string
+}
+
+interface Props extends IResponsivePicture {
+  loading: 'eager' | 'lazy'
+  variant: string
+}
+
+const ResponsivePicture: FC<Props> = ({ sources, variant, ...imgProps }) => (
+  <picture
+    sx={{
+      variant: `${variant}.responsivePicture.picture`,
+    }}
+  >
+    {sources.map((source) => (
+      <source key={source.srcSet} {...source} />
+    ))}
+    <img
+      sx={{
+        variant: `${variant}.responsivePicture.img`,
+      }}
+      {...imgProps}
+    />
+  </picture>
+)
+
+export default ResponsivePicture
