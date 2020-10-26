cce2bf8001a275314007f57b86e637c2db2a9706
Luciano Júnior <luciannojunior@gmail.com>
2020-10-26 16:53:29 -0300

    Adding nullish operator

--- packages/gatsby-theme-vtex/src/components/Error/ErrorHandler.tsx
+++ packages/gatsby-theme-vtex/src/components/Error/ErrorHandler.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 import Error from './Error'
 
 const DefaultErrorHandler: FC<{ error: any }> = ({ error }) => {
-  if (error.extensions.exception.status === 404) {
+  if (error?.extensions?.exception?.status === 404) {
     window.location.href = `/404?from=${window.location.pathname}`
 
     return null
