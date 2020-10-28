7f66bb3b5f640926cfffcdb83118112d8d449845
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-27 21:02:50 -0300

    suspense ssr

--- packages/gatsby-theme-vtex/src/components/Suspense/SSR.tsx
+++ packages/gatsby-theme-vtex/src/components/Suspense/SSR.tsx
@@ -3,7 +3,11 @@ import React, { FC, Suspense, SuspenseProps, useEffect, useState } from 'react'
 const SuspenseSSR: FC<SuspenseProps> = ({ fallback, children }) => {
   const [hydrating, setHydrating] = useState(true)
 
-  useEffect(() => setHydrating(false), [])
+  useEffect(() => {
+    const id = window.requestIdleCallback(() => setHydrating(false))
+
+    return () => window.cancelIdleCallback(id)
+  }, [])
 
   if (hydrating) {
     return <>{fallback}</>
