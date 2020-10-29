1b93461bd1040c0ee99f1b5e0678240d7ea6b288
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-29 14:39:00 -0300

    remove cookies in graphql request

--- packages/gatsby-theme-vtex/src/sdk/graphql/request.ts
+++ packages/gatsby-theme-vtex/src/sdk/graphql/request.ts
@@ -14,6 +14,7 @@ export const request = async <
   const { data, errors } = await baseRequest<Variables, Query>('/graphql/', {
     ...options,
     fetchOptions: {
+      credentials: 'omit', // we should NOT use cookies in graphql
       ...options.fetchOptions,
       headers: {
         'x-vtex-graphql-referer': window.location.host,
