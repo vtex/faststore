dc16c66241c4c992b8019dd24ec96e494ee74353
Carla Cosenza <carla.cosenza@vtex.com.br>
2020-10-29 15:34:06 -0300

    Fixing incorrect import

--- packages/gatsby-theme-vtex/src/components/ProductQuantity/index.tsx
+++ packages/gatsby-theme-vtex/src/components/ProductQuantity/index.tsx
@@ -2,14 +2,12 @@ import { Box } from '@vtex/store-ui'
 import { ProductQuantityTitle, NumericStepper } from '@vtex/store-ui'
 import React, { FC } from 'react'
 
-import useProductQuantity from './useProductQuantity'
-
 interface Props {
   slug: string
 }
 
 const Quantity: FC<Props> = ({ slug }) => {
-  const quantity = { maxValue: undefined }
+  const quantity = { maxValue: undefined, minValue: 1 }
 
   return (
     <Box>
