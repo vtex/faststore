bca41038c4bfb3efd4f2853e527f5507dec19ae3
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-10-29 11:47:56 -0300

    fix: callback in updateItems

--- packages/gatsby-theme-vtex/src/components/Minicart/Drawer/useUpdateItems.ts
+++ packages/gatsby-theme-vtex/src/components/Minicart/Drawer/useUpdateItems.ts
@@ -5,8 +5,11 @@ export const useUpdateItems = (index: number) => {
   const { updateItems } = useOrderForm()
   const item = useItem(index)
 
-  const trigger = (quantity: number) => {
-    updateItems([{ id: item.id, quantity, seller: item.seller, index }])
+  const trigger = (quantity: number, callback?: any) => {
+    updateItems(
+      [{ id: item.id, quantity, seller: item.seller, index }],
+      callback
+    )
   }
 
   return trigger
