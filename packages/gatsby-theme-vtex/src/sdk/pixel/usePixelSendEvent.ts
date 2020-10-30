217a116281af36d11b23554ded8ad87e1c9e734f
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-30 14:28:40 -0300

    add event files

---
+++ packages/gatsby-theme-vtex/src/sdk/pixel/usePixelSendEvent.ts
@@ -0,0 +1,22 @@
+import { PixelEvent, wrap } from './pixel'
+import { useIdleEffect } from '../useIdleEffect'
+
+type EventGenerator = () => PixelEvent
+
+export const sendPixelEvent = (event: PixelEvent) => {
+  try {
+    window.postMessage(wrap(event), window.origin)
+  } catch (e) {
+    // IE and Edge have a bug on postMessage inside promises.
+    // Ignoring for now, will try to find a workaround that
+    // makes postMessage work on those browsers.
+    // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/14719328/
+  }
+}
+
+export const usePixelSendEvent = (event: PixelEvent | EventGenerator) => {
+  useIdleEffect(
+    () => sendPixelEvent(typeof event === 'function' ? event() : event),
+    []
+  )
+}
