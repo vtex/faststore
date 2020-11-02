4cbf000940deb0781258fb821e7e28da868c525c
Luciano Júnior <luciannojunior@gmail.com>
2020-11-02 17:58:09 -0300

    Changing Item to support video

--- packages/gatsby-theme-vtex/src/components/ProductImageGallery/Page.tsx
+++ packages/gatsby-theme-vtex/src/components/ProductImageGallery/Page.tsx
@@ -1,20 +1,29 @@
 import { AspectImage, ProgressiveImage } from '@vtex/store-ui'
 import React, { ComponentPropsWithoutRef, FC } from 'react'
 
-export type Item = ComponentPropsWithoutRef<typeof ProgressiveImage>
+export type Item =
+  | {
+      type: 'video'
+    }
+  | {
+      type: 'image'
+      src: string
+      props: ComponentPropsWithoutRef<typeof ProgressiveImage>
+    }
 
 interface Props {
   item: Item
   variant: string
 }
 
-const ProductImageGalleryPage: FC<Props> = ({ item, variant }) => (
-  <ProgressiveImage
-    {...item}
-    variant={`${variant}.img`}
-    ratio={1}
-    as={AspectImage}
-  />
-)
+const ProductImageGalleryPage: FC<Props> = ({ item, variant }) =>
+  item.type === 'image' ? (
+    <ProgressiveImage
+      {...item.props}
+      variant={`${variant}.img`}
+      ratio={1}
+      as={AspectImage}
+    />
+  ) : null
 
 export default ProductImageGalleryPage
