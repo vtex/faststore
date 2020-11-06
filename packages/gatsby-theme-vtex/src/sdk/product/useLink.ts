1212e254abec5746f33dfa347d53ac4e4554b66c
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-06 12:34:57 -0300

    fix product page navigation

--- packages/gatsby-theme-vtex/src/sdk/product/useLink.ts
+++ packages/gatsby-theme-vtex/src/sdk/product/useLink.ts
@@ -1,1 +1,15 @@
-export const useLink = (linkText: string) => `/${linkText}/p`
+import { MouseEvent } from 'react'
+
+import { ProductSummary_ProductFragment } from '../../components/ProductSummary/__generated__/ProductSummary_product.graphql'
+import { sendPixelEvent } from '../pixel/usePixelSendEvent'
+
+export const useLink = (product: ProductSummary_ProductFragment) => ({
+  to: `/${product.linkText}/p`,
+  onClick: (_: MouseEvent<HTMLAnchorElement>) =>
+    sendPixelEvent({
+      type: 'vtex:productClick',
+      data: {
+        product,
+      },
+    }),
+})
