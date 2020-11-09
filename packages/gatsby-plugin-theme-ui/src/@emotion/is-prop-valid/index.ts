124de7d812bba4176a0690b340987172fdcfd04f
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-09 20:43:45 -0300

    is prop valid

--- packages/gatsby-plugin-theme-ui/src/@emotion/is-prop-valid/index.ts
+++ packages/gatsby-plugin-theme-ui/src/@emotion/is-prop-valid/index.ts
@@ -4,9 +4,16 @@ import { reactProps } from './props'
 
 const reactPropsRegex = /^(([Dd][Aa][Tt][Aa]|[Aa][Rr][Ii][Aa]|x)-.*)$/
 
-const isPropValid = memoize((prop: string) => (
-  ( prop.charCodeAt(0) === 111 /* o */ && prop.charCodeAt(1) === 110 /* n */ && prop.charCodeAt(2) < 91 /* Z+1 */ ) ||
-  ( !!reactProps[prop] || reactPropsRegex.test(prop) )
-))
+const isPropValid = memoize((prop: string) => {
+  console.log('isPropValid')
+
+  return (
+    (prop.charCodeAt(0) === 111 /* o */ &&
+    prop.charCodeAt(1) === 110 /* n */ &&
+      prop.charCodeAt(2) < 91) /* Z+1 */ ||
+    !!reactProps[prop] ||
+    reactPropsRegex.test(prop)
+  )
+})
 
 export default isPropValid
