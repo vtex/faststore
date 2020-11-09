01acbb4113dff6c30c8bdb61e4c35bbbc156d4c2
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-09 17:01:16 -0300

    wip

--- packages/gatsby-plugin-theme-ui/src/gatsby-ssr.ts
+++
@@ -1,7 +0,0 @@
-import { createElement } from 'react'
-import { WrapRootElementBrowserArgs } from 'gatsby'
-
-const { ThemeProvider } = require('./src/components/provider')
-
-export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) =>
-  createElement(ThemeProvider, { children: element })
