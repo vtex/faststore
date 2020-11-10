9a2713fb15af1949a61994f517aa6eeb86ac0da3
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-10 13:07:12 -0300

    drop colormode

--- packages/gatsby-plugin-theme-ui/src/components/provider.tsx
+++ packages/gatsby-plugin-theme-ui/src/components/provider.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { ThemeProvider as ThemeUIProvider } from 'theme-ui'
+import { ThemeProvider as ThemeUIProvider } from '@theme-ui/core'
 
 import theme from '../index'
 
