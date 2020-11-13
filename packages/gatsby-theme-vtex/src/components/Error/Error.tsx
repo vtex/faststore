a35df968fc542f7140f5ec6e1ae1262f866ee80e
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-13 18:44:51 -0300

    add layout

--- packages/gatsby-theme-vtex/src/components/Error/Error.tsx
+++ packages/gatsby-theme-vtex/src/components/Error/Error.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from 'react'
 
+import Layout from '../Layout'
+
 type Props = {
   error: any
 }
 
-const Error: FC<Props> = ({ error }) => {
-  return (
+const Error: FC<Props> = ({ error }) => (
+  <Layout>
     <div>
       Something went wrong. <pre>{JSON.stringify(error, null, 2)}</pre>
     </div>
-  )
-}
+  </Layout>
+)
 
 export default Error
