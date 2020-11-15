9232479f402fb4127133e90527f655f66a0f1f4d
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-14 23:16:58 -0300

    use sku

--- packages/gatsby-theme-vtex/src/sdk/product/useSku.ts
+++ packages/gatsby-theme-vtex/src/sdk/product/useSku.ts
@@ -1,4 +1,6 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
+
+import { useSearchParams } from '../state/useSearchParams'
 
 export interface Sku {
   itemId: string
@@ -8,17 +10,31 @@ interface Props {
   items: Sku[]
 }
 
-export const useSku = (product: Props | null, skuId?: string) =>
-  useMemo(() => {
+export const useSku = (product: Props) => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const skuId = searchParams.get('skuId')
+
+  const setSku = useCallback(
+    (id: string) => {
+      searchParams.set('skuId', id)
+      setSearchParams(searchParams, { replace: true })
+    },
+    [searchParams, setSearchParams]
+  )
+
+  const sku = useMemo(() => {
     if (product == null) {
       return
     }
 
     if (skuId === undefined) {
-      return product.items![0]
+      return product.items[0]
     }
 
     return (
-      product.items!.find((sku) => sku?.itemId === skuId) ?? product.items![0]
+      product.items.find((item) => item.itemId === skuId) ?? product.items[0]
     )
   }, [product, skuId])
+
+  return [sku, setSku] as [Sku, typeof setSku]
+}
--- packages/gatsby-theme-vtex/src/sdk/state/useSearchParams.tsx
+++ packages/gatsby-theme-vtex/src/sdk/state/useSearchParams.tsx
@@ -1,19 +1,20 @@
 import { useMemo } from 'react'
+import { navigate } from 'gatsby'
+import { useLocation } from '@reach/router'
 
-import { isServer } from '../../utils/env'
-
-export const setSearchParams = (qs: string) => {
-  window.location.search =
-    window.location.search.length === 0
-      ? `?${qs}`
-      : `${window.location.search}&${qs}`
-}
+export const setSearchParams = (
+  search: URLSearchParams,
+  options?: {
+    state?: any
+    replace?: boolean
+  }
+) => navigate(`?${search.toString()}`, options)
 
 export const useSearchParams = (): [
   URLSearchParams,
   typeof setSearchParams
 ] => {
-  const search = !isServer ? window.location.search : ''
+  const { search } = useLocation()
   const searchParams = useMemo(() => new URLSearchParams(search), [search])
 
   return [searchParams, setSearchParams]
