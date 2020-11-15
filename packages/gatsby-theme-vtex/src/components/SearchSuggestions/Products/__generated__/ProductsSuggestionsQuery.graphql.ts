cb7680b8c803ed56db16226c7ce60be9a5373f94
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-15 11:29:32 -0300

    test

--- packages/gatsby-theme-vtex/src/components/SearchSuggestions/Products/__generated__/ProductsSuggestionsQuery.graphql.ts
+++ packages/gatsby-theme-vtex/src/components/SearchSuggestions/Products/__generated__/ProductsSuggestionsQuery.graphql.ts
@@ -87,8 +87,7 @@ export type ProductsSuggestionsQueryQuery = {
 // Query Related Code
 
 export const ProductsSuggestionsQuery = {
-  query:
-    'query ProductsSuggestionsQuery($fullText: String!, $facetKey: String, $facetValue: String, $productOriginVtex: Boolean = false, $simulationBehavior: VTEX_SimulationBehavior = default) {\n  vtex {\n    productSuggestions(fullText: $fullText, facetKey: $facetKey, facetValue: $facetValue, productOriginVtex: $productOriginVtex, simulationBehavior: $simulationBehavior) {\n      count\n      products {\n        key: productId\n        productId\n        productName\n        linkText\n        productClusters {\n          name\n        }\n        items {\n          itemId\n          images {\n            imageUrl\n            imageText\n          }\n          sellers {\n            sellerId\n            commercialOffer: commertialOffer {\n              maxInstallments: Installments(criteria: MAX_WITHOUT_INTEREST) {\n                value: Value\n                numberOfInstallments: NumberOfInstallments\n              }\n              installments: Installments(criteria: ALL) {\n                value: Value\n                numberOfInstallments: NumberOfInstallments\n                interestRate: InterestRate\n              }\n              availableQuantity: AvailableQuantity\n              price: Price\n              listPrice: ListPrice\n              spotPrice\n              teasers {\n                name\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}\n',
+  query: undefined,
   sha256Hash:
     'f86944c7b1321edcc6e096b995cc2d8eed1db294f2a6b61472bc261aab343325',
   operationName: 'ProductsSuggestionsQuery',
