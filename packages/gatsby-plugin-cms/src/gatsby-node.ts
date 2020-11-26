8860f7ab21cbb97c7f748c337609e5477813c393
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-26 17:57:20 -0300

    wip

--- packages/gatsby-plugin-cms/src/gatsby-node.ts
+++ packages/gatsby-plugin-cms/src/gatsby-node.ts
@@ -1,19 +1,114 @@
 import { join } from 'path'
 
+import { JSONSchema6 } from 'json-schema'
 import chokidar from 'chokidar'
-import { ensureDir, outputFileSync, readJSONSync, unlink } from 'fs-extra'
+import {
+  ensureDir,
+  outputFileSync,
+  pathExists,
+  readJSONSync,
+  unlink,
+  outputJSON,
+} from 'fs-extra'
 import { CreatePagesArgs } from 'gatsby'
 
 import { ContentDOM } from './builder/compiler'
 import { getMeta, isContent } from './common'
+import { Components, ContentTypes, Schemas } from './index'
+
+interface CMSContentType {
+  previewUrl: string
+  messages: Record<string, string>
+  blocks: Array<{ name: string; schema: JSONSchema6 }>
+  beforeBlocks: Array<{ name: string; schema: JSONSchema6 }>
+  afterBlocks: Array<{ name: string; schema: JSONSchema6 }>
+  extraBlocks: Array<{
+    name: string
+    blocks: Array<{ name: string; schema: JSONSchema6 }>
+  }>
+}
+
+const { name } = require('./package.json')
 
 const root = process.cwd()
+
+export const onPostBootstrap = async () => {
+  // Read index.ts from shadowed plugin
+  const filepath = join(root, 'src', name, 'index.ts')
+  const exists = await pathExists(filepath)
+
+  if (!exists) {
+    return
+  }
+
+  require('@babel/register')({
+    extensions: ['.ts'],
+    presets: ['@babel/preset-typescript'],
+  })
+  const { components, schemas, contentTypes } = require(filepath) as {
+    components: Components
+    schemas: Schemas
+    contentTypes: ContentTypes
+  }
+
+  // Make sure all components have a schema
+  for (const component in components) {
+    if (!(component in schemas)) {
+      throw new Error(
+        `${component} does not have a schema. Please define it and export in schemas at index.ts`
+      )
+    }
+  }
+
+  // Transform all contentTypes into CMS ready contentType json
+  const contentTypesCMS = Object.keys(contentTypes).reduce((acc, key) => {
+    const ct = contentTypes[key]
+
+    const blocks = Object.keys(ct.blocks).map((k) => ({
+      name: k,
+      schema: ct.blocks[k],
+    }))
+
+    const beforeBlocks = Object.keys(ct.beforeBlocks).map((k) => ({
+      name: k,
+      schema: ct.beforeBlocks[k],
+    }))
+
+    const afterBlocks = Object.keys(ct.afterBlocks).map((k) => ({
+      name: k,
+      schema: ct.afterBlocks[k],
+    }))
+
+    const extraBlocks = Object.keys(ct.extraBlocks).map((k) => ({
+      name: k,
+      blocks: Object.keys(ct.extraBlocks[k]).map((kk) => ({
+        name: kk,
+        schema: ct.extraBlocks[k][kk],
+      })),
+    }))
+
+    acc.push({
+      ...ct,
+      blocks,
+      beforeBlocks,
+      afterBlocks,
+      extraBlocks,
+    })
+
+    return acc
+  }, [] as CMSContentType[])
+
+  await outputJSON(
+    join(root, 'public/page-data/_cms/contentTypes.json'),
+    contentTypesCMS
+  )
+}
 
 export const createPages = async ({
   actions: { createPage },
   reporter,
 }: CreatePagesArgs) => {
-  const blocksRootPath = join(root, 'src/@vtex/gatsby-plugin-cms/templates')
+  const blocksRootPath = join(root, 'src/@vtex/gatsby-plugin-cms/contents')
 
   // ensure dist folder
   const generatedRootPath = join(blocksRootPath, '/__generated__')
@@ -51,6 +146,8 @@ export const createPages = async ({
     if (typeof slug !== 'string') {
       throw new Error(`No slug found for CMS block ${filename}`)
     }
+
+    reporter.info(`[${name}]: Updated page: ${slug}`)
 
     return {
       src,
--- packages/gatsby-plugin-cms/src/index.ts
+++ packages/gatsby-plugin-cms/src/index.ts
@@ -1,3 +1,25 @@
 import { LazyExoticComponent } from 'react'
+import { JSONSchema6 } from 'json-schema'
 
-export const components: Record<string, LazyExoticComponent<any>> = {}
+export type Components = Record<string, LazyExoticComponent<any>>
+
+export const components: Components = {}
+
+type Schema = JSONSchema6 & Required<Pick<JSONSchema6, 'title' | 'description'>>
+
+export type Schemas = Record<string, Schema>
+
+export const schemas: Schemas = {}
+
+interface ContentType {
+  previewUrl: string
+  blocks: Schemas
+  extraBlocks: Record<string, Schemas>
+  beforeBlocks: Schemas
+  afterBlocks: Schemas
+  messages: Record<string, string>
+}
+
+export type ContentTypes = Record<string, ContentType>
+
+export const contentTypes: ContentTypes = {}
