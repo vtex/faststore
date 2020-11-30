1f2b0ecddfabbdc924014b3f0fb16db5f4c51055
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-30 15:14:00 -0300

    allows gtm to be configured

--- packages/gatsby-theme-store/src/sdk/pixel/GTM/index.tsx
+++ packages/gatsby-theme-store/src/sdk/pixel/GTM/index.tsx
@@ -1,27 +1,55 @@
-import React, { FC, Fragment } from 'react'
+import React, { FC, Fragment, useMemo } from 'react'
 
 import { isServer } from '../../../utils/env'
 import { useLazyScript } from '../../lazyScript/useLazyScript'
+import { once } from '../../once'
 import { usePixelEvent } from '../usePixelEvent'
 import { handler } from './handler'
 
-if (!isServer) {
+const DEFAULT_DATALAYER_CONFIG = [{ 'gtm.blacklist': ['html'] }]
+
+const setupGTM = once((dataLayerConfig: any[]) => {
+  if (isServer) {
+    return
+  }
+
   window.dataLayer = window.dataLayer || []
-  window.dataLayer.push({ 'gtm.blacklist': ['html'] })
+
+  for (const item of dataLayerConfig) {
+    window.dataLayer.push(item)
+  }
+
   window.dataLayer.push({ 'gtm.start': Date.now(), event: 'gtm.js' })
-}
+})
+
+const useSetupGTM = (dataLayerConfig: any[]) =>
+  // We don't need dataLayerConfig in hooks deps since setupGTM is run once
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useMemo(() => setupGTM(dataLayerConfig), [])
 
 interface Props {
   gtmId: string
+  timeout?: number
+  dataLayerConfig?: any[]
 }
 
-const Provider: FC<Props> = ({ children, gtmId }) => {
+const Provider: FC<Props> = ({
+  children,
+  gtmId,
+  timeout = 5500,
+  dataLayerConfig = DEFAULT_DATALAYER_CONFIG,
+}) => {
+  // Setup GTM
+  useSetupGTM(dataLayerConfig)
+
+  // Include GTM's scripts into the page
   useLazyScript({
     src: `https://www.googletagmanager.com/gtm.js?id=${gtmId}`,
     id: gtmId,
-    timeout: 5500,
+    timeout,
   })
 
+  // Register Pixel event handler
   usePixelEvent(handler)
 
   return <Fragment>{children}</Fragment>
