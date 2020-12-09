e63731ffd52735509541b4ceec208b71bbdcc8f3
Tiago Gimenes <tlgimenes@gmail.com>
2020-12-09 12:44:17 -0300

    export the whole thing

--- packages/gatsby-plugin-i18n/src/index.ts
+++ packages/gatsby-plugin-i18n/src/index.ts
@@ -1,20 +1,4 @@
-export {
-  useIntl,
-  FormattedMessage,
-  FormattedDate,
-  FormattedDateTimeRange,
-  FormattedDateParts,
-  FormattedDisplayName,
-  FormattedList,
-  FormattedNumber,
-  FormattedNumberParts,
-  FormattedPlural,
-  FormattedRelativeTime,
-  FormattedTime,
-  FormattedTimeParts,
-} from 'react-intl'
-
-export type { IntlShape } from 'react-intl'
+export * from 'react-intl'
 
 export { localizedPath } from './helpers/path'
 export { useLocalizedPath } from './helpers/useLocalizedPath'
