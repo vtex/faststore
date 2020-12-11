108bfb29a67ec37b6028edd6380df9275b4fc503
Tiago Gimenes <tlgimenes@gmail.com>
2020-12-10 21:38:18 -0300

    simpler orderForm

--- packages/gatsby-theme-store/src/components/Minicart/Drawer/useUpdateItems.ts
+++ packages/gatsby-theme-store/src/components/Minicart/Drawer/useUpdateItems.ts
@@ -5,12 +5,6 @@ export const useUpdateItems = (index: number) => {
   const { updateItems } = useOrderForm()
   const item = useItem(index)
 
-  const trigger = (quantity: number, callback?: any) => {
-    updateItems(
-      [{ id: item.id, quantity, seller: item.seller, index }],
-      callback
-    )
-  }
-
-  return trigger
+  return (quantity: number) =>
+    updateItems([{ id: item.id, quantity, seller: item.seller, index }])
 }
