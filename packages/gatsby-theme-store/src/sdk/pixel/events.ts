f6b0cbf8f172d5c121f98a2121a953f39a4022b1
Tiago Gimenes <tlgimenes@gmail.com>
2021-01-12 19:51:00 -0300

    send one click buy in event

--- packages/gatsby-theme-store/src/sdk/pixel/events.ts
+++ packages/gatsby-theme-store/src/sdk/pixel/events.ts
@@ -45,6 +45,7 @@ export interface InternalSiteSearchViewData extends PageViewData {
 
 export interface AddToCartData {
   items: OrderFormItem[]
+  oneClickBuy?: boolean
 }
 
 export interface RemoveFromCartData {
