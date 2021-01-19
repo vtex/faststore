454910e9965efd08096595e60378c463cf4e9699
Tiago Gimenes <tlgimenes@gmail.com>
2021-01-19 14:08:15 -0300

    remove unecessary change

--- packages/gatsby-theme-store/src/components/Error/ErrorHandler.tsx
+++ packages/gatsby-theme-store/src/components/Error/ErrorHandler.tsx
@@ -4,10 +4,7 @@ import type { FC } from 'react'
 import Error from './Error'
 
 const ErrorHandler: FC<{ error: any }> = ({ error }) => {
-  if (
-    error?.extensions?.exception?.status === 404 ||
-    error.message === 'Not Found'
-  ) {
+  if (error?.extensions?.exception?.status === 404) {
     window.location.href = `/404?from=${window.location.pathname}`
 
     return null
