1a54fa59b59f5d808a810028739a71de4c601cc4
João Marcos Gris <joao.gris@vtex.com.br>
2021-01-21 13:44:18 -0300

    Remove enforcement to have dataLayerConfig as option

--- packages/gatsby-plugin-google-tag-manager/src/gatsby-node.ts
+++ packages/gatsby-plugin-google-tag-manager/src/gatsby-node.ts
@@ -3,5 +3,5 @@ import type { PluginOptionsSchemaArgs } from 'gatsby'
 export const pluginOptionsSchema = ({ Joi }: PluginOptionsSchemaArgs) =>
   Joi.object({
     gtmId: Joi.string().required(),
-    dataLayerConfig: Joi.array().items(Joi.object()).required(),
+    dataLayerConfig: Joi.array().items(Joi.object()),
   })
