9a8e5ab61dc76befc33e21920c16cd50543c753b
Gabriel Antiqueira <gabriel.antiqueira@vtex.com.br>
2021-01-21 15:05:29 -0300

    fixing type importing/exporting

--- packages/store-ui/src/Auth/Providers/index.tsx
+++ packages/store-ui/src/Auth/Providers/index.tsx
@@ -3,19 +3,19 @@ export { default as EmailAndPasswordButton } from './EmailAndPassword/Button'
 export { default as EmailAndPasswordEmailForm } from './EmailAndPassword/EmailForm'
 export { default as EmailAndPasswordSignInForm } from './EmailAndPassword/SignInForm'
 export { default as EmailAndPasswordSignUpForm } from './EmailAndPassword/SignUpForm'
-export {
+export { reducer as EmailAndPasswordReducer } from './EmailAndPassword/state'
+export type {
   State as EmailAndPasswordState,
   Action as EmailAndPasswordAction,
-  reducer as EmailAndPasswordReducer,
 } from './EmailAndPassword/state'
 // EmailVerification
 export { default as EmailVerificationAccessCodeForm } from './EmailVerification/AccessCodeForm'
 export { default as EmailVerificationButton } from './EmailVerification/Button'
 export { default as EmailVerificationEmailForm } from './EmailVerification/EmailForm'
-export {
+export { reducer as EmailVerificationReducer } from './EmailVerification/state'
+export type {
   State as EmailVerificationState,
   Action as EmailVerificationAction,
-  reducer as EmailVerificationReducer,
 } from './EmailVerification/state'
 // Facebook
 export { default as FacebookOAuthButton } from './Facebook/Button'
