973125a5664965ec0360cb3499cf10ac490ba398
Gabriel Antiqueira <gabriel.antiqueira@vtex.com.br>
2021-01-29 12:02:32 -0300

    updated old stories to new storybook

--- packages/store-ui/.storybook/decorators/ThemeProvider.jsx
+++ packages/store-ui/.storybook/decorators/ThemeProvider.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 import globalTheme from '../themes'
 
-console.log(globalTheme)
-
 const Provider = (Story) => {
   return <ThemeProvider theme={globalTheme}>{Story()}</ThemeProvider>
 }
