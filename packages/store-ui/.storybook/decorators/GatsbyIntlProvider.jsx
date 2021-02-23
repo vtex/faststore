6d15fecf68f19b60fa1ef9790e995dfeb1510761
VTEX <gabriel.antiqueira@vtex.com.br>
2021-02-23 13:55:17 -0300

    converting PT config to EN for storybook

--- packages/store-ui/.storybook/decorators/GatsbyIntlProvider.jsx
+++ packages/store-ui/.storybook/decorators/GatsbyIntlProvider.jsx
@@ -2,20 +2,9 @@ import React from 'react'
 import { IntlProvider } from '@vtex/gatsby-plugin-i18n'
 import MessagesPt from '../i18n/pt.json'
 
-// We can use the code below if we add more languages in the future.
-
-// const locales = ['pt']
-
-// const messages: any = locales.reduce((acc, locale) => {
-//   return {
-//     ...acc,
-//     [locale]: require(`../i18n/${locale}.json`),
-//   }
-// }, {})
-
 const Provider = (Story) => {
   return (
-    <IntlProvider locale="pt" defaultLocale="pt" messages={MessagesPt}>
+    <IntlProvider locale="en" defaultLocale="en" messages={MessagesPt}>
       {Story()}
     </IntlProvider>
   )
