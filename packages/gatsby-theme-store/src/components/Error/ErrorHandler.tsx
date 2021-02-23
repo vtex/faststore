ca102b07a6e9a365f99f9a636decdc8ad25e0dd6
victorhmp <victor2142@gmail.com>
2021-02-23 14:36:46 -0300

    Add offline check logic to ErrorHandler

--- packages/gatsby-theme-store/src/components/Error/ErrorHandler.tsx
+++ packages/gatsby-theme-store/src/components/Error/ErrorHandler.tsx
@@ -2,7 +2,7 @@
  * WARNING: Do not shadow this component and use Theme-ui's component
  * or any other dependency that uses React context API. If you want to
  * style or change the error page, please shadow the `pages/500.tsx` instead.
- * This component is synchronouly imported and has a big TBT implication
+ * This component is synchronously imported and has a big TBT implication
  */
 import type { FC } from 'react'
 import { useEffect } from 'react'
@@ -21,15 +21,31 @@ export const handleError = ({ error, errorId }: Props) => {
   // prevent infinite loop
   if (
     window.location.pathname.startsWith('/404') ||
-    window.location.pathname.startsWith('/500')
+    window.location.pathname.startsWith('/500') ||
+    window.location.pathname.startsWith('/offline')
   ) {
     return
   }
 
-  window.location.href =
-    error?.extensions?.exception?.status === 404
-      ? `/404?from=${window.location.pathname}`
-      : `/500?from=${window.location.pathname}&errorId=${errorId ?? uuidv4()}`
+  const isUserOffline = !navigator.onLine
+
+  if (error?.extensions?.exception?.status === 404) {
+    window.location.href = `/404?from=${window.location.pathname}`
+
+    return
+  }
+
+  if (isUserOffline) {
+    window.location.href = '/offline'
+  }
+
+  window.location.href = `/500?from=${window.location.pathname}&errorId=${
+    errorId ?? uuidv4()
+  }`
+  // window.location.href =
+  //   error?.extensions?.exception?.status === 404
+  //     ? `/404?from=${window.location.pathname}`
+  //     : `/500?from=${window.location.pathname}&errorId=${errorId ?? uuidv4()}`
 }
 
 const ErrorHandler: FC<Props> = ({ error, errorId }) => {
