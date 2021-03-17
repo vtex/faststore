a713ffc513e2ecbb8f35632355275b291195f15b
igorbrasileiro <brasileiro456@gmail.com>
2021-03-17 15:05:20 -0300

    Refactor the OrderForm Providers

--- packages/gatsby-theme-store/src/sdk/orderForm/Providers.tsx
+++ packages/gatsby-theme-store/src/sdk/orderForm/Providers.tsx
@@ -1,83 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import type { FC } from 'react'
 import { OrderQueueProvider } from '@vtex/order-manager/src/OrderQueue'
 import { OrderItemsProvider } from 'order-items/src/OrderItems'
 import { createOrderFormProvider } from '@vtex/order-manager/src/createOrderFormProvider'
 
-import { getOrderformId } from './controller/orderForm'
 import { clearOrderFormMessages } from './clearOrderFormMessage'
 import { useToast } from './useToast'
-import type { getOrderForm, addToCart, updateItems } from './controller'
-import type { OrderFormFragment_OrderFormFragment as OrderForm } from './controller/__generated__/OrderFormFragment_orderForm.graphql'
-import type { AddToCartMutationMutationVariables } from './controller/__generated__/AddToCartMutation.graphql'
-import type { UpdateItemsMutationMutationVariables } from './controller/__generated__/UpdateItemsMutation.graphql'
+import { useGetOrderForm } from './useGetOrderForm'
+import type { OrderFormFragment_OrderFormFragment } from './controller/__generated__/OrderFormFragment_orderForm.graphql'
 
-let controller: {
-  addToCart: typeof addToCart
-  getOrderForm: typeof getOrderForm
-  updateItems: typeof updateItems
-} | null = null
-
-const getController = async () => {
-  if (!controller) {
-    controller = await import('./controller')
-  }
-
-  return controller
-}
-
-const useGetOrderForm = () => {
-  const [orderForm, setOrderForm] = useState<OrderForm>()
-
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<any>(null)
-
-  useEffect(() => {
-    getController().then((localController) => {
-      localController
-        .getOrderForm()
-        .then((newOrderForm) => {
-          setOrderForm(newOrderForm)
-          setLoading(false)
-        })
-        .catch(setError)
-    })
-  }, [])
-
-  return { data: orderForm ? { orderForm } : undefined, loading, error }
-}
-
-interface GraphQLMutation<T> {
-  variables: T
-}
-
-const parseResponseToData = (orderForm: OrderForm) => ({
-  data: orderForm,
-})
-
-const handleAddItem = async ({
-  variables: { items },
-}: GraphQLMutation<AddToCartMutationMutationVariables>) => {
-  const id = getOrderformId()
-
-  const localController = await getController()
-
-  return localController.addToCart(id!, items).then(parseResponseToData)
-}
-
-const handleUpdateQuantity = async ({
-  variables: { items },
-}: GraphQLMutation<UpdateItemsMutationMutationVariables>) => {
-  const id = getOrderformId()
-
-  const localController = await getController()
-
-  return localController
-    .updateItems({ orderFormId: id!, items })
-    .then(parseResponseToData)
-}
-
-const { OrderFormProvider, useOrderForm } = createOrderFormProvider<OrderForm>({
+const {
+  OrderFormProvider,
+  useOrderForm,
+} = createOrderFormProvider<OrderFormFragment_OrderFormFragment>({
   useGetOrderForm,
   clearOrderFormMessages,
   useToast,
@@ -87,10 +22,7 @@ export const OrderFormProviders: FC = ({ children }) => {
   return (
     <OrderQueueProvider>
       <OrderFormProvider>
-        <OrderItemsProvider
-          handleAddItem={handleAddItem}
-          handleUpdateQuantity={handleUpdateQuantity}
-        >
+        <OrderItemsProvider>
           {children}
         </OrderItemsProvider>
       </OrderFormProvider>
@@ -99,4 +31,3 @@ export const OrderFormProviders: FC = ({ children }) => {
 }
 
 export { useOrderForm }
-// TODO: substitute the order form provider here /packages/gatsby-theme-store/src/gatsby-browser.tsx
