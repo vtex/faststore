7220248301a8711683a22fadb7c33dc327b9dc6e
igorbrasileiro <brasileiro456@gmail.com>
2021-03-17 15:05:20 -0300

    Refactor the OrderForm Providers

---
+++ packages/gatsby-theme-store/src/sdk/orderForm/useGetOrderForm.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+
+import type { OrderFormFragment_OrderFormFragment } from './controller/__generated__/OrderFormFragment_orderForm.graphql'
+
+const getOrderFormController = () => import('./controller')
+
+export const useGetOrderForm = () => {
+  const [
+    orderForm,
+    setOrderForm,
+  ] = useState<OrderFormFragment_OrderFormFragment>()
+
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<any>(null)
+
+  useEffect(() => {
+    getOrderFormController().then((localController) => {
+      localController
+        .getOrderForm()
+        .then((newOrderForm) => {
+          setOrderForm(newOrderForm)
+          setLoading(false)
+        })
+        .catch(setError)
+    })
+  }, [])
+
+  return { data: orderForm ? { orderForm } : undefined, loading, error }
+}
