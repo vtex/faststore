b681368a8d62d86351a4f07392627cc608f9d932
igorbrasileiro <brasileiro456@gmail.com>
2021-03-26 15:45:19 -0300

    Add toast component and the provider

---
+++ packages/gatsby-theme-store/src/components/ToastProvider/index.tsx
@@ -0,0 +1,20 @@
+// snippet of code to use ToastProvider lazy
+
+// import React, { Suspense, lazy } from 'react'
+// import type { FC } from 'react'
+
+// const loader = () => import('@vtex/store-ui/src/ToastMessage/ToastProvider')
+
+// const ToastProvider = lazy(loader)
+
+// export const Provider: FC = ({ children }) => {
+//   return (
+//     <Suspense fallback={null}>
+//       <ToastProvider>{children}</ToastProvider>
+//     </Suspense>
+//   )
+// }
+
+import type { PropsWithChildren } from 'react'
+
+export const Provider = ({ children }: PropsWithChildren<null>) => children
