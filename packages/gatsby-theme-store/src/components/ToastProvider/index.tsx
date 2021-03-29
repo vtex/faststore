020ea20bc53e1078c0a2e83e840e185bf71cbb52
igorbrasileiro <brasileiro456@gmail.com>
2021-03-29 15:35:40 -0300

    Use lazy provider

--- packages/gatsby-theme-store/src/components/ToastProvider/index.tsx
+++ packages/gatsby-theme-store/src/components/ToastProvider/index.tsx
@@ -1,20 +1,14 @@
-// snippet of code to use ToastProvider lazy
+import React, { Suspense, lazy } from 'react'
+import type { FC } from 'react'
 
-// import React, { Suspense, lazy } from 'react'
-// import type { FC } from 'react'
+const loader = () => import('@vtex/store-ui/src/ToastMessage/ToastProvider')
 
-// const loader = () => import('@vtex/store-ui/src/ToastMessage/ToastProvider')
+const ToastProvider = lazy(loader)
 
-// const ToastProvider = lazy(loader)
-
-// export const Provider: FC = ({ children }) => {
-//   return (
-//     <Suspense fallback={() => children}>
-//       <ToastProvider>{children}</ToastProvider>
-//     </Suspense>
-//   )
-// }
-
-import type { PropsWithChildren } from 'react'
-
-export const Provider = ({ children }: PropsWithChildren<null>) => children
+export const Provider: FC = ({ children }) => {
+  return (
+    <Suspense fallback={<>{children}</>}>
+      <ToastProvider>{children}</ToastProvider>
+    </Suspense>
+  )
+}
