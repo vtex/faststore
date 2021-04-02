9831ceec3fb4c9296446effc4f849705b025c549
Tiago Gimenes <tlgimenes@gmail.com>
2021-04-02 20:19:02 -0300

    adds content-visibility auto to below the fold loader

--- packages/gatsby-theme-store/src/components/HomePage/BelowTheFoldPreview.tsx
+++ packages/gatsby-theme-store/src/components/HomePage/BelowTheFoldPreview.tsx
@@ -1,11 +1,20 @@
 import { Center, Spinner } from '@vtex/store-ui'
-import type { FC } from 'react'
+import type { FC, CSSProperties } from 'react'
 import React from 'react'
 
 const BelowTheFoldPreview: FC = () => (
-  <Center height="500px">
-    <Spinner />
-  </Center>
+  <div
+    style={
+      ({
+        contentVisibility: 'auto',
+        containIntrinsicSize: '500px',
+      } as unknown) as CSSProperties
+    }
+  >
+    <Center height="500px">
+      <Spinner />
+    </Center>
+  </div>
 )
 
 export default BelowTheFoldPreview
