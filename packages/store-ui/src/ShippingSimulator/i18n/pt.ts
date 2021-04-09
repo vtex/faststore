92ba8c798c809068c9315cb303b3fef245ca25fc
Tiago Gimenes <tlgimenes@gmail.com>
2021-04-09 13:37:10 -0300

    move shipping simulator messages to store-ui

---
+++ packages/store-ui/src/ShippingSimulator/i18n/pt.ts
@@ -0,0 +1,18 @@
+export default {
+  'shipping.label': 'OK',
+  'shipping.postalCodeLabel': 'Calcular frete e prazo de entrega:',
+  'shipping.free': 'Entrega Grátis',
+  'shipping.label.id': 'Tipo',
+  'shipping.label.estimate': 'Prazo',
+  'shipping.label.price': 'Preço',
+  'shippingEstimate-bd':
+    '{timeAmount, plural, =0 {Hoje} one {Em # dia útil} other {Até # dias úteis}}',
+  'shippingEstimate-d':
+    '{timeAmount, plural, =0 {Hoje} one {Em # dia} other {Até # dias}}',
+  'shippingEstimate-h':
+    '{timeAmount, plural, =0 {Now} one {Em # hora} other {Até # horas}}',
+  'shippingEstimate-m':
+    '{timeAmount, plural, =0 {Now} one {Em # minuto} other {Até # minutos}}',
+  'shippingEstimate-scheduled': '{date}, entre {startDate} e {endDate}',
+  'shippingEstimate-scheduled-no-dates': 'Marcado',
+}
