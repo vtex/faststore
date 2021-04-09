cdee1964cb6a716d494ff3d9df9f15b6f6d85075
Tiago Gimenes <tlgimenes@gmail.com>
2021-04-09 17:20:49 -0300

    wip

--- packages/gatsby-theme-store/src/index.ts
+++ packages/gatsby-theme-store/src/index.ts
@@ -47,6 +47,8 @@ export {
   IMAGE_DEFAULT,
 } from './sdk/product/constants'
 
+export { search } from './sdk/search/controller'
+
 export { useProfile } from './sdk/session/useProfile'
 export { useSession } from './sdk/session/useSession'
 
