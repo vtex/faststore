edc4d0336eee31178bbba2dc485d5ec467600ceb
igorbrasileiro <brasileiro456@gmail.com>
2021-04-09 16:57:57 -0300

    Add type property to Toast and respective layouts

--- packages/store-ui/src/Toast/index.tsx
+++ packages/store-ui/src/Toast/index.tsx
@@ -4,21 +4,44 @@ import { Box } from 'theme-ui'
 import type { ReactNode, FC } from 'react'
 
 import { CrossIcon } from './CrossIcon'
+import { ErrorIcon } from './ErrorIcon'
+import { WarningIcon } from './WarningIcon'
+import { CheckIcon } from './CheckIcon'
+
+type ToastType = 'warning' | 'error' | 'success'
+
+const typeIcons: Record<ToastType, FC> = {
+  error: ErrorIcon,
+  warning: WarningIcon,
+  success: CheckIcon,
+}
 
 interface Props {
   message: ReactNode
   isVisible: boolean
   hideToast: () => void
+  type: ToastType
 }
 
-const Toast: FC<Props> = ({ message, isVisible, hideToast }) => {
+const Toast: FC<Props> = ({
+  message,
+  isVisible,
+  hideToast,
+  type = 'success',
+}) => {
   if (!isVisible) {
     return null
   }
 
+  const containerVariant = `toast.container.${type}`
+  const Icon = typeIcons[type]
+
   const content = (
-    <Box variant="toast.container">
-      {message}
+    <Box variant={containerVariant}>
+      <Box variant="toast.icon">
+        <Icon />
+      </Box>
+      <Box variant="toast.message">{message}</Box>
       <Box as="button" variant="toast.closeButton" onClick={hideToast}>
         <CrossIcon />
       </Box>
