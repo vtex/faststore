ecd77491f8084ddf137e414e6dc50fa90f715459
igorbrasileiro <brasileiro456@gmail.com>
2021-04-09 16:57:57 -0300

    Add type property to Toast and respective layouts

--- packages/store-ui/src/Toast/theme.ts
+++ packages/store-ui/src/Toast/theme.ts
@@ -1,39 +1,59 @@
 import type { SxStyleProp } from 'theme-ui'
 
+const defaultContainerTheme: SxStyleProp = {
+  alignItems: 'center',
+  borderRadius: 5,
+  boxShadow: '0 0 10px rgb(0 0 0 / 28%)',
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'row',
+  fontSize: '14px',
+  justifyContent: 'space-between',
+  zIndex: 99999,
+
+  bottom: 30,
+  position: 'sticky',
+
+  marginLeft: ['10px', 'auto'],
+  marginRight: ['10px', '5%'],
+  py: '5px',
+  px: '10px',
+
+  minHeight: 60,
+  width: ['auto', '100%'],
+  maxWidth: ['none', 420],
+  fill: 'white',
+}
+
 const theme: SxStyleProp = {
   toast: {
     container: {
-      alignItems: 'center',
-      background: '#40aa60',
-      borderRadius: 5,
-      boxShadow: '0 0 10px rgb(0 0 0 / 28%)',
-      color: 'white',
-      display: 'flex',
-      flexDirection: 'row',
-      fontSize: '14px',
-      justifyContent: 'space-between',
-      zIndex: 998,
-
-      bottom: 30,
-      position: 'sticky',
-
-      marginLeft: ['10px', 'auto'],
-      marginRight: ['10px', '5%'],
-      py: '5px',
-      paddingRight: 20,
-      paddingLeft: 20,
-
-      minHeight: 60,
-      width: ['auto', '100%'],
-      maxWidth: ['none', 420],
+      error: {
+        ...defaultContainerTheme,
+        background: '#DC143C',
+      },
+      success: {
+        ...defaultContainerTheme,
+        background: '#40aa60',
+      },
+      warning: {
+        ...defaultContainerTheme,
+        background: '#f3e32d',
+      },
     },
     closeButton: {
       backgroundColor: 'transparent',
       border: 0,
       cursor: 'pointer',
       display: 'flex',
-      minWidth: '36px',
+      minWidth: 36,
     },
+    icon: {
+      display: 'flex',
+      minWidth: 36,
+      py: '1px',
+    },
+    message: {},
   },
 }
 
