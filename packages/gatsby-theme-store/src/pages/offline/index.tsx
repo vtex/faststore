037ba15357c2bc63ccbe008e392f4e65fb3befc9
victorhmp <victor2142@gmail.com>
2021-04-14 10:57:23 -0300

    Use layout components from store-ui

--- packages/gatsby-theme-store/src/pages/offline/index.tsx
+++ packages/gatsby-theme-store/src/pages/offline/index.tsx
@@ -1,6 +1,10 @@
 import type { PageProps } from 'gatsby'
 import type { FC } from 'react'
+import { Box, Flex } from '@vtex/store-ui'
 import React, { useEffect } from 'react'
+
+import Layout from '../../components/Layout'
+import Container from '../../components/Container'
 
 /** There are three different cases that we need to consider when the user
   is back online.
@@ -45,6 +49,15 @@ type PagePropsWithNavigationState = PageProps<
   { previousPagePath: string }
 >
 
+const VTEXSvgIcon = (
+  <svg viewBox="0 0 800 800">
+    <path
+      fill="#f71963"
+      d="M656.59,186.14h-374c-29,0-47.57,30.83-34,56.46L286,313.44H218.18A24.94,24.94,0,0,0,196.12,350L316.45,577.75a24.94,24.94,0,0,0,44.08,0l32.68-61.52,41,77.62c14.43,27.3,53.52,27.35,68,.08L689.67,241.17C702.92,216.22,684.84,186.14,656.59,186.14Zm-168,150.72L407.76,489a16.6,16.6,0,0,1-29.33,0l-80.05-151.5A16.6,16.6,0,0,1,313,313.13H474.33A16.15,16.15,0,0,1,488.59,336.86Z"
+    />
+  </svg>
+)
+
 const Page: FC<PagePropsWithNavigationState> = ({ location }) => {
   useEffect(() => {
     window.addEventListener('online', (_) => handleUserBackOnline(location))
@@ -55,53 +68,37 @@ const Page: FC<PagePropsWithNavigationState> = ({ location }) => {
   }, [location])
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        maxWidth: '96rem',
-        marginRight: 'auto',
-        marginLeft: 'auto',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <div style={{ width: '150px', height: '150px' }}>
-          <svg viewBox="0 0 800 800">
-            <path
-              fill="#f71963"
-              d="M656.59,186.14h-374c-29,0-47.57,30.83-34,56.46L286,313.44H218.18A24.94,24.94,0,0,0,196.12,350L316.45,577.75a24.94,24.94,0,0,0,44.08,0l32.68-61.52,41,77.62c14.43,27.3,53.52,27.35,68,.08L689.67,241.17C702.92,216.22,684.84,186.14,656.59,186.14Zm-168,150.72L407.76,489a16.6,16.6,0,0,1-29.33,0l-80.05-151.5A16.6,16.6,0,0,1,313,313.13H474.33A16.15,16.15,0,0,1,488.59,336.86Z"
-            />
-          </svg>
-        </div>
-        <h3
-          style={{
-            fontSize: '43px',
-            fontWeight: 600,
-            textAlign: 'center',
-            color: '#0f3e99',
-          }}
-        >
-          You are offline
-        </h3>
-        <p
-          style={{
-            paddingTop: '15px',
-            paddingBottom: '15px',
-            fontSize: '20px',
-            textAlign: 'center',
-            marginBottom: '30px',
-          }}
-        >
-          Once your connection is back, you will be redirected to the page you
-          were trying to visit.
-        </p>
-      </div>
-    </div>
+    <Layout>
+      <Container>
+        <Flex p={2} sx={{ alignItems: 'center', flexDirection: 'column' }}>
+          <div style={{ width: '150px', height: '150px' }}>{VTEXSvgIcon}</div>
+          <Box
+            as="h3"
+            sx={{
+              fontSize: '43px',
+              fontWeight: '600',
+              textAlign: 'center',
+              color: 'primary',
+              mt: '50px',
+            }}
+          >
+            You are offline
+          </Box>
+          <Box
+            as="p"
+            sx={{
+              paddingX: '15px',
+              fontSize: '20px',
+              textAlign: 'center',
+              marginBottom: '30px',
+            }}
+          >
+            Once your connection is back, you will be redirected to the page you
+            were trying to visit.
+          </Box>
+        </Flex>
+      </Container>
+    </Layout>
   )
 }
 
