ff5ffd8de7f3615af818e4cf95138bf85cb05949
Tiago Gimenes <tlgimenes@gmail.com>
2021-04-26 18:27:53 -0300

    remove container

--- packages/store-ui/src/SearchSuggestions/index.tsx
+++ packages/store-ui/src/SearchSuggestions/index.tsx
@@ -5,22 +5,19 @@ import Provider from './base/Provider'
 import SuspenseDevice from '../Suspense/Device'
 import SearchSuggestionMobile from './Mobile'
 import SearchSuggestionDesktop from './Desktop'
-import SearchSuggestionsContainer from './Container'
 
 type Props = ComponentPropsWithoutRef<typeof SearchSuggestionMobile> &
   ComponentPropsWithoutRef<typeof SearchSuggestionDesktop>
 
 const SearchSuggestions: FC<Props> = (props) => (
-  <SearchSuggestionsContainer>
-    <Provider>
-      <SuspenseDevice device="mobile" fallback={null}>
-        <SearchSuggestionMobile {...props} />
-      </SuspenseDevice>
-      <SuspenseDevice device="desktop" fallback={null}>
-        <SearchSuggestionDesktop {...props} />
-      </SuspenseDevice>
-    </Provider>
-  </SearchSuggestionsContainer>
+  <Provider>
+    <SuspenseDevice device="mobile" fallback={null}>
+      <SearchSuggestionMobile {...props} />
+    </SuspenseDevice>
+    <SuspenseDevice device="desktop" fallback={null}>
+      <SearchSuggestionDesktop {...props} />
+    </SuspenseDevice>
+  </Provider>
 )
 
 export default SearchSuggestions
