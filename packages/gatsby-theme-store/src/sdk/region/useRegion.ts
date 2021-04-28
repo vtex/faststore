60ae8528fd5102ccbb85784a822486293ec9fa79
Lucas Bebber <lucasbbebber@gmail.com>
2021-04-28 15:31:10 -0300

    Delays useRegion value for better hydration

--- packages/gatsby-theme-store/src/sdk/region/useRegion.ts
+++ packages/gatsby-theme-store/src/sdk/region/useRegion.ts
@@ -1,5 +1,19 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import { RegionContext } from '.'
 
-export const useRegion = () => useContext(RegionContext)
+export const useRegion = () => {
+  const contextValue = useContext(RegionContext)
+
+  const [a, setA] = useState<typeof contextValue>({
+    ...contextValue,
+    regionId: null,
+    postalCode: null,
+  })
+
+  useEffect(() => {
+    setA(contextValue)
+  }, [contextValue])
+
+  return a
+}
