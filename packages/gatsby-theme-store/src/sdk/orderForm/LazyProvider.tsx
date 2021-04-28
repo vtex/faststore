4b99e745dcbb7835a365232b82f8a9faff311f84
victorhmp <victor2142@gmail.com>
2021-04-28 13:32:32 -0300

    Avoid using lazy loading while rendering an offline page

--- packages/gatsby-theme-store/src/sdk/orderForm/LazyProvider.tsx
+++ packages/gatsby-theme-store/src/sdk/orderForm/LazyProvider.tsx
@@ -1,13 +1,21 @@
 import type { PropsWithChildren } from 'react'
 import React, { lazy, Suspense } from 'react'
 
-const LazyProvider = lazy(() =>
-  import('./Provider').then(({ Provider }) => ({
-    default: Provider,
-  }))
-)
+const isOfflinePage = window.location.pathname.startsWith('/offline')
+
+const LazyProvider = isOfflinePage
+  ? null
+  : lazy(() =>
+      import('./Provider').then(({ Provider }) => ({
+        default: Provider,
+      }))
+    )
 
 export const Provider = ({ children }: PropsWithChildren<null>) => {
+  if (LazyProvider == null) {
+    return children
+  }
+
   return (
     <Suspense fallback={null}>
       <LazyProvider>{children}</LazyProvider>
