6fd5813bedb379e7d6aad43c66fbe7e014f1ce8b
victorhmp <victor2142@gmail.com>
2021-04-28 14:21:00 -0300

    Simplify offline error handling

--- packages/gatsby-theme-store/src/components/Error/ErrorHandler.tsx
+++ packages/gatsby-theme-store/src/components/Error/ErrorHandler.tsx
@@ -4,9 +4,8 @@
  * style or change the error page, please shadow the `pages/500.tsx` instead.
  * This component is synchronously imported and has a big TBT implication
  */
-import type { FC, Dispatch, SetStateAction } from 'react'
-import React, { Fragment, useState, useEffect } from 'react'
-import { navigate } from 'gatsby'
+import type { FC } from 'react'
+import { useEffect } from 'react'
 
 import { uuidv4 } from '../../sdk/uuid'
 
@@ -15,32 +14,26 @@ interface Props {
   errorId?: string
 }
 
-export const handleError = (
-  { error, errorId }: Props,
-  updateStateFunction: Dispatch<SetStateAction<boolean>>
-) => {
+export const handleError = ({ error, errorId }: Props) => {
   console.error(error)
   console.error(errorId)
 
   const isUserOffline = !window.navigator.onLine
 
-  if (isUserOffline) {
-    const previousPagePath = window.location.pathname
-
-    navigate('/offline', {
-      state: { previousPagePath },
-    })
-
-    updateStateFunction(true)
-
-    return
-  }
-
   // prevent infinite loop
   if (
     window.location.pathname.startsWith('/404') ||
-    window.location.pathname.startsWith('/500')
+    window.location.pathname.startsWith('/500') ||
+    window.location.pathname.startsWith('/offline')
   ) {
+    return
+  }
+
+  if (isUserOffline) {
+    const previousPagePath = window.location.pathname
+
+    window.location.href = `/offline?from=${previousPagePath}`
+
     return
   }
 
@@ -50,16 +43,10 @@ export const handleError = (
       : `/500?from=${window.location.pathname}&errorId=${errorId ?? uuidv4()}`
 }
 
-const ErrorHandler: FC<Props> = ({ error, errorId, children }) => {
-  const [shouldRenderChildren, setShouldRenderChildren] = useState(false)
-
+const ErrorHandler: FC<Props> = ({ error, errorId }) => {
   useEffect(() => {
-    handleError({ error, errorId }, setShouldRenderChildren)
-  }, [setShouldRenderChildren, error, errorId])
-
-  if (shouldRenderChildren) {
-    return <Fragment>{children}</Fragment>
-  }
+    handleError({ error, errorId })
+  }, [error, errorId])
 
   return null
 }
