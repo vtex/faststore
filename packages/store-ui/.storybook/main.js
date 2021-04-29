4dfcc20c55bfe9f51cceb4a61b0c6649c99846c9
Emerson Laurentino <emersonlaurentino@hotmail.com>
2021-04-29 15:31:18 -0300

    Setup storybook (#658)
    * chore: remove storybook deps
    
    * chore: remove stories
    
    * chore: configue storybook
    
    * chore: remove stories

--- packages/store-ui/.storybook/main.js
+++ packages/store-ui/.storybook/main.js
@@ -1,17 +1,10 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs',
-    '@storybook/addon-a11y/register',
-    '@storybook/addon-contexts/register',
-    '@storybook/addon-backgrounds/register',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      },
-    },
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
 }
--- packages/store-ui/.storybook/preview.js
+++ packages/store-ui/.storybook/preview.js
@@ -1,19 +1,10 @@
-import ThemeProvider from './decorators/ThemeProvider'
-import GatsbyIntlProvider from './decorators/GatsbyIntlProvider'
-import { addDecorator } from '@storybook/react'
-import { withContexts } from '@storybook/addon-contexts/react'
-import { contexts } from './contexts'
 
 export const parameters = {
-  a11y: {
-    element: '#root',
-    config: {},
-    options: {},
-    manual: true,
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
   },
-  layout: 'fullscreen',
 }
-
-addDecorator(withContexts(contexts))
-addDecorator(GatsbyIntlProvider)
-addDecorator(ThemeProvider)
