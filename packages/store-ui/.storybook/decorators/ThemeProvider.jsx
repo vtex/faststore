14ea4f4a4b4b21c01fd73690ece8270bf09b8cee
Emerson Laurentino <emerson@laurentino.co>
2021-04-29 13:54:26 -0300

    chore: remove stories

--- packages/store-ui/.storybook/decorators/ThemeProvider.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ThemeProvider } from 'theme-ui'
-import globalTheme from '../themes'
-
-const Provider = (Story) => {
-  return <ThemeProvider theme={globalTheme}>{Story()}</ThemeProvider>
-}
-
-export default Provider
--- packages/store-ui/.storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons'
-import VTEXTheme from './storybook-theme'
-
-addons.setConfig({
-  theme: VTEXTheme,
-})
