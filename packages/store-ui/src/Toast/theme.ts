16e865eaa2f7f8b7262d0bf43a7a23609d0b4bb6
igorbrasileiro <brasileiro456@gmail.com>
2021-04-30 11:33:08 -0300

    Fix typo

--- packages/store-ui/src/Toast/theme.ts
+++ packages/store-ui/src/Toast/theme.ts
@@ -51,7 +51,7 @@ const containerWarning: SxStyleProp = {
 
 const theme: SxStyleProp = {
   toast: {
-    outerContaienr: {
+    outerContainer: {
       overflow: 'hidden',
     },
     container: {
