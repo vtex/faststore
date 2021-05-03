0cd96828d572a9d8b061de26054cfdc6754e93f9
victorhmp <victor2142@gmail.com>
2021-05-03 16:01:25 -0300

    Catch dynamic import error on LazyProvider

--- packages/gatsby-theme-store/src/sdk/orderForm/LazyProvider.tsx
+++ packages/gatsby-theme-store/src/sdk/orderForm/LazyProvider.tsx
@@ -1,21 +1,25 @@
-import type { PropsWithChildren } from 'react'
-import React, { lazy, Suspense } from 'react'
+import type { FC, PropsWithChildren } from 'react'
+import React, { Fragment, lazy, Suspense } from 'react'
 
 const isOfflinePage = window.location.pathname.startsWith('/offline')
 
-const LazyProvider = isOfflinePage
-  ? null
-  : lazy(() =>
-      import('./Provider').then(({ Provider }) => ({
-        default: Provider,
-      }))
-    )
+const Noop: FC = ({ children }) => <Fragment>{children}</Fragment>
+
+const LazyProvider = lazy(() =>
+  import('./Provider')
+    .then(({ Provider }) => ({
+      default: Provider,
+    }))
+    .catch((e) => {
+      if (isOfflinePage) {
+        return { default: Noop }
+      }
+
+      throw e
+    })
+)
 
 export const Provider = ({ children }: PropsWithChildren<null>) => {
-  if (LazyProvider == null) {
-    return children
-  }
-
   return (
     <Suspense fallback={null}>
       <LazyProvider>{children}</LazyProvider>
