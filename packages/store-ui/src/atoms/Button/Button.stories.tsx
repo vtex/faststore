b89bc5aeb34d3b1b137ace80aa043c755c93cd51
Emerson Laurentino <emerson@laurentino.co>
2021-05-03 18:46:37 -0300

    fix: remove ui button from atom

--- packages/store-ui/src/atoms/Button/Button.stories.tsx
+++ packages/store-ui/src/atoms/Button/Button.stories.tsx
@@ -3,23 +3,14 @@ import React from 'react'
 
 import type { ButtonProps } from './Button'
 import Root from './Button'
-import type { UIButtonProps } from './UIButton'
-import UI from './UIButton'
 
 export default {
   title: 'Atoms/Button',
 } as Meta
 
 const ButtonTemplate: Story<ButtonProps> = (args) => <Root {...args} />
-const UIButtonTemplate: Story<UIButtonProps> = (args) => <UI {...args} />
 
 export const Button = ButtonTemplate.bind({})
 Button.args = {
   children: 'Button',
-}
-
-export const UIButton = UIButtonTemplate.bind({})
-UIButton.args = {
-  children: 'UIButton',
-  variant: 'sui-button',
 }
--- packages/store-ui/src/atoms/Button/Button.tsx
+++ packages/store-ui/src/atoms/Button/Button.tsx
@@ -1,12 +1,22 @@
-import type { FC, ReactNode } from 'react'
-import React from 'react'
+import type { ReactNode } from 'react'
+import React, { forwardRef } from 'react'
 
 export type ButtonProps = {
   children: ReactNode
+  className?: string
 }
 
-const Button: FC<ButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+  { children, ...props },
+  ref
+) {
+  const commonProps = {
+    ref,
+    'data-store-button': '',
+    ...props,
+  }
+
+  return <button {...commonProps}>{children}</button>
+})
 
 export default Button
