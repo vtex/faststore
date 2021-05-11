5566d7ddc441a9c18483ca62926c91a0e6344cec
Lucas Bebber <lucasbbebber@gmail.com>
2021-05-11 10:05:55 -0300

    Unlazy lazy provider

--- packages/gatsby-theme-store/src/sdk/orderForm/LazyProvider.tsx
+++ packages/gatsby-theme-store/src/sdk/orderForm/LazyProvider.tsx
@@ -1,26 +1,29 @@
 import React, { Fragment, lazy, Suspense } from 'react'
 import type { FC } from 'react'
 
+import { Provider as OrderFormProvider } from './Provider'
+
 const isOfflinePage = window.location.pathname.startsWith('/offline')
 
 const Noop: FC = ({ children }) => <Fragment>{children}</Fragment>
 
-const LazyProvider = lazy(() =>
-  import('./Provider')
-    .then(({ Provider }) => ({
-      default: Provider,
-    }))
-    .catch((e) => {
-      if (isOfflinePage) {
-        return { default: Noop }
-      }
+// const LazyProvider = lazy(() =>
+//   import('./Provider')
+//     .then(({ Provider }) => ({
+//       default: Provider,
+//     }))
+//     .catch((e) => {
+//       if (isOfflinePage) {
+//         return { default: Noop }
+//       }
 
-      throw e
-    })
-)
+//       throw e
+//     })
+// )
 
 export const Provider: FC = ({ children }) => (
-  <Suspense fallback={null}>
-    <LazyProvider>{children}</LazyProvider>
-  </Suspense>
+  // <Suspense fallback={<>{children}</>}>
+  //   <LazyProvider>{children}</LazyProvider>
+  // </Suspense>
+  <OrderFormProvider>{children}</OrderFormProvider>
 )
