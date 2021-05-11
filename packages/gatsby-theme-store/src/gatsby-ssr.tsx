acadf902aae1b785688eb141888f197e61a83d0b
Lucas Bebber <lucasbbebber@gmail.com>
2021-05-10 22:08:20 -0300

    Use setheadcomponents instead

--- packages/gatsby-theme-store/src/gatsby-ssr.tsx
+++ packages/gatsby-theme-store/src/gatsby-ssr.tsx
@@ -4,25 +4,41 @@ import type { RenderBodyArgs } from 'gatsby'
 import { handleUserBackOnline } from './sdk/offline'
 
 
-exports.onPreRenderHTML = ({
-  replacePostBodyComponents,
-  getPostBodyComponents,
-}: any) => {
-  const postBodyComponents = getPostBodyComponents()
-  const version="0.0.0-experimental-7f28234f8"
-  const reactScript = React.createElement('script', {
-    src: `https://unpkg.com/react@${version}/umd/react.production.min.js`
-  })
-  const reactDomScript = React.createElement('script', {
-    src: `https://unpkg.com/react-dom@${version}/umd/react-dom.production.min.js`
-  })
-  replacePostBodyComponents(([] as any).concat([reactScript, reactDomScript]).concat(React.Children.toArray(postBodyComponents)))
-}
+// exports.onRenderBody = ({
+//   // replacePostBodyComponents,
+//   // getPostBodyComponents,
+//   // replaceHeadComponents,
+//   setHeadComponents,
+//   // getHeadComponents,
+// }: any) => {
+//   // const prevComponents = getPostBodyComponents()
+//   // const prevComponents = getHeadComponents()
+//   // replaceHeadComponents(([] as any).concat(scripts).concat(React.Children.toArray(prevComponents)))
+// }
 
 export const onRenderBody = ({
   setPostBodyComponents,
+  setHeadComponents,
   pathname,
 }: RenderBodyArgs) => {
+
+  const version="0.0.0-experimental-7f28234f8"
+
+  const createScript = (src: string) => {
+    return [
+      React.createElement('link', { rel: "preload", as: "script", href: src }),
+      React.createElement('script', { src }),
+    ]
+  }
+
+  const scripts = [
+    ...createScript(`https://unpkg.com/react@${version}/umd/react.production.min.js`),
+    ...createScript(`https://unpkg.com/react-dom@${version}/umd/react-dom.production.min.js`),
+    ...createScript('https://unpkg.com/intl-messageformat@9.5.3/intl-messageformat.iife.js'),
+  ]
+
+  setHeadComponents(scripts)
+
   if (!pathname.startsWith('/offline')) {
     return null
   }
