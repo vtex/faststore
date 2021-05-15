8cf3d9e0593280147140b5d0a25bc1f06beab3b7
Tiago Gimenes <tlgimenes@gmail.com>
2021-05-15 09:27:24 -0300

    fix lint

--- packages/gatsby-plugin-pixel-facebook/src/pixel/handler.ts
+++ packages/gatsby-plugin-pixel-facebook/src/pixel/handler.ts
@@ -45,7 +45,8 @@ export const handler: PixelEventHandler = (event) => {
 
       fbq('track', 'AddToCart', {
         value: items.reduce(
-          (acc, item) => acc + (item.price ? item.price : 0),
+          (acc: number, { price }) =>
+            acc + (typeof price === 'number' ? price : 0),
           0
         ),
         content_ids: items.map((sku) => sku.id),
