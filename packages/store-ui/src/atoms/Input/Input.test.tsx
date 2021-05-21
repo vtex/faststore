12bc346e30ce3eb0442bb98a1457b6c2e39b7b2d
Emerson Laurentino <emersonlaurentino@hotmail.com>
2021-05-21 18:42:52 -0300

    feat: Input (#696)
    * feat: atom input
    
    * fix: using error instead of isError
    
    * fix: input args on storybook
    
    * feat: success state to input
    
    * chore: using placeholder as arg on input storybook template
    
    * feat: more args to input story
    
    * chore: organize the state object
    
    * fix: input have one prop to state

---
+++ packages/store-ui/src/atoms/Input/Input.test.tsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+import Input from './Input'
+
+describe('Input', () => {
+  it('`data-store-input` is present', () => {
+    const { getByPlaceholderText } = render(
+      <Input placeholder="Hello World!" />
+    )
+
+    expect(getByPlaceholderText('Hello World!')).toHaveAttribute(
+      'data-store-input'
+    )
+  })
+  it('`data-error` is present', () => {
+    const { getByPlaceholderText } = render(
+      <Input placeholder="Hello World!" state="error" />
+    )
+
+    expect(getByPlaceholderText('Hello World!')).toHaveAttribute('data-error')
+  })
+  it('`data-success` is present', () => {
+    const { getByPlaceholderText } = render(
+      <Input placeholder="Hello World!" state="success" />
+    )
+
+    expect(getByPlaceholderText('Hello World!')).toHaveAttribute('data-success')
+  })
+})
