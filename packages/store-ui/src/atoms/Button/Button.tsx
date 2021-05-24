dd9a6d12fbd2c93d1de83d9cfe5efad9907f01e0
Emerson Laurentino <emerson@laurentino.co>
2021-05-24 12:46:47 -0300

    feat: testId to atoms component

--- packages/store-ui/src/atoms/Button/Button.tsx
+++ packages/store-ui/src/atoms/Button/Button.tsx
@@ -4,14 +4,15 @@ import React, { forwardRef } from 'react'
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   href?: string
   as?: ElementType
+  testId?: string
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
-  { children, as: Component = 'button', ...props },
+  { children, as: Component = 'button', testId = 'store-button', ...props },
   ref
 ) {
   return (
-    <Component ref={ref} data-store-button {...props}>
+    <Component ref={ref} data-store-button data-testid={testId} {...props}>
       {children}
     </Component>
   )
