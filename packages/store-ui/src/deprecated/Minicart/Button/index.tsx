ce17a52dcb1ff3475313b008017ebbc194e8117c
Bento Pereira <bento.pereira@vtex.com>
2021-05-28 13:15:16 -0300

    removing data-testid

--- packages/store-ui/src/deprecated/Minicart/Button/index.tsx
+++ packages/store-ui/src/deprecated/Minicart/Button/index.tsx
@@ -24,12 +24,7 @@ const MinicartButton: FC<Props> = ({
   const variant = `minicart.${v}.button`
 
   return (
-    <Button
-      data-testid="openCart"
-      aria-label="Open Cart"
-      variant={variant}
-      {...props}
-    >
+    <Button aria-label="Open Cart" variant={variant} {...props}>
       <MinicartButtonIcon
         height={height}
         width={width}
