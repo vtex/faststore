e9a5deca239bcbc274f4a629344642eeba726215
Tiago Gimenes <tlgimenes@gmail.com>
2021-06-11 19:57:00 -0300

    upgrade react deps

--- packages/gatsby-theme-store/src/gatsby-browser.tsx
+++ packages/gatsby-theme-store/src/gatsby-browser.tsx
@@ -3,14 +3,14 @@
 
 import 'requestidlecallback-polyfill'
 
-import { UIProvider } from '@vtex/store-sdk'
 import React, { StrictMode } from 'react'
-import { unstable_createRoot as createRoot } from 'react-dom'
+import { UIProvider } from '@vtex/store-sdk'
+import ReactDOM from 'react-dom'
 import type { WrapRootElementBrowserArgs } from 'gatsby'
 import type { ReactChild } from 'react'
 
 import ErrorBoundary from './components/Error/ErrorBoundary'
-import { Provider as OrderFormProvider } from './sdk/orderForm/LazyProvider'
+import { Provider as OrderFormProvider } from './sdk/orderForm/Provider'
 import { Provider as VTEXRCProvider } from './sdk/pixel/vtexrc/Provider'
 import {
   onRouteUpdate as progressOnRouteUpdate,
@@ -19,28 +19,37 @@ import {
 import { Provider as RegionProvider } from './sdk/region/Provider'
 import { Provider as ToastProvider } from './sdk/toast/Provider'
 
-export const replaceHydrateFunction = () => async (
-  element: ReactChild,
-  container: Element,
-  callback: any
-) => {
+export const onClientEntry = async () => {
   if (typeof IntersectionObserver === 'undefined') {
     await import('intersection-observer')
   }
-
-  const development = (process.env.GATSBY_BUILD_STAGE as any).includes(
-    'develop'
-  )
-
-  const root = createRoot(container, {
-    hydrate: !development,
-  })
-
-  root.render(element, callback)
 }
 
-export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) => {
-  const root = (
+export const replaceHydrateFunction = () => (
+  element: ReactChild,
+  container: Element
+) => {
+  let hydrate = true
+
+  // This part will be removed by webpack on production builds since this only
+  // serves for React not complaining about mismatches on devMode.
+  // We can not just default to `render` mode on devMode because the user may be using
+  // DEV_SSR=true flag
+  if (process.env.NODE_ENV !== 'production') {
+    const focusEl = document.getElementById(`gatsby-focus-wrapper`)
+
+    hydrate = !!focusEl && focusEl.children.length > 0
+  }
+
+  // There are versions of React currently exporting createRoot and others exporting unstable_createRoot
+  const createRoot =
+    ReactDOM.createRoot || (ReactDOM as any).unstable_createRoot
+
+  createRoot(container, { hydrate }).render(element)
+}
+
+export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) => (
+  <StrictMode>
     <ErrorBoundary>
       <VTEXRCProvider>
         <ToastProvider>
@@ -52,14 +61,8 @@ export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) => {
         </ToastProvider>
       </VTEXRCProvider>
     </ErrorBoundary>
-  )
-
-  if (process.env.NODE_ENV === 'development') {
-    return <StrictMode>{root}</StrictMode>
-  }
-
-  return root
-}
+  </StrictMode>
+)
 
 export const onInitialClientRender = () => {
   globalThis.__REACT_HYDRATED__ = true
