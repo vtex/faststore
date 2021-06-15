a3faed4af7acad5a970c0b59489f370fe16cb541
Emerson Laurentino <emerson@laurentino.co>
2021-06-15 11:39:06 -0300

    chore: move index to deprecated folder

--- packages/store-ui/src/deprecated/Header/theme/index.ts
+++ packages/store-ui/src/deprecated/Header/theme/index.ts
@@ -1,6 +1,6 @@
 import type { SxStyleProp } from 'theme-ui'
 
-import { createTheme } from '../../../createTheme'
+import { createTheme } from '../../createTheme'
 import { menuTheme } from './menu'
 import { notificationbarTheme } from './notificationbar'
 import { overmenuTheme } from './overmenu'
