abeff16c376255d1a8b788db704fd1bb4d0cdfe4
Tiago Gimenes <tlgimenes@gmail.com>
2021-06-16 22:26:41 -0300

    handle errors

--- packages/gatsby-theme-store/src/gatsby-browser.tsx
+++ packages/gatsby-theme-store/src/gatsby-browser.tsx
@@ -6,7 +6,7 @@ import 'requestidlecallback-polyfill'
 import React, { StrictMode } from 'react'
 import { UIProvider } from '@vtex/store-sdk'
 import ReactDOM from 'react-dom'
-import type { RouteUpdateArgs, WrapRootElementBrowserArgs } from 'gatsby'
+import type { WrapRootElementBrowserArgs } from 'gatsby'
 import type { ReactChild } from 'react'
 
 import ErrorBoundary from './components/Error/ErrorBoundary'
@@ -84,19 +84,3 @@ export const wrapPageElement = ({
 export const onRouteUpdate = () => {
   progressOnRouteUpdate()
 }
-
-let nextRoute: string | undefined
-
-export const onPreRouteUpdate = ({ location }: RouteUpdateArgs) => {
-  nextRoute = location.pathname
-}
-
-window.addEventListener('unhandledrejection', (event) => {
-  if (event.reason.name === 'ChunkLoadError') {
-    if (nextRoute) {
-      window.location.pathname = nextRoute
-    } else {
-      window.location.reload()
-    }
-  }
-})
