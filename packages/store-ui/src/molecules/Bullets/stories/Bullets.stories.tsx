287a809524b05cd963dcdeda48196d9b81f826d3
victorhmp <victor2142@gmail.com>
2021-06-24 14:35:40 -0300

    Remove unnecessary React.Fragment

--- packages/store-ui/src/molecules/Bullets/stories/Bullets.stories.tsx
+++ packages/store-ui/src/molecules/Bullets/stories/Bullets.stories.tsx
@@ -1,17 +1,11 @@
 import type { Story } from '@storybook/react'
-import React, { Fragment } from 'react'
+import React from 'react'
 
 import type { BulletsProps } from '../Bullets'
 import Component from '../Bullets'
 import mdx from './Bullets.mdx'
 
-const BulletsTemplate: Story<BulletsProps> = (args) => {
-  return (
-    <Fragment>
-      <Component {...args} />
-    </Fragment>
-  )
-}
+const BulletsTemplate: Story<BulletsProps> = (args) => <Component {...args} />
 
 export const Bullets = BulletsTemplate.bind({})
 Bullets.args = {
