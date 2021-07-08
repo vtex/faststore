06daf69f695753c9ae629c6f34d79e102493d47b
Tiago Gimenes <tlgimenes@gmail.com>
2021-07-08 18:46:40 -0300

    remove host from ssr

--- packages/gatsby-theme-store/src/sdk/seo/product/useMetadata.ts
+++ packages/gatsby-theme-store/src/sdk/seo/product/useMetadata.ts
@@ -27,7 +27,7 @@ export const useMetadata = (
   const price = product?.items[0].sellers?.[0].commercialOffer.spotPrice
   const title = product?.titleTag ?? siteMetadata.title
   const description = product?.metaTagDescription ?? siteMetadata.description
-  const canonical = `https://${host}${pathname}`
+  const canonical = host !== undefined ? `https://${host}${pathname}` : pathname
   const images = useMemo(
     () =>
       product?.items[0].images.map((image) => ({
