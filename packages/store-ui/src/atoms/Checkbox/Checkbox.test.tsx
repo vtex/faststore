520c76736cd4e3da5cfc5bb2625cbd5ad5dadfec
igorbrasileiro <brasileiro456@gmail.com>
2021-07-09 14:34:43 -0300

    Fix test ids from test

--- packages/store-ui/src/atoms/Checkbox/Checkbox.test.tsx
+++ packages/store-ui/src/atoms/Checkbox/Checkbox.test.tsx
@@ -5,14 +5,14 @@ import Checkbox from './Checkbox'
 
 describe('Checkbox', () => {
   it('data-store-checkbox is present', () => {
-    const { getByTestId } = render(<Checkbox testId="checkbox" />)
+    const { getByTestId } = render(<Checkbox testId="store-checkbox" />)
 
-    expect(getByTestId('checkbox')).toHaveAttribute('data-store-checkbox')
+    expect(getByTestId('store-checkbox')).toHaveAttribute('data-store-checkbox')
   })
 
   it('type checkbox is present', () => {
-    const { getByTestId } = render(<Checkbox testId="checkbox" />)
+    const { getByTestId } = render(<Checkbox testId="store-checkbox" />)
 
-    expect(getByTestId('checkbox')).toHaveAttribute('type', 'checkbox')
+    expect(getByTestId('store-checkbox')).toHaveAttribute('type', 'checkbox')
   })
 })
