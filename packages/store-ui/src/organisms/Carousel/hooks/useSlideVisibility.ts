181f7118fe268e4e291fd9ddfd53b99936357b1a
victorhmp <victor2142@gmail.com>
2021-07-15 11:00:13 -0300

    Create 'useSliderVisibility' hook

---
+++ packages/store-ui/src/organisms/Carousel/hooks/useSlideVisibility.ts
@@ -0,0 +1,46 @@
+import { useRef, useEffect } from 'react'
+
+function isSlideVisible({
+  itemsPerPage,
+  currentSlide,
+  slideIdx,
+}: {
+  itemsPerPage: number
+  currentSlide: number
+  slideIdx: number
+}) {
+  return slideIdx >= currentSlide && slideIdx < currentSlide + itemsPerPage
+}
+
+export const useSliderVisibility = ({
+  currentSlide,
+  itemsPerPage,
+}: {
+  currentSlide: number
+  itemsPerPage: number
+}) => {
+  /** Keeps track of slides that have been visualized before.
+   * We want to keep rendering them because the issue is mostly rendering
+   * slides that might never be viewed; On the other hand, hiding slides
+   * that were visible causes visual glitches */
+  const visitedSlides = useRef<Set<number>>(new Set())
+
+  useEffect(() => {
+    for (let i = 0; i < itemsPerPage; i++) {
+      visitedSlides.current.add(currentSlide + i)
+    }
+  }, [currentSlide, itemsPerPage])
+
+  const isItemVisible = (index: number) =>
+    isSlideVisible({
+      slideIdx: index,
+      currentSlide,
+      itemsPerPage,
+    })
+
+  const shouldRenderItem = (index: number) => {
+    return visitedSlides.current.has(index) || isItemVisible(index)
+  }
+
+  return { shouldRenderItem, isItemVisible }
+}
