b28d807a175c5c920add231ed75df0b42c920e6a
victorhmp <victor2142@gmail.com>
2021-07-19 12:22:03 -0300

    Update Carousel storybook setup

--- packages/store-ui/src/organisms/Carousel/stories/Carousel.stories.tsx
+++ packages/store-ui/src/organisms/Carousel/stories/Carousel.stories.tsx
@@ -1,39 +1,64 @@
 import React from 'react'
 import type { Story } from '@storybook/react'
 
+import type { ComponentArgTypes } from '../../../typings/utils'
 import Component from '../Carousel'
 import type { CarouselProps } from '../Carousel'
 import mdx from './Carousel.mdx'
 
-const CarouselTemplate: Story<CarouselProps> = () => (
-  <Component>
-    <img alt="" width={450} src="https://source.unsplash.com/1600x900/?beach" />
-    <img alt="" width={450} src="https://source.unsplash.com/1600x900/?sun" />
-    <img alt="" width={450} src="https://source.unsplash.com/1600x900/?rio" />
+const CarouselTemplate: Story<CarouselProps> = ({
+  itemsPerPage,
+  swipeableConfigOverrides,
+  testId,
+}) => (
+  <Component
+    testId={testId}
+    itemsPerPage={itemsPerPage}
+    swipeableConfigOverrides={swipeableConfigOverrides}
+  >
+    <img alt="" width={500} src="https://source.unsplash.com/1600x900/?rio" />
     <img
       alt=""
-      width={450}
-      src="https://source.unsplash.com/1600x900/?brazil"
+      width={500}
+      src="https://source.unsplash.com/1600x900/?sao-paulo"
     />
     <img
       alt=""
       width={450}
-      src="https://source.unsplash.com/1600x900/?icecream"
+      src="https://source.unsplash.com/1600x900/?teresopolis"
     />
     <img
       alt=""
-      width={450}
-      src="https://source.unsplash.com/1600x900/?summer"
+      width={500}
+      src="https://source.unsplash.com/1600x900/?curitiba"
+    />
+    <img
+      alt=""
+      width={500}
+      src="https://source.unsplash.com/1600x900/?joão-pessoa--pb"
+    />
+    <img
+      alt=""
+      width={500}
+      src="https://source.unsplash.com/1600x900/?brasil"
     />
   </Component>
 )
 
 export const Carousel = CarouselTemplate.bind({})
 
+const argTypes: ComponentArgTypes<CarouselProps> = {
+  itemsPerPage: {
+    control: { type: 'number', min: 1 },
+    defaultValue: 1,
+    min: 1,
+  },
+}
+
 export default {
   title: 'Organisms/Carousel',
   component: Carousel,
-  argTypes: {},
+  argTypes,
   parameters: {
     docs: {
       page: mdx,
