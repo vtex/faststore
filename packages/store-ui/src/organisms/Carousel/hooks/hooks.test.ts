d0c23f5636a99a0ca9c17e03abe904d622d216a2
victorhmp <victor2142@gmail.com>
2021-07-26 17:53:26 -0300

    Update useCarousel import

--- packages/store-ui/src/organisms/Carousel/hooks/hooks.test.ts
+++ packages/store-ui/src/organisms/Carousel/hooks/hooks.test.ts
@@ -1,6 +1,6 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 
-import { useCarousel } from './useCarousel'
+import useCarousel from './useCarousel'
 import { useSlideVisibility } from './useSlideVisibility'
 import type { UseSlideVisibilityArgs } from './useSlideVisibility'
 
