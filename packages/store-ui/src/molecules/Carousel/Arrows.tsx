8689cc14485fb8d7a879cbc41705dbd4df68a717
victorhmp <victor2142@gmail.com>
2021-08-09 12:11:17 -0300

    Make Carousel arrows slimmer

--- packages/store-ui/src/molecules/Carousel/Arrows.tsx
+++ packages/store-ui/src/molecules/Carousel/Arrows.tsx
@@ -22,9 +22,13 @@ export const LeftArrowIcon = ({
     height={size.height}
   >
     <path
-      d="M7.72356 8.48154C7.32958 8.89452 6.67042 8.89452 6.27644 8.48154L0 1.90235L1.81481 0L7 5.43529L12.1852 0L14 1.90235L7.72356 8.48154Z"
-      transform="translate(9.23999) rotate(90)"
-      fill={color}
+      d="M11 1L4 8L11 15"
+      strokeWidth="2"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      stroke={color}
+      fill="none"
     />
   </svg>
 )
@@ -42,9 +46,13 @@ export const RightArrowIcon = ({
     height={size.height}
   >
     <path
-      d="M7.72356 8.48154C7.32958 8.89452 6.67042 8.89452 6.27644 8.48154L0 1.90235L1.81481 0L7 5.43529L12.1852 0L14 1.90235L7.72356 8.48154Z"
-      transform="translate(0 14) rotate(-90)"
-      fill={color}
+      d="M5 15L12 8L5 1"
+      strokeWidth="2"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      stroke={color}
+      fill="none"
     />
   </svg>
 )
