a0fe61cb7572465f3ce3a884d917c00c4153a510
Bento Pereira <bento.pereira@vtex.com>
2021-08-10 12:40:58 -0300

    fix: simplifying select props
    - extending the props from a different base

--- packages/store-ui/src/atoms/Select/Select.tsx
+++ packages/store-ui/src/atoms/Select/Select.tsx
@@ -1,8 +1,7 @@
 import type { SelectHTMLAttributes } from 'react'
 import React, { forwardRef } from 'react'
 
-export interface SelectProps<T = HTMLSelectElement>
-  extends SelectHTMLAttributes<T> {
+export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   /**
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
