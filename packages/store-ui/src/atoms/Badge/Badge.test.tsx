df9eb6e0e3f22ee922f1576378c3401a1c74b908
victorhmp <victor2142@gmail.com>
2021-08-13 09:56:34 -0300

    Implement first test for Badge component

--- packages/store-ui/src/atoms/Badge/Badge.test.tsx
+++ packages/store-ui/src/atoms/Badge/Badge.test.tsx
@@ -1,3 +1,14 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Badge from './Badge'
+
 describe('Badge', () => {
-  it.todo('should have `data-store-bullets` attribute')
+  it('should have `data-store-badge` attribute', () => {
+    const { getByText } = render(<Badge>-25%</Badge>)
+
+    const renderedBadge = getByText('-25%')
+
+    expect(renderedBadge).toHaveAttribute('data-store-badge')
+  })
 })
