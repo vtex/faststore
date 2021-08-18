cdcf279873b58c8fcf0990ff01717dc204c876cf
Emerson Laurentino <emerson@laurentino.co>
2021-08-18 17:12:51 -0300

    feat(store-ui): radio component

---
+++ packages/store-ui/src/atoms/Radio/stories/Radio.stories.tsx
@@ -0,0 +1,39 @@
+import type { Story, Meta } from '@storybook/react'
+import React from 'react'
+
+import type { ComponentArgTypes } from '../../../typings/utils'
+import type { RadioProps } from '../Radio'
+import Component from '../Radio'
+import mdx from './Radio.mdx'
+
+const RadioTemplate: Story<RadioProps> = (props) => (
+  <Component {...props} />
+)
+
+export const Radio = RadioTemplate.bind({})
+
+const controls: ComponentArgTypes<RadioProps> = {
+  checked: {
+    control: {
+      type: 'boolean',
+    },
+  },
+}
+
+const actions: ComponentArgTypes<RadioProps> = {
+  onClick: { action: 'clicked', table: { disable: true } },
+}
+
+export default {
+  title: 'Atoms/Radio',
+  component: Radio,
+  argTypes: {
+    ...controls,
+    ...actions,
+  },
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+} as Meta
