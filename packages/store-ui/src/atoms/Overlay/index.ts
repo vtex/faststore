a0757bcf1155084f5b66f4625f47c8efa18285b3
Igor Brasileiro <brasileiro456@gmail.com>
2021-08-18 09:19:16 -0300

    feat(store-ui): Overlay Atom Component (#890)
    * Add Overlay component
    
    * Add overlay stories css
    
    * Add role presentation
    
    * Fix story css

---
+++ packages/store-ui/src/atoms/Overlay/index.ts
@@ -0,0 +1,2 @@
+export { default } from './Overlay'
+export type { Props as OverlayProps } from './Overlay'
---
+++ packages/store-ui/src/atoms/Overlay/stories/Overlay.stories.tsx
@@ -0,0 +1,53 @@
+import type { Story, Meta } from '@storybook/react'
+import React from 'react'
+
+import type { ComponentArgTypes } from '../../../typings/utils'
+import type { Props as OverlayProps } from '../Overlay'
+import Component from '../Overlay'
+import mdx from './Overlay.mdx'
+
+type ColorType = 'black' | 'green'
+type OverlayStoryProps = OverlayProps & { color: ColorType }
+
+const OverlayTemplate: Story<OverlayStoryProps> = ({
+  color = 'black',
+  ...props
+}) => {
+  const dataColor =
+    color === 'black' ? { 'data-black': true } : { 'data-green': true }
+
+  return (
+    <div data-store-overlay-out-container>
+      A content outside me
+      <Component {...props} {...dataColor}>
+        <div data-store-overlay-modal>
+          <div data-store-overlay-modal-content>Scroll me</div>
+        </div>
+      </Component>
+    </div>
+  )
+}
+
+export const Overlay = OverlayTemplate.bind({})
+
+const controls: ComponentArgTypes<OverlayStoryProps> = {
+  color: {
+    options: ['green', 'black'],
+    control: {
+      type: 'select',
+    },
+  },
+}
+
+export default {
+  title: 'Atoms/Overlay',
+  component: Overlay,
+  argTypes: {
+    ...controls,
+  },
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+} as Meta
