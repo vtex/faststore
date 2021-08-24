90b021e58789414ccacf4860f209a2e2346c0238
Victor Hugo Miranda Pinto <victor2142@gmail.com>
2021-08-24 10:27:07 -0300

    Create Gatsby plugin for OneSignal integration (#663)
    * Create gatsby-plugin-onesignal
    
    Co-authored-by: Larícia Mota <lmmc2@cin.ufpe.br>

---
+++ packages/gatsby-plugin-onesignal/src/gatsby-ssr.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import type { RenderBodyArgs } from 'gatsby'
+
+import type { PluginOptions } from './gatsby-node'
+
+export function onRenderBody(
+  { setHeadComponents, setPostBodyComponents }: RenderBodyArgs,
+  pluginOptions: PluginOptions
+) {
+  if (process.env.NODE_ENV !== 'production') {
+    return null
+  }
+
+  const { oneSignalAppId } = pluginOptions
+
+  setHeadComponents([
+    <link
+      rel="preconnect dns-prefetch"
+      key="preconnect-onesignal"
+      href="https://cdn.onesignal.com"
+    />,
+  ])
+
+  return setPostBodyComponents([
+    <script
+      key="gatsby-plugin-onesignal-cdn"
+      src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+      async
+    />,
+    <script
+      key="gatsby-plugin-onesignal"
+      dangerouslySetInnerHTML={{
+        __html: `
+             var OneSignal = window.OneSignal || [];
+             OneSignal.push(function() {
+                 OneSignal.init({
+                 appId: "${oneSignalAppId}",
+                 });
+             });
+         `,
+      }}
+    />,
+  ])
+}
