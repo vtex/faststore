272824c0870bce0d0d1fede8aceeca866583c5e4
victorhmp <victor2142@gmail.com>
2021-09-02 09:46:21 -0300

    Make 'unordered' the default List variation

--- packages/store-ui/src/atoms/List/List.tsx
+++ packages/store-ui/src/atoms/List/List.tsx
@@ -1,9 +1,9 @@
 import type { ElementType, ReactElement, ComponentPropsWithRef } from 'react'
 import React, { forwardRef } from 'react'
 
-import type { PolymorphicComponentPropsWithRef } from '../../index'
+import type { PolymorphicComponentPropsWithRef } from '../../typings'
 
-export type ListVariant = 'default' | 'description' | 'ordered' | 'unordered'
+export type ListVariant = 'description' | 'ordered' | 'unordered'
 
 const variantToType = (variant: ListVariant) => {
   switch (variant) {
@@ -25,16 +25,16 @@ type ListPropsPolymorphic<
   C extends ElementType
 > = PolymorphicComponentPropsWithRef<C>
 
-type ListComponent = <C extends ElementType = 'div'>(
+type ListComponent = <C extends ElementType = 'ul'>(
   props: ListPropsPolymorphic<C>
 ) => ReactElement | null
 
-export type ListProps<C extends ElementType = 'div'> = ListPropsPolymorphic<C>
+export type ListProps<C extends ElementType = 'ul'> = ListPropsPolymorphic<C>
 
 const List: ListComponent = forwardRef(function List<C extends ElementType>(
   {
     testId = 'store-list',
-    variant = 'default',
+    variant = 'unordered',
     as,
     ...rawProps
   }: ListPropsPolymorphic<C>,
