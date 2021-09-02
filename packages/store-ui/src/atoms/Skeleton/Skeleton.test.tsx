0d50bf24ff7ac5bc229b873d58037070f70d3855
Larícia Mota <laricia.mota@vtex.com.br>
2021-09-02 17:51:53 -0300

    Update test

--- packages/store-ui/src/atoms/Skeleton/Skeleton.test.tsx
+++ packages/store-ui/src/atoms/Skeleton/Skeleton.test.tsx
@@ -4,9 +4,14 @@ import React from 'react'
 import Skeleton from './Skeleton'
 
 describe('Skeleton', () => {
-  it('`data-store-skeleton` is present', () => {
-    const { getByTestId } = render(<Skeleton testId="store-skeleton" />)
+  const { getByTestId } = render(<Skeleton testId="store-skeleton" />)
+  const skeleton = getByTestId('store-skeleton')
 
-    expect(getByTestId('store-skeleton')).toHaveAttribute('data-store-skeleton')
+  it('`data-store-skeleton-container` is present', () => {
+    expect(skeleton).toHaveAttribute('data-store-skeleton-container')
+  })
+
+  it('`data-store-skeleton` is present', () => {
+    expect(skeleton.firstChild).toHaveAttribute('data-store-skeleton')
   })
 })
