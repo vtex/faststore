5618710f880e08c887f3573a75c2673ca2177d6e
Rafaella Bambokian <rafaella.ribeiro@vtex.com.br>
2021-09-02 18:09:57 -0300

    Add numeric input component to storybook

---
+++ packages/store-ui/src/molecules/NumericInput/stories/NumericInput.stories.tsx
@@ -0,0 +1,53 @@
+import type { Story } from '@storybook/react'
+import React from 'react'
+
+import type { ComponentArgTypes } from '../../../typings/utils'
+import type { NumericInputProps } from '../NumericInput'
+import Component from '../NumericInput'
+import mdx from './NumericInput.mdx'
+
+const NumericInputTemplate: Story<NumericInputProps> = (props) => (
+  <Component {...props} />
+)
+
+export const NumericInput = NumericInputTemplate.bind({})
+
+const argTypes: ComponentArgTypes<NumericInputProps> = {
+  value: {
+    control: { type: 'number', min: 0 },
+    defaultValue: 0,
+    min: 0,
+  },
+  defaultValue: {
+    control: { type: 'number', min: 0 },
+    defaultValue: 0,
+    min: 0,
+  },
+  min: {
+    control: { type: 'number', min: 0 },
+    defaultValue: 0,
+  },
+  max: {
+    control: { type: 'number', min: 1 },
+    defaultValue: 4,
+  },
+  isDisabled: {
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+  onChange: {
+    action: 'Input value changed',
+    table: { disable: true },
+  },
+}
+
+export default {
+  title: 'Molecules/NumericInput',
+  component: NumericInput,
+  argTypes,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+}
