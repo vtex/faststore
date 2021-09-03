1eea48d8d74c32d4e99954a5fa9c9de26caebd0d
Larícia Mota <laricia.mota@vtex.com.br>
2021-09-03 15:03:00 -0300

    Extend HTMLDivElement

--- packages/store-ui/src/atoms/Skeleton/Skeleton.tsx
+++ packages/store-ui/src/atoms/Skeleton/Skeleton.tsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from 'react'
+import type { HTMLAttributes } from 'react'
 
-export interface SkeletonProps {
+export interface SkeletonProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
