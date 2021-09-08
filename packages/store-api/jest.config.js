4fae4a39c85088d7e5031eac3325fb2aa190a2c3
Tiago Gimenes <tlgimenes@gmail.com>
2021-09-08 18:38:50 -0300

    fix tests: agora vai

--- packages/store-api/jest.config.js
+++ packages/store-api/jest.config.js
@@ -1,5 +1,11 @@
+// File created to resolve .graphql files.
+// The main content was extracted from https://github.com/formium/tsdx/blob/master/src/createJestConfig.ts
+// Copying some code was necessary because tsdx does shallow merges
+
 module.exports = {
   transform: {
-    '\\.(gql|graphql)$': 'jest-transform-graphql',
+    '.(graphql)$': 'jest-transform-graphql',
+    '.(ts|tsx)$': 'ts-jest/dist',
+    '.(js|jsx)$': 'babel-jest', // jest's default
   },
 }
