df1194b657e3ce47c555fbf67b0ab22b434cd5b7
Ícaro Azevedo <icaro.azevedo@vtex.com.br>
2021-09-09 14:23:21 -0300

    feat(store-sdk): custom events, tests and docs (#937)
    * fix: general wrapper types for analytics and removed unecessary code from unwrap function
    
    * chore: renaming analytics index file to wrap
    
    * chore: adding wrap and unwrap functions tests
    
    * chore: adding tests to analytics layer
    
    * feat: allows sending custom analytics events
    
    * feat: add docs to analytics
    
    * chore: add test for custom event
    
    * docs: adding link to GA4 spec
    
    * chore: removing tsdx incompatible code
    
    * fix: change title name to analytics
    
    * docs: correct grammar
    
    Co-authored-by: Emerson Laurentino <emersonlaurentino@hotmail.com>
    
    * docs: improve readability and fix erros on code
    
    Co-authored-by: Larícia Mota <laricia.mota@vtex.com.br>
    
    * chore: improve error message from useAnalyticsEvent
    
    Co-authored-by: Larícia Mota <laricia.mota@vtex.com.br>
    
    * chore: moving event samples to fixture file
    
    Co-authored-by: Emerson Laurentino <emersonlaurentino@hotmail.com>
    Co-authored-by: Larícia Mota <laricia.mota@vtex.com.br>

---
+++ packages/store-sdk/test/analytics/useAnalyticsEvent.test.ts
@@ -0,0 +1,44 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { useAnalyticsEvent } from '../../src/analytics/useAnalyticsEvent'
+import { wrap } from '../../src/analytics/wrap'
+import { ADD_TO_CART_SAMPLE } from './__fixtures__/EventSamples'
+
+describe('useAnalyticsEvent', () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('useAnalyticsEvent calls handler with correct params when an AnalyticsEvent is fired', async () => {
+    const handler = jest.fn()
+
+    jest.spyOn(window, 'addEventListener').mockImplementation((_, fn) => {
+      if (typeof fn === 'function') {
+        fn(new MessageEvent('message', { data: wrap(ADD_TO_CART_SAMPLE) }))
+      }
+    })
+
+    renderHook(() => useAnalyticsEvent(handler))
+
+    expect(handler).toHaveBeenCalled()
+    expect(handler).toHaveBeenCalledWith(ADD_TO_CART_SAMPLE)
+  })
+
+  it('useAnalyticsEvent ignores events that are not AnalyticsEvent', async () => {
+    const handler = jest.fn()
+
+    jest.spyOn(window, 'addEventListener').mockImplementation((_, fn) => {
+      if (typeof fn === 'function') {
+        fn(
+          new MessageEvent('message', {
+            data: { ...wrap(ADD_TO_CART_SAMPLE), type: 'OtherEventType' },
+          })
+        )
+      }
+    })
+
+    renderHook(() => useAnalyticsEvent(handler))
+
+    expect(handler).not.toHaveBeenCalled()
+  })
+})
