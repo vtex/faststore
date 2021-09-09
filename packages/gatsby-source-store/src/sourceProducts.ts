38e29e59f21fdf7cc83e2e253d225f18822967a7
Tiago Gimenes <tlgimenes@gmail.com>
2021-09-09 10:41:49 -0300

    add source-store

---
+++ packages/gatsby-source-store/src/sourceProducts.ts
@@ -0,0 +1,37 @@
+import type { SourceNodesArgs } from 'gatsby'
+
+import { sourceStoreType } from './sourceStore'
+import type { Options } from './gatsby-node'
+
+export const sourceProducts = async (
+  gatsbyApi: SourceNodesArgs,
+  options: Options
+) => {
+  const { maxNumProducts = Infinity } = options
+  const gatsbyNodeTypes = [
+    {
+      remoteTypeName: `StoreProduct`,
+      queries: `
+        query LIST_PRODUCTS($first: Int!, $after: String!) {
+          allProducts(first: $first, after: $after) {
+            edges {
+              node {
+                ..._ProductFragment_
+              }
+              cursor
+            }
+            pageInfo {
+              hasNextPage
+            }
+          }
+        }
+        fragment _ProductFragment_ on StoreProduct {
+          id: productID
+          __typename
+        }
+      `,
+    },
+  ]
+
+  await sourceStoreType(gatsbyApi, options, gatsbyNodeTypes, maxNumProducts)
+}
