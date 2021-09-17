22ee311221163b7405c636bf9110a4efca964afc
VTEX <gabriel.antiqueira@vtex.com.br>
2021-09-17 15:40:02 -0300

    Loading Button molecule

---
+++ packages/store-ui/src/molecules/LoadingButton/LoadingButton.tsx
@@ -0,0 +1,31 @@
+import React, { forwardRef } from 'react'
+
+import Button from '../../atoms/Button'
+import type { ButtonProps } from '../../atoms/Button'
+import Spinner from '../../atoms/Spinner'
+
+export interface LoadingButtonProps extends ButtonProps {
+  /**
+   * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
+   */
+  testId?: string
+  /**
+   * The current loading state of the button.
+   */
+  loading: boolean
+}
+
+const LoadingButton = forwardRef<HTMLButtonElement, LoadingButtonProps>(
+  function LoadingButton(
+    { children, loading = false, testId = 'store-loading-button', ...props },
+    ref
+  ) {
+    return (
+      <Button ref={ref} data-store-loading-button testId={testId} {...props}>
+        {loading ? <Spinner /> : children}
+      </Button>
+    )
+  }
+)
+
+export default LoadingButton
