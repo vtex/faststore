fe5a339fc9e64ade0ee886910a7f4f8a270069c5
victorhmp <victor2142@gmail.com>
2021-10-13 10:34:24 -0300

    Implement unit tests for Table component

--- packages/store-ui/src/molecules/Table/Table.test.tsx
+++ packages/store-ui/src/molecules/Table/Table.test.tsx
@@ -3,11 +3,83 @@ import { render } from '@testing-library/react'
 import { axe } from 'jest-axe'
 
 import Table from './Table'
-// import TableFooter from '../TableFooter'
 import TableHead from './TableHead'
 import TableRow from './TableRow'
 import TableBody from './TableBody'
 import TableCell from './TableCell'
+import TableFooter from './TableFooter'
+
+describe('Data attributes', () => {
+  it('should render a simple table with all expected data-attributes', () => {
+    const { getByTestId, queryAllByTestId } = render(
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell variant="header">Name</TableCell>
+            <TableCell variant="header">Age</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>John</TableCell>
+            <TableCell>25</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Carter</TableCell>
+            <TableCell>25</TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Age</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    )
+
+    const table = getByTestId('store-table')
+
+    expect(table).toHaveAttribute('data-store-table')
+
+    const tableHead = getByTestId('store-table-head')
+
+    expect(tableHead).toHaveAttribute('data-store-table-head')
+
+    const tableBody = getByTestId('store-table-body')
+
+    expect(tableBody).toHaveAttribute('data-store-table-body')
+
+    const tableFooter = getByTestId('store-table-footer')
+
+    expect(tableFooter).toHaveAttribute('data-store-table-footer')
+
+    const tableRows = queryAllByTestId('store-table-row')
+
+    expect(tableRows).toHaveLength(4)
+    tableRows.forEach((row) => {
+      expect(row).toHaveAttribute('data-store-table-row')
+    })
+
+    const tableCells = queryAllByTestId('store-table-cell')
+
+    // Make sure 8 cells were rendered and all contain the
+    // data-store-table-cell attribute.
+    expect(tableCells).toHaveLength(8)
+    tableCells.forEach((row) => {
+      expect(row).toHaveAttribute('data-store-table-cell')
+    })
+
+    // Make sure that 2 header cells and 6 data cells were rendered, with their
+    // corresponding attributes.
+    expect(
+      table.querySelectorAll('[data-store-table-cell=header]')
+    ).toHaveLength(2)
+    expect(table.querySelectorAll('[data-store-table-cell=data]')).toHaveLength(
+      6
+    )
+  })
+})
 
 // WAI-ARIA tests
 // https://www.w3.org/WAI/tutorials/tables/
@@ -109,25 +181,55 @@ describe('Table WAI-ARIA Specifications', () => {
   })
 
   describe('Tables with two headers', () => {
-    it.todo(
-      'should have no violations on a table with header cells in the top row and first column'
-    )
+    it('should have no violations on a table with header cells in the top row and first column', async () => {
+      const { container } = render(
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell variant="header" scope="col">
+                Monday
+              </TableCell>
+              <TableCell variant="header" scope="col">
+                Tuesday
+              </TableCell>
+              <TableCell variant="header" scope="col">
+                Wednesday
+              </TableCell>
+              <TableCell variant="header" scope="col">
+                Thursday
+              </TableCell>
+              <TableCell variant="header" scope="col">
+                Friday
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell variant="header" scope="row">
+                09:00 - 11:00
+              </TableCell>
+              <TableCell>Closed</TableCell>
+              <TableCell>Open</TableCell>
+              <TableCell>Open</TableCell>
+              <TableCell>Closed</TableCell>
+              <TableCell>Closed</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell variant="header" scope="row">
+                11:00 - 13:00
+              </TableCell>
+              <TableCell>Open</TableCell>
+              <TableCell>Open</TableCell>
+              <TableCell>Closed</TableCell>
+              <TableCell>Closed</TableCell>
+              <TableCell>Closed</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      )
 
-    it.todo(
-      'should have no violations on a table with an offset column of header cells'
-    )
-  })
-
-  describe('Tables with irregular headers', () => {
-    it.todo('should have no violations on a table with two tier headers')
-    it.todo(
-      'should have no violations on a table with headers spanning multiple rows or columns'
-    )
-  })
-
-  describe('Tables with multi-level headers', () => {
-    it.todo(
-      'should have no violations on a table with multiple column headers in each column'
-    )
+      expect(await axe(container)).toHaveNoViolations()
+    })
   })
 })
