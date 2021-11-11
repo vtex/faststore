529e9a28dca50ae5a70907ea8e52fb16ad8f77ea
eduardoformiga <eduardo@guava.com.br>
2021-11-11 20:49:06 -0300

    Tweaks Alert stories

--- packages/ui/src/molecules/Alert/stories/Alert.stories.tsx
+++ packages/ui/src/molecules/Alert/stories/Alert.stories.tsx
@@ -1,18 +1,51 @@
-import React from 'react'
 import type { Story } from '@storybook/react'
+import React from 'react'
 
+import type { ComponentArgTypes } from '../../../typings/utils'
 import type { AlertProps } from '../Alert'
 import Component from '../Alert'
-import mdx from './Form.mdx'
+import ShoppingCartIcon from '../../../atoms/Icon/stories/assets/ShoppingCart'
+import mdx from './Alert.mdx'
 
-const AlertTemplate: Story<AlertProps> = ({ testId }) => {
-  return <Component testId={testId} />
-}
+const AlertTemplate: Story<AlertProps> = ({
+  testId,
+  dismissible,
+  children,
+  onClose,
+}) => (
+  <Component
+    testId={testId}
+    icon={<ShoppingCartIcon />}
+    dismissible={dismissible}
+    onClose={onClose}
+  >
+    {children}
+  </Component>
+)
 
 export const Alert = AlertTemplate.bind({})
 
+const argTypes: ComponentArgTypes<AlertProps> = {
+  // icon: {
+  //   control: { type: 'text' },
+  // },
+  dismissible: {
+    control: {
+      type: 'boolean',
+    },
+  },
+  children: {
+    control: { type: 'text' },
+    defaultValue: 'Alert',
+  },
+  onClose: {
+    action: 'Alert closed',
+  },
+}
+
 export default {
   title: 'Molecules/Alert',
+  argTypes,
   parameters: {
     docs: {
       page: mdx,
