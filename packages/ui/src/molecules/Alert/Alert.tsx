861deb40414b020c6abdf3f30d72c08409ed3b9b
eduardoformiga <eduardo@guava.com.br>
2021-11-11 20:48:24 -0300

    Adds new props to the Alert

--- packages/ui/src/molecules/Alert/Alert.tsx
+++ packages/ui/src/molecules/Alert/Alert.tsx
@@ -1,18 +1,48 @@
-import type { HTMLAttributes } from 'react'
+import type { HTMLAttributes, MouseEvent, ReactNode } from 'react'
 import React, { forwardRef } from 'react'
 
-export interface AlertProps extends HTMLAttributes<HTMLDivElement> {
+import Icon from '../../atoms/Icon'
+import Button from '../../atoms/Button'
+
+export interface AlertProps
+  extends Omit<HTMLAttributes<HTMLDivElement>, 'onClose' | 'role'> {
   /**
    * ID to find this component in testing tools (e.g.: cypress,
    * testing-library, and jest).
    */
   testId?: string
+
+  /**
+   * A React component that will be rendered as an icon.
+   */
+  icon?: ReactNode
+
+  /**
+   * Boolean that controls the close button.
+   */
+  dismissible?: boolean
+
+  /**
+   * Function that is triggered when the alert is closed.
+   */
+  onClose?: (event: MouseEvent) => void
 }
 
-const Alert = forwardRef<HTMLDivElement, AlertProps>(function Form(
-  { testId = 'store-alert', children, ...otherProps },
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  {
+    testId = 'store-alert',
+    children,
+    icon,
+    dismissible,
+    onClose,
+    ...otherProps
+  },
   ref
 ) {
+  const handleClose = (event: MouseEvent) => {
+    onClose?.(event)
+  }
+
   return (
     <div
       ref={ref}
@@ -21,7 +51,20 @@ const Alert = forwardRef<HTMLDivElement, AlertProps>(function Form(
       data-testid={testId}
       {...otherProps}
     >
+      {icon && <Icon component={icon} />}
+
       {children}
+
+      {dismissible && (
+        <Button
+          type="button"
+          data-dismiss="alert"
+          aria-label="Close"
+          onClick={handleClose}
+        >
+          <span>&times;</span>
+        </Button>
+      )}
     </div>
   )
 })
