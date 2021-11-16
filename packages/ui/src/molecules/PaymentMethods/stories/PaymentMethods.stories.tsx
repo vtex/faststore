a30307b182382ea154abe55f9dbd961e50eb6eac
Sara Lins <saranicoly0@gmail.com>
2021-11-16 15:23:26 -0300

    add basic structure to component story

---
+++ packages/ui/src/molecules/PaymentMethods/stories/PaymentMethods.stories.tsx
@@ -0,0 +1,22 @@
+import type { Story, Meta } from '@storybook/react'
+import React from 'react'
+
+import type { PaymentMethodsProps } from '../PaymentMethods'
+import Component from '../PaymentMethods'
+import mdx from './PaymentMethods.mdx'
+
+const PaymentMethodsTemplate: Story<PaymentMethodsProps> = ({ testId }) => (
+  <Component testId={testId} />
+)
+
+export const PaymentMethods = PaymentMethodsTemplate.bind({})
+PaymentMethods.storyName = 'PaymentMethods'
+
+export default {
+  title: 'Molecules/PaymentMethods',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+} as Meta
