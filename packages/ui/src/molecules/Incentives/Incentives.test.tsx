d98786ee8fb770add4f9b8131ed2aee83e340ab0
Lucas Feijó <lucas.portela@vtex.com.br>
2021-11-26 13:20:18 -0300

    Fix and improve tests

--- packages/ui/src/molecules/Incentives/Incentives.test.tsx
+++ packages/ui/src/molecules/Incentives/Incentives.test.tsx
@@ -3,39 +3,51 @@ import { axe } from 'jest-axe'
 import React from 'react'
 
 import Incentives from './Incentives'
+import IncentivesItem from './IncentivesItem'
 
 const TestIncentives = () => {
-  const incentives = [
-    {
-      icon: <div />,
-      title: 'A title',
-      description: 'A description',
-    },
-  ]
-
-  return <Incentives incentives={incentives} />
+  return (
+    <Incentives>
+      <IncentivesItem>
+        <span>Item title</span>
+        <span>Item description</span>
+      </IncentivesItem>
+      <IncentivesItem>
+        <span>Another item title</span>
+        <span>Another item description</span>
+      </IncentivesItem>
+    </Incentives>
+  )
 }
 
 describe('Incentives', () => {
   let incentives: HTMLElement
+  let incentivesItems: HTMLElement[]
 
   beforeEach(() => {
-    const { getByTestId } = render(<TestIncentives />)
+    const { getByTestId, getAllByTestId } = render(<TestIncentives />)
 
     incentives = getByTestId('store-incentives')
+    incentivesItems = getAllByTestId('store-incentives-item')
   })
 
   afterEach(cleanup)
 
   describe('Data attributes', () => {
-    it('`Incentives` component should have `data-store-incentives` attribute', () => {
-      expect(incentives).toHaveAttribute('data-store-incentives')
+    it('`Incentives` component should have `data-incentives` attribute', () => {
+      expect(incentives).toHaveAttribute('data-incentives')
+    })
+
+    it('`IncentivesItem` component should have `data-incentives-item` attribute', () => {
+      for (const item of incentivesItems) {
+        expect(item).toHaveAttribute('data-incentives-item')
+      }
     })
   })
 
   describe('Accessibility', () => {
     it('should not have violations', async () => {
-      expect(await axe(incentives)).toHaveNoViolations()
+      expect(await axe(document.body)).toHaveNoViolations()
     })
   })
 })
