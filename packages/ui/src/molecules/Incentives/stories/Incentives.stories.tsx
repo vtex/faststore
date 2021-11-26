99bb942745552c59d2ff479ef0f02ee74a41becc
Lucas Feijó <lucas.portela@vtex.com.br>
2021-11-26 13:19:05 -0300

    Fix Incentives story

--- packages/ui/src/molecules/Incentives/stories/Incentives.stories.tsx
+++ packages/ui/src/molecules/Incentives/stories/Incentives.stories.tsx
@@ -3,28 +3,32 @@ import React from 'react'
 
 import type { IncentivesProps } from '../Incentives'
 import Component from '../Incentives'
-import ShoppingCartIcon from '../../../atoms/Icon/stories/assets/ShoppingCart'
+import ComponentItem from '../IncentivesItem'
+import { GuaranteeIcon, QualityIcon, SafetyIcon, ShippingIcon } from './assets'
 import mdx from './Incentives.mdx'
 
-const IncentivesTemplate: Story<IncentivesProps> = ({ testId, incentives }) => (
-  <Component testId={testId} incentives={incentives} />
+const IncentivesTemplate: Story<IncentivesProps> = ({ testId }) => (
+  <Component testId={testId}>
+    <ComponentItem>
+      <SafetyIcon />
+      <span>Trusted by SafeCompany</span>
+    </ComponentItem>
+    <ComponentItem>
+      <QualityIcon />
+      <span>Quality Products</span>
+    </ComponentItem>
+    <ComponentItem>
+      <GuaranteeIcon />
+      <span>3-year Guarantee</span>
+    </ComponentItem>
+    <ComponentItem>
+      <ShippingIcon />
+      <span>Free Shipping</span>
+    </ComponentItem>
+  </Component>
 )
 
 export const Incentives = IncentivesTemplate.bind({})
-Incentives.args = {
-  incentives: [
-    {
-      title: 'A title',
-      icon: <ShoppingCartIcon />,
-      description: 'A great description',
-    },
-    {
-      title: 'Another title',
-      icon: <ShoppingCartIcon />,
-      description: 'Another great description',
-    },
-  ],
-}
 
 export default {
   title: 'Molecules/Incentives',
