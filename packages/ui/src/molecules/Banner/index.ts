48eb8cfd70570ff7e65a98563685bb20604ec7df
Fanny <fannychienn@gmail.com>
2021-11-30 19:09:34 -0300

    Removes extra BannerTitle and BannerDescription components

--- packages/ui/src/molecules/Banner/index.ts
+++ packages/ui/src/molecules/Banner/index.ts
@@ -7,11 +7,5 @@ export type { BannerImageProps } from './BannerImage'
 export { default as BannerContent } from './BannerContent'
 export type { BannerContentProps } from './BannerContent'
 
-export { default as BannerTitle } from './BannerTitle'
-export type { BannerTitleProps } from './BannerTitle'
-
-export { default as BannerDescription } from './BannerDescription'
-export type { BannerDescriptionProps } from './BannerDescription'
-
 export { default as BannerButton } from './BannerButton'
 export type { BannerButtonProps } from './BannerButton'
