fe8f617146700dbfd4d5d74de391d28b3b1cff07
Lucas Feijó <lucas.portela@vtex.com.br>
2021-12-08 14:31:00 -0300

    Add ProductCardInfo 'aria-label' prop description and default value

--- packages/ui/src/organisms/ProductCard/ProductCardInfo.tsx
+++ packages/ui/src/organisms/ProductCard/ProductCardInfo.tsx
@@ -1,4 +1,4 @@
-import type { HTMLAttributes } from 'react'
+import type { HTMLAttributes, AriaAttributes } from 'react'
 import React, { forwardRef } from 'react'
 
 export interface ProductCardInfoProps extends HTMLAttributes<HTMLDivElement> {
@@ -6,17 +6,27 @@ export interface ProductCardInfoProps extends HTMLAttributes<HTMLDivElement> {
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
   testId?: string
+  /**
+   * Defines a string value that labels the current element.
+   */
+  'aria-label'?: AriaAttributes['aria-label']
 }
 
 const ProductCardInfo = forwardRef<HTMLDivElement, ProductCardInfoProps>(
   function ProductCardInfo(
-    { testId = 'store-product-card-info', children, ...otherProps },
+    {
+      testId = 'store-product-card-info',
+      'aria-label': ariaLabel = 'Product info',
+      children,
+      ...otherProps
+    },
     ref
   ) {
     return (
       <div
         ref={ref}
         role="region"
+        aria-label={ariaLabel}
         data-store-product-card-info
         data-testid={testId}
         {...otherProps}
