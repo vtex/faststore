5e0490fc489893a1bba61020ffb0d1e80ddbb4c9
Carolina Menezes <carolinamenezes@hotmail.com.br>
2021-12-09 18:52:46 -0300

    Create Starters section

---
+++ docs/src/pages/starters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import StarterPage from './sections/StarterPage/StarterPage'
+
+
+function STARTERS() {
+  return (
+    <Layout title="FastStore Starters">
+      <StarterPage />
+    </Layout>
+  );
+}
+
+export default STARTERS;
