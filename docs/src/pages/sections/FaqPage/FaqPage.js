d2712f601e3aac202653f4539322b427a10a903a
Tiago Gimenes <tlgimenes@gmail.com>
2021-12-10 13:43:49 -0300

    docs: FAQ (#1068)
    * respond faq
    
    * improve links
    
    * Apply suggestions from code review
    
    Co-authored-by: Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
    
    Co-authored-by: Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>

--- docs/src/pages/sections/FaqPage/FaqPage.js
+++ docs/src/pages/sections/FaqPage/FaqPage.js
@@ -2,6 +2,41 @@ import React from 'react'
 import styles from './FaqPage.module.css'
 import FaqQuestion from '../../../components/FaqQuestion/FaqQuestion'
 
+const data = [
+  {
+    question: "What is FastStore?",
+    answer: `
+FastStore is a full-stack toolkit composed of three independent packages <strong>(UI, SDK, and API)</strong> that help you create performant ecommerce experiences. Use <strong>FastStore UI</strong> to add accessibility-ready components to your ecommerce, <strong>FastStore SDK</strong> to manage the states related to your store behavior, and <strong>FastStore API</strong> to connect to your favorite ecommerce platform. Notice that you can use these three packages individually or combined with other solutions on the web. <br/><br/><em>Please refer to <a href="/reference/overview">our documentation</a> for more information.</em>
+    `,
+  },
+  {
+    question: "How I start a FastStore project?",
+    answer: `The easiest way to start a FastStore project is to use one of our official starters. Find your favorite starter at our <a href="/starters">Starter Library</a>, fork it, customize it and deploy it on your favorite Jamstack platform.`
+  },
+  {
+    question: "Can I integrate my FastStore project with a CMS?",
+    answer: `Yes. Since FastStore is Jamstack-based, you can use any <a href="https://jamstack.org/headless-cms/">headless CMS</a> available for Jamstack. Notice that we strongly recommend using VTEX Headless CMS for VTEX projects.`
+  },
+  {
+    question: "Which tech stack do you recommend?",
+    answer: `
+For styling, consider using a static CSS solution, such as Tailwind, Less, or raw CSS. Keep in mind that runtime-based CSS-in-JS solutions tend to harm performance. For deployment, consider using VTEX IO WebOps for better stability and performance.
+    `
+  },
+  {
+    question: "Can I use Tachyons or other CSS frameworks?",
+    answer: "Yes. FastStore UI is styled via data attributes. This means we are compatible with all major CSS frameworks, such as Emotion, Tailwind, Styled components, Stitches, etc."
+  },
+  {
+    question: "Can I adapt UI components to my business needs?",
+    answer: "Yes, because all FastStore UI components are open source, you can simply copy the source code from Github and modify it to your liking if you need to extend their behavior in any way for your project."
+  },
+  {
+    question: "How can I contribute to FastStore?",
+    answer: "There are multiple ways to contribute to the project whether you are a technical or non-technical user: correct typos, fix translations, enhance documentation, create new starters, improve components and behaviors. <br/><em>Check out our contributions guide (coming soon).</em>"
+  }
+]
+
 const FaqPage = () => {
   return (
     <div className={styles.FaqPage}>
@@ -14,16 +49,16 @@ const FaqPage = () => {
               <h2>Didn't find your answers?</h2> Don’t hesitate to get in touch! Find us at:
               <ul>
                 <li>
-                  <a href="https://community.vtex.com/">VTEX Community</a>
-                </li>
-                <li>
                   <a href="https://www.youtube.com/channel/UCReNhDqLOVL4edqENJ4k7Fg">
                     Office Hours
                   </a>
                 </li>
                 <li>
-                  <a href="https://support.vtex.com/hc/en-us/requests">
-                    Support
+                  <a href="https://github.com/vtex/faststore/issues">Github Issues</a>
+                </li>
+                <li>
+                  <a href="https://github.com/vtex/faststore/discussions">
+                    Github Discussions
                   </a>
                 </li>
               </ul>
@@ -32,76 +67,9 @@ const FaqPage = () => {
         </div>
         <div className={styles.contentFaq}>
           <h2>FastStore</h2>
-          <FaqQuestion
-            question="What is FastStore?"
-            answer="Teste"
-          />
-          <FaqQuestion
-            question="What is the difference between FastStore and Store Framework?"
-            answer=""
-          />
-          <FaqQuestion
-            question="How I start a FastStore project?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Can I integrate my FastStore project with a CMS?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Which tech stack do you recommend?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Can I use Tachyons or other CSS frameworks?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Can I adapt FastStore UI components to my business needs?"
-            answer=""
-          />
-          <FaqQuestion
-            question="How can I contribute to FastStore?"
-            answer=""
-          />
-          <h2>VTEX Headless CMS</h2>
-          <FaqQuestion
-            question="What is VTEX Headless CMS?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Why should I use VTEX Headless CMS?"
-            answer=""
-          />
-          <FaqQuestion
-            question="What types of pages editors can create?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Can I add my own components to the CMS?"
-            answer=""
-          />
-          <h2>VTEX IO WebOps</h2>
-          <FaqQuestion
-            question="What is VTEX IO WebOps?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Why should I use VTEX IO WebOps?"
-            answer=""
-          />
-          <FaqQuestion
-            question="What are the bots running in my Pull Request?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Why the Lighthouse bot is failing?"
-            answer=""
-          />
-          <FaqQuestion
-            question="Can I customize the CI/CD pipeline?"
-            answer=""
-          />
+          {data.map(({question, answer}) => (
+            <FaqQuestion question={question} answer={answer}></FaqQuestion>
+          ))}
         </div>
       </div>
     </div>
