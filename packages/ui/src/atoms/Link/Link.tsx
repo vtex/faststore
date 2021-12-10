cbdd1220422553b28b5da4ce509057af62c90003
Lucas Feijó <lucas.portela@vtex.com.br>
2021-12-10 18:00:58 -0300

    Refactor Link atom to work as a polymorphic component

--- packages/ui/src/atoms/Link/Link.tsx
+++ packages/ui/src/atoms/Link/Link.tsx
@@ -1,112 +1,47 @@
 import React, { forwardRef } from 'react'
+import type { FC, ElementType, ReactElement } from 'react'
+
 import type {
-  ForwardedRef,
-  HTMLAttributes,
-  AriaAttributes,
-  AnchorHTMLAttributes,
-  PropsWithChildren,
-} from 'react'
+  PolymorphicRef,
+  PolymorphicComponentPropsWithRef,
+} from '../../typings'
 
-interface LinkAsDivProps extends HTMLAttributes<HTMLDivElement> {
-  testId?: string
-  ref?: ForwardedRef<HTMLDivElement>
-  ariaLabel?: AriaAttributes['aria-label']
-}
-
-const LinkAsDiv = ({
-  ref,
-  testId,
-  children,
-  ariaLabel,
-  ...otherProps
-}: PropsWithChildren<LinkAsDivProps>) => {
-  return (
-    <div
-      ref={ref}
-      data-store-link
-      data-testid={testId}
-      aria-label={ariaLabel}
-      {...otherProps}
-    >
-      {children}
-    </div>
-  )
-}
-
-interface LinkAsAnchorProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
-  href?: string
-  testId?: string
-  ariaLabel?: AriaAttributes['aria-label']
-}
-
-const LinkAsAnchor = ({
-  href,
-  testId,
-  children,
-  ariaLabel,
-  ...otherProps
-}: PropsWithChildren<LinkAsAnchorProps>) => {
-  if (!href) {
-    throw new Error('Missing required parameters for <a> tag: href')
-  }
-
-  return (
-    <a
-      href={href}
-      aria-label={ariaLabel}
-      data-store-link
-      data-testid={testId}
-      {...otherProps}
-    >
-      {children}
-    </a>
-  )
-}
-
-type ElementType = 'a' | 'div'
-
-export interface LinkProps
-  extends Omit<HTMLAttributes<HTMLDivElement | HTMLAnchorElement>, 'href'> {
-  /**
-   * Set the HTML tag of this component.
-   */
-  as?: ElementType
-  /**
-   * The URL the link points to
-   */
-  href?: string
+interface Props {
   /**
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
   testId?: string
-  'aria-label'?: AriaAttributes['aria-label']
 }
 
-const Link = forwardRef<HTMLDivElement, LinkProps>(function Link(
-  {
-    href,
-    children,
-    as = 'a',
-    'aria-label': ariaLabel,
-    testId = 'store-link',
-    ...otherProps
-  },
-  ref
+export type LinkProps<C extends ElementType> = PolymorphicComponentPropsWithRef<
+  C,
+  Props
+>
+
+type LinkComponent = <C extends ElementType = 'a'>(
+  props: LinkProps<C>
+) => ReactElement | null
+
+const Link: LinkComponent = forwardRef(function Link<
+  C extends ElementType = 'a'
+>(
+  { as, children, testId = 'store-link', ...otherProps }: LinkProps<C>,
+  ref?: PolymorphicRef<C>
 ) {
-  return as === 'div' ? (
-    <LinkAsDiv ref={ref} ariaLabel={ariaLabel} testId={testId} {...otherProps}>
+  const Component = as ?? 'a'
+
+  return (
+    <Component ref={ref} data-store-link testId={testId} {...otherProps}>
       {children}
-    </LinkAsDiv>
-  ) : (
-    <LinkAsAnchor
-      href={href}
-      testId={testId}
-      ariaLabel={ariaLabel}
-      {...otherProps}
-    >
-      {children}
-    </LinkAsAnchor>
+    </Component>
   )
 })
+
+/**
+ * This is only being exported to make it easier to use in Storybook.
+ * **DON'T** import this directly to use this component, use the default export
+ * instead.
+ */
+export const StorybookLink = Link as FC<Props>
 
 export default Link
