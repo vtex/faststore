75bf9e5a8f7f9ff1150393d7700d058262e50db0
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2021-12-10 19:13:46 -0300

    docs: Add Alert, Banner and PaymentMethods docs (#1076)
    * docs: Add Alert, Banner and PaymentMethods docs
    
    * Update docs/docs/reference/ui/molecules/Alert.mdx
    
    * Update docs/docs/reference/ui/molecules/Banner.mdx
    
    * Update Banner.mdx

--- docs/src/theme/ReactLiveScope/index.tsx
+++ docs/src/theme/ReactLiveScope/index.tsx
@@ -4,182 +4,202 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import '@vtex/theme-b2c-tailwind'
+ import '@vtex/theme-b2c-tailwind'
 
-import React, { useEffect, useState } from 'react'
-import * as UI from '@faststore/ui'
-
-const ShoppingCart = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <circle cx="9" cy="21" r="1" />
-    <circle cx="20" cy="21" r="1" />
-    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
-  </svg>
+ import React, { useEffect, useState } from 'react'
+ import * as UI from '@faststore/ui'
+ 
+ const RightArrow = () => (
+   <svg
+     xmlns="http://www.w3.org/2000/svg"
+     width="18"
+     height="18"
+     viewBox="0 0 18 18"
+   >
+     <path
+       d="M10.6553 3.40717C10.3624 3.11428 9.88756 3.11428 9.59467 3.40717C9.30178 3.70006 9.30178 4.17494 9.59467 4.46783L13.3768 8.25H2.8125C2.39829 8.25 2.0625 8.58579 2.0625 9C2.0625 9.41421 2.39829 9.75 2.8125 9.75H13.3768L9.59467 13.5322C9.30178 13.8251 9.30178 14.2999 9.59467 14.5928C9.88756 14.8857 10.3624 14.8857 10.6553 14.5928L15.7178 9.53033C15.8643 9.38388 15.9375 9.19194 15.9375 9C15.9375 8.89831 15.9173 8.80134 15.8806 8.71291C15.844 8.62445 15.7897 8.54158 15.7178 8.46967L10.6553 3.40717Z"
+       fill="currentColor"
+     />
+   </svg>
+ )
+ 
+ const Warning = () => (
+  <i className="fas fa-exclamation-triangle"></i>
 )
 
-
-const Caret = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="feather feather-chevron-down"
-  >
-    <polyline points="6 9 12 15 18 9" />
-  </svg>
-)
-
-const CustomIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <circle cx="12" cy="12" r="10" />
-    <line x1="15" y1="9" x2="9" y2="15" />
-    <line x1="9" y1="9" x2="15" y2="15" />
-  </svg>
-)
-
-/* Price */
-
-function priceFormatter(price) {
-  const formattedPrice = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(price)
-
-  return formattedPrice
-}
-
-
-/* Bullet */
-const BulletsComponent = ({
-  onClick,
-  activeBullet,
-  totalQuantity,
-  ariaLabelGenerator,
-  testId,
-}) => {
-  const [localActiveBullet, setLocalActiveBullet] = useState(activeBullet)
-
-  useEffect(() => {
-    setLocalActiveBullet(activeBullet)
-  }, [activeBullet, setLocalActiveBullet])
-
-  return (
-    <UI.Bullets
-      onClick={(e, idx) => {
-        onClick?.(e, idx)
-        setLocalActiveBullet(idx)
-      }}
-      activeBullet={localActiveBullet}
-      totalQuantity={totalQuantity}
-      ariaLabelGenerator={ariaLabelGenerator}
-      testId={testId}
-    />
-  )
-}
-
-export const BulletsTemplate = BulletsComponent.bind({})
-
-/* Accordion */
-const AccordionTemplate = ({ testId }) => {
-  const [indices, setIndices] = useState(new Set([]))
-  const onChange = (index) => {
-    if (indices.has(index)) {
-      indices.delete(index)
-      setIndices(new Set(indices))
-    } else {
-      setIndices(new Set(indices.add(index)))
-    }
-  }
-
-  return (
-    <UI.Accordion testId={testId} indices={indices} onChange={onChange}>
-      <UI.AccordionItem>
-        <UI.AccordionButton>Clothing</UI.AccordionButton>
-        <UI.AccordionPanel>
-          <ul>
-            <li>
-              <a href="/">Shorts</a>
-            </li>
-            <li>
-              <a href="/">Sweatshirt</a>
-            </li>
-            <li>
-              <a href="/">Tank tops</a>
-            </li>
-          </ul>
-        </UI.AccordionPanel>
-      </UI.AccordionItem>
-      <UI.AccordionItem>
-        <UI.AccordionButton>Bags</UI.AccordionButton>
-        <UI.AccordionPanel>
-          <ul>
-            <li>
-              <a href="/">Backpacks</a>
-            </li>
-            <li>
-              <a href="/">Necessaire</a>
-            </li>
-          </ul>
-        </UI.AccordionPanel>
-      </UI.AccordionItem>
-      <UI.AccordionItem>
-        <UI.AccordionButton>Sale</UI.AccordionButton>
-        <UI.AccordionPanel>
-          <ul>
-            <li>
-              <a href="/">Smartphones</a>
-            </li>
-            <li>
-              <a href="/">TVs</a>
-            </li>
-          </ul>
-        </UI.AccordionPanel>
-      </UI.AccordionItem>
-    </UI.Accordion>
-  )
-}
-
-export const MultipleAndCollapsible = AccordionTemplate.bind({})
-
-
- // Add react-live imports you need here
- const ReactLiveScope = {
-   React,
-   ...React,
-   ...UI,
-
-   ShoppingCart,
-   MultipleAndCollapsible,
-   Caret,
-   BulletsTemplate,
-   CustomIcon,
-   priceFormatter
- };
-
- export default ReactLiveScope;
+ const ShoppingCart = () => (
+   <svg
+     xmlns="http://www.w3.org/2000/svg"
+     width="1em"
+     height="1em"
+     viewBox="0 0 24 24"
+     fill="none"
+     stroke="currentColor"
+     strokeWidth="2"
+     strokeLinecap="round"
+     strokeLinejoin="round"
+   >
+     <circle cx="9" cy="21" r="1" />
+     <circle cx="20" cy="21" r="1" />
+     <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+   </svg>
+ )
+ 
+ 
+ const Caret = () => (
+   <svg
+     xmlns="http://www.w3.org/2000/svg"
+     width="24"
+     height="24"
+     viewBox="0 0 24 24"
+     fill="none"
+     stroke="currentColor"
+     strokeWidth="2"
+     strokeLinecap="round"
+     strokeLinejoin="round"
+     className="feather feather-chevron-down"
+   >
+     <polyline points="6 9 12 15 18 9" />
+   </svg>
+ )
+ 
+ const CustomIcon = () => (
+   <svg
+     xmlns="http://www.w3.org/2000/svg"
+     width="1em"
+     height="1em"
+     viewBox="0 0 24 24"
+     fill="none"
+     stroke="currentColor"
+     strokeWidth="2"
+     strokeLinecap="round"
+     strokeLinejoin="round"
+   >
+     <circle cx="12" cy="12" r="10" />
+     <line x1="15" y1="9" x2="9" y2="15" />
+     <line x1="9" y1="9" x2="15" y2="15" />
+   </svg>
+ )
+ 
+ /* Price */
+ 
+ function priceFormatter(price) {
+   const formattedPrice = new Intl.NumberFormat('en-US', {
+     style: 'currency',
+     currency: 'USD',
+   }).format(price)
+ 
+   return formattedPrice
+ }
+ 
+ 
+ /* Bullet */
+ const BulletsComponent = ({
+   onClick,
+   activeBullet,
+   totalQuantity,
+   ariaLabelGenerator,
+   testId,
+ }) => {
+   const [localActiveBullet, setLocalActiveBullet] = useState(activeBullet)
+ 
+   useEffect(() => {
+     setLocalActiveBullet(activeBullet)
+   }, [activeBullet, setLocalActiveBullet])
+ 
+   return (
+     <UI.Bullets
+       onClick={(e, idx) => {
+         onClick?.(e, idx)
+         setLocalActiveBullet(idx)
+       }}
+       activeBullet={localActiveBullet}
+       totalQuantity={totalQuantity}
+       ariaLabelGenerator={ariaLabelGenerator}
+       testId={testId}
+     />
+   )
+ }
+ 
+ export const BulletsTemplate = BulletsComponent.bind({})
+ 
+ /* Accordion */
+ const AccordionTemplate = ({ testId }) => {
+   const [indices, setIndices] = useState(new Set([]))
+   const onChange = (index) => {
+     if (indices.has(index)) {
+       indices.delete(index)
+       setIndices(new Set(indices))
+     } else {
+       setIndices(new Set(indices.add(index)))
+     }
+   }
+ 
+   return (
+     <UI.Accordion testId={testId} indices={indices} onChange={onChange}>
+       <UI.AccordionItem>
+         <UI.AccordionButton>Clothing</UI.AccordionButton>
+         <UI.AccordionPanel>
+           <ul>
+             <li>
+               <a href="/">Shorts</a>
+             </li>
+             <li>
+               <a href="/">Sweatshirt</a>
+             </li>
+             <li>
+               <a href="/">Tank tops</a>
+             </li>
+           </ul>
+         </UI.AccordionPanel>
+       </UI.AccordionItem>
+       <UI.AccordionItem>
+         <UI.AccordionButton>Bags</UI.AccordionButton>
+         <UI.AccordionPanel>
+           <ul>
+             <li>
+               <a href="/">Backpacks</a>
+             </li>
+             <li>
+               <a href="/">Necessaire</a>
+             </li>
+           </ul>
+         </UI.AccordionPanel>
+       </UI.AccordionItem>
+       <UI.AccordionItem>
+         <UI.AccordionButton>Sale</UI.AccordionButton>
+         <UI.AccordionPanel>
+           <ul>
+             <li>
+               <a href="/">Smartphones</a>
+             </li>
+             <li>
+               <a href="/">TVs</a>
+             </li>
+           </ul>
+         </UI.AccordionPanel>
+       </UI.AccordionItem>
+     </UI.Accordion>
+   )
+ }
+ 
+ export const MultipleAndCollapsible = AccordionTemplate.bind({})
+ 
+ 
+  // Add react-live imports you need here
+  const ReactLiveScope = {
+    React,
+    ...React,
+    ...UI,
+    RightArrow,
+    ShoppingCart,
+    Warning,
+    MultipleAndCollapsible,
+    Caret,
+    BulletsTemplate,
+    CustomIcon,
+    priceFormatter
+  };
+ 
+  export default ReactLiveScope;
+ 
