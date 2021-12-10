abe1e67e404b247f35e19d8ce92fd2a30b1da51f
Tiago Gimenes <tlgimenes@gmail.com>
2021-12-10 13:29:25 -0300

    Apply suggestions from code review
    Co-authored-by: Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>

--- docs/src/pages/sections/FaqPage/FaqPage.js
+++ docs/src/pages/sections/FaqPage/FaqPage.js
@@ -6,37 +6,34 @@ const data = [
   {
     question: "What is FastStore?",
     answer: `
-    Faststore is a fullstack toolkit that helps you develop your ecommerce experience. It is split into three modules (UI/SDK/API) that you can use individually or combine with other solutions on the Web. Use UI for easily adding accessibility ready components to your ecommerce. Use SDK to manage common states of your ecommerce, like optimistic cart, faceted search and user session. Finally, connect to your favorite ecommerce platform using API.
-
-    To speed up your development, we also provide starters on your favorite React framework, like Gatsby and Next.JS featuring UI/SDK and API.
+FastStore is a full-stack toolkit composed of three independent packages <strong>(UI, SDK, and API)</strong> that help you create performant ecommerce experiences. Use <strong>FastStore UI</strong> to add accessibility-ready components to your ecommerce, <strong>FastStore SDK</strong> to manage the states related to your store behavior, and <strong>FastStore API</strong> to connect to your favorite ecommerce platform. Notice that you can use these three packages individually or combined with other solutions on the web. <br/><br/><em>Please refer to <a href="/reference/overview">our documentation</a> for more information.</em>
     `,
   },
   {
     question: "How I start a FastStore project?",
-    answer: "The easiest way is to use one of our official starters. Find your favorite starter, fork it, customize it and deploy on your favorite Jamstack platform. Currently we suport VTEX WebOps, Netlify, Gatsby Cloud and Vercell"
+    answer: `The easiest way to start a FastStore project is to use one of our official starters. Find your favorite starter at our <a href="/starters">Starter Library</a>, fork it, customize it and deploy it on your favorite Jamstack platform.`
   },
   {
     question: "Can I integrate my FastStore project with a CMS?",
-    answer: `Headless CMS is a great way to empower business users and speed up your iteration time. Check out the list of <a href="https://jamstack.org/headless-cms/">supported headless CMS</a>`
+    answer: `Yes. Since FastStore is Jamstack-based, you can use any <a href="https://jamstack.org/headless-cms/">headless CMS</a> available for Jamstack. Notice that we strongly recommend using VTEX Headless CMS for VTEX projects.`
   },
   {
     question: "Which tech stack do you recommend?",
     answer: `
-    Faststore can run with any stack. However, for a better stability and performance, consider using Jamstack and deploying on VTEX WebOps, Netlify, Gatsby Cloud or Vercell.
-    Also, runtime-based CSS-in-JS solutions tend to harm performance. Consider using a static CSS solution, like Tailwind, Less or raw CSS.
+For styling, consider using a static CSS solution, such as Tailwind, Less, or raw CSS. Keep in mind that runtime-based CSS-in-JS solutions tend to harm performance. For deployment, consider using VTEX IO WebOps for better stability and performance.
     `
   },
   {
     question: "Can I use Tachyons or other CSS frameworks?",
-    answer: "UI is styled via data attributes. This means we are compatible with all major CSS frameworks, like emotion, tailwind, styled components, stichches etc"
+    answer: "Yes. FastStore UI is styled via data attributes. This means we are compatible with all major CSS frameworks, such as Emotion, Tailwind, Styled components, Stitches, etc."
   },
   {
     question: "Can I adapt UI components to my business needs?",
-    answer: "UI uses the atomic desing principle. This means that you can use our atoms and molecules to create custom components tailored for your business needs"
+    answer: "Yes, because all FastStore UI components are open source, you can simply copy the source code from Github and modify it to your liking if you need to extend their behavior in any way for your project."
   },
   {
     question: "How can I contribute to FastStore?",
-    answer: "There are multiple ways to contribute to the project whether you are a technical or non technical user. Correct typos, fix translations, enhance documentation, improve componets, behaviors, submit new starters etc. Check out our contributions guide (comming soon)"
+    answer: "There are multiple ways to contribute to the project whether you are a technical or non-technical user: correct typos, fix translations, enhance documentation, create new starters, improve components and behaviors. <br/><em>Check out our contributions guide (coming soon).</em>"
   }
 ]
 
