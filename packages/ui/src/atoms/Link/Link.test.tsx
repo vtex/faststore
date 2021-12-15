a7518cb537f1ea7963dad9578bd7661db8688ae7
Lucas Feijó <lucas.portela@vtex.com.br>
2021-12-15 15:02:08 -0300

    Add tests for when Link act as polymorphic component

--- packages/ui/src/atoms/Link/Link.test.tsx
+++ packages/ui/src/atoms/Link/Link.test.tsx
@@ -3,33 +3,45 @@
 import { render } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import React from 'react'
+import type { ElementType } from 'react'
 
-import Link from '.'
+import type { LinkProps } from '.'
+import LinkComponent from '.'
 
-const TestLink = () => {
-  return <Link href="/">Link</Link>
+const TestLink = ({ href = '/', testId }: LinkProps<'a'>) => {
+  return (
+    <LinkComponent testId={testId} href={href}>
+      Link
+    </LinkComponent>
+  )
 }
 
 describe('Link', () => {
-  let link: HTMLElement
-  let linkContainer: HTMLElement
-
-  beforeEach(() => {
-    const { getByTestId, container } = render(<TestLink />)
-
-    link = getByTestId('store-link')
-    linkContainer = container
-  })
-
   describe('Data Attributes', () => {
     it('`Link` should have `data-store-link` attribute', () => {
-      expect(link).toHaveAttribute('data-store-link')
+      const { getByTestId } = render(<TestLink />)
+
+      expect(getByTestId('store-link')).toHaveAttribute('data-store-link')
+    })
+
+    it('`Link` should receive a custom component', () => {
+      const { getByTestId } = render(
+        <LinkComponent
+          as={(props: ElementType) => (
+            <div {...props} data-testid="container">
+              <a href="/">Link</a>
+            </div>
+          )}
+        />
+      )
+
+      expect(getByTestId('container')).toHaveAttribute('data-store-link')
     })
   })
 
   describe('Accessibility', () => {
     it('`Link` should have no violations', async () => {
-      expect(await axe(linkContainer)).toHaveNoViolations()
+      expect(await axe(document.body)).toHaveNoViolations()
     })
   })
 })
