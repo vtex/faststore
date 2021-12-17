4bd90e684568d9bc1bc64bbe39456685dacfc2e9
Fanny Chien <fannychienn@gmail.com>
2021-12-17 14:58:05 -0300

    chore: Adds other props to Badge component (#1085)
    * Adds missing other props prop
    
    * Extends interface to html attributes

--- packages/ui/src/atoms/Badge/Badge.tsx
+++ packages/ui/src/atoms/Badge/Badge.tsx
@@ -1,7 +1,7 @@
-import type { ReactNode } from 'react'
+import type { ReactNode, HTMLAttributes } from 'react'
 import React, { forwardRef } from 'react'
 
-export interface BadgeProps {
+export interface BadgeProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
@@ -10,11 +10,11 @@ export interface BadgeProps {
 }
 
 const Badge = forwardRef<HTMLDivElement, BadgeProps>(function Badge(
-  { testId = 'store-badge', children }: BadgeProps,
+  { testId = 'store-badge', children, ...otherProps }: BadgeProps,
   ref
 ) {
   return (
-    <div ref={ref} data-testid={testId} data-store-badge>
+    <div ref={ref} data-store-badge data-testid={testId} {...otherProps}>
       {children}
     </div>
   )
