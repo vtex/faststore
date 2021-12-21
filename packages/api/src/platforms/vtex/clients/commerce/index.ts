8f8fa00f2955ff8f0be33ca44f60ce3ae5ef7c83
Guilherme Carvalho <guilherme@guava.com.br>
2021-12-21 17:05:07 -0300

    Add getRegionID

--- packages/api/src/platforms/vtex/clients/commerce/index.ts
+++ packages/api/src/platforms/vtex/clients/commerce/index.ts
@@ -101,6 +101,18 @@ export const VtexCommerce = (
           }
         )
       },
+      getRegionID: ({
+        postalCode,
+      }: {
+        postalCode: string
+      }): Promise<string> => {
+        const params = new URLSearchParams({
+          country: 'BRA',
+          postalCode,
+        })
+
+        return fetchAPI(`${base}/api/checkout/pub/regions?${params}`)
+      },
     },
   }
 }
