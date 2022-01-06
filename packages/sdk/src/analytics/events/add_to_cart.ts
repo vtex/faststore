a3a05f743573b7750d7ce987515c05c58ce19454
Ícaro Azevedo <icazevedo10@gmail.com>
2022-01-06 15:57:24 -0300

    feat: add item generics to analytics types for easy customization

--- packages/sdk/src/analytics/events/add_to_cart.ts
+++ packages/sdk/src/analytics/events/add_to_cart.ts
@@ -1,12 +1,12 @@
 import type { CurrencyCode, Item } from './common'
 
-export interface AddToCartParams {
+export interface AddToCartParams<T extends Item = Item> {
   currency?: CurrencyCode
   value?: number
-  items?: Item[]
+  items?: T[]
 }
 
-export interface AddToCartEvent {
+export interface AddToCartEvent<T extends Item = Item> {
   name: 'add_to_cart'
-  params: AddToCartParams
+  params: AddToCartParams<T>
 }
--- packages/sdk/src/analytics/events/add_to_wishlist.ts
+++ packages/sdk/src/analytics/events/add_to_wishlist.ts
@@ -1,12 +1,12 @@
 import type { CurrencyCode, Item } from './common'
 
-export interface AddToWishlistParams {
+export interface AddToWishlistParams<T extends Item = Item> {
   currency?: CurrencyCode
   value?: number
-  items?: Item[]
+  items?: T[]
 }
 
-export interface AddToWishlistEvent {
+export interface AddToWishlistEvent<T extends Item = Item> {
   name: 'add_to_wishlist'
-  params: AddToWishlistParams
+  params: AddToWishlistParams<T>
 }
