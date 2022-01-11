25d3c2bd07158ffca75bfe377a5b932c8fb36ae5
Tiago Gimenes <tlgimenes@gmail.com>
2022-01-11 11:18:54 -0300

    fix(api): Breadcrumb data on collection pages (#1104)
    * fix breadcrumb on collection pages
    
    * Update packages/api/src/platforms/vtex/resolvers/collection.ts
    
    Co-authored-by: Igor Brasileiro <brasileiro456@gmail.com>
    
    * fix lint
    
    Co-authored-by: Igor Brasileiro <brasileiro456@gmail.com>

--- packages/api/src/platforms/vtex/resolvers/collection.ts
+++ packages/api/src/platforms/vtex/resolvers/collection.ts
@@ -1,8 +1,8 @@
+import { slugify as baseSlugify } from '../utils/slugify'
 import type { Resolver } from '..'
 import type { Brand } from '../clients/commerce/types/Brand'
 import type { CategoryTree } from '../clients/commerce/types/CategoryTree'
 import type { PortalPagetype } from '../clients/commerce/types/Portal'
-import { slugify } from '../utils/slugify'
 
 type Root = Brand | (CategoryTree & { level: number }) | PortalPagetype
 
@@ -11,9 +11,21 @@ const isBrand = (x: any): x is Brand => x.type === 'brand'
 const isPortalPageType = (x: any): x is PortalPagetype =>
   typeof x.pageType === 'string'
 
+const slugify = (root: Root) => {
+  if (isBrand(root)) {
+    return baseSlugify(root.name)
+  }
+
+  if (isPortalPageType(root)) {
+    return new URL(`https://${root.url}`).pathname.slice(1)
+  }
+
+  return new URL(root.url).pathname.slice(1)
+}
+
 export const StoreCollection: Record<string, Resolver<Root>> = {
   id: ({ id }) => id.toString(),
-  slug: ({ name }) => slugify(name),
+  slug: (root) => slugify(root),
   seo: (root) =>
     isBrand(root) || isPortalPageType(root)
       ? {
@@ -35,7 +47,7 @@ export const StoreCollection: Record<string, Resolver<Root>> = {
   meta: (root) =>
     isBrand(root)
       ? {
-          selectedFacets: [{ key: 'brand', value: slugify(root.name) }],
+          selectedFacets: [{ key: 'brand', value: baseSlugify(root.name) }],
         }
       : {
           selectedFacets: new URL(
@@ -45,11 +57,38 @@ export const StoreCollection: Record<string, Resolver<Root>> = {
             .split('/')
             .map((segment, index) => ({
               key: `category-${index + 1}`,
-              value: slugify(segment),
+              value: baseSlugify(segment),
             })),
         },
-  breadcrumbList: () => ({
-    itemListElement: [],
-    numberOfItems: 0,
-  }),
+  breadcrumbList: async (root, _, ctx) => {
+    const {
+      clients: { commerce },
+    } = ctx
+
+    const slug = slugify(root)
+
+    /**
+     * Split slug into segments so we fetch all data for
+     * the breadcrumb. For instance, if we get `/foo/bar`
+     * we need all metadata for both `/foo` and `/bar` and
+     * thus we need to fetch pageType for `/foo` and `/bar`
+     */
+    const segments = slug.split('/').filter((segment) => Boolean(segment))
+    const slugs = segments.map((__, index) =>
+      segments.slice(0, index + 1).join('/')
+    )
+
+    const pageTypes = await Promise.all(
+      slugs.map((s) => commerce.catalog.portal.pagetype(s))
+    )
+
+    return {
+      itemListElement: pageTypes.map((pageType, index) => ({
+        item: new URL(`https://${pageType.url}`).pathname,
+        name: pageType.name,
+        position: index + 1,
+      })),
+      numberOfItems: pageTypes.length,
+    }
+  },
 }
