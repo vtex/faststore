1c42a8903ad06323844c7da52ebd7e394f432d60
Tiago Gimenes <tlgimenes@gmail.com>
2022-02-14 15:48:21 -0300

    budgets

--- packages/lighthouse-config/src/index.ts
+++ packages/lighthouse-config/src/index.ts
@@ -17,26 +17,27 @@ const lhConfig = ({ urls, server, assertions = {} }: Params) => {
       collect: {
         url,
         settings: {
-          formFactor: 'mobile',
-          throttling: {
-            rttMs: 150,
-            throughputKbps: 1638.4,
-            requestLatencyMs: 562.5,
-            downloadThroughputKbps: 1474.56,
-            uploadThroughputKbps: 675,
-            cpuSlowdownMultiplier: 4,
-          },
-          throttlingMethod: 'simulate',
-          screenEmulation: {
-            mobile: true,
-            width: 360,
-            height: 640,
-            deviceScaleFactor: 2.625,
-            disabled: false,
-          },
-          emulatedUserAgent:
-            'Mozilla/5.0 (Linux; Android 7.0; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4143.7 Mobile Safari/537.36 Chrome-Lighthouse',
-          skipAudits: ['is-on-https'],
+          budgets: [
+            {
+              path: '/*',
+              resourceSizes: [
+                { resourceType: 'document', budget: 20 },
+                { resourceType: 'font', budget: 70 },
+                { resourceType: 'image', budget: 500 },
+                { resourceType: 'script', budget: 200 },
+                { resourceType: 'stylesheet', budget: 100 },
+                { resourceType: 'third-party', budget: 500 },
+                { resourceType: 'total', budget: 600 },
+              ],
+              resourceCounts: [
+                { resourceType: 'font', budget: 3 },
+                { resourceType: 'image', budget: 20 },
+                { resourceType: 'script', budget: 15 },
+                { resourceType: 'stylesheet', budget: 3 },
+                { resourceType: 'third-party', budget: 100 },
+              ],
+            },
+          ],
         },
       },
       assert: {
@@ -45,20 +46,20 @@ const lhConfig = ({ urls, server, assertions = {} }: Params) => {
           'categories:accessibility': ['error', { minScore: 0.95 }],
           'categories:best-practices': ['error', { minScore: 0.95 }],
           'categories:performance': ['error', { minScore: 0.95 }],
-          'categories:pwa': 'off',
           'categories:seo': ['error', { minScore: 0.9 }],
+          'categories:pwa': 'off',
           'cumulative-layout-shift': ['error', { maxNumericValue: 0.2 }],
           'first-contentful-paint': ['error', { maxNumericValue: 2000 }],
-          'is-crawlable': 'off', // preview pages are not crawlable
           'largest-contentful-paint': ['error', { maxNumericValue: 3500 }],
           'max-potential-fid': ['error', { maxNumericValue: 300 }],
           'total-blocking-time': ['error', { maxNumericValue: 200 }],
           'unused-javascript': ['error', { maxLength: 10 }],
-          'uses-long-cache-ttl': 'off',
-          'uses-rel-preconnect': 'warn', // somehow lighthouse ci can't find the dns-prefetch tags we are adding
           'uses-text-compression': ['warn', { maxLength: 1 }],
-          bypass: 'off',
           interactive: ['error', { maxNumericValue: 3000 }],
+          'uses-long-cache-ttl': 'off',
+          'is-crawlable': 'off', // preview pages are not crawlable
+          'uses-rel-preconnect': 'warn', // somehow lighthouse ci can't find the dns-prefetch tags we are adding
+          bypass: 'off',
           ...assertions,
         },
       },
