01502220fb903c62f0b33b9a703fb45daafae141
Ícaro Azevedo <icazevedo10@gmail.com>
2022-02-21 13:20:32 -0300

    fix: allCollections query pagination and pagetype timeout errors (#1140)

---
+++ packages/api/src/platforms/vtex/loaders/collection.ts
@@ -0,0 +1,50 @@
+import DataLoader from 'dataloader'
+import pLimit from 'p-limit'
+
+import { NotFoundError } from '../utils/errors'
+import type { CollectionPageType } from '../clients/commerce/types/Portal'
+import type { Options } from '..'
+import type { Clients } from '../clients'
+
+// Limits concurrent requests to 20 so that they don't timeout
+const CONCURRENT_REQUESTS_MAX = 20
+
+const collectionPageTypes = new Set([
+  'brand',
+  'category',
+  'department',
+  'subcategory',
+] as const)
+
+export const isCollectionPageType = (x: any): x is CollectionPageType =>
+  typeof x.pageType === 'string' &&
+  collectionPageTypes.has(x.pageType.toLowerCase())
+
+export const getCollectionLoader = (_: Options, clients: Clients) => {
+  const limit = pLimit(CONCURRENT_REQUESTS_MAX)
+
+  const loader = async (
+    slugs: readonly string[]
+  ): Promise<CollectionPageType[]> => {
+    return Promise.all(
+      slugs.map((slug: string) =>
+        limit(async () => {
+          const page = await clients.commerce.catalog.portal.pagetype(slug)
+
+          if (isCollectionPageType(page)) {
+            return page
+          }
+
+          throw new NotFoundError(
+            `Catalog returned ${page.pageType} for slug: ${slug}. This usually happens when there is more than one category with the same name in the same category tree level.`
+          )
+        })
+      )
+    )
+  }
+
+  return new DataLoader<string, CollectionPageType>(loader, {
+    // DataLoader is being used to cache requests, not to batch them
+    batch: false,
+  })
+}
--- packages/api/src/platforms/vtex/resolvers/query.ts
+++ packages/api/src/platforms/vtex/resolvers/query.ts
@@ -1,5 +1,4 @@
 import { enhanceSku } from '../utils/enhanceSku'
-import { NotFoundError } from '../utils/errors'
 import { transformSelectedFacet } from '../utils/facets'
 import { SORT_MAP } from '../utils/sort'
 import { StoreCollection } from './collection'
@@ -29,24 +28,12 @@ export const Query = {
 
     return skuLoader.load(locator.map(transformSelectedFacet))
   },
-  collection: async (
-    _: unknown,
-    { slug }: QueryCollectionArgs,
-    ctx: Context
-  ) => {
+  collection: (_: unknown, { slug }: QueryCollectionArgs, ctx: Context) => {
     const {
-      clients: { commerce },
+      loaders: { collectionLoader },
     } = ctx
 
-    const result = await commerce.catalog.portal.pagetype(slug)
-
-    const whitelist = ['Brand', 'Category', 'Department', 'Subcategory']
-
-    if (whitelist.includes(result.pageType)) {
-      return result
-    }
-
-    throw new NotFoundError(`Not Found: ${slug}`)
+    return collectionLoader.load(slug)
   },
   search: async (
     _: unknown,
@@ -100,6 +87,7 @@ export const Query = {
         endCursor: products.total.toString(),
         totalCount: products.total,
       },
+      // after + index is bigger than after+first itself because of the array flat() above
       edges: skus.map((sku, index) => ({
         node: sku,
         cursor: (after + index).toString(),
@@ -140,20 +128,24 @@ export const Query = {
       ...categories,
     ]
 
+    const validCollections = collections
+      // Nullable slugs may cause one route to override the other
+      .filter((node) => Boolean(StoreCollection.slug(node, null, ctx, null)))
+
     return {
       pageInfo: {
-        hasNextPage: false,
-        hasPreviousPage: false,
+        hasNextPage: validCollections.length - after > first,
+        hasPreviousPage: after > 0,
         startCursor: '0',
-        endCursor: '0',
+        endCursor: (
+          Math.min(first, validCollections.length - after) - 1
+        ).toString(),
       },
-      edges: collections
-        // Nullable slugs may cause one route to override the other
-        .filter((node) => Boolean(StoreCollection.slug(node, null, ctx, null)))
-        .slice(after, first)
+      edges: validCollections
+        .slice(after, after + first)
         .map((node, index) => ({
           node,
-          cursor: index.toString(),
+          cursor: (after + index).toString(),
         })),
     }
   },
