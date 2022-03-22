90e394be6fd5f3dcc3f106dc1b9b35b199bb34d8
Heitor Carvalho <heitor.scz@gmail.com>
2022-03-22 09:23:21 -0300

    Fix found typos on packages/gatsby-plugin-nginx

--- packages/gatsby-plugin-nginx/src/typings/types.d.ts
+++ packages/gatsby-plugin-nginx/src/typings/types.d.ts
@@ -49,14 +49,14 @@ declare global {
       | undefined
     /**
      * When set, the plugin will generate an incomplete nginx configuration with only location directives.
-     * May be overriden with environment variable NGINX_LOCATIONS_ONLY
+     * May be overridden with environment variable NGINX_LOCATIONS_ONLY
      * @default false
      */
     writeOnlyLocations: boolean
     /**
      * Specify the nginx directive used to serve files. The placeholder $file must be used and
      * will be substituted with the asset path relative to the folder public without a leading slash.
-     * May be overriden with environment variable NGINX_SERVE_FILE_DIRECTIVE in JSON format
+     * May be overridden with environment variable NGINX_SERVE_FILE_DIRECTIVE in JSON format
      * @example
      * // serve files from a s3 bucket
      * [ "proxy_pass", "https://s3.amazonaws.com/static-site-bucket/$file" ]
