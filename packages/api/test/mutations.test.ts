a5b083a3774551436d1f71a50857203d36ae18dd
victorhmp <victor2142@gmail.com>
2022-03-30 15:41:24 -0300

    Update tests for `api` mutations to use mocked `fetch` calls

--- packages/api/test/mutations.test.ts
+++ packages/api/test/mutations.test.ts
@@ -7,6 +7,11 @@ import {
   ValidateCartMutation,
   InvalidCart,
   ValidCart,
+  checkoutOrderFormItemsValidFetch,
+  checkoutOrderFormValidFetch,
+  checkoutOrderFormInvalidFetch,
+  checkoutOrderFormItemsInvalidFetch,
+  productSearchPage1Count1Fetch,
 } from '../mocks/ValidateCartMutation'
 
 let schema: GraphQLSchema
@@ -20,6 +25,17 @@ const apiOptions = {
   hideUnavailableItems: false,
 } as Options
 
+const mockedFetch = jest.fn((..._) =>
+  // eslint-disable-next-line no-console
+  console.log(
+    'fetch was called without a mocked implementation.\nThis could be caused by an extra fetch call being fired when a certain mutation was executed.'
+  )
+)
+
+jest.mock('../src/platforms/vtex/clients/fetch.ts', () => ({
+  fetchAPI: (info: any, init?: any) => mockedFetch(info, init),
+}))
+
 beforeAll(async () => {
   schema = await getSchema(apiOptions)
 
@@ -28,7 +44,13 @@ beforeAll(async () => {
   context = contextFactory({})
 })
 
+beforeEach(() => mockedFetch.mockClear())
+
 test('`validateCart` mutation should return `null` when a valid cart is passed', async () => {
+  mockedFetch
+    .mockImplementationOnce(() => checkoutOrderFormValidFetch.result)
+    .mockImplementationOnce(() => checkoutOrderFormItemsValidFetch.result)
+
   const response = await execute(
     schema,
     parse(ValidateCartMutation),
@@ -37,16 +59,45 @@ test('`validateCart` mutation should return `null` when a valid cart is passed',
     { cart: ValidCart }
   )
 
+  expect(mockedFetch).toHaveBeenCalledTimes(2)
+  expect(mockedFetch).toHaveBeenCalledWith(
+    checkoutOrderFormValidFetch.info,
+    checkoutOrderFormValidFetch.init
+  )
+  expect(mockedFetch).toHaveBeenCalledWith(
+    checkoutOrderFormItemsValidFetch.info,
+    checkoutOrderFormItemsValidFetch.init
+  )
+
   expect(response).toEqual({ data: { validateCart: null } })
 })
 
 test('`validateCart` mutation should return the full order when an invalid cart is passed', async () => {
+  mockedFetch
+    .mockImplementationOnce(() => checkoutOrderFormInvalidFetch.result)
+    .mockImplementationOnce(() => checkoutOrderFormItemsInvalidFetch.result)
+    .mockImplementationOnce(() => productSearchPage1Count1Fetch.result)
+
   const response = await execute(
     schema,
     parse(ValidateCartMutation),
     null,
     context,
     { cart: InvalidCart }
+  )
+
+  expect(mockedFetch).toHaveBeenCalledTimes(3)
+  expect(mockedFetch).toHaveBeenCalledWith(
+    checkoutOrderFormInvalidFetch.info,
+    checkoutOrderFormInvalidFetch.init
+  )
+  expect(mockedFetch).toHaveBeenCalledWith(
+    checkoutOrderFormItemsInvalidFetch.info,
+    checkoutOrderFormItemsInvalidFetch.init
+  )
+  expect(mockedFetch).toHaveBeenCalledWith(
+    productSearchPage1Count1Fetch.info,
+    productSearchPage1Count1Fetch.init
   )
 
   expect(response).toMatchSnapshot()
