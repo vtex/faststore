511453e41de43c22bf4f6e0f3c5ef5aa5023cdb5
Arthur Andrade <arthurfelandrade@gmail.com>
2022-03-30 16:29:27 -0300

    test: Refactor the usePosition test
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- packages/ui/src/molecules/Dropdown/hooks/useDropdownPosition.ts
+++ packages/ui/src/molecules/Dropdown/hooks/useDropdownPosition.ts
@@ -12,6 +12,8 @@ export const useDropdownPosition = (): DropdownPosition => {
   const buttonRect = dropdownButtonRef?.current?.getBoundingClientRect()
   const topLevel: number = buttonRect?.top ?? 0
   const topOffset: number = buttonRect?.height ?? 0
+
+  // The scroll properties fix the position of DropdownMenu when the scroll is activated.
   const topPosition = topLevel + topOffset + document.documentElement.scrollTop
   const leftLevel = buttonRect?.left ?? 0
   const leftPosition = leftLevel + document.documentElement.scrollLeft
