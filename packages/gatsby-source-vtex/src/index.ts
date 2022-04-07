235622fa90c8f3613c749a3c4e279385aab87523
Tiago Gimenes <tlgimenes@gmail.com>
2022-04-07 14:05:03 -0300

    remove pre gatsby4 packages

--- packages/gatsby-source-vtex/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * as GatsbyNode from './gatsby-node'
-export { config as GatsbyConfig } from './gatsby-config'
-
-export {
-  createNode as sourceStoreCollectionNode,
-  fetchAllNodes as fetchAllStoreCollectionNodes,
-} from './graphql/types/collection'
-export type { StoreCollection } from './graphql/types/collection'
