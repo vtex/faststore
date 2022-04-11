f9cf4044fa28e10f54f34e151abe65d3476ab502
Tiago Gimenes <tlgimenes@gmail.com>
2022-04-11 12:00:49 -0300

    [BREAKING CHANGE]: Remove Gatsby plugins (#1215)

--- packages/gatsby-plugin-nginx/src/utils/functions.ts
+++
@@ -1,1 +0,0 @@
-export const identity = <T>(value: T) => value
--- packages/gatsby-plugin-thumbor/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { urlBuilder } from './utils/urlBuilder'
-export { getThumborImageData } from './utils/getImageData'
-
-export { Provider } from './Provider'
-export { useUrlBuilder } from './useUrlBuilder'
-export { useThumborImageData } from './useThumborImageData'
-export { useGetThumborImageData } from './useGetThumborImageData'
-export type { ThumborImageOptions } from './useThumborImageData'
