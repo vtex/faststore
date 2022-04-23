07e46f4c5c359075eb9a115021d914482add29bf
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2022-04-22 21:42:55 -0300

    docs: Update Starter page and create Starter Submissions (#1239)

--- docs/src/pages/sections/StarterPage/StarterPage.js
+++ docs/src/pages/sections/StarterPage/StarterPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './StarterPage.module.css'
 import StarterComponent from '../../../components/StarterComponent/StarterComponent'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Link from '@docusaurus/Link'
 
 const StarterPage = () => {
   const { siteConfig } = useDocusaurusContext()
@@ -9,21 +10,16 @@ const StarterPage = () => {
 
   return (
     <div className={styles.StarterPage}>
-      <div className={styles.StarterHeader}>
-        <h1>Starter Library</h1>
-        <div className={styles.Subtitle}>
-          Check our starters to quickly get up and running your next FastStore
-          website. Ranging from minimal to complex implementations, FastStore
-          Starters are templates developers can use to create new ecommerce
-          experiences straightaway.{' '}
-        </div>
+      <div className={styles.header}>
+        <p className="uppercase text-sm text-seriousBlack font-bold">Quickly get up and running your next FastStore website</p>
+        <h1 className="text-5xl text-seriousBlack font-VTEXTrust pt-5 pb-0">FastStore Starter Library</h1>
+        <p className='text-lg pt-3 text-details px-5 sm:w-2/5 pb-5 inline-block mx-auto'>Ranging from minimal to complex implementations, FastStore Starters are templates developers can use to create new ecommerce experiences straightaway.</p>
+        <Link className="hover:text-white mx-auto py-3 px-4 rounded text-white uppercase font-VTEXMedium text-sm bg-secondary inline-block" href="/starters/submissions">Submit your starter</Link>
       </div>
-      <div className={styles.Filters}>
-        <a href="#gatsby">Gatsby</a>
-      </div>
-      <div className={styles.StarterContent}>
-        <h2 id="gatsby">Gatsby</h2>
-        <div className={styles.StarterLib}>
+
+      <div className="w-6/12 mx-auto py-16">
+      <h2 className='text-3xl font-VTEXTrust mb-8'>Gatsby</h2>
+        <div className="flex flex-wrap justify-between pb-10">
           {starter.map(({ name, imgPath, url }) => (
             <StarterComponent name={name} imgPath={imgPath} url={url} />
           ))}
