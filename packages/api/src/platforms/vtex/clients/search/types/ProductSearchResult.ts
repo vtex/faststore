639508e9c95c39d81471217debc5d2d2e04c480b
Emerson Laurentino <emerson@laurentino.co>
2022-05-06 14:46:46 -0300

    fix: suggestions api url

--- packages/api/src/platforms/vtex/clients/search/types/ProductSearchResult.ts
+++ packages/api/src/platforms/vtex/clients/search/types/ProductSearchResult.ts
@@ -57,17 +57,6 @@ export interface First {
   index: number
 }
 
-export interface SuggestedTerms {
-  total: number
-  sampling: boolean
-  translated: boolean
-  locale: string
-  query: string
-  operator: string
-  suggestion: Suggestion
-  correction: Correction
-}
-
 export interface Suggestion {
   searches: Search[]
 }
