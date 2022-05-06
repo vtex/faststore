c833e99b50a9d02fc9b8dfbe3330f2ec15246634
Fanny Chien <fannychienn@gmail.com>
2022-05-05 21:32:42 -0300

    Adds tests for ProductCard

--- packages/ui/src/molecules/ProductCard/ProductCard.test.tsx
+++ packages/ui/src/molecules/ProductCard/ProductCard.test.tsx
@@ -3,21 +3,65 @@ import { axe } from 'jest-axe'
 import React from 'react'
 
 import ProductCard from './ProductCard'
+import ProductCardImage from './ProductCardImage'
+import ProductCardContent from './ProductCardContent'
+import ProductCardActions from './ProductCardActions'
+
+const TestProductCard = () => {
+  return (
+    <ProductCard>
+      <ProductCardImage>
+        <div>An image</div>
+      </ProductCardImage>
+      <ProductCardContent>
+        <h3>A title</h3>
+        <p>A little text here: The quick brown fox jumps over the lazy dog.</p>
+      </ProductCardContent>
+      <ProductCardActions>
+        <button>A button</button>
+      </ProductCardActions>
+    </ProductCard>
+  )
+}
 
 describe('ProductCard', () => {
-  it('should have `data-store-product-card` attribute', () => {
-    const { getByTestId } = render(<ProductCard>Testing</ProductCard>)
+  let productCard: HTMLElement
+  let productCardImage: HTMLElement
+  let productCardContent: HTMLElement
+  let productCardActions: HTMLElement
+  let productCardContainer: HTMLElement
 
-    expect(getByTestId('store-product-card')).toHaveAttribute(
-      'data-store-product-card'
-    )
+  beforeEach(() => {
+    const { getByTestId, container } = render(<TestProductCard />)
+
+    productCardContainer = container
+    productCard = getByTestId('store-product-card')
+    productCardImage = getByTestId('store-product-card-image')
+    productCardContent = getByTestId('store-product-card-content')
+    productCardActions = getByTestId('store-product-card-actions')
+  })
+
+  describe('Data attributes', () => {
+    it('`Card` component should have `data-store-product-card` attribute', () => {
+      expect(productCard).toHaveAttribute('data-store-product-card')
+    })
+
+    it('`CardContent` component should have `data-product-card-content` attribute', () => {
+      expect(productCardContent).toHaveAttribute('data-product-card-content')
+    })
+
+    it('`CardImage` component should have `data-product-card-image` attribute', () => {
+      expect(productCardImage).toHaveAttribute('data-product-card-image')
+    })
+
+    it('`CardActions` component should have `data-product-card-actions` attribute', () => {
+      expect(productCardActions).toHaveAttribute('data-product-card-actions')
+    })
   })
 
   describe('Accessibility', () => {
-    it('should have no violations', async () => {
-      const { getByTestId } = render(<ProductCard>Testing</ProductCard>)
-
-      expect(await axe(getByTestId('store-product-card'))).toHaveNoViolations()
+    it('should not have violations', async () => {
+      expect(await axe(productCardContainer)).toHaveNoViolations()
     })
   })
 })
