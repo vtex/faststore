da9e3424c1b02d9ecd16b2db21a305e454c217b3
Tiago Gimenes <tlgimenes@gmail.com>
2022-05-10 15:08:49 -0300

    fix some tests

--- packages/api/src/platforms/vtex/utils/productStock.ts
+++ packages/api/src/platforms/vtex/utils/productStock.ts
@@ -1,15 +1,20 @@
 import type { CommertialOffer } from '../clients/search/types/ProductSearchResult'
 
-export const inStock = (offer: CommertialOffer) => offer.AvailableQuantity > 0
+export const inStock = (offer: Pick<CommertialOffer, 'AvailableQuantity'>) =>
+  offer.AvailableQuantity > 0
 
-export const price = (offer: CommertialOffer) => offer.spotPrice ?? 0
+export const price = (offer: Pick<CommertialOffer, 'spotPrice'>) =>
+  offer.spotPrice ?? 0
 export const sellingPrice = (offer: CommertialOffer) => offer.Price ?? 0
 
 export const availability = (available: boolean) =>
   available ? 'https://schema.org/InStock' : 'https://schema.org/OutOfStock'
 
 // Smallest Available Selling Price First
-export const bestOfferFirst = (a: CommertialOffer, b: CommertialOffer) => {
+export const bestOfferFirst = (
+  a: Pick<CommertialOffer, 'AvailableQuantity' | 'spotPrice'>,
+  b: Pick<CommertialOffer, 'AvailableQuantity' | 'spotPrice'>
+) => {
   if (inStock(a) && !inStock(b)) {
     return -1
   }
