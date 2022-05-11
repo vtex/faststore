65bd223b3c17a3068a47d701aaf9fec766201ae4
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2022-05-11 13:14:47 -0300

    docs: update starter pages metadata (#1285)
    * docs: update starter pages metadata
    
    * docs: update image thumb
    
    * docs: import Head

--- docs/src/pages/starters/beauty.js
+++ docs/src/pages/starters/beauty.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import StarterComponentPage from '../sections/StarterComponentPage/StarterComponentPage'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import StarterComponentPage from '../sections/StarterComponentPage/StarterComponentPage';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Head from '@docusaurus/Head';
 
-function STARTERS() {
+function BeautyStarter() {
   const { siteConfig } = useDocusaurusContext()
   const data = JSON.parse(JSON.stringify(siteConfig.customFields.starters.community))[0]
     return (
-    <Layout title="Beauty Store Starter">
+    <Layout title="Beauty Store Starter" description="An m3 starter made for Beauty & Health ecommerce stores">
+      <Head>
+        <meta property="og:image" content="https://vtexhelp.vtexassets.com/assets/docs/src/beautyPageThumbnail___9720ebeb724fb398256f94b9a87e8180.png" />
+      </Head>
       <StarterComponentPage data={data} />
     </Layout>
   );
 }
 
-export default STARTERS;
+export default BeautyStarter;
--- docs/src/pages/starters/cms-base.js
+++ docs/src/pages/starters/cms-base.js
@@ -3,14 +3,14 @@ import Layout from '@theme/Layout';
 import StarterComponentPage from '../sections/StarterComponentPage/StarterComponentPage'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 
-function STARTERS() {
+function CMSStarter() {
   const { siteConfig } = useDocusaurusContext()
   const data = JSON.parse(JSON.stringify(siteConfig.customFields.starters.official))[1]
   return (
-    <Layout title="CMS Base Starter">
+    <Layout title="CMS Base Starter" description="A VTEX starter made for quick integration with VTEX Headless CMS">
       <StarterComponentPage data={data} />
     </Layout>
   );
 }
 
-export default STARTERS;
+export default CMSStarter;
