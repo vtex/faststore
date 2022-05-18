24b4942b78a409c54b739559579f7d1b6493aca3
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2022-05-18 17:34:46 -0300

    docs: add pets starter (#1307)

--- docs/src/pages/starters/pets.js
+++ docs/src/pages/starters/pets.js
@@ -4,17 +4,17 @@ import StarterComponentPage from '../sections/StarterComponentPage/StarterCompon
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Head from '@docusaurus/Head';
 
-function BaseStarter() {
+function PetsStarter() {
   const { siteConfig } = useDocusaurusContext()
-  const data = JSON.parse(JSON.stringify(siteConfig.customFields.starters.official))[0]
+  const data = JSON.parse(JSON.stringify(siteConfig.customFields.starters.community))[2]
     return (
-    <Layout title="Base Starter" description="A VTEX starter made for quick setup of standard B2C stores">
+    <Layout title="Pets Store Starter" description="A Whirpool starter made for Pet Supplies stores">
       <Head>
-        <meta property="og:image" content="https://vtexhelp.vtexassets.com/assets/docs/src/basePageThumbnail___f4a77f508aac5d3d06c25c5cf8f824dd.png" />
+        <meta property="og:image" content="https://vtexhelp.vtexassets.com/assets/docs/src/petsPageThumbnail___91d901c507a3a443dbae3992aa4e4c92.png" />
       </Head>
       <StarterComponentPage data={data} />
     </Layout>
   );
 }
 
-export default BaseStarter;
+export default PetsStarter;
