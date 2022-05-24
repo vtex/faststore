7bdccfaecdb1e74ef14ed5ae7520e1b02dda07e8
Lucas Feijó <lucas.portela@vtex.com.br>
2022-05-24 15:49:11 -0300

    fix: Fixes custom attributes for img and link tags (#60)
    * Fixes custom attributes for `img` and `link` tags
    
    * Adds changelog entry

--- packages/core/CHANGELOG.md
+++ packages/core/CHANGELOG.md
@@ -34,6 +34,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+- Limit custom props only for `img` and `link` tags ([#60](https://github.com/vtex-sites/nextjs.store/pull/60))
 - Warning related to `fetchPriority` prop not being recognized as `img` and `link`'s prop ([#54](https://github.com/vtex-sites/nextjs.store/pull/54))
 - Error on Storybook build when trying to import base CSS styles/mixins in CSS module files ([#53](https://github.com/vtex-sites/nextjs.store/pull/53))
 - A missing gap between the Sign In link and Cart button on desktop ([#11](https://github.com/vtex-sites/nextjs.store/pull/11)).
--- packages/core/src/components/ui/Image/Image.tsx
+++ packages/core/src/components/ui/Image/Image.tsx
@@ -7,8 +7,12 @@ import type { ImageOptions } from './useImage'
 // React still don't have imageSizes declared on its types. Somehow,
 // it generated the right html
 declare module 'react' {
-  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    imageSizes?: string
+  interface ImgHTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    fetchpriority?: string
+  }
+
+  interface LinkHTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    imagesizes?: string
     fetchpriority?: string
   }
 }
@@ -33,7 +37,7 @@ const Image = forwardRef<HTMLImageElement, Props>(
               rel="preload"
               href={src}
               imageSrcSet={srcSet}
-              imageSizes={sizes}
+              imagesizes={sizes}
               fetchpriority={fetchPriority}
             />
           </Head>
