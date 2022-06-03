767c0cdc5eaf6bd56020d64fd7718b1379c09390
Fanny Chien <fannychienn@gmail.com>
2022-06-03 14:27:57 -0300

    feat(UI): Add Hero component (#1336)
    * Adds base code for Hero component
    
    * Adds Hero components
    
    * Adds base styles to example
    
    * Updates styles
    
    * Renames `HeroContent` to `HeroHeading`
    
    * Updates styles
    
    * Adds missing flie
    
    * Fixes typo
    
    * Removes `HeroLink` component
    
    * Update packages/ui/src/organisms/Hero/Hero.test.tsx
    
    Co-authored-by: Eduardo Formiga <eduardo.formiga@gmail.com>
    
    * Update packages/ui/src/organisms/Hero/HeroHeading.tsx
    
    Co-authored-by: Filipe W. Lima <filipe.lima@vtex.com.br>
    
    * Removing children prop
    
    Co-authored-by: Eduardo Formiga <eduardo.formiga@gmail.com>
    Co-authored-by: Filipe W. Lima <filipe.lima@vtex.com.br>

---
+++ packages/ui/src/organisms/Hero/index.tsx
@@ -0,0 +1,8 @@
+export { default } from './Hero'
+export type { HeroProps } from './Hero'
+
+export { default as HeroImage } from './HeroImage'
+export type { HeroImageProps } from './HeroImage'
+
+export { default as HeroHeading } from './HeroHeading'
+export type { HeroHeadingProps } from './HeroHeading'
