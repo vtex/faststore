df5baec6f1ba56c6061b2f07faf2124349340a16
Renata Motta <renata.motta@vtex.com.br>
2022-06-06 16:16:09 -0300

    Feat: Adds tokens to `Alert` (#94)
    * Feat: Adds tokens to `Alert` (#77)
    
    * Adds tokens to `Alert`
    
    * Fix typo
    
    * Updates Changelog
    
    * Updates Storybook titles
    
    * Updates Changelog

--- packages/core/src/components/ui/Alert/Alert.tsx
+++ packages/core/src/components/ui/Alert/Alert.tsx
@@ -7,9 +7,20 @@ import FrameworkLink from 'src/components/common/Link'
 import Button from 'src/components/ui/Button'
 import Icon from 'src/components/ui/Icon'
 
-type Props = AlertProps & {
+import styles from './alert.module.scss'
+
+export type Props = AlertProps & {
+  /**
+   * Icon component for additional customization
+   */
   icon?: ReactNode
+  /**
+   * Add the possibility to make the component dismissible
+   */
   dismissible?: boolean
+  /**
+   * The href and label used at the link
+   */
   link?: {
     to: string
     text: string
@@ -37,20 +48,27 @@ function Alert({
   )
 
   return (
-    <UIAlert {...otherProps}>
+    <UIAlert
+      data-fs-alert
+      data-fs-alert-dismissible={dismissible}
+      className={styles.fsAlert}
+      {...otherProps}
+    >
       {icon && <UIIcon component={icon} />}
 
-      <div data-alert-content>{children}</div>
+      <p data-fs-alert-content>{children}</p>
 
       {link && (
-        <div data-alert-link>
-          <FrameworkLink href={link.to}>{link.text}</FrameworkLink>
-        </div>
+        <FrameworkLink data-fs-alert-link href={link.to}>
+          {link.text}
+        </FrameworkLink>
       )}
 
       {dismissible && (
-        <Button data-alert-button aria-label="Close" onClick={handleClose}>
-          <Icon name="X" width={18} height={18} weight="bold" />
+        <Button data-fs-alert-button aria-label="Close" onClick={handleClose}>
+          <span>
+            <Icon name="X" width={18} height={18} weight="bold" />
+          </span>
         </Button>
       )}
     </UIAlert>
