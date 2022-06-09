8ffab6177d9713dfbd18bf205d16bb0c45db647a
Emerson Laurentino <emerson@laurentino.co>
2022-06-09 12:37:02 -0300

    Revert "fix: tailwind config on docs (#1345)" (#1351)
    This reverts commit 265a01af86610d4e104eccb123778097d272da2e.

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -218,17 +218,7 @@ module.exports = {
     ],
   ],
   plugins: [
-    async function myPlugin(context, options) {
-      return {
-        name: "docusaurus-tailwindcss",
-        configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require("tailwindcss"));
-          postcssOptions.plugins.push(require("autoprefixer"));
-          return postcssOptions;
-        },
-      };
-    },
+    'docusaurus-tailwindcss-loader',
     require.resolve('docusaurus-plugin-image-zoom'),
     [
       'docusaurus-plugin-react-docgen-typescript',
