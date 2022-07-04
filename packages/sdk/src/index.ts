5a029e3584f060dba53aabc3edbcdd53acf7c444
Tiago Gimenes <tlgimenes@gmail.com>
2022-07-04 11:40:09 -0300

    export type

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,105 +1,108 @@
-export type { ShareEvent, ShareParams } from './analytics/events/share'
-export type { SearchEvent, SearchParams } from './analytics/events/search'
-export type { SignupEvent, SignupParams } from './analytics/events/signup'
-export type { LoginEvent, LoginParams } from './analytics/events/login'
-export type { RefundEvent, RefundParams } from './analytics/events/refund'
-export type { PurchaseEvent, PurchaseParams } from './analytics/events/purchase'
+export type { ShareEvent, ShareParams } from "./analytics/events/share";
+export type { SearchEvent, SearchParams } from "./analytics/events/search";
+export type { SignupEvent, SignupParams } from "./analytics/events/signup";
+export type { LoginEvent, LoginParams } from "./analytics/events/login";
+export type { RefundEvent, RefundParams } from "./analytics/events/refund";
+export type {
+  PurchaseEvent,
+  PurchaseParams,
+} from "./analytics/events/purchase";
 export type {
   AddShippingInfoEvent,
   AddShippingInfoParams,
-} from './analytics/events/add_shipping_info'
+} from "./analytics/events/add_shipping_info";
 export type {
   AddPaymentInfoEvent,
   AddPaymentInfoParams,
-} from './analytics/events/add_payment_info'
+} from "./analytics/events/add_payment_info";
 export type {
   BeginCheckoutEvent,
   BeginCheckoutParams,
-} from './analytics/events/begin_checkout'
+} from "./analytics/events/begin_checkout";
 export type {
   ViewCartEvent,
   ViewCartParams,
-} from './analytics/events/view_cart'
+} from "./analytics/events/view_cart";
 export type {
   RemoveFromCartEvent,
   RemoveFromCartParams,
-} from './analytics/events/remove_from_cart'
+} from "./analytics/events/remove_from_cart";
 export type {
   AddToCartEvent,
   AddToCartParams,
-} from './analytics/events/add_to_cart'
+} from "./analytics/events/add_to_cart";
 export type {
   SelectItemEvent,
   SelectItemParams,
-} from './analytics/events/select_item'
+} from "./analytics/events/select_item";
 export type {
   AddToWishlistEvent,
   AddToWishlistParams,
-} from './analytics/events/add_to_wishlist'
+} from "./analytics/events/add_to_wishlist";
 export type {
   SelectPromotionEvent,
-  SelectPromotionParams,
   SelectPromotionItems,
-} from './analytics/events/select_promotion'
+  SelectPromotionParams,
+} from "./analytics/events/select_promotion";
 export type {
   ViewPromotionEvent,
-  ViewPromotionParams,
   ViewPromotionItems,
-} from './analytics/events/view_promotion'
+  ViewPromotionParams,
+} from "./analytics/events/view_promotion";
 export type {
   ViewItemEvent,
   ViewItemParams,
-} from './analytics/events/view_item'
+} from "./analytics/events/view_item";
 export type {
   ViewItemListEvent,
   ViewItemListParams,
-} from './analytics/events/view_item_list'
+} from "./analytics/events/view_item_list";
 export type {
+  CurrencyCode,
+  Item,
   ItemId,
   ItemName,
   ItemUniqueIdentifier,
   ItemWithoutIdentifier,
-  Item,
-  PromotionParams,
   PromotionItem,
-  CurrencyCode,
-} from './analytics/events/common'
+  PromotionParams,
+} from "./analytics/events/common";
 export type {
   AnalyticsEvent,
+  UnknownEvent,
   WrappedAnalyticsEvent,
   WrappedAnalyticsEventParams,
-  UnknownEvent,
-} from './analytics/wrap'
-export { STORE_EVENT_PREFIX, ANALYTICS_EVENT_TYPE } from './analytics/wrap'
-export { sendAnalyticsEvent } from './analytics/sendAnalyticsEvent'
-export { useAnalyticsEvent } from './analytics/useAnalyticsEvent'
+} from "./analytics/wrap";
+export { ANALYTICS_EVENT_TYPE, STORE_EVENT_PREFIX } from "./analytics/wrap";
+export { sendAnalyticsEvent } from "./analytics/sendAnalyticsEvent";
+export { useAnalyticsEvent } from "./analytics/useAnalyticsEvent";
 
 // Faceted Search
-export { parse as parseSearchState } from './search/serializer'
+export { parse as parseSearchState } from "./search/serializer";
 
-export { default as formatSearchState } from './utils/format'
+export { default as formatSearchState } from "./utils/format";
 
-export { initialize as initSearchState } from './search/useSearchState'
+export { initialize as initSearchState } from "./search/useSearchState";
 export {
   isSearchSort,
   removeFacet,
   setFacet,
   toggleFacet,
   toggleFacets,
-} from './search/facets'
-export { Provider as SearchProvider } from './search/Provider'
-export { useSearch } from './search/useSearch'
-export { usePagination } from './search/usePagination'
-export type { State as SearchState } from './types'
+} from "./search/facets";
+export { Provider as SearchProvider } from "./search/Provider";
+export { useSearch } from "./search/useSearch";
+export { usePagination } from "./search/usePagination";
+export type { State as SearchState } from "./types";
 
 // UI
-export { Provider as UIProvider, Context as UIContext } from './ui/Provider'
+export { Context as UIContext, Provider as UIProvider } from "./ui/Provider";
 export type {
   Actions as UIActions,
   Effects as UIEffects,
   InitialState as UIInitialState,
-} from './ui/Provider'
-export { useGlobalUIState } from './ui/useGlobalUIState'
+} from "./ui/Provider";
+export { useGlobalUIState } from "./ui/useGlobalUIState";
 
 // Session
 export { createSessionStore } from "./session";
@@ -120,3 +123,4 @@ export { optimistic } from "./store/optimistic";
 export { persisted } from "./store/persisted";
 export { singleton } from "./store/singleton";
 export { compose } from "./utils/compose";
+export type { Store } from "./store/base";
