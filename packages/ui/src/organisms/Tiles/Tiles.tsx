2ee105ca2c335e1a7dad60efa55f29cfdc587c22
Filipe W. Lima <381395+filipewl@users.noreply.github.com>
2022-08-01 17:54:13 -0300

    refactor: Import the `Tiles` & `Tile` components from the starter
    ## What's the purpose of this pull request?
    
    Extract the `Tiles` and `Tile` component from the [starters](https://github.com/vtex-sites/nextjs.store/tree/81b2cbcae8157703130ab18356178cb15c8bcf12/src/components/ui/Tiles) and move it to the `@faststore/ui` package.
    
    ## How to test it?
    
    Run `yarn storybook` locally and check the [`Tiles` component page](http://localhost:6006/?path=/docs/organisms-tiles--tiles).
    
    Its Storybook page:
    ![CleanShot 2022-07-29 at 15 26 25](https://user-images.githubusercontent.com/381395/181822535-577ec95e-0076-4191-ab20-d4a64386e9d4.png)
    
    The same component being used on the starter:
    ![CleanShot 2022-07-29 at 15 41 38](https://user-images.githubusercontent.com/381395/181824154-5f0a8e64-7520-470a-b26a-452c0f837149.png)
    
    ### Starters Deploy Preview
    
    - https://github.com/vtex-sites/nextjs.store/pull/185 and the [deploy preview](https://preview-185--nextjs.preview.vtex.app/)

---
+++ packages/ui/src/organisms/Tiles/Tiles.tsx
@@ -0,0 +1,64 @@
+import React, { Children, forwardRef } from 'react'
+import type { HTMLAttributes, ReactElement } from 'react'
+
+import { Tile } from '.'
+
+export interface TilesProps extends HTMLAttributes<HTMLUListElement> {
+  /**
+   * ID to find this component in testing tools (e.g.: Cypress,
+   * Testing Library, and Jest).
+   */
+  testId?: string
+}
+
+const MIN_CHILDREN = 2
+const MAX_CHILDREN = 4
+const NUMBER_ITEMS_TO_EXPAND_FIRST_TWO = 2
+const NUMBER_ITEMS_TO_EXPAND_FIRST = 3
+
+const Tiles = forwardRef<HTMLUListElement, TilesProps>(function Tiles(
+  { testId = 'store-tiles', children, ...otherProps },
+  ref
+) {
+  const childrenCount = Children.count(children)
+
+  if (process.env.NODE_ENV === 'development') {
+    const isOutOfBounds =
+      childrenCount < MIN_CHILDREN || childrenCount > MAX_CHILDREN
+
+    if (isOutOfBounds) {
+      throw new Error(
+        `Tiles cannot receive less than ${MIN_CHILDREN} or more than ${MAX_CHILDREN} children.`
+      )
+    }
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    Children.forEach(children as ReactElement, (child) => {
+      if (child.type !== Tile) {
+        throw new Error('Only Tile components allowed as children.')
+      }
+    })
+  }
+
+  const expandedClass =
+    childrenCount === NUMBER_ITEMS_TO_EXPAND_FIRST
+      ? 'expanded-first'
+      : childrenCount === NUMBER_ITEMS_TO_EXPAND_FIRST_TWO
+      ? 'expanded-first-two'
+      : ''
+
+  return (
+    <ul
+      ref={ref}
+      data-fs-tiles
+      data-fs-tiles-variant={expandedClass}
+      data-testid={testId}
+      {...otherProps}
+    >
+      {children}
+    </ul>
+  )
+})
+
+export default Tiles
