63c7a893717a22ccb38508b173da37c393a1974d
Arthur Andrade <arthurfelandrade@gmail.com>
2022-08-02 10:41:18 -0300

    feat: Add `ProductTitle` import from faststore (#187)

--- packages/core/src/components/ui/ProductTitle/ProductTitle.tsx
+++ packages/core/src/components/ui/ProductTitle/ProductTitle.tsx
@@ -1,38 +1,11 @@
 import { memo } from 'react'
-import type { ReactNode } from 'react'
+import type { ProductTitleProps } from '@faststore/ui'
+import { ProductTitle as UIProductTitle } from '@faststore/ui'
 
 import styles from './product-title.module.scss'
 
-interface ProductTitleProp {
-  /**
-   * A react component to be used as the product title, e.g. a `h1`
-   */
-  title: ReactNode
-  /**
-   * A react component to be used as the product label, e.g. a `DiscountBadge`
-   */
-  label?: ReactNode
-  /**
-   * A text to be used below the title and the label, such as the product's reference number.
-   */
-  refNumber?: string
-}
-
-function ProductTitle({ title, label, refNumber }: ProductTitleProp) {
-  return (
-    <header data-fs-product-title className={styles.fsProductTitle}>
-      <div data-fs-product-title-header>
-        {title}
-        {!!label && label}
-      </div>
-
-      {refNumber && (
-        <p data-fs-product-title-addendum className="text__legend">
-          Ref.: {refNumber}
-        </p>
-      )}
-    </header>
-  )
+function ProductTitle({ ...otherProps }: ProductTitleProps) {
+  return <UIProductTitle className={styles.fsProductTitle} {...otherProps} />
 }
 
 export default memo(ProductTitle)
