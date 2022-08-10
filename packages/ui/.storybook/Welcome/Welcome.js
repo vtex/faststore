06164e340694769b30ce75c2889e1061983a2170
Fanny <fanny.chien@vtex.com.br>
2022-08-10 13:28:29 +0800

    Updates storybook welcome page content

--- packages/ui/.storybook/Welcome/Welcome.js
+++ packages/ui/.storybook/Welcome/Welcome.js
@@ -5,10 +5,13 @@ import PackageInfo from './../../package.json'
 export const Welcome = () => {
   return (
     <div className="welcome__container">
-      <h2 className="welcome__heading">Store UI v{`${PackageInfo.version}`}</h2>
+      <h2 className="welcome__heading">
+        FastStore UI v{`${PackageInfo.version}`}
+      </h2>
       <p>
-        A complete, framework agnostic library for building your next digital
-        experience
+        FastStore UI is an ecommerce-focused library that provides best-of-breed
+        components, blazing-fast performance, and a frictionless development
+        experience to create storefronts.
       </p>
     </div>
   )
