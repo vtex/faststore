255b236b8573f88768a0ba0ec0ead001e36daa7d
Tiago Gimenes <tlgimenes@gmail.com>
2022-08-15 14:52:29 -0300

    feat: Product releaseDate (#1438)
    * Return product releaseDate
    
    * return release date

--- packages/api/src/platforms/vtex/resolvers/product.ts
+++ packages/api/src/platforms/vtex/resolvers/product.ts
@@ -116,4 +116,5 @@ export const StoreProduct: Record<string, Resolver<Root>> & {
 
     return [...propertyValueSpecifications, ...propertyValueAttachments]
   },
+  releaseDate: ({ isVariantOf: { releaseDate } }) => releaseDate ?? ''
 }
