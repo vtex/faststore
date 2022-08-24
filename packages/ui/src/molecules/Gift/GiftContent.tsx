553c680e03c1fc90dae081550897da5ffad23a19
Fanny <fanny.chien@vtex.com.br>
2022-08-24 16:22:40 +0800

    feat: Adds Gift components and build example

---
+++ packages/ui/src/molecules/Gift/GiftContent.tsx
@@ -0,0 +1,29 @@
+import type { HTMLAttributes } from 'react'
+import React, { forwardRef } from 'react'
+
+export interface GiftContentProps extends HTMLAttributes<HTMLElement> {
+  /**
+   * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
+   */
+  testId?: string
+}
+
+const GiftContent = forwardRef<HTMLElement, GiftContentProps>(
+  function GiftContent(
+    { testId = 'store-card-content', children, ...otherProps },
+    ref
+  ) {
+    return (
+      <section
+        ref={ref}
+        data-fs-gift-content
+        data-testid={testId}
+        {...otherProps}
+      >
+        {children}
+      </section>
+    )
+  }
+)
+
+export default GiftContent
--- packages/ui/src/molecules/Gift/stories/Gift.stories.tsx
+++ packages/ui/src/molecules/Gift/stories/Gift.stories.tsx
@@ -1,12 +1,42 @@
 import type { Story, Meta } from '@storybook/react'
-import React from 'react'
+import React, { useMemo } from 'react'
 
+import Badge from '../../../atoms/Badge/Badge'
+import Price from '../../../atoms/Price/Price'
+// Gift components
+import GiftComponent from '../Gift'
+import GiftContent from '../GiftContent'
+import GiftImage from '../GiftImage'
 import type { GiftProps } from '../Gift'
-import Component from '../Gift'
 import mdx from './Gift.mdx'
 
+function useIntlFormatter(price: number) {
+  return useMemo(() => {
+    const formattedPrice = new Intl.NumberFormat('en-GB', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price)
+
+    return formattedPrice
+  }, [price])
+}
+
 const GiftTemplate: Story<GiftProps> = ({ testId }) => (
-  <Component testId={testId}>Gift</Component>
+  <GiftComponent testId={testId}>
+    <GiftImage>
+      <img
+        alt="Aedle VK-1 L Headphone"
+        src="https://assets.vtex.app/unsafe/1608x1206/center/middle/https%3A%2F%2Fstoreframework.vtexassets.com%2Farquivos%2Fids%2F190901%2Funsplash-headphone.jpg%3Fv%3D637800115948430000"
+      />
+    </GiftImage>
+    <GiftContent>
+      <h3>Get a pair of Aedle VK-1 L Headphone</h3>
+      <div>
+        <Price value={145} formatter={useIntlFormatter} />
+        <Badge>Free</Badge>
+      </div>
+    </GiftContent>
+  </GiftComponent>
 )
 
 export const Gift = GiftTemplate.bind({})
