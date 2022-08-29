e36ad6f6e1af1a03c0dd47c4263e96bc9d0b7b8d
Tiago Gimenes <tlgimenes@gmail.com>
2022-08-29 15:13:04 -0300

    feat: Cart with session info (#1444)

--- packages/api/src/platforms/vtex/clients/commerce/index.ts
+++ packages/api/src/platforms/vtex/clients/commerce/index.ts
@@ -85,6 +85,17 @@ export const VtexCommerce = (
           }
         )
       },
+      shippingData: (
+        { id, body }: { id: string; body: unknown },
+      ): Promise<OrderForm> => {
+        return fetchAPI(
+          `${base}/api/checkout/pub/orderForm/${id}/attachments/shippingData`,
+          {
+            ...BASE_INIT,
+            body: JSON.stringify(body),
+          },
+        );
+      },
       orderForm: ({
         id,
         refreshOutdatedData = true,
