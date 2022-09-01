f5488be15f78f0e968ad98f7df3b1ae7a6f9f30a
Fanny <fanny.chien@vtex.com.br>
2022-09-01 14:08:17 +0800

    fix: Replaces single quotes to avoid no-unescaped-entities

--- apps/docs/src/components/Hero/Hero.js
+++ apps/docs/src/components/Hero/Hero.js
@@ -3,16 +3,18 @@ import Link from '@docusaurus/Link'
 
 function Hero() {
   return (
-    <div className='bg-whiteIce mb-24'>
+    <div className="bg-whiteIce mb-24">
       <div className="mx-auto p-4 w-full lg:max-w-[1170px] lg:py-16">
         <div className="flex justify-between flex-col md:flex-row">
           <div className="my-auto md:pr-24">
-            <h1 className='font-VTEXTrust text-sm text-details'>FastStore</h1>
+            <h1 className="font-VTEXTrust text-sm text-details">FastStore</h1>
             <h2 className="text-[52px] text-fontSecondary font-VTEXTrust leading-tight">
-            The ultimate toolkit for building blazing-fast storefronts
+              The ultimate toolkit for building blazing-fast storefronts
             </h2>
             <p className="text-base text-details my-6 leading-7">
-            In the ecommerce world, speed matters. That's why we've built FastStore: an open-source toolkit to create blazing-fast storefronts that convert.
+              In the ecommerce world, speed matters. That&rsquo;s why
+              we&rsquo;ve built FastStore: an open-source toolkit to create
+              blazing-fast storefronts that convert.
             </p>
             <div>
               <div>
@@ -24,14 +26,18 @@ function Hero() {
                 </Link>
                 <Link
                   to="https://docs.google.com/forms/d/e/1FAIpQLSfmNyT5BepQw2AFyHnrI9-4fqvkOTWvdeBLdkQgxhQ2r0-g4A/viewform"
-                  className="button-tertiary">
+                  className="button-tertiary"
+                >
                   Join the waiting list
                 </Link>
               </div>
             </div>
           </div>
 
-          <img src="https://vtexhelp.vtexassets.com/assets/docs/src/newperf___473c0852ff70a357649f54f62eb968d1.png" className="md:block md:w-2/5 md:object-contain my-10" />
+          <img
+            src="https://vtexhelp.vtexassets.com/assets/docs/src/newperf___473c0852ff70a357649f54f62eb968d1.png"
+            className="md:block md:w-2/5 md:object-contain my-10"
+          />
         </div>
       </div>
     </div>
