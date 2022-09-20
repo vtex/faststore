371d5b1e592ce7b90b274d086f5f6c9e069739c5
Ícaro Azevedo <icazevedo10@gmail.com>
2022-09-20 16:46:03 -0300

    feat: add missing native <table> props to Table molecule

--- packages/ui/src/molecules/Table/Table.tsx
+++ packages/ui/src/molecules/Table/Table.tsx
@@ -1,7 +1,7 @@
-import type { HTMLAttributes } from 'react'
+import type { DetailedHTMLProps, TableHTMLAttributes } from 'react'
 import React, { forwardRef } from 'react'
 
-export interface TableProps extends HTMLAttributes<HTMLTableElement> {
+export interface TableProps extends DetailedHTMLProps<TableHTMLAttributes<HTMLTableElement>, HTMLTableElement> {
   /**
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
