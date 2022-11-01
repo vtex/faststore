fa1bfb1a48f7263897cfc5662131a4524113578e
Fanny <fanny.chien@vtex.com.br>
2022-11-01 19:11:45 +0800

    chore: Adds custom breakpoints to storybook

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,5 +1,44 @@
 import '../src/styles/global.scss'
 
+// Viewports from src/styles/utilities.scss
+const customViewports = {
+  phone: {
+    name: 'Phone',
+    styles: {
+      width: '320px',
+      height: '480px',
+    },
+  },
+  phonemid: {
+    name: 'PhoneMid',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  notebook: {
+    name: 'Notebook',
+    styles: {
+      width: '1280px',
+      height: '960px',
+    },
+  },
+  desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1440px',
+      height: '1024px',
+    },
+  },
+}
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,5 +46,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  viewport: {
+    viewports: customViewports,
   },
 }
