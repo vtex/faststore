8d860f125efc2d670089d0cab64c4a3b79f7f505
Fanny <fanny.chien@vtex.com.br>
2022-11-04 20:42:41 +0800

    chore: Adds TokenDivider

---
+++ apps/docs/src/components/Tokens/index.ts
@@ -0,0 +1,7 @@
+export { default as TokenTable } from './TokenTable'
+export type { TokenTableProps } from './TokenTable'
+
+export { default as TokenRow } from './TokenRow'
+export type { TokenRowProps } from './TokenRow'
+
+export { default as TokenDivider } from './TokenDivider'
