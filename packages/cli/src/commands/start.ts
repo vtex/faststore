9bcceca01419bb80095d97c8ccff9f329c550a8d
Ícaro Azevedo <icazevedo10@gmail.com>
2022-11-07 14:44:20 -0300

    feat: adds start command to serve stores

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -1,7 +1,20 @@
 import { Command } from '@oclif/core'
+import { spawn } from 'child_process'
+import fse from 'fs-extra'
+import { tmpDir } from '../utils/directory'
 
 export default class Start extends Command {
   async run() {
-    console.log('running start command')
+    if (!fse.existsSync(tmpDir)) {
+      throw Error(
+        'The ".faststore" directory could not be found. If you are trying to serve your store, run "faststore build" first.'
+      )
+    }
+
+    return spawn(`yarn serve`, {
+      shell: true,
+      cwd: tmpDir,
+      stdio: 'inherit',
+    })
   }
 }
