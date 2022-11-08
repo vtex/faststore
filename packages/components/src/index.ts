aa66445d0b28ff7046eb89c7f0ae7ebd745e7187
Fanny <fanny.chien@vtex.com.br>
2022-11-08 16:50:35 +0800

    chore: Adds RadioField component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,6 +1,11 @@
+// Atoms
 export { default as Icon } from './atoms/Icon'
 export type { IconProps } from './atoms/Icon'
 export { default as Radio } from './atoms/Radio'
 export type { RadioProps } from './atoms/Radio'
 export { default as Label } from './atoms/Label'
 export type { LabelProps } from './atoms/Label'
+
+// Molecules
+export { default as RadioField } from './molecules/RadioField'
+export type { RadioFieldProps } from './molecules/RadioField'
---
+++ packages/components/src/molecules/RadioField/RadioField.tsx
@@ -0,0 +1,41 @@
+import { Radio, Label } from '@faststore/components'
+import React, { forwardRef } from 'react'
+
+export type RadioFieldProps = {
+  /**
+   * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
+   */
+  testId?: string
+  /**
+   * ID to identify input and corresponding label.
+   */
+  id: string
+  /**
+   * The text displayed to identify the input radio.
+   */
+  label: string
+  /**
+   * The value to identify the input radio.
+   */
+  value?: string
+  /**
+   * Identify radio in the same group.
+   */
+  name?: string
+}
+
+const RadioField = forwardRef<HTMLDivElement, RadioFieldProps>(
+  function RadioField(
+    { testId = 'fs-radio-field', id, label, value, name, ...otherProps },
+    ref
+  ) {
+    return (
+      <div ref={ref} data-fs-radio-field data-testid={testId}>
+        <Radio id={id} value={value ?? label} name={name} {...otherProps} />
+        <Label htmlFor={id}>{label}</Label>
+      </div>
+    )
+  }
+)
+
+export default RadioField
