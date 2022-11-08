bdbe3afedb7a3c6de61ddb0ccd934b3dcda8c4f5
Ícaro Azevedo <icazevedo10@gmail.com>
2022-11-08 17:10:08 -0300

    use named imports for fs-extra

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -1,11 +1,11 @@
 import { Command } from '@oclif/core'
 import { spawn } from 'child_process'
-import fse from 'fs-extra'
+import { existsSync } from 'fs-extra'
 import { tmpDir } from '../utils/directory'
 
 export default class Start extends Command {
   async run() {
-    if (!fse.existsSync(tmpDir)) {
+    if (!existsSync(tmpDir)) {
       throw Error(
         'The ".faststore" directory could not be found. If you are trying to serve your store, run "faststore build" first.'
       )
