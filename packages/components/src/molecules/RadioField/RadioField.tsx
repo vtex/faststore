90ad8281a3cc1935beb12aa14675de4369109f39
Fanny Chien <fanny.chien@vtex.com.br>
2022-11-11 16:06:42 +0800

    Feat: Adds `RadioField` component (#1513)
    * chore: Creates Label componente on fs/c
    
    * chore: Adjusts Label component on fs/ui
    
    * chore: Adjusts Label component import
    
    * docs: Updates Label doc page
    
    * chore: Adds RadioField component
    
    * chore: Adds RadioField styles
    
    * chore: Adds stories
    
    * docs: Adds RadioField documentation
    
    * fix: Fixing component name
    
    * docs: Adds related components section to Radio docs
    
    * docs: Adds sections dividers
    
    * fix: Updates import
    
    * docs: Updates preview image
    
    * Update packages/components/src/molecules/RadioField/RadioField.tsx
    
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com.br>
    
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com.br>

---
+++ packages/components/src/molecules/RadioField/RadioField.tsx
@@ -0,0 +1,42 @@
+import Label from '../../atoms/Label'
+import Radio from '../../atoms/Radio'
+import React, { forwardRef } from 'react'
+
+export type RadioFieldProps = {
+  /**
+   * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
+   */
+  testId?: string
+  /**
+   * ID to identify input and corresponding label.
+   */
+  id: string
+  /**
+   * The text displayed to identify the input radio.
+   */
+  label: string
+  /**
+   * The value to identify the input radio.
+   */
+  value?: string
+  /**
+   * Identify radio in the same group.
+   */
+  name?: string
+}
+
+const RadioField = forwardRef<HTMLDivElement, RadioFieldProps>(
+  function RadioField(
+    { testId = 'fs-radio-field', id, label, value, name, ...otherProps },
+    ref
+  ) {
+    return (
+      <div ref={ref} data-fs-radio-field data-testid={testId}>
+        <Radio id={id} value={value ?? label} name={name} {...otherProps} />
+        <Label htmlFor={id}>{label}</Label>
+      </div>
+    )
+  }
+)
+
+export default RadioField
