4713032c501fe44672f8255fbfa80e3983c3b18c
Eduardo Formiga <eduardo.formiga@gmail.com>
2022-11-17 13:02:10 -0300

    Feat: `Checkbox` component. (#1504)
    * feat: radio styles
    
    * feat: radio at components
    
    * feat: remove radio from ui
    
    * fix: using radio from components on radio group
    
    * fix: fsc dependency
    
    * fix: script name
    
    * Chore: Adds basic styles to Storybook (#1497)
    
    * chore: Begins to install styles dependencies for Storybook
    
    * chore: attempts to use styles (#1498)
    
    * chore: attempts to use styles
    
    * fix: remove stylesheet
    
    * chore: moves preset-scss to dev dependencies
    
    * Update packages/ui/src/styles/global.scss
    
    Co-authored-by: Emerson Laurentino <emerson@laurentino.co>
    
    * fix: Changes order
    
    * chore: Include `components` and `cli` packages
    
    * fix: Remove `cli` package
    
    * chore: removes unnecessary stylesheet
    
    Co-authored-by: Emerson Laurentino <emerson@laurentino.co>
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com.br>
    
    * fix: Attempts to fix path
    
    * chore: Adds custom breakpoints to storybook
    
    * chore: renames docs label
    
    * chore: Updates package.json
    
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com.br>
    Co-authored-by: Emerson Laurentino <emerson@laurentino.co>
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com.br>
    
    * chore: removes main from package.json (fix exports)
    
    * feat: adds Checkbox to components package
    
    * feat: Uses checkbox from components package
    
    * Adds SCSS loaders and include global scss to storybook
    
    * Adds Checkbox stories
    
    * Adds TS support to storybook
    
    * feat: Adds custom components to storybook
    
    * feat: adds tokens to checkbox stories
    
    * Trigger CI
    
    * chore: removes rootDir
    
    * feat: adds partial prop
    
    * feat: adds components semicolom
    
    * Revert "feat: adds tokens to checkbox stories"
    
    This reverts commit e09dc1395ddd78a603765ef6f554956d50ddba3b.
    
    * Revert "feat: Adds custom components to storybook"
    
    This reverts commit ca894e9a6042ed1a434a444bd1dbdce2d27d4292.
    
    * Revert "Adds TS support to storybook"
    
    This reverts commit 6fdcc12e4f5fff6325969d8901e17842a24e767e.
    
    * Revert "Adds Checkbox stories"
    
    This reverts commit 7b9dc71d76ae6d627760187ce3cde128c2673a84.
    
    * Revert "Adds SCSS loaders and include global scss to storybook"
    
    This reverts commit d80b5adba9f50ddf858350a4f4e683ff7f360788.
    
    * feat: removes checkbox stories
    
    * feat: adds checkbox docusaurus
    
    Co-authored-by: Emerson Laurentino <emerson@laurentino.co>
    Co-authored-by: Renata Motta <renata.motta@vtex.com.br>
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com.br>
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com.br>

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,12 +1,14 @@
 // Atoms
-export { default as Icon } from './atoms/Icon'
-export type { IconProps } from './atoms/Icon'
-export { default as Radio } from './atoms/Radio'
-export type { RadioProps } from './atoms/Radio'
 export { default as Badge } from './atoms/Badge'
 export type { BadgeProps } from './atoms/Badge'
+export { default as Checkbox } from './atoms/Checkbox'
+export type { CheckboxProps } from './atoms/Checkbox'
+export { default as Icon } from './atoms/Icon'
+export type { IconProps } from './atoms/Icon'
 export { default as Label } from './atoms/Label'
 export type { LabelProps } from './atoms/Label'
+export { default as Radio } from './atoms/Radio'
+export type { RadioProps } from './atoms/Radio'
 
 // Molecules
 export { default as RadioField } from './molecules/RadioField'
