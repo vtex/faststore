7284621aa3ba490881d493eb4d9883a9ae8210dc
Fanny <fanny.chien@vtex.com.br>
2022-11-18 16:47:53 +0800

    chore: Puts back fs/styles import for docs

--- apps/docs/src/theme/ReactLiveScope/index.tsx
+++ apps/docs/src/theme/ReactLiveScope/index.tsx
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-// import '../../../../../packages/styles'
+import '@faststore/styles'
 
 import React, { useEffect, useState } from 'react'
 import * as UI from '@faststore/ui'
