585822dd765085aee9e6dacb62f145c4d5db7406
Lucas Feijó <lucas.portela@vtex.com.br>
2022-11-30 10:42:10 -0300

    fix: CI errors

--- apps/docs/src/components/TokensTable/Table.tsx
+++ apps/docs/src/components/TokensTable/Table.tsx
@@ -8,7 +8,7 @@ interface TableProps<T extends object> {
   columns: Column<T>[]
   data: T[]
   filters: string[]
-  filter: string 
+  filter: string
 }
 
 export function Table<T extends { id: string }>({
@@ -16,7 +16,7 @@ export function Table<T extends { id: string }>({
   data,
   filters,
   filter,
-}: TableProps<T>): React.ReactElement {  
+}: TableProps<T>): React.ReactElement {
   const ourGlobalFilterFunction = useCallback(
     (rows: Row<T>[], ids: IdType<T>[], query: string) => {
       return matchSorter(rows, query, {
@@ -43,36 +43,42 @@ export function Table<T extends { id: string }>({
   )
 
   useEffect(() => {
-    setGlobalFilter(filter) 
+    setGlobalFilter(filter)
   }, [filter, setGlobalFilter])
 
   return (
     <table {...getTableProps()} className={styles.TokenTable}>
       <thead>
         <tr>
-        {headers.map((column:any) => (
-          <th {...column.getHeaderProps()}>
-            {column.render('Header')}
-          </th>
-        ))}
+          {headers.map((column: any, index) => (
+            <th key={index} {...column.getHeaderProps()}>
+              {column.render('Header')}
+            </th>
+          ))}
         </tr>
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map((row:any, i:any) => {
+        {rows.map((row: any, i: any) => {
           prepareRow(row)
           return (
             <tr key={i} {...row.getRowProps()}>
-              {row.cells.map((cell:any) => {
+              {row.cells.map((cell: any) => {
                 if (cell.column.id == 'example') {
                   return (
                     <td key={i}>
-                      <div style={JSON.parse(cell.value)} className="rounded-full w-8 h-8 border"/>
+                      <div
+                        style={JSON.parse(cell.value)}
+                        className="rounded-full w-8 h-8 border"
+                      />
                     </td>
                   )
                 } else if (cell.column.id !== 'type') {
                   return (
                     <td key={i} {...cell.getCellProps()}>
-                      <CopyButton code={(cell.value)} className={`flex items-center p-2 rounded hover:text-rebelPink float-left`}/>
+                      <CopyButton
+                        code={cell.value}
+                        className={`flex items-center p-2 rounded hover:text-rebelPink float-left`}
+                      />
                       <code>{cell.render('Cell')}</code>
                     </td>
                   )
