dc4fb1f382940b62fe93d10458d1331cc8165e30
renatamottam <renatamotta.m@gmail.com>
2022-12-05 12:10:21 -0300

    chore: Creates `SelectField` component on fs/components

--- packages/components/src/atoms/Select/Select.tsx
+++ packages/components/src/atoms/Select/Select.tsx
@@ -5,19 +5,34 @@ import { CaretDown } from '@faststore/ui/src/assets'
 
 export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   /**
+   * Redefines the id property to be required when using the Select component. The
+   * id will be used to link the UISelect component and its label.
+   */
+  id: string
+  /**
+   * Defines the options available in the select. The SelectOptions object
+   * keys are the property names, while the values correspond to the text that
+   * will be displayed in the UI.
+   */
+  options: Record<string, string>
+  /**
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
   testId?: string
 }
 
 const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select(
-  { children, testId = 'fs-select', ...otherProps }: SelectProps,
+  { options, id, testId = 'fs-select', ...otherProps }: SelectProps,
   ref
 ) {
   return (
     <div data-fs-select>
-      <select ref={ref} data-testid={testId} {...otherProps}>
-        {children}
+      <select id={id} ref={ref} data-testid={testId} {...otherProps}>
+        {Object.keys(options).map((key) => (
+          <option key={key} value={key}>
+            {options[key]}
+          </option>
+        ))}
       </select>
       <Icon data-fs-select-icon component={<CaretDown />}/>
     </div>
