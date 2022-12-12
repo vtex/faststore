574f93d2fb49131fe10c708da58f1e0dd2aea7ef
Arthur Andrade <arthurfelandrade@gmail.com>
2022-12-12 14:01:32 -0300

    Update packages/components/src/atoms/Link/Link.tsx
    Co-authored-by: Emerson Laurentino <emerson@laurentino.co>

--- packages/components/src/atoms/Link/Link.tsx
+++ packages/components/src/atoms/Link/Link.tsx
@@ -37,10 +37,9 @@ type LinkComponent = <C extends LinkElementType>(
 const Link: LinkComponent = forwardRef(function Link<
   C extends LinkElementType = 'a'
 >(
-  { as, children, variant = 'default', size="regular", inverse, testId = 'fs-link', ...otherProps }: LinkProps<C>,
+  { as: Component = "a", children, variant = 'default', size="regular", inverse, testId = 'fs-link', ...otherProps }: LinkProps<C>,
   ref?: PolymorphicRef<C>
 ) {
-  const Component = as ?? 'a'
 
   return (
     <Component 
