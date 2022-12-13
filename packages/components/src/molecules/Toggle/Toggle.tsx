cd69a7f6ff487d70ebd7606ac609112ae1c19565
Fanny <fanny.chien@vtex.com.br>
2022-12-13 12:49:10 +0800

    feat: Adds Toggle component

---
+++ packages/components/src/molecules/Toggle/Toggle.tsx
@@ -0,0 +1,65 @@
+import React, { forwardRef } from 'react'
+import type { InputHTMLAttributes } from 'react'
+import { Input, Label, Icon } from './../../'
+import { Checked } from '../../assets'
+
+export interface ToggleProps
+  extends InputHTMLAttributes<Omit<HTMLInputElement, 'disabled' | 'type'>> {
+  /**
+   * ID to identify input and corresponding label.
+   */
+  id: string
+  /**
+   * The text displayed to identify the input.
+   */
+  label: string
+  /**
+   * Controls whether the label will be displayed or not.
+   */
+  displayLabel?: boolean
+  /**
+   * Specifies that this input should be disabled.
+   */
+  disabled?: boolean
+  /**
+   * Controls the component's direction.
+   */
+  variant?: 'horizontal' | 'vertical'
+}
+
+const Toggle = forwardRef<HTMLInputElement, ToggleProps>(function Toggle(
+  {
+    id,
+    label,
+    disabled,
+    displayLabel = true,
+    variant = 'horizontal',
+    ...otherProps
+  }: ToggleProps,
+  ref
+) {
+  return (
+    <div data-fs-toggle data-fs-toggle-variant={variant}>
+      <div data-fs-toggle-container>
+        <Input
+          id={id}
+          ref={ref}
+          role="switch"
+          type="checkbox"
+          disabled={disabled}
+          {...otherProps}
+        />
+        <span data-fs-toggle-knob>
+          <Icon component={<Checked />} />
+        </span>
+      </div>
+      {displayLabel ?? (
+        <Label data-fs-label htmlFor={id}>
+          {label}
+        </Label>
+      )}
+    </div>
+  )
+})
+
+export default Toggle
