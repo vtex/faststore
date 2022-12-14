7c4beb085c893ad540e1a4440cc7b902fa36a75c
Renata Motta <renata.motta@vtex.com.br>
2022-12-14 09:41:42 -0300

    feat: Adds `Select` and `SelectField` components (#1547)
    > cherry-pick from https://github.com/vtex/faststore/pull/1529
    
    ## What's the purpose of this pull request?
    
    - [x] Moves `Select` component to @faststore/components 
    - [x] Moves `Select` styles on @faststore/ui
    - [x] Adds basic `Icon` styles on @faststore/ui
    - [x] Adds `Select` documentation page on Docusaurus
    - [x] Creates `Select Field` component on @faststore/components 
    - [x] Creates `Select Field` styles on @faststore/ui
    - [x] Adds `Select Field` documentation page on Docusaurus
    
    ## How it works?
    `Select` atom component is composed by `default select html tag` +
    `Icon`
    `Select Field` molecule component is composed by `Select` + `Label`
    
    ## How to test it?
    
    run yarn start in app/docs folder and checks the `Select` and
    `SelectField` components or test it using the doc preview link:
    -
    [Select](https://faststore-git-feat-select-components-2-fs-616-faststore.vercel.app/reference/ui/atoms/Select)
    -
    [SelectField](https://faststore-git-feat-select-components-2-fs-616-faststore.vercel.app/reference/ui/molecules/SelectField)
    
    ### Starters Deploy Preview
    See it on Next PR: https://github.com/vtex-sites/nextjs.store/pull/314
    
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com.br>

---
+++ packages/components/src/molecules/SelectField/SelectField.tsx
@@ -0,0 +1,28 @@
+import React, { forwardRef } from 'react'
+import type { PropsWithChildren } from 'react'
+import Select from '../../atoms/Select'
+import type {SelectProps} from '../../atoms/Select'
+import Label from '../../atoms/Label'
+
+export interface SelectFieldProps extends SelectProps {
+  /**
+   * Defines the text displayed in the label right next to the Select.
+   */
+  label: string
+}
+
+const SelectField = forwardRef<HTMLDivElement, PropsWithChildren<SelectFieldProps>>(
+  function SelectField(
+    { id, label, options, testId = 'fs-select-field', ...otherProps },
+    ref
+  ) {
+    return (
+      <div ref={ref} data-fs-select-field data-testid={testId}>
+        <Label data-fs-select-field-label htmlFor={id}>{label}</Label>
+        <Select id={id} options={options} {...otherProps} />
+      </div>
+    )
+  }
+)
+
+export default SelectField
