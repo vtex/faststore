61da0b5756e2ef64ce581abd9d99387ea110c4c9
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-12-16 15:51:52 -0300

    chore: makes HeroImage flexible

--- packages/components/src/organisms/Hero/HeroImage.tsx
+++ packages/components/src/organisms/Hero/HeroImage.tsx
@@ -3,34 +3,18 @@ import React, { forwardRef } from 'react'
 
 export interface HeroImageProps extends HTMLAttributes<HTMLDivElement> {
   /**
-   * Specifies the image URL.
-   */
-  imageSrc: string
-  /**
-   * Alternative description of the image.
-   */
-  imageAlt: string
-  /**
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
   testId?: string
 }
 
 const HeroImage = forwardRef<HTMLDivElement, HeroImageProps>(function HeroImage(
-  { imageAlt, imageSrc, children, testId = 'fs-hero-image', ...otherProps },
+  { children, testId = 'fs-hero-image', ...otherProps },
   ref
 ) {
   return (
     <div ref={ref} data-fs-hero-image data-testid={testId} {...otherProps}>
-      <img
-        data-fs-image
-        loading="eager"
-        src={imageSrc}
-        alt={imageAlt}
-        width={360}
-        height={240}
-        sizes="(max-width: 768px) 70vw, 50vw"
-      />
+      {children}
     </div>
   )
 })
