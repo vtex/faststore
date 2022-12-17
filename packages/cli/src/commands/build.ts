68f73122254a49bb74ed4dcbbc13af6f5f8daec8
Emerson Laurentino <emerson@laurentino.co>
2022-12-17 02:23:10 -0300

    fix: copy .next folder to userDir

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -1,16 +1,16 @@
 import { Command } from '@oclif/core'
 import { spawnSync } from 'child_process'
-import { tmpDir } from '../utils/directory'
+import { copy } from 'fs-extra'
+import { tmpDir, userDir } from '../utils/directory'
 import { generate } from '../utils/generate'
-
 export default class Build extends Command {
   async run() {
     await generate({ setup: true })
-
-    return spawnSync(`yarn build`, {
-     shell: true,
-     cwd: tmpDir,
-     stdio: 'inherit',
-   })
+    spawnSync(`yarn next build`, {
+      shell: true,
+      cwd: tmpDir,
+      stdio: 'inherit',
+    })
+    await copy(`${tmpDir}/.next`, `${userDir}/.next`)
   }
 }
