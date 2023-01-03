3dadd680e59f563362eac5b5108bc8ea4d013be1
Fanny <fanny.chien@vtex.com.br>
2023-01-03 23:10:59 +0800

    chore: Refactor OverviewSection components

--- apps/site/components/OverviewSection/OverviewSection.tsx
+++ apps/site/components/OverviewSection/OverviewSection.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import styles from './OverviewSection.module.css'
 
+type Direction = 'column' | 'row'
+
 export type OverviewSectionProps = {
   dark?: boolean
+  direction?: Direction
 }
 
-
-const OverviewSection = ({children, dark}) => {
+const OverviewSection = ({ children, dark, direction }) => {
   return (
-    <div className={styles.overviewSection} data-doc-overview-dark={dark}>
+    <div
+      className={styles.overviewSection}
+      data-doc-overview-dark={dark}
+      data-doc-overview-direction={direction}
+    >
       {children}
     </div>
   )
