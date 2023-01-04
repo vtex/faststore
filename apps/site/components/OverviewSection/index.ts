7423d2326321dce003bb0bc367e6cb16dbaf5903
Fanny <fanny.chien@vtex.com.br>
2023-01-04 23:20:33 +0800

    chore: Refactors OverviewSection

--- apps/site/components/OverviewSection/index.ts
+++ apps/site/components/OverviewSection/index.ts
@@ -1,2 +1,2 @@
 export { default as OverviewSection } from './OverviewSection'
-export { default as OverviewSectionRow } from './OverviewSectionRow'
+export { default as OverviewSectionItem } from './OverviewSectionItem'
