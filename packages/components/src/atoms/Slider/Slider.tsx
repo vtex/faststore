4aa65d113760d39407826fcf631986c6df24bbd8
Fanny <fanny.chien@vtex.com.br>
2023-01-13 00:21:38 +0800

    chore: Refactors Slider to add absolute values and labels

--- packages/components/src/atoms/Slider/Slider.tsx
+++ packages/components/src/atoms/Slider/Slider.tsx
@@ -101,6 +101,8 @@ const Slider = forwardRef<SliderRefType | undefined, SliderProps>(
       Math.round(min.absolute + maxPercent * widthPercent)
     )
 
+    const percentage = (value: number) => (value / max.absolute) * 100
+
     useImperativeHandle(ref, () => ({
       setSliderValues: (values: { min: number; max: number }) => {
         const sliderMinValue = Math.min(Number(values.min), maxVal)
@@ -120,61 +122,90 @@ const Slider = forwardRef<SliderRefType | undefined, SliderProps>(
     }))
 
     return (
-      <div data-fs-slider data-testid={testId} className={className}>
-        <div
-          data-fs-slider-range
-          style={{
-            left: `${minPercent}%`,
-            width: `${maxPercent - minPercent}%`,
-          }}
-        />
-        <input
-          type="range"
-          min={Math.round(min.absolute)}
-          max={Math.round(max.absolute)}
-          value={minVal}
-          step={step}
-          onMouseUp={() => onEnd?.({ min: minVal, max: maxVal })}
-          onTouchEnd={() => onEnd?.({ min: minVal, max: maxVal })}
-          onChange={(event) => {
-            const minValue = Math.min(Number(event.target.value), maxVal)
+      <div data-fs-slider>
+        <div data-fs-slider-absolute-values>
+          <span>{min.absolute}</span>
+          <span>{max.absolute}</span>
+        </div>
+        <div data-fs-slider-wrapper data-testid={testId} className={className}>
+          <div
+            data-fs-slider-range
+            style={{
+              left: `${minPercent}%`,
+              width: `${maxPercent - minPercent}%`,
+            }}
+          />
+          <input
+            type="range"
+            min={Math.round(min.absolute)}
+            max={Math.round(max.absolute)}
+            value={minVal}
+            step={step}
+            onMouseUp={() => onEnd?.({ min: minVal, max: maxVal })}
+            onTouchEnd={() => onEnd?.({ min: minVal, max: maxVal })}
+            onChange={(event) => {
+              const minValue = Math.min(Number(event.target.value), maxVal)
 
-            setMinVal(minValue)
-            setMinPercent(percent(minValue, min.absolute, max.absolute))
-            onChange?.({ min: minValue, max: maxVal })
-          }}
-          data-fs-slider-thumb="left"
-          aria-valuemin={min.absolute}
-          aria-valuemax={max.absolute}
-          aria-valuenow={minVal}
-          aria-label={String(minVal)}
-          aria-labelledby={getAriaValueText?.(minVal, 'min')}
-        />
-        {minValueLabelComponent && minValueLabelComponent(minVal)}
-        <input
-          type="range"
-          min={Math.round(min.absolute)}
-          max={Math.round(max.absolute)}
-          value={maxVal}
-          step={step}
-          onMouseUp={() => onEnd?.({ min: minVal, max: maxVal })}
-          onTouchEnd={() => onEnd?.({ min: minVal, max: maxVal })}
-          onChange={(event) => {
-            console.log(event)
-            const maxValue = Math.max(Number(event.target.value), minVal)
+              setMinVal(minValue)
+              setMinPercent(percent(minValue, min.absolute, max.absolute))
+              onChange?.({ min: minValue, max: maxVal })
+            }}
+            data-fs-slider-thumb="left"
+            aria-valuemin={min.absolute}
+            aria-valuemax={max.absolute}
+            aria-valuenow={minVal}
+            aria-label={String(minVal)}
+            aria-labelledby={getAriaValueText?.(minVal, 'min')}
+          />
+          {minValueLabelComponent && (
+            <span
+              data-fs-slider-label="min"
+              style={{
+                left: `calc(${percentage(minVal)}% + (${
+                  8 - percentage(minVal) * 0.2
+                }px))`,
+              }}
+            >
+              {minValueLabelComponent(minVal)}
+            </span>
+          )}
 
-            setMaxVal(maxValue)
-            setMaxPercent(percent(maxValue, min.absolute, max.absolute))
-            onChange?.({ min: minVal, max: maxValue })
-          }}
-          data-fs-slider-thumb="right"
-          aria-valuemin={min.absolute}
-          aria-valuemax={max.absolute}
-          aria-valuenow={maxVal}
-          aria-label={String(maxVal)}
-          aria-labelledby={getAriaValueText?.(maxVal, 'max')}
-        />
-        {maxValueLabelComponent && maxValueLabelComponent(maxVal)}
+          <input
+            type="range"
+            min={Math.round(min.absolute)}
+            max={Math.round(max.absolute)}
+            value={maxVal}
+            step={step}
+            onMouseUp={() => onEnd?.({ min: minVal, max: maxVal })}
+            onTouchEnd={() => onEnd?.({ min: minVal, max: maxVal })}
+            onChange={(event) => {
+              console.log(event)
+              const maxValue = Math.max(Number(event.target.value), minVal)
+
+              setMaxVal(maxValue)
+              setMaxPercent(percent(maxValue, min.absolute, max.absolute))
+              onChange?.({ min: minVal, max: maxValue })
+            }}
+            data-fs-slider-thumb="right"
+            aria-valuemin={min.absolute}
+            aria-valuemax={max.absolute}
+            aria-valuenow={maxVal}
+            aria-label={String(maxVal)}
+            aria-labelledby={getAriaValueText?.(maxVal, 'max')}
+          />
+          {maxValueLabelComponent && (
+            <span
+              data-fs-slider-label="max"
+              style={{
+                left: `calc(${percentage(maxVal)}% + (${
+                  8 - percentage(maxVal) * 0.2
+                }px))`,
+              }}
+            >
+              {maxValueLabelComponent(maxVal)}
+            </span>
+          )}
+        </div>
       </div>
     )
   }
