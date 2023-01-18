56add313e564fd21ca121e5f6690a65a4b6f3922
Eduardo Formiga <eduardo.formiga@gmail.com>
2023-01-18 16:25:29 -0300

    Update packages/components/src/assets/TagIcon.tsx
    Co-authored-by: Renata Motta <renata.motta@vtex.com.br>

--- packages/components/src/assets/TagIcon.tsx
+++ packages/components/src/assets/TagIcon.tsx
@@ -14,7 +14,7 @@ const TagIcon: FC = () => (
     <path
       d="M122.7,25.9,42,42,25.9,122.7a8,8,0,0,0,2.2,7.2L132.5,234.3a7.9,7.9,0,0,0,11.3,0l90.5-90.5a7.9,7.9,0,0,0,0-11.3L129.9,28.1A8,8,0,0,0,122.7,25.9Z"
       fill="none"
-      stroke="#000000"
+      stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth="16"
