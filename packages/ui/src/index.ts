7370103955e9796f63771b222812ee034d3df3b1
Fanny <fanny.chien@vtex.com.br>
2023-01-20 19:02:36 +0800

    fix: Fixes index

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -102,12 +102,6 @@ export type { PaymentMethodsProps } from './components/molecules/PaymentMethods'
 export { default as Breadcrumb } from './components/molecules/Breadcrumb'
 export type { BreadcrumbProps } from './components/molecules/Breadcrumb'
 
-export { default as LoadingButton } from './components/molecules/LoadingButton'
-export type { LoadingButtonProps } from './components/molecules/LoadingButton'
-
-export { default as PriceRange } from './components/molecules/PriceRange'
-export type { PriceRangeProps } from './components/molecules/PriceRange'
-
 export { default as Form } from './components/molecules/Form'
 export type { FormProps } from './components/molecules/Form'
 
