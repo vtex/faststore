90abeba53dbfe4b7327857dcdcc1b1623e6ffb15
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-01-23 19:49:18 -0300

    chore: copies .next folder to user folder

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -1,16 +1,37 @@
 import { Command } from '@oclif/core'
+import chalk from 'chalk'
 import { spawnSync } from 'child_process'
-import { tmpDir } from '../utils/directory'
+import { existsSync } from 'fs'
+import { copy, removeSync } from 'fs-extra'
+import { tmpDir, userDir } from '../utils/directory'
 import { generate } from '../utils/generate'
 
 export default class Build extends Command {
   async run() {
     await generate({ setup: true })
 
-    return spawnSync(`yarn build`, {
-     shell: true,
-     cwd: tmpDir,
-     stdio: 'inherit',
-   })
+    spawnSync(`yarn build`, {
+      shell: true,
+      cwd: tmpDir,
+      stdio: 'inherit',
+    })
+    await copyFolder(`${tmpDir}/.next`, `${userDir}/.next`)
+  }
+}
+
+async function copyFolder(from: string, to: string) {
+  try {
+    if (existsSync(to)) {
+      removeSync(to)
+    }
+
+    await copy(from, to)
+    console.log(
+      `${chalk.green('success')} - Folder ${chalk.dim(
+        from
+      )} copied to ${chalk.dim(to)}`
+    )
+  } catch (err) {
+    console.error(`${chalk.red('error')} - ${err}`)
   }
 }
