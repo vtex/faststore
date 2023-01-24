457fa59c34bf4509d87945b06d35b3b0b896e64e
renatamottam <renatamotta.m@gmail.com>
2023-01-24 11:52:53 -0300

    feat: Adds `ProductCard` props

--- packages/components/src/molecules/ProductCard/ProductCard.tsx
+++ packages/components/src/molecules/ProductCard/ProductCard.tsx
@@ -3,26 +3,44 @@ import type { HTMLAttributes } from 'react'
 
 export interface ProductCardProps extends HTMLAttributes<HTMLDivElement> {
   /**
+   * Sets a border to the component.
+   */
+  bordered?: boolean
+  /**
+   * Sets the component's size.
+   */
+  variant?: 'wide' | 'default'
+  /**
    * ID to find this component in testing tools (e.g.: cypress,
    * testing-library, and jest).
    */
   testId?: string
 }
 
-const ProductCard = forwardRef<HTMLDivElement, ProductCardProps>(function Card(
-  { testId = 'fs-product-card', children, ...otherProps },
-  ref
-) {
-  return (
-    <article
-      ref={ref}
-      data-fs-product-card
-      data-testid={testId}
-      {...otherProps}
-    >
-      {children}
-    </article>
-  )
-})
+const ProductCard = forwardRef<HTMLDivElement, ProductCardProps>(
+  function ProductCard(
+    {
+      testId = 'fs-product-card',
+      variant = 'default',
+      bordered = false,
+      children,
+      ...otherProps
+    },
+    ref
+  ) {
+    return (
+      <article
+        ref={ref}
+        data-fs-product-card
+        data-fs-product-card-variant={variant}
+        data-fs-product-card-bordered={bordered}
+        data-testid={testId}
+        {...otherProps}
+      >
+        {children}
+      </article>
+    )
+  }
+)
 
 export default ProductCard
