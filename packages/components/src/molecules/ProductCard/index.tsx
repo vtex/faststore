67b4d364dffa750a1df49c317aa4e55e2debac15
renatamottam <renatamotta.m@gmail.com>
2023-01-24 11:52:28 -0300

    chore: Deletes ProductCardActions

--- packages/components/src/molecules/ProductCard/index.tsx
+++ packages/components/src/molecules/ProductCard/index.tsx
@@ -6,6 +6,3 @@ export type { ProductCardImageProps } from './ProductCardImage'
 
 export { default as ProductCardContent } from './ProductCardContent'
 export type { ProductCardContentProps } from './ProductCardContent'
-
-export { default as ProductCardActions } from './ProductCardActions'
-export type { ProductCardActionsProps } from './ProductCardActions'
