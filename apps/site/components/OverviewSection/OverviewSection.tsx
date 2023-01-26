01f3ce2d6071a8fcb8b07de0930f0b362df734d6
renatamottam <renatamotta.m@gmail.com>
2023-01-26 17:02:29 -0300

    docs: Updates `OverviewSection` to receive external styles

--- apps/site/components/OverviewSection/OverviewSection.tsx
+++ apps/site/components/OverviewSection/OverviewSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import styles from './OverviewSection.module.css'
 
 type Direction = 'column' | 'row'
@@ -7,15 +7,23 @@ export type OverviewSectionProps = {
   dark?: boolean
   direction?: Direction
   bigGap?: boolean
+  containerStyle?: CSSProperties
 }
 
-const OverviewSection = ({ children, dark, direction, bigGap }) => {
+const OverviewSection = ({
+  children,
+  dark,
+  direction,
+  containerStyle,
+  bigGap,
+}) => {
   return (
     <div
       className={styles.overviewSection}
       data-doc-overview-dark={dark}
       data-doc-overview-direction={direction}
       data-doc-overview-big-gap={bigGap}
+      style={containerStyle}
     >
       {children}
     </div>
