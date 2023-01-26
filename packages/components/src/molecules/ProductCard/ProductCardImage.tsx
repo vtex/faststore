01df5f87f396457c7dd7e7c78aae41cf4bf6a884
renatamottam <renatamotta.m@gmail.com>
2023-01-26 18:11:13 -0300

    feat: Adds missing props on `ProductCard`

--- packages/components/src/molecules/ProductCard/ProductCardImage.tsx
+++ packages/components/src/molecules/ProductCard/ProductCardImage.tsx
@@ -6,11 +6,20 @@ export interface ProductCardImageProps extends HTMLAttributes<HTMLDivElement> {
    * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
    */
   testId?: string
+  /**
+   * Specifies the ProductCard image's aspect ratio.
+   */
+  aspectRatio?: number
 }
 
 const ProductCardImage = forwardRef<HTMLDivElement, ProductCardImageProps>(
   function ProductCardImage(
-    { testId = 'fs-product-card-image', children, ...otherProps },
+    {
+      testId = 'fs-product-card-image',
+      aspectRatio = 1,
+      children,
+      ...otherProps
+    },
     ref
   ) {
     return (
@@ -18,6 +27,11 @@ const ProductCardImage = forwardRef<HTMLDivElement, ProductCardImageProps>(
         ref={ref}
         data-fs-product-card-image
         data-testid={testId}
+        style={
+          {
+            '--fs-product-card-image-aspect-ratio': aspectRatio
+          } as React.CSSProperties
+        }
         {...otherProps}
       >
         {children}
