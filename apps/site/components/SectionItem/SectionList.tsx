6baa4af04a94efaa9fb0b004b618172ed0a0b295
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-02-13 12:48:17 -0300

    chore: adds tokensLinks css

--- apps/site/components/SectionItem/SectionList.tsx
+++ apps/site/components/SectionItem/SectionList.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
 import type { PropsWithChildren } from 'react'
 import styles from './SectionItem.module.css'
 
 export type SectionListProps = {
   grid?: 'row' | 'column'
+  classes?: string
 }
 
 const SectionList = ({
   children,
   grid = 'row',
+  classes,
 }: PropsWithChildren<SectionListProps>) => {
+  const scopedClasses =
+    classes?.split(' ').map((klass) => styles[klass] ?? '') ?? []
+  const stylizedClasses = scopedClasses.reduce(
+    (acc, klass) => `${acc} ${klass}`,
+    ''
+  )
+
   return (
-    <section className={styles.sectionList}>
-      <ul data-doc-section-grid={grid}>
-        {children}
-      </ul>
+    <section className={`${styles.sectionList} ${stylizedClasses}`}>
+      <ul data-doc-section-grid={grid}>{children}</ul>
     </section>
   )
 }
