95714aaa2647ddb54f38aa716194460b50375f3a
Arthur Andrade <arthurfelandrade@gmail.com>
2023-03-15 16:11:35 -0300

    Feat/SKU selector fs 631 new approach (#1667)
    ## What's the purpose of this pull request?
    - [x] Moves `SkuSelector` component to @faststore/components 
    - [x] Adds `SkuSelector` styles on @faststore/ui
    - [x] Review `SkuSelector` structure
     
    ## How to test it?
    
    run `yarn dev` and checks the `SkuSelector` component or test it using
    the doc preview link:
    - [SkuSelector
    Component](https://faststore-site-git-feat-sku-selector-fs-631-ne-3ebef2-faststore.vercel.app/components/molecules/sku-selector)
    
    ### Starter PR
    [chore: Updates version with
    SkuSelector](https://github.com/vtex-sites/starter.store/pull/16)
    
    ---------
    
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- packages/core/src/components/ui/SkuSelector/Selectors.tsx
+++ packages/core/src/components/ui/SkuSelector/Selectors.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes, useCallback, useMemo } from 'react'
 
 import { Image } from '../Image'
 import {
@@ -10,6 +10,7 @@ import type { SkuVariantsByName } from './SkuSelectorsContext'
 import { SkuSelectorsContext } from './SkuSelectorsContext'
 
 import NextLink from 'next/link'
+import { getSkuSlug } from './skuVariants'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   /**
@@ -24,19 +25,6 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
    * SKU property values for the current SKU.
    */
   activeVariations: Record<string, string>
-  /**
-   * Name of the property that's considered **dominant**. Which means that all
-   * other varying properties will be filtered according to the current value
-   * of this property.
-   *
-   * Ex: If `Red` is the current value for the 'Color' variation, we'll only
-   * render possible values for 'Size' that are available in `Red`.
-   */
-  dominantVariation: string
-  /**
-   * Function that determines the href string.
-   */
-  getItemHref: (option: SkuOption) => string
 }
 
 const ImageComponent: SkuSelectorProps['ImageComponent'] = ({
@@ -57,15 +45,28 @@ const ImageComponent: SkuSelectorProps['ImageComponent'] = ({
 function Selectors({
   slugsMap,
   activeVariations,
-  dominantVariation,
   availableVariations,
-  getItemHref,
   ...otherProps
 }: Props) {
-  // `dominantVariation` variants are singled-out here because they will always
-  // be rendered as 'image' variants.
-  const { [dominantVariation]: dominantOptions, ...otherSkuVariants } =
-    availableVariations
+  // `dominantVariation` will be ever the first variant
+  const [dominantVariation] = useMemo(() => {
+    return Object.keys(activeVariations)
+  }, [activeVariations])
+
+  const getItemHref = useCallback(
+    (option: SkuOption) => {
+      const currentItemHref = `/${getSkuSlug(
+        slugsMap,
+        {
+          ...activeVariations,
+          [dominantVariation]: option.value,
+        },
+        dominantVariation
+      )}/p`
+      return currentItemHref
+    },
+    [activeVariations, dominantVariation, slugsMap]
+  )
 
   return (
     <section {...otherProps}>
@@ -77,28 +78,13 @@ function Selectors({
           dominantProperty: dominantVariation,
         }}
       >
-        {dominantOptions && (
-          <UISkuSelector
-            variant="color"
-            skuPropertyName={dominantVariation}
-            options={dominantOptions}
-            // ImageComponent={ImageComponent}
-            activeVariations={activeVariations}
-            linkProps={{
-              as: NextLink,
-              legacyBehavior: false,
-            }}
-            getItemHref={getItemHref}
-          />
-        )}
-        {otherSkuVariants &&
-          Object.keys(otherSkuVariants).map((skuVariant) => (
+        {availableVariations &&
+          Object.keys(availableVariations).map((skuVariant) => (
             <UISkuSelector
               key={skuVariant}
-              variant="label"
               skuPropertyName={skuVariant}
-              options={otherSkuVariants[skuVariant]}
-              // ImageComponent={ImageComponent}
+              options={availableVariations[skuVariant]}
+              ImageComponent={ImageComponent}
               activeVariations={activeVariations}
               linkProps={{
                 as: NextLink,
