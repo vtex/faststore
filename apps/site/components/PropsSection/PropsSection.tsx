06db12c7e77812571bbd4c561f9d6f0e83fdaa76
renatamottam <renatamotta.m@gmail.com>
2023-03-15 20:31:16 -0300

    feat: Updates `PropsSection`

--- apps/site/components/PropsSection/PropsSection.tsx
+++ apps/site/components/PropsSection/PropsSection.tsx
@@ -1,9 +1,58 @@
 import React from 'react'
 
+import styles from './props-section.module.css'
 
-const PropsSection = () => {
+export interface RowItem {
+  name: string
+  type: string | []
+  description: string
+  default: string
+  required: boolean
+}
+
+export interface PropsSectionProps {
+  propsList?: RowItem[]
+}
+
+const PropsSection = ({ propsList }: PropsSectionProps) => {
   return (
-    <i>Coming Soon</i>
+    <table className={styles.propsSection}>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Description</th>
+          <th>Default</th>
+        </tr>
+      </thead>
+      <tbody>
+        {propsList.map((prop) => {
+          return (
+            <tr key={prop.name}>
+              <td data-nx-props-section-name>
+                <strong>
+                  {prop.name}
+                  {prop.required && <span>*</span>}
+                </strong>
+              </td>
+              <td data-nx-props-section-type>
+                {prop.type instanceof Array ? (
+                  <span>
+                    {prop.type.map((option) => {
+                      return <code key={option}>{option}</code>
+                    })}
+                  </span>
+                ) : (
+                  <code>{prop.type}</code>
+                )}
+              </td>
+              <td>{prop.description}</td>
+              <td>{prop.default && <code>{prop.default}</code>}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
 
