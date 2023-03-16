a27381ca900f2dbd5060014a6d3968a3b9026ae8
renatamottam <renatamotta.m@gmail.com>
2023-03-16 13:52:56 -0300

    feat: Updates `PropsSection` styles

--- apps/site/components/PropsSection/PropsSection.tsx
+++ apps/site/components/PropsSection/PropsSection.tsx
@@ -16,49 +16,59 @@ export interface PropsSectionProps {
 
 const PropsSection = ({ propsList }) => {
   return (
-    <table className={styles.propsSection}>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Type</th>
-          <th>Description</th>
-          <th>Default</th>
-        </tr>
-      </thead>
-      <tbody>
-        {propsList ? (
-          propsList.map((prop) => {
-            return (
-              <tr key={prop.name}>
-                <td data-nx-props-section-name>
-                  <strong>
-                    {prop.name}
-                    {prop.required && <span>*</span>}
-                  </strong>
-                </td>
-                <td data-nx-props-section-type>
-                  {prop.type instanceof Array ? (
-                    <span>
-                      {prop.type.map((option) => {
-                        return <code key={option}>{option}</code>
-                      })}
-                    </span>
-                  ) : (
-                    <code>{prop.type}</code>
-                  )}
-                </td>
-                <td>{prop.description}</td>
-                <td>{prop.default && <code>{prop.default}</code>}</td>
-              </tr>
-            )
-          })
-        ) : (
+    <section className={styles.propsSection}>
+      <table>
+        <colgroup>
+          <col data-nx-props-section-name />
+          <col data-nx-props-section-type />
+          <col data-nx-props-section-description />
+          <col data-nx-props-section-default />
+        </colgroup>
+        <thead>
           <tr>
-            <td>Coming Soon</td>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Description</th>
+            <th>Default</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {propsList ? (
+            propsList.map((prop) => {
+              return (
+                <tr key={prop.name}>
+                  <td data-nx-props-section-name>
+                    <strong>
+                      {prop.name}
+                      {prop.required && <span title="Required">*</span>}
+                    </strong>
+                  </td>
+                  <td data-nx-props-section-type>
+                    {prop.type instanceof Array ? (
+                      <span>
+                        {prop.type.map((option) => {
+                          return <code key={option}>{option}</code>
+                        })}
+                      </span>
+                    ) : (
+                      <code>{prop.type}</code>
+                    )}
+                  </td>
+                  <td data-nx-props-section-description>{prop.description}</td>
+                  <td data-nx-props-section-default>
+                    {prop.default && <code>{prop.default}</code>}
+                  </td>
+                </tr>
+              )
+            })
+          ) : (
+            <tr>
+              <td>Coming Soon</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </section>
   )
 }
 
