11d2612121d6617604ec41165796f8553a5e562b
Fanny <fanny.chien@vtex.com.br>
2023-03-22 11:41:35 -0300

    docs: Updates paths and search input doc page

---
+++ apps/site/components/Search/SearchInputUsage.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { SearchInputField, SearchInput, SearchProvider } from '@faststore/ui'
+import SearchDropdownUsage from 'site/components/Search/SearchDropdownUsage'
+import { product } from 'site/mocks/product'
+
+const SearchInputUsage = () => {
+  const [searchDropdownVisible, setSearchDropdownVisible] = useState(false)
+  const [term, setTerm] = useState('')
+  function onChangeValue(e) {
+    setSearchDropdownVisible(true)
+    if (e.target.value == '') {
+      setTerm('')
+    } else {
+      setTerm('App')
+    }
+  }
+  function onClickButton() {
+    setSearchDropdownVisible(false)
+  }
+  return (
+    <SearchProvider
+      term={term}
+      terms={[
+        {
+          value: 'apple',
+        },
+        {
+          value: 'apple magic mouse',
+        },
+      ]}
+      isLoading={false}
+      products={[product]}
+    >
+      <SearchInput
+        visibleDropdown={searchDropdownVisible}
+        term={term}
+        terms={[
+          {
+            value: 'apple',
+          },
+          {
+            value: 'apple magic mouse',
+          },
+        ]}
+        products={[product]}
+        isLoading={false}
+        style={{
+          width: '100%',
+        }}
+      >
+        <SearchInputField
+          placeholder="Search everything at the store"
+          onFocus={() => setSearchDropdownVisible(true)}
+          onSubmit={() => setSearchDropdownVisible(false)}
+          onChange={(e) => onChangeValue(e)}
+          buttonProps={{
+            onClick: onClickButton,
+          }}
+        />
+        {searchDropdownVisible && <SearchDropdownUsage term={term} />}
+      </SearchInput>
+    </SearchProvider>
+  )
+}
+
+export default SearchInputUsage
