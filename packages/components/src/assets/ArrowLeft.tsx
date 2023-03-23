64bb1be7263e94951fac5787e7a6aba1fdf08287
renatamottam <renatamotta.m@gmail.com>
2023-03-22 21:00:48 -0300

    feat: Updates `ArrowLeft` icon

--- packages/components/src/assets/ArrowLeft.tsx
+++ packages/components/src/assets/ArrowLeft.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import type { FC } from 'react'
+import { IconProps } from './IconProps'
 
 // Icon from Phosphor Icons
-const ArrowLeft: FC = () => (
+const ArrowLeft = ({ size = 24 }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="currentColor"
     viewBox="0 0 256 256"
     strokeWidth="16"
-    width={24}
-    height={24}
+    width={size}
+    height={size}
   >
     <path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path>
   </svg>
