72d0522f092cc8390e6fb4c46b4f1c57a55ce5d0
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-04-11 21:05:01 -0300

    chore: removes global components styles

--- packages/core/src/pages/_app.tsx
+++ packages/core/src/pages/_app.tsx
@@ -1,17 +1,15 @@
 // FastStore UI's base styles
 import '@faststore/ui/src/styles/global.scss'
 
-import '../styles/global/components.scss'
-
 import '../customizations/themes/index.scss'
 
-import NextNProgress from 'nextjs-progressbar'
 import type { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar'
 
+import { UIProvider } from '@faststore/ui'
 import Layout from 'src/Layout'
 import AnalyticsHandler from 'src/sdk/analytics'
 import ErrorBoundary from 'src/sdk/error/ErrorBoundary'
-import { UIProvider } from '@faststore/ui'
 
 function App({ Component, pageProps }: AppProps) {
   return (
