2f93a2ea09cca380ab80a0734fb187fcd000074e
Fanny <fanny.chien@vtex.com.br>
2023-04-13 16:10:58 -0300

    feat: Adds new props to EmptyState

--- packages/components/src/organisms/EmptyState/EmptyState.tsx
+++ packages/components/src/organisms/EmptyState/EmptyState.tsx
@@ -1,25 +1,42 @@
-import React, { ReactNode } from 'react'
-import type { PropsWithChildren } from 'react'
+import React from 'react'
+import type { PropsWithChildren, ReactNode } from 'react'
 
 type Variant = 'default' | 'rounded'
 
 export interface EmptyStateProps {
   /**
+   * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
+   */
+  testId?: string
+  /**
+   * The EmptyState component's title.
+   */
+  title?: string
+  /**
+   * A React component that will be rendered as an icon on the Title.
+   */
+  titleIcon?: ReactNode
+  /**
    * Specifies the component border variant.
    */
   variant?: Variant
-  title?: string
-  titleIcon?: ReactNode
 }
 
 function EmptyState({
+  testId = 'fs-empty-state',
   variant = 'default',
   title,
   titleIcon,
   children,
+  ...otherProps
 }: PropsWithChildren<EmptyStateProps>) {
   return (
-    <section data-fs-empty-state data-fs-empty-state-variant={variant}>
+    <section
+      data-fs-empty-state
+      data-fs-empty-state-variant={variant}
+      data-testid={testId}
+      {...otherProps}
+    >
       {title && (
         <header data-fs-empty-state-title>
           {titleIcon && <>{titleIcon}</>}
