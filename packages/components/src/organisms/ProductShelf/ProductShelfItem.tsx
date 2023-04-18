6a387a665aa8a1eb65122abcce67d82b9ead4234
Renata Motta <renata.motta@vtex.com.br>
2023-04-18 15:20:26 -0300

    Feat: Add `ProductShelf` component (#1713)
    ## What's the purpose of this pull request?
    `fs/components`
    - [x] Creates `ProductShelf` component
    
    `fs/ui`
    - [x] Moves `ProductShelf` styles
    
    `fs/core`
    - [x] Review `ProductShelf` structure
    
    ## How to test it?
    
    #### You can test the usage of this component in the `faststore/core`
    application:
    1. Run `yarn dev`
    2. Check the `Homepage` page
    
    #### Or test it using the doc preview link:
    [ProductShelf](https://faststore-site-git-feat-product-shelf-fs-777-faststore.vercel.app/components/organisms/product-shelf)
    
    
    ### Starters Deploy Preview
    - [Starter PR](https://github.com/vtex-sites/starter.store/pull/47)
    - [Store
    Preview](https://evergreen-git-feat-product-shelf-fs-777-faststore.vercel.app/)
    
    ---------
    
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com.br>
    Co-authored-by: Lucas Feijó <lucasfjportela@gmail.com>
    Co-authored-by: Arthur Andrade <arthurfelandrade@gmail.com>

---
+++ packages/components/src/organisms/ProductShelf/ProductShelfItem.tsx
@@ -0,0 +1,24 @@
+import type { HTMLAttributes } from 'react'
+import React, { forwardRef } from 'react'
+
+export interface ProductShelfItemProps extends HTMLAttributes<HTMLLIElement> {
+  /**
+   * ID to find this component in testing tools (e.g.: Cypress, Testing Library, and Jest).
+   */
+  testId?: string
+}
+
+const ProductShelfItem = forwardRef<HTMLLIElement, ProductShelfItemProps>(
+  function ProductShelfItem(
+    { testId = 'fs-product-shelf-item', children, ...otherProps },
+    ref
+  ) {
+    return (
+      <li ref={ref} data-fs-product-shelf-item data-testid={testId} {...otherProps}>
+        {children}
+      </li>
+    )
+  }
+)
+
+export default ProductShelfItem
