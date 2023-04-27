d833250f6e7c2863547022dec6eb6bf095918406
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-04-27 14:13:36 -0300

    chore: imports nextra styles after global

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
+
+import '../styles/_global.scss'
 import '../styles/custom-nextra.css'
-import '../styles/global.scss'
 
 export default function Nextra({
   Component,
