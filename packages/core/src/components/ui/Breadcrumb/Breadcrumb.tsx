d3aa16f74be9e40f72f5d7746d4b97500a51cc50
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-05-05 16:37:45 -0300

    chore: Uses props in breadcrumb core component

--- packages/core/src/components/ui/Breadcrumb/Breadcrumb.tsx
+++ packages/core/src/components/ui/Breadcrumb/Breadcrumb.tsx
@@ -1,31 +1,28 @@
 import type { BreadcrumbProps as UIBreadcrumbProps } from '@faststore/ui'
-import { Breadcrumb as UIBreadcrumb, Icon } from '@faststore/ui'
+import { Icon, Breadcrumb as UIBreadcrumb } from '@faststore/ui'
 import { memo } from 'react'
 
 import Link from 'src/components/ui/Link'
 
-type ItemElement = {
-  item: string
-  name: string
-  position: number
-}
-export interface BreadcrumbProps extends UIBreadcrumbProps {
-  /**
-   * Array of ItemElement that represents each breadcrumb item.
-   */
-  breadcrumbList: ItemElement[]
+interface BreadcrumbProps extends UIBreadcrumbProps {
+  icon: string
+  alt: string
 }
 
-interface BaseBreadcrumbProps extends BreadcrumbProps {
-  isDesktop?: boolean
-}
-
-const Breadcrumb = ({ breadcrumbList, ...otherProps }: BaseBreadcrumbProps) => (
+const Breadcrumb = ({
+  icon = 'Home',
+  alt = 'Go to homepage',
+  ...otherProps
+}: BreadcrumbProps) => (
   <UIBreadcrumb
-    breadcrumbList={breadcrumbList}
     homeLink={
-      <Link aria-label="Go to homepage" href="/">
-        <Icon name="House" width={18} height={18} weight="bold" />
+      <Link
+        data-fs-breadcrumb-link
+        data-fs-breadcrumb-link-home
+        aria-label={`${alt}`}
+        href="/"
+      >
+        <Icon name={`${icon}`} width={18} height={18} weight="bold" />
       </Link>
     }
     renderLink={({ itemProps: { item: link, name } }) => (
