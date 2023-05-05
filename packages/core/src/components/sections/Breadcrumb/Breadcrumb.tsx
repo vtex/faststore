df71b0788e4cb985cc6765671dcf5a13de36fda1
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-05-05 16:38:22 -0300

    chore: Uses props in breadcrumb core Section

--- packages/core/src/components/sections/Breadcrumb/Breadcrumb.tsx
+++ packages/core/src/components/sections/Breadcrumb/Breadcrumb.tsx
@@ -1,26 +1,32 @@
 import { memo } from 'react'
 
 import type { BreadcrumbProps } from 'src/components/ui/Breadcrumb'
-import UIBreadcrumb from 'src/components/ui/Breadcrumb'
+import Breadcrumb from 'src/components/ui/Breadcrumb'
 
 import Section from '../Section'
 
 import styles from './section.module.scss'
 
-interface BreadcrumbWrapperProps
+interface BreadcrumbSectionProps
   extends Partial<Pick<BreadcrumbProps, 'breadcrumbList'>> {
   name: string
+  icon: string
+  alt: string
 }
 
-function Breadcrumb({ breadcrumbList, name }: BreadcrumbWrapperProps) {
+function BreadcrumbSection({
+  breadcrumbList,
+  name,
+  ...otherProps
+}: BreadcrumbSectionProps) {
   const fallback = [{ item: '/', name, position: 1 }]
   const list = breadcrumbList ?? fallback
 
   return (
     <Section className={`${styles.section} section-breadcrumb layout__content`}>
-      <UIBreadcrumb breadcrumbList={list} />
+      <Breadcrumb breadcrumbList={list} {...otherProps} />
     </Section>
   )
 }
 
-export default memo(Breadcrumb)
+export default memo(BreadcrumbSection)
