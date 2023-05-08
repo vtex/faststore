83a9c42f609f59c0286ba578815ef6725607dcb8
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-05-08 12:51:04 -0300

    chore: adds context to Breadcrumb section

--- packages/core/src/components/sections/Breadcrumb/Breadcrumb.tsx
+++ packages/core/src/components/sections/Breadcrumb/Breadcrumb.tsx
@@ -1,30 +1,43 @@
 import { memo } from 'react'
 
-import type { BreadcrumbProps } from 'src/components/ui/Breadcrumb'
 import Breadcrumb from 'src/components/ui/Breadcrumb'
 
 import Section from '../Section'
 
+import {
+  ProductDetailsFragment_ProductFragment,
+  ServerCollectionPageQueryQuery,
+} from '@generated/graphql'
 import styles from './section.module.scss'
 
-interface BreadcrumbSectionProps
-  extends Partial<Pick<BreadcrumbProps, 'breadcrumbList'>> {
-  name: string
+type BreadcrumbContext =
+  | ProductDetailsFragment_ProductFragment
+  | ServerCollectionPageQueryQuery['collection']
+
+interface BreadcrumbSectionProps {
+  context?: BreadcrumbContext
   icon: string
   alt: string
 }
 
-function BreadcrumbSection({
-  breadcrumbList,
-  name,
-  ...otherProps
-}: BreadcrumbSectionProps) {
-  const fallback = [{ item: '/', name, position: 1 }]
-  const list = breadcrumbList ?? fallback
+const isProduct = (x: any): x is ProductDetailsFragment_ProductFragment =>
+  !!x?.sku
+const isCollection = (
+  x: any
+): x is ServerCollectionPageQueryQuery['collection'] => !!x?.seo
+
+function BreadcrumbSection({ context, ...otherProps }: BreadcrumbSectionProps) {
+  const title = isCollection(context) ? context?.seo?.title : 'AllProducts'
+  const fallback = [{ item: '/', name: title, position: 1 }]
+  const breadcrumbList = isProduct(context)
+    ? context?.breadcrumbList?.itemListElement
+    : isCollection(context)
+    ? context?.breadcrumbList?.itemListElement
+    : fallback
 
   return (
     <Section className={`${styles.section} section-breadcrumb layout__content`}>
-      <Breadcrumb breadcrumbList={list} {...otherProps} />
+      <Breadcrumb breadcrumbList={breadcrumbList} {...otherProps} />
     </Section>
   )
 }
