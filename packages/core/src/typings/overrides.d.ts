ae9cfe2d998e93fefee1ef5b981096671bd301ce
Fanny <fanny.chien@vtex.com.br>
2023-05-08 18:35:01 -0300

    chore: Adds new overridable sections

--- packages/core/src/typings/overrides.d.ts
+++ packages/core/src/typings/overrides.d.ts
@@ -1,18 +1,20 @@
 export const SECTIONS = {
-    ProductDetails: { components: ["Price"] },
-    ProductShelf: { components: ["ProductCard"] }
-  } as const;
-  
-  // export type ComponentOrProps =
-  //   | { Component: React.ElementType }
-  //   | { props: Record<string, unknown> };
-  
-  export type SectionOverride = {
-    [K in keyof typeof SECTIONS]: {
-      name: K;
-      components: {
-        [ComponentKey in typeof SECTIONS[K]["components"][number]]?: React.ElementType;
-      };
-    };
-  };
-  
+  ProductDetails: { components: ['Price'] },
+  ProductShelf: { components: ['ProductCard'] },
+  Hero,
+  BannerText,
+  Newsletter,
+} as const
+
+// export type ComponentOrProps =
+//   | { Component: React.ElementType }
+//   | { props: Record<string, unknown> };
+
+export type SectionOverride = {
+  [K in keyof typeof SECTIONS]: {
+    name: K
+    components: {
+      [ComponentKey in (typeof SECTIONS)[K]['components'][number]]?: React.ElementType
+    }
+  }
+}
