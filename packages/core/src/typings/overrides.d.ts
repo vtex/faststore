67bcfba7b9ff4a18ccc062189469adb51fdc4b2d
Fanny <fanny.chien@vtex.com.br>
2023-05-10 16:11:37 -0300

    chore: Removes changes

--- packages/core/src/typings/overrides.d.ts
+++ packages/core/src/typings/overrides.d.ts
@@ -1,20 +1,17 @@
 export const SECTIONS = {
   ProductDetails: { components: ['Price'] },
   ProductShelf: { components: ['ProductCard'] },
-  Hero,
-  BannerText,
-  Newsletter,
 } as const
 
-export type ComponentOrProps =
-  | { Component: React.ElementType }
-  | { props: Record<string, unknown> }
+// export type ComponentOrProps =
+//   | { Component: React.ElementType }
+//   | { props: Record<string, unknown> };
 
 export type SectionOverride = {
   [K in keyof typeof SECTIONS]: {
     name: K
     components: {
-      [ComponentKey in (typeof SECTIONS)[K]['components'][number]]?: ComponentOrProps
+      [ComponentKey in (typeof SECTIONS)[K]['components'][number]]?: React.ElementType
     }
   }
 }
