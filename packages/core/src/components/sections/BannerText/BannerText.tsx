5ef4e3ae658bfe1d647ab444b2a4e60734d1f39e
renatamottam <renatamotta.m@gmail.com>
2023-05-11 18:38:06 -0300

    feat: Adds `BannerText` as an overridable section

--- packages/core/src/components/sections/BannerText/BannerText.tsx
+++ packages/core/src/components/sections/BannerText/BannerText.tsx
@@ -1,7 +1,6 @@
-import UIBannerText, {
-  BannerTextProps as UIBannerTextProps,
-} from 'src/components/ui/BannerText'
+import { BannerTextProps as UIBannerTextProps } from 'src/components/ui/BannerText'
 import Section from '../Section'
+import { Components, Props } from './Overrides'
 
 import styles from './section.module.scss'
 
@@ -16,6 +15,8 @@ export interface BannerTextProps {
   variant?: UIBannerTextProps['variant']
 }
 
+const { UIBannerText } = Components
+
 // TODO: Change actionPath and actionLabel with Link
 function BannerText({
   title,
@@ -26,14 +27,17 @@ function BannerText({
 }: BannerTextProps) {
   return (
     <Section className={`${styles.section} section-banner layout__section`}>
-      <UIBannerText
-        variant={variant}
-        title={title}
-        caption={caption}
-        actionPath={link?.url}
-        actionLabel={link?.text}
-        colorVariant={colorVariant}
-      />
+      <div className="layout__content">
+        <UIBannerText
+          variant={variant}
+          title={title}
+          caption={caption}
+          actionPath={link?.url}
+          actionLabel={link?.text}
+          colorVariant={colorVariant}
+          {...Props['UIBannerText']}
+        />
+      </div>
     </Section>
   )
 }
---
+++ packages/core/src/components/sections/BannerText/Overrides.tsx
@@ -0,0 +1,26 @@
+import UIBannerText from '../../ui/BannerText'
+
+import BannerTextCustomizations from 'src/customizations/components/overrides/BannerText'
+
+const bannerTextComponentsCustomization = {}
+
+const bannerTextPropsCustomization = {} as any
+
+Object.entries(BannerTextCustomizations.components).forEach(([key, value]) => {
+  if (value.Component) {
+    bannerTextComponentsCustomization[key] = value.Component
+  }
+})
+
+Object.entries(BannerTextCustomizations.components).forEach(([key, value]) => {
+  if (value.props) {
+    bannerTextPropsCustomization[key] = value.props
+  }
+})
+
+const Components = {
+  UIBannerText,
+  ...bannerTextComponentsCustomization,
+}
+
+export { Components, bannerTextPropsCustomization as Props }
