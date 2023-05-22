5cbd31544bf57336f6d53780142338f80b61226b
Lucas Feijó <lucas.portela@vtex.com.br>
2023-05-22 10:46:05 -0300

    feat: Adjust `ProductDetails` for CMS (#1740)
    ## What's the purpose of this pull request?
    
    This PR intends to adjust the `ProductDetails` and the related
    components to support incoming data from CMS.
    
    ### How to test it?
    
    - Build the store locally (`yarn workspace @faststore/core dev`);
    - Access the Product Details page on this [Headless
    CMS](https://lucasportela--storeframework.myvtex.com/admin/new-cms/faststore/)
    development workspace;
    - There is a version called `Mock Info`, similar to a draft version.
    Click on this version and by clicking the `Preview` button you'll be
    redirected.

---
+++ packages/core/src/components/ui/ProductDetails/AddToCartLoadingSkeleton.tsx
@@ -0,0 +1,57 @@
+export default function AddToCartLoadingSkeleton() {
+  return (
+    // Generated via https://skeletonreact.com/.
+    <svg
+      role="img"
+      width="100%"
+      height="48"
+      aria-labelledby="loading-aria"
+      viewBox="0 0 112 48"
+      preserveAspectRatio="none"
+    >
+      <title id="loading-aria">Loading...</title>
+      <rect
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+        clipPath="url(#clip-path)"
+        style={{ fill: 'url("#fill")' }}
+      />
+      <defs>
+        <clipPath id="clip-path">
+          <rect x="0" y="0" rx="2" ry="2" width="112" height="48" />
+        </clipPath>
+        <linearGradient id="fill">
+          <stop offset="0.599964" stopColor="#f3f3f3" stopOpacity="1">
+            <animate
+              attributeName="offset"
+              values="-2; -2; 1"
+              keyTimes="0; 0.25; 1"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </stop>
+          <stop offset="1.59996" stopColor="#ecebeb" stopOpacity="1">
+            <animate
+              attributeName="offset"
+              values="-1; -1; 2"
+              keyTimes="0; 0.25; 1"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </stop>
+          <stop offset="2.59996" stopColor="#f3f3f3" stopOpacity="1">
+            <animate
+              attributeName="offset"
+              values="0; 0; 3"
+              keyTimes="0; 0.25; 1"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
