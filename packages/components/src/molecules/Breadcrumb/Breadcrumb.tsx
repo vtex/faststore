5844f83fe290fcee475a6537ac73fbbe556bb471
Mariana-Caetano <mariana.caetano@vtex.com.br>
2023-05-22 14:07:53 -0300

    update breadcrumb

--- packages/components/src/molecules/Breadcrumb/Breadcrumb.tsx
+++ packages/components/src/molecules/Breadcrumb/Breadcrumb.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import BreadcrumbBase, { BreadcrumbBaseProps } from './BreadcrumbBase'
 
-export interface BreadcrumbProps = Omit<BreadcrumbBaseProps, "isDesktop">
+export type BreadcrumbProps = Omit<BreadcrumbBaseProps, "isDesktop">
 
 
 const Breadcrumb = ({
