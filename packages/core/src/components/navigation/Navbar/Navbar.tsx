ce8c625a595471c5f9bcf5753b23292b5c9b6ce2
Lucas Feijó <lucas.portela@vtex.com>
2023-05-22 20:33:52 -0300

    chore: Removes unnecessary import

--- packages/core/src/components/navigation/Navbar/Navbar.tsx
+++ packages/core/src/components/navigation/Navbar/Navbar.tsx
@@ -17,9 +17,9 @@ import CartToggle from 'src/components/cart/CartToggle'
 import type { SearchInputRef } from 'src/components/search/SearchInput'
 import SearchInput from 'src/components/search/SearchInput'
 
-import { ButtonSignIn, ButtonSignInFallback } from 'src/components/ui/Button'
 import Link from 'src/components/ui/Link'
 import Logo from 'src/components/ui/Logo'
+import { ButtonSignIn } from 'src/components/ui/Button'
 
 import type { NavbarProps as SectionNavbarProps } from '../../sections/Navbar'
 import NavbarLinks from '../NavbarLinks'
