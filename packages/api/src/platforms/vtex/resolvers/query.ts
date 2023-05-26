d033056c1550a17d1445dac9ab8d39ff7cc2d3b3
beatrizmaselli <beatriz.maselli@vtex.com.br>
2023-05-26 16:12:11 +0200

    adjust the conflict

--- packages/api/src/platforms/vtex/resolvers/query.ts
+++ packages/api/src/platforms/vtex/resolvers/query.ts
@@ -1,7 +1,7 @@
-import { FACET_CROSS_SELLING_MAP } from "./../utils/facets"
-import { BadRequestError, NotFoundError } from "../../errors"
-import { mutateChannelContext, mutateLocaleContext } from "../utils/contex"
-import { enhanceSku } from "../utils/enhanceSku"
+import { FACET_CROSS_SELLING_MAP } from './../utils/facets'
+import { BadRequestError, NotFoundError } from '../../errors'
+import { mutateChannelContext, mutateLocaleContext } from '../utils/contex'
+import { enhanceSku } from '../utils/enhanceSku'
 import {
   findChannel,
   findCrossSelling,
@@ -9,9 +9,9 @@ import {
   findSkuId,
   findSlug,
   transformSelectedFacet,
-} from "../utils/facets"
-import { SORT_MAP } from "../utils/sort"
-import { StoreCollection } from "./collection"
+} from '../utils/facets'
+import { SORT_MAP } from '../utils/sort'
+import { StoreCollection } from './collection'
 import type {
   QueryAllCollectionsArgs,
   QueryAllProductsArgs,
@@ -20,12 +20,12 @@ import type {
   QuerySearchArgs,
   QuerySellersArgs,
   QueryShippingArgs,
-  QueryRedirectArgs
-} from "../../../__generated__/schema"
-import type { CategoryTree } from "../clients/commerce/types/CategoryTree"
-import type { Context } from "../index"
-import { isValidSkuId, pickBestSku } from "../utils/sku"
-import { SearchArgs } from "../clients/search"
+  QueryRedirectArgs,
+} from '../../../__generated__/schema'
+import type { CategoryTree } from '../clients/commerce/types/CategoryTree'
+import type { Context } from '../index'
+import { isValidSkuId, pickBestSku } from '../utils/sku'
+import { SearchArgs } from '../clients/search'
 
 export const Query = {
   product: async (_: unknown, { locator }: QueryProductArgs, ctx: Context) => {
@@ -61,15 +61,16 @@ export const Query = {
        * Here be dragons 🦄🦄🦄
        *
        * In some cases, the slug has a valid skuId for a different
-       * product. This condition makes sure that the fetched sku 
+       * product. This condition makes sure that the fetched sku
        * is the one we actually asked for
        * */
       if (
-        slug && sku.isVariantOf.linkText &&
+        slug &&
+        sku.isVariantOf.linkText &&
         !slug.startsWith(sku.isVariantOf.linkText)
       ) {
         throw new Error(
-          `Slug was set but the fetched sku does not satisfy the slug condition. slug: ${slug}, linkText: ${sku.isVariantOf.linkText}`,
+          `Slug was set but the fetched sku does not satisfy the slug condition. slug: ${slug}, linkText: ${sku.isVariantOf.linkText}`
         )
       }
 
@@ -139,13 +140,17 @@ export const Query = {
      * etc
      */
     if (crossSelling) {
-      const products = await ctx.clients.commerce.catalog.products.crossselling({
-        type: FACET_CROSS_SELLING_MAP[crossSelling.key],
-        productId: crossSelling.value,
-      })
+      const products = await ctx.clients.commerce.catalog.products.crossselling(
+        {
+          type: FACET_CROSS_SELLING_MAP[crossSelling.key],
+          productId: crossSelling.value,
+        }
+      )
 
-      query = `product:${products.map((x) => x.productId).slice(0, first).join(";")
-        }`
+      query = `product:${products
+        .map((x) => x.productId)
+        .slice(0, first)
+        .join(';')}`
     }
 
     const after = maybeAfter ? Number(maybeAfter) : 0
@@ -294,7 +299,7 @@ export const Query = {
     })
 
     return {
-      url: redirect
+      url: redirect,
     }
   },
 
@@ -307,13 +312,18 @@ export const Query = {
       clients: { commerce },
     } = ctx
 
-    const regionData = await commerce.checkout.region({ postalCode, geoCoordinates, country, salesChannel })
+    const regionData = await commerce.checkout.region({
+      postalCode,
+      geoCoordinates,
+      country,
+      salesChannel,
+    })
     const region = regionData?.[0]
     const { id, sellers } = region
 
     return {
       id,
-      sellers
+      sellers,
     }
   },
 }
