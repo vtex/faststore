7e5bc48a2d2396ca54dc6d252e54889211117287
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-05-31 12:27:07 -0300

    chore: updates comment

--- packages/core/src/customizations/fonts/WebFonts.tsx
+++ packages/core/src/customizations/fonts/WebFonts.tsx
@@ -1,6 +1,5 @@
-/* WebFonts from core is the default font,
- * Can be override by src/fonts/WebFonts from starter
- * along with --fs-text-face-body token
+/* WebFonts from core are default,
+ * can be override by src/fonts/WebFonts from starter.store along with --fs-text-face-body token
  */
 
 import WebFonts from 'src/fonts/WebFonts'
