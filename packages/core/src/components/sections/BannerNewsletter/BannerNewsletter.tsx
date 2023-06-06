bb592e0cf76797395be80f6aba834b87b6513a0c
Arthur Andrade <arthurfelandrade@gmail.com>
2023-06-06 12:58:17 -0300

    feat: Newslet6ter and toast to cms
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- packages/core/src/components/sections/BannerNewsletter/BannerNewsletter.tsx
+++ packages/core/src/components/sections/BannerNewsletter/BannerNewsletter.tsx
@@ -1,31 +1,39 @@
-import {
-  Banner as UIBanner,
-  BannerContent as UIBannerContent,
-} from '@faststore/ui'
-import Newsletter from '../Newsletter'
+import BannerText, { BannerTextProps } from 'src/components/sections/BannerText'
+import Newsletter, { NewsletterProps } from 'src/components/sections/Newsletter'
 import Section from '../Section'
 import styles from './section.module.scss'
 
-function BannerNewsletter() {
+function BannerNewsletter({
+  banner,
+  newsletter,
+}: {
+  banner: BannerTextProps
+  newsletter: NewsletterProps
+}) {
   return (
     <Section
       className={`${styles.section} section-banner-newsletter layout__content`}
     >
       <div data-fs-banner-newsletter>
-        <UIBanner variant="secondary" colorVariant="light">
-          <UIBannerContent
-            title="Get to Know Our Next Release"
-            caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elit nisi, vehicula in turpis sit amet, posuere aliquam nisl. "
-            link="/"
-            linkText="Shop Now"
-          />
-        </UIBanner>
-
+        <BannerText
+          title={banner.title}
+          caption={banner.caption}
+          link={banner?.link}
+          variant={banner.variant}
+          colorVariant={banner.colorVariant}
+        />
         <Newsletter
-          title="Get News and Special Offers!"
-          description="Receive our news and promotions in advance. Enjoy and get 10% off your first purchase. For more information click here."
-          card
-          lite
+          title={newsletter.title}
+          description={newsletter.description}
+          icon={newsletter.icon}
+          privacyPolicy={newsletter.privacyPolicy}
+          emailInputLabel={newsletter.emailInputLabel}
+          displayNameInput={newsletter.displayNameInput}
+          nameInputLabel={newsletter.nameInputLabel}
+          subscribeButtonLabel={newsletter.subscribeButtonLabel}
+          toastSubscribe={newsletter.toastSubscribe}
+          toastSubscribeError={newsletter.toastSubscribeError}
+          card={newsletter.card}
         />
       </div>
     </Section>
