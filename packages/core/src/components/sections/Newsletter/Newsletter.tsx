1b489b4a2a53905a4f7ee1b3f9148ae2e8bb1908
Arthur Andrade <arthurfelandrade@gmail.com>
2023-06-13 11:05:47 -0300

    Feat: Adds `Newsletter` Section from CMS (#1826)
    [⚠️ This PR is a reference to that and was created to avoid the lint
    error
    ](https://github.com/vtex/faststore/pull/1773)
    
    ## What's the purpose of this pull request?
    
    Adjusts Newsletter and BannerNewsletter section for CMS.
    
    ## How it works?
    
    - [x] Adjust sections in sections.json
    - [x] Adapt Newsletter and BannerNewsletter to receive CMS props.
    - [x] Remove Privacy Policy Disclaimer. (I don't know if it's possible
    to make it dynamic, since we have a link with the string)
    - [x] Remove the `lite` prop, that behavior is controlled by
    `displayNameInput` now.
    
    ## How to test it?
    
    - Publish BannerNewsletter and Newsletter in CMS.
    - `yarn dev`.
    - Check the CMS preview.
    
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- packages/core/src/components/sections/Newsletter/Newsletter.tsx
+++ packages/core/src/components/sections/Newsletter/Newsletter.tsx
@@ -1,10 +1,6 @@
-import { Button as UIButton, InputField as UIInputField } from '@faststore/ui'
-import type { ComponentPropsWithRef, FormEvent, ReactNode } from 'react'
-import { forwardRef, useRef } from 'react'
-
-import { Icon, useUI } from '@faststore/ui'
-import Link from 'src/components/ui/Link'
-import { useNewsletter } from 'src/sdk/newsletter/useNewsletter'
+import { ComponentPropsWithRef } from 'react'
+import UINewsletter from 'src/components/ui/Newsletter'
+import { SubscribeMessage } from 'src/components/ui/Newsletter/Newsletter'
 
 import Section from '../Section'
 import styles from './section.module.scss'
@@ -12,132 +8,82 @@ import styles from './section.module.scss'
 export interface NewsletterProps
   extends Omit<ComponentPropsWithRef<'form'>, 'title' | 'onSubmit'> {
   /**
+   * Icon for the section.
+   */
+  icon: {
+    icon: string
+    alt: string
+  }
+  /**
    * Title for the section.
    */
-  title: ReactNode
+  title: string
   /**
    * A description for the section.
    */
-  description?: ReactNode
+  description?: string
+  /**
+   * The Privacy Policy disclaimer.
+   */
+  privacyPolicy?: string
+  /**
+   * The email input label.
+   */
+  emailInputLabel?: string
+  /**
+   * The name input visibility.
+   */
+  displayNameInput?: boolean
+  /**
+   * The name input label.
+   */
+  nameInputLabel?: string
+  /**
+   * The subscribe button label.
+   */
+  subscribeButtonLabel?: string
   /**
    * The card Variant
    */
-  card?: boolean
-  /**
-   * The compact version of the Newsletter component
-   */
-  lite?: boolean
+  card: Boolean
+
+  toastSubscribe: SubscribeMessage
+
+  toastSubscribeError: SubscribeMessage
 }
 
-const Newsletter = forwardRef<HTMLFormElement, NewsletterProps>(
-  function Newsletter({ title, description, card = false, lite = false }, ref) {
-    const { subscribeUser, loading, data } = useNewsletter()
-    const nameInputRef = useRef<HTMLInputElement>(null)
-    const emailInputRef = useRef<HTMLInputElement>(null)
-
-    const { pushToast } = useUI()
-
-    const handleSubmit = (event: FormEvent) => {
-      event.preventDefault()
-      subscribeUser({
-        data: {
-          name: nameInputRef.current?.value ?? '',
-          email: emailInputRef.current?.value ?? '',
-        },
-      })
-
-      if (data?.subscribeToNewsletter?.id) {
-        pushToast({
-          title: 'Hooray!',
-          message: 'Thank for your subscription.',
-          status: 'INFO',
-          icon: <Icon name="CircleWavyCheck" width={30} height={30} />,
-        })
-      } else {
-        pushToast({
-          title: 'Oops.',
-          message: 'Something went wrong. Please Try again.',
-          status: 'ERROR',
-          icon: <Icon name="CircleWavyWarning" width={30} height={30} />,
-        })
-      }
-
-      const formElement = event.currentTarget as HTMLFormElement
-
-      formElement.reset()
-    }
-
-    return (
-      <Section className={`${styles.section} section-newsletter`}>
-        <div data-fs-newsletter={card ? 'card' : ''}>
-          <form
-            ref={ref}
-            data-fs-newsletter-form
-            onSubmit={handleSubmit}
-            className="layout__content"
-          >
-            <header data-fs-newsletter-header>
-              <h3>
-                <Icon name="Envelop" width={32} height={32} />
-                {title}
-              </h3>
-              {description && <span> {description}</span>}
-            </header>
-
-            <div data-fs-newsletter-controls>
-              {lite ? (
-                <>
-                  <UIInputField
-                    inputRef={emailInputRef}
-                    id="newsletter-email"
-                    label="Your Email"
-                    type="email"
-                    required
-                    actionable
-                    onSubmit={() => undefined}
-                    onClear={() => undefined}
-                    buttonActionText="Subscribe"
-                    displayClearButton={false}
-                  />
-                  <span data-fs-newsletter-addendum>
-                    By subscribing to our newsletter you agree to to our{' '}
-                    <Link href="/" inverse variant="inline">
-                      Privacy Policy.
-                    </Link>
-                  </span>
-                </>
-              ) : (
-                <>
-                  <UIInputField
-                    inputRef={nameInputRef}
-                    id="newsletter-name"
-                    label="Your Name"
-                    required
-                  />
-                  <UIInputField
-                    inputRef={emailInputRef}
-                    id="newsletter-email"
-                    label="Your Email"
-                    type="email"
-                    required
-                  />
-                  <span data-fs-newsletter-addendum>
-                    By subscribing to our newsletter you agree to to our{' '}
-                    <Link href="/" inverse variant="inline">
-                      Privacy Policy.
-                    </Link>
-                  </span>
-                  <UIButton variant="secondary" inverse type="submit">
-                    {loading ? 'Loading...' : 'Subscribe'}
-                  </UIButton>
-                </>
-              )}
-            </div>
-          </form>
-        </div>
-      </Section>
-    )
-  }
-)
+const Newsletter = function Newsletter({
+  icon,
+  title,
+  description,
+  privacyPolicy,
+  emailInputLabel,
+  displayNameInput,
+  nameInputLabel,
+  subscribeButtonLabel,
+  card,
+  toastSubscribe,
+  toastSubscribeError,
+  ...otherProps
+}: NewsletterProps) {
+  return (
+    <Section className={`${styles.section} section-newsletter`}>
+      <UINewsletter
+        icon={icon}
+        title={title}
+        description={description}
+        privacyPolicy={privacyPolicy}
+        emailInputLabel={emailInputLabel}
+        displayNameInput={displayNameInput}
+        nameInputLabel={nameInputLabel}
+        subscribeButtonLabel={subscribeButtonLabel}
+        toastSubscribe={toastSubscribe}
+        toastSubscribeError={toastSubscribeError}
+        card={card}
+        {...otherProps}
+      />
+    </Section>
+  )
+}
 
 export default Newsletter
---
+++ packages/core/src/components/ui/Newsletter/Newsletter.tsx
@@ -0,0 +1,198 @@
+import { Button as UIButton, InputField as UIInputField } from '@faststore/ui'
+import { ComponentPropsWithRef, FormEvent } from 'react'
+import { forwardRef, useRef } from 'react'
+import { convertFromRaw } from 'draft-js'
+import { stateToHTML } from 'draft-js-export-html'
+import { Icon, useUI } from '@faststore/ui'
+import { useNewsletter } from 'src/sdk/newsletter/useNewsletter'
+
+const cmsToHtml = (content) => {
+  if (!content) {
+    return ''
+  }
+  const rawDraftContentState = JSON.parse(content)
+  const html = stateToHTML(convertFromRaw(rawDraftContentState), {
+    entityStyleFn: (entity) => {
+      const entityType = entity.get('type').toLowerCase()
+      if (entityType === 'link') {
+        const data = entity.getData()
+        return {
+          element: 'a',
+          attributes: {
+            'data-fs-link': 'true',
+            'data-fs-link-variant': 'inline',
+            'data-fs-link-inverse': 'true',
+            'data-fs-link-size': 'regular',
+            'data-testid': 'fs-link',
+            href: data.url,
+          },
+        }
+      }
+    },
+  })
+
+  return html
+}
+
+export type SubscribeMessage = {
+  title: string
+  message: string
+  icon: string
+}
+
+export interface NewsletterProps
+  extends Omit<ComponentPropsWithRef<'form'>, 'title' | 'onSubmit'> {
+  /**
+   * Icon for the section.
+   */
+  icon: {
+    icon: string
+    alt: string
+  }
+  /**
+   * Title for the section.
+   */
+  title: string
+  /**
+   * A description for the section.
+   */
+  description?: string
+  /**
+   * The Privacy Policy disclaimer.
+   */
+  privacyPolicy?: string
+  /**
+   * The email input label.
+   */
+  emailInputLabel?: string
+  /**
+   * The name input visibility.
+   */
+  displayNameInput?: boolean
+  /**
+   * The name input label.
+   */
+  nameInputLabel?: string
+  /**
+   * The subscribe button label.
+   */
+  subscribeButtonLabel?: string
+  /**
+   * The subscribe button loading label.
+   */
+  subscribeButtonLoadingLabel?: string
+  /**
+   * The card Variant
+   */
+  card: Boolean
+
+  toastSubscribe: SubscribeMessage
+
+  toastSubscribeError: SubscribeMessage
+}
+
+const Newsletter = forwardRef<HTMLFormElement, NewsletterProps>(
+  function Newsletter(
+    {
+      icon,
+      title,
+      description,
+      privacyPolicy,
+      emailInputLabel,
+      displayNameInput,
+      nameInputLabel,
+      subscribeButtonLabel,
+      subscribeButtonLoadingLabel,
+      card,
+      toastSubscribe,
+      toastSubscribeError,
+      ...otherProps
+    },
+    ref
+  ) {
+    const { subscribeUser, loading, data } = useNewsletter()
+    const nameInputRef = useRef<HTMLInputElement>(null)
+    const emailInputRef = useRef<HTMLInputElement>(null)
+
+    const { pushToast } = useUI()
+
+    const handleSubmit = (event: FormEvent) => {
+      event.preventDefault()
+      subscribeUser({
+        data: {
+          name: nameInputRef.current?.value ?? '',
+          email: emailInputRef.current?.value ?? '',
+        },
+      })
+
+      if (data?.subscribeToNewsletter?.id) {
+        pushToast({
+          ...toastSubscribe,
+          status: 'INFO',
+          icon: <Icon name={toastSubscribe.icon} width={30} height={30} />,
+        })
+      } else {
+        pushToast({
+          ...toastSubscribeError,
+          status: 'ERROR',
+          icon: <Icon name={toastSubscribeError.icon} width={30} height={30} />,
+        })
+      }
+
+      const formElement = event.currentTarget as HTMLFormElement
+
+      formElement.reset()
+    }
+
+    return (
+      <div data-fs-newsletter={card ? 'card' : ''}>
+        <form
+          data-fs-newsletter-form
+          ref={ref}
+          onSubmit={handleSubmit}
+          {...otherProps}
+          className="layout__content"
+        >
+          <header data-fs-newsletter-header>
+            <h3>
+              <Icon name={icon.icon} width={32} height={32} />
+              {title}
+            </h3>
+            {description && <span> {description}</span>}
+          </header>
+
+          <div data-fs-newsletter-controls>
+            <>
+              {displayNameInput ? (
+                <UIInputField
+                  inputRef={nameInputRef}
+                  id="newsletter-name"
+                  label={nameInputLabel}
+                  required
+                />
+              ) : null}
+              <UIInputField
+                inputRef={emailInputRef}
+                id="newsletter-email"
+                label={emailInputLabel}
+                type="email"
+                required
+              />
+              <span
+                data-fs-newsletter-addendum
+                dangerouslySetInnerHTML={{
+                  __html: cmsToHtml(privacyPolicy),
+                }}
+              ></span>
+              <UIButton variant="secondary" inverse type="submit">
+                {loading ? subscribeButtonLoadingLabel : subscribeButtonLabel}
+              </UIButton>
+            </>
+          </div>
+        </form>
+      </div>
+    )
+  }
+)
+
+export default Newsletter
