1e5398728aa79e499c0b5fb29b0a7a072ade6c15
Fanny Chien <fannychienn@gmail.com>
2023-06-13 09:57:39 -0300

    Update apps/site/theme.config.tsx

--- apps/site/theme.config.tsx
+++ apps/site/theme.config.tsx
@@ -53,7 +53,7 @@ const config: DocsThemeConfig = {
       <meta property="og:title" content="FastStore Documentation" />
       <meta
         property="og:description"
-        content="Welcome to the FastStore Documentation."
+        content="Welcome to the FastStore documentation, the toolkit for building blazing-fast storefronts."
       />
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
     </>
