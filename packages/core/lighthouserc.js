3c9ad5426b0680fcf80f08f39d67d06afa16cc2c
Lucas Feijó <lucas.portela@vtex.com>
2023-06-20 15:12:15 -0300

    chore: Loosen script size metric

--- packages/core/lighthouserc.js
+++ packages/core/lighthouserc.js
@@ -19,5 +19,6 @@ module.exports = lhConfig({
     deprecations: 'warn',
     'resource-summary:stylesheet:count': ['warn', { maxNumericValue: 400 }],
     'resource-summary:script:count': ['warn', { maxNumericValue: 20 }],
+    'resource-summary:script:size': ['error', { maxNumericValue: 201 * 1024 }],
   },
 })
