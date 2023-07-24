02c951c69236c7c1b03d15ac4f93d964a4209607
beatrizmaselli <beatriz.maselli@vtex.com.br>
2023-07-24 22:21:43 +0200

    adjust Session interface

--- packages/api/src/platforms/vtex/clients/commerce/types/Session.ts
+++ packages/api/src/platforms/vtex/clients/commerce/types/Session.ts
@@ -7,7 +7,7 @@ export interface Namespaces {
   profile?: Profile
   store?: Store
   checkout?: Checkout
-  public?: Checkout
+  public?: Public
 }
 
 export interface Value {
@@ -31,4 +31,9 @@ export interface Profile {
 
 export interface Checkout {
   orderFormId?: Value
+}
+
+export interface Public {
+  orderFormId?: Value
+  items?: Value
 }
