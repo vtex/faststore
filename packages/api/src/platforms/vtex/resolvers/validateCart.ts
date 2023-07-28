17ff3c60b73460955c469b3e599a17b9d70fdb5b
beatrizmaselli <beatriz.maselli@vtex.com.br>
2023-07-28 19:56:07 +0200

    change to public namespace

--- packages/api/src/platforms/vtex/resolvers/validateCart.ts
+++ packages/api/src/platforms/vtex/resolvers/validateCart.ts
@@ -213,7 +213,7 @@ async function getOrderNumberFromSession(
 
   if (cookieSession) {
     const { namespaces } = await commerce.getSessionOrder()
-    return namespaces.checkout?.orderFormId?.value ?? undefined
+    return namespaces.public?.orderFormId?.value ?? undefined
   }
   return
 }
