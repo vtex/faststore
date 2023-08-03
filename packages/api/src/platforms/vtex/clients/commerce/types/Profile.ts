4f30bd73188a09791571c060ffec2b37b2680d37
beatrizmaselli <beatriz.maselli@vtex.com.br>
2023-08-03 14:54:51 +0200

    format Profile

--- packages/api/src/platforms/vtex/clients/commerce/types/Profile.ts
+++ packages/api/src/platforms/vtex/clients/commerce/types/Profile.ts
@@ -1,29 +1,29 @@
 export interface Profile {
-    id: string
-    document: Document
-    meta: Meta
+  id: string
+  document: Document
+  meta: Meta
 }
 
 export interface Document {
-    email?: string | null
-    firstName?: string | null
-    lastName?: string | null
-    documentType?: string | null
-    homePhone?: string | null
-    isPJ?: string | null
-    userId?: string | null
-    id?: string | null
-    name?: string | null
-    document?: string | null
-    title?: string | null
-    phoneNumber?: string | null
-    birthDate?: string | null
+  email?: string | null
+  firstName?: string | null
+  lastName?: string | null
+  documentType?: string | null
+  homePhone?: string | null
+  isPJ?: string | null
+  userId?: string | null
+  id?: string | null
+  name?: string | null
+  document?: string | null
+  title?: string | null
+  phoneNumber?: string | null
+  birthDate?: string | null
 }
 
 export interface Meta {
-    version: string | null
-    author: string | null
-    creationDate: string | null
-    lastUpdateDate: string | null
-    expirationDate: string | null
+  version: string | null
+  author: string | null
+  creationDate: string | null
+  lastUpdateDate: string | null
+  expirationDate: string | null
 }
