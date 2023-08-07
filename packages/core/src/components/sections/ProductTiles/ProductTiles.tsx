590f7ebe6e5162d68d1c987bcab0998dcd80e8b8
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-08-07 18:32:38 -0300

    chore: renames query in productTiles

--- packages/core/src/components/sections/ProductTiles/ProductTiles.tsx
+++ packages/core/src/components/sections/ProductTiles/ProductTiles.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 import { useInView } from 'react-intersection-observer'
 
-import type { ProductsQueryQueryVariables } from '@generated/graphql'
+import type { BrowserProductsQueryQueryVariables } from '@generated/graphql'
 import ProductCard from 'src/components/product/ProductCard'
 import ProductTilesSkeleton from 'src/components/skeletons/ProductTilesSkeleton'
 import Tiles, { Tile } from 'src/components/ui/Tiles'
@@ -12,7 +12,8 @@ import Section from '../Section'
 
 import styles from './section.module.scss'
 
-interface ProductTilesProps extends Partial<ProductsQueryQueryVariables> {
+interface ProductTilesProps
+  extends Partial<BrowserProductsQueryQueryVariables> {
   title: string
 }
 
