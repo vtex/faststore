111a8bde56f8458acb44c441c4789a6b5f52f818
Victor Santos <victor.santos@hearts.com>
2023-08-09 20:44:34 -0400

    clean up

--- packages/core/src/components/cms/GlobalSections.tsx
+++ packages/core/src/components/cms/GlobalSections.tsx
@@ -11,7 +11,6 @@ import Alert from 'src/components/sections/Alert'
 import Footer from 'src/components/sections/Footer'
 import Navbar from 'src/components/sections/Navbar'
 import RegionBar from 'src/components/sections/RegionBar'
-import { SectionProvider } from 'src/sdk/ui/SectionContex'
 
 const RegionModal = lazy(() => import('src/components/region/RegionModal'))
 const CartSidebar = lazy(() => import('src/components/cart/CartSidebar'))
@@ -40,6 +39,7 @@ function GlobalSections({
   return (
     <RenderSections components={COMPONENTS} {...otherProps}>
       <Toast />
+
       <main>{children}</main>
     </RenderSections>
   )
