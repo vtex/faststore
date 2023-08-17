959ff7365065be3fa23daf15f1e56b6d8128a8c9
Arthur Andrade <arthurfelandrade@gmail.com>
2023-08-17 00:45:45 -0300

    docs: Add ExtandableQueryTable component
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

---
+++ apps/site/components/ExtendableQueryTable/ExtendableQueryTable.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from "react"
+
+export type ExtendableQueryTableProps = {
+    children: ReactNode
+}
+
+const ExtendableQueryTable = ({ children }: ExtendableQueryTableProps) => {
+    return (
+        <table className="nx-simple-table">
+            <thead>
+                <tr>
+                    <th>Side</th>
+                    <th>Query operation</th>
+                    <th>Page</th>
+                    <th>Where is used</th>
+                    <th>Return</th>
+                    <th>Parameters</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    {
+                        children
+                    }
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default ExtendableQueryTable
