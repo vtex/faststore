6fd19fe74b09e14ebd12ce5ece4eaf16c195aed1
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-08-23 18:06:22 -0300

    chore: brings back ProductShelf query fetching into ui component

--- packages/core/src/components/sections/CrossSellingShelf/CrossSellingShelf.tsx
+++ packages/core/src/components/sections/CrossSellingShelf/CrossSellingShelf.tsx
@@ -22,15 +22,18 @@ const CrossSellingShelf = ({ items: first, title, kind }: Props) => {
     () => [{ key: kind, value: productGroupID }],
     [kind, productGroupID]
   )
-  const data = useProductsQuery({ selectedFacets, first })
-  const products = data?.search?.products
 
   return (
     <Section
       className={`${styles.section} section-product-shelf layout__section`}
       ref={ref}
     >
-      <UIProductShelf inView={inView} title={title} products={products} />
+      <UIProductShelf
+        inView={inView}
+        first={first}
+        title={title}
+        selectedFacets={selectedFacets}
+      />
     </Section>
   )
 }
--- packages/core/src/components/sections/ProductShelf/ProductShelf.tsx
+++ packages/core/src/components/sections/ProductShelf/ProductShelf.tsx
@@ -1,48 +1,20 @@
 import { useInView } from 'react-intersection-observer'
 import Section from '../Section'
 
-import ProductShelf from 'src/components/ui/ProductShelf'
-import { useProductsQuery } from 'src/sdk/product/useProductsQuery'
+import ProductShelf, { ProductShelfProps } from 'src/components/ui/ProductShelf'
 import styles from './section.module.scss'
 
-type Sort =
-  | 'discount_desc'
-  | 'name_asc'
-  | 'name_desc'
-  | 'orders_desc'
-  | 'price_asc'
-  | 'price_desc'
-  | 'release_desc'
-  | 'score_desc'
-
-export type ProductShelfProps = {
-  title: string
-  first?: number
-  after?: string
-  sort?: Sort
-  term?: string
-  selectedFacets?: {
-    key: string
-    value: string
-  }[]
-  productCardConfiguration?: {
-    showDiscountBadge?: boolean
-    bordered?: boolean
-  }
-}
-
-function ProductShelfSection(props: ProductShelfProps) {
+function ProductShelfSection({
+  ...otherProps
+}: Omit<ProductShelfProps, 'inView'>) {
   const { ref, inView } = useInView()
-  const { first, after, sort, term, selectedFacets, ...otherProps } = props
-  const data = useProductsQuery({ first, after, sort, term, selectedFacets })
-  const products = data?.search?.products
 
   return (
     <Section
       className={`${styles.section} section-product-shelf layout__section`}
       ref={ref}
     >
-      <ProductShelf inView={inView} products={products} {...otherProps} />
+      <ProductShelf inView={inView} {...otherProps} />
     </Section>
   )
 }
