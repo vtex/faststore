4c3ab7eeb2360ca6dd629ca3518c44eaa7f4006a
Victor Santos <victor.santos@hearst.com>
2023-08-24 14:24:42 -0400

    handling single or multiples scripts

--- packages/core/src/customizations/components/overrides/ThirdPartyScripts.tsx
+++ packages/core/src/customizations/components/overrides/ThirdPartyScripts.tsx
@@ -1,22 +1,30 @@
 import { default as ThirdPartyScripts } from 'src/customizations/scripts/ThirdPartyScripts'
 
-const RenderThirdPartyScripts = (ThirdPartyScripts: () => string) => {
-  const scriptContent = ThirdPartyScripts()
+const RenderThirdPartyScripts = (
+  ThirdPartyScripts: () => string[] | string
+) => {
+  const scriptContent = ThirdPartyScripts() || []
 
-  const scriptInjection = () => {
-    return (
-      <>
-        <script
-          key="thirdpartyscript"
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: scriptContent,
-          }}
-        />
-      </>
-    )
+  const buildScript = (script: string, index?: number) => (
+    <script
+      key={`thirdpartyscript-${index}`}
+      type="text/partytown"
+      dangerouslySetInnerHTML={{
+        __html: script,
+      }}
+    />
+  )
+
+  const renderScriptTags = (): JSX.Element => {
+    if (Array.isArray(scriptContent)) {
+      return (
+        <>{scriptContent.map((script, index) => buildScript(script, index))}</>
+      )
+    }
+    return buildScript(scriptContent)
   }
-  return scriptInjection
+
+  return renderScriptTags
 }
 
 const overrides = {
--- packages/core/src/customizations/scripts/ThirdPartyScripts.ts
+++ packages/core/src/customizations/scripts/ThirdPartyScripts.ts
@@ -1,7 +1,7 @@
 /**
  * Place holder function to return JS script
- * @returns string JS themplate
- * Example: `console.log("my script is running")`
+ * @returns string or array of string of JS themplate
+ * Example: `console.log("my script is running")` or [`console.log("console1")`, `console.log("console2")`]
  */
 const ThirdPartyScripts = () => {
   return ``
