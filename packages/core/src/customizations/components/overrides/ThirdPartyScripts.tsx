b00e66216e6a95b3cd5f49a8638d7aee0d5784b2
Victor Santos <victor.santos@hearst.com>
2023-08-31 10:17:00 -0400

    simplifying overwrite for thirdparty scripts

--- packages/core/src/customizations/components/overrides/ThirdPartyScripts.tsx
+++ packages/core/src/customizations/components/overrides/ThirdPartyScripts.tsx
@@ -1,35 +1,8 @@
 import { default as ThirdPartyScripts } from 'src/customizations/scripts/ThirdPartyScripts'
 
-const RenderThirdPartyScripts = (
-  ThirdPartyScripts: () => string[] | string
-) => {
-  const scriptContent = ThirdPartyScripts() || []
-
-  const buildScript = (script: string, index?: number) => (
-    <script
-      key={`thirdpartyscript-${index}`}
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: script,
-      }}
-    />
-  )
-
-  const renderScriptTags = (): JSX.Element => {
-    if (Array.isArray(scriptContent)) {
-      return (
-        <>{scriptContent.map((script, index) => buildScript(script, index))}</>
-      )
-    }
-    return buildScript(scriptContent)
-  }
-
-  return renderScriptTags
-}
-
 const overrides = {
   components: {
-    ThirdPartyScripts: RenderThirdPartyScripts(ThirdPartyScripts),
+    ThirdPartyScripts,
   },
 }
 
--- packages/core/src/customizations/scripts/ThirdPartyScripts.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Place holder function to return JS script
- * @returns string or array of string of JS themplate
- * Example: `console.log("my script is running")` or [`console.log("console1")`, `console.log("console2")`]
- */
-const ThirdPartyScripts = () => {
-  return ``
-}
-export default ThirdPartyScripts
