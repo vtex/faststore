4c7e82f03ab13d41ac17aa4797ba997a29399630
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-08-31 17:48:14 -0300

    chore: exports PageProvider functions and types

---
+++ packages/core/index.ts
@@ -0,0 +1,15 @@
+export {
+  usePage,
+  usePDP,
+  usePLP,
+  useSearchPage,
+  isPDP,
+  isPLP,
+  isSearchPage,
+} from './src/sdk/overrides/PageProvider'
+
+export type {
+  PDPContext,
+  PLPContext,
+  SearchPageContext,
+} from './src/sdk/overrides/PageProvider'
