f63b8b03ddf623bc90cbfd300bd1e77c3618a264
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-08-31 20:13:32 -0300

    chore: attempts to use exported types

---
+++ packages/core/src/index.ts
@@ -0,0 +1,15 @@
+export {
+  usePage,
+  usePDP,
+  usePLP,
+  useSearchPage,
+  isPDP,
+  isPLP,
+  isSearchPage,
+} from './sdk/overrides/PageProvider'
+
+export type {
+  PDPContext,
+  PLPContext,
+  SearchPageContext,
+} from './sdk/overrides/PageProvider'
