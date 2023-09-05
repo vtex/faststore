8c2c59d76ef00d78913eca6dd25f74a348695ab6
Lucas Feijó <lucas.portela@vtex.com>
2023-09-05 19:10:58 -0300

    chore: Revert not related change

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -30,13 +30,10 @@ export default class Build extends Command {
       `${tmpDir}/lighthouserc.js`,
       `${userDir}/lighthouserc.js`
     )
-
-    if (existsSync(`${tmpDir}/cms-webhook-urls.json`)) {
-      await copyResource(
-        `${tmpDir}/cms-webhook-urls.json`,
-        `${userDir}/cms-webhook-urls.json`
-      )
-    }
+    await copyResource(
+      `${tmpDir}/cms-webhook-urls.json`,
+      `${userDir}/cms-webhook-urls.json`
+    )
     
     if (existsSync(`.next/standalone`)) {
       await copyResource(
