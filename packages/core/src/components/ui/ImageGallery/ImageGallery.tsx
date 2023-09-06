b82b0eff383b9cf1e4c3b60f9120aaeefc3bb104
Gabriel Antiqueira <gabriel.antiqueira@vtex.com.br>
2023-09-06 13:31:47 -0300

    Update prop line position
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com.br>

--- packages/core/src/components/ui/ImageGallery/ImageGallery.tsx
+++ packages/core/src/components/ui/ImageGallery/ImageGallery.tsx
@@ -44,10 +44,10 @@ const ImageGallery = ({
       <ImageZoom.Component {...ImageZoom.props}>
         <Image.Component
           sizes="(max-width: 360px) 50vw, (max-width: 768px) 90vw, 50vw"
+          data-fs-image-gallery-position={imagePosition}
           width={691}
           height={691 * (3 / 4)}
           loading="eager"
-          data-fs-image-gallery-position={imagePosition}
           {...Image.props}
           src={currentImage.url}
           alt={currentImage.alternateName}
