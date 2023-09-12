8331cd061dd64b704909e24fa54a192edf5e6157
Fanny Chien <fannychienn@gmail.com>
2023-09-12 16:45:31 -0300

    feat: Sends search.query event

--- packages/core/src/sdk/analytics/platform/vtex/search.ts
+++ packages/core/src/sdk/analytics/platform/vtex/search.ts
@@ -2,7 +2,10 @@
  * More info at: https://www.notion.so/vtexhandbook/Event-API-Documentation-48eee26730cf4d7f80f8fd7262231f84
  */
 import type { AnalyticsEvent } from '@faststore/sdk'
-import type { SearchSelectItemEvent } from '../../types'
+import type {
+  IntelligentSearchQueryEvent,
+  SearchSelectItemEvent,
+} from '../../types'
 
 import config from '../../../../../faststore.config'
 import { getCookie } from '../../../../utils/getCookie'
@@ -52,6 +55,15 @@ type SearchEvent =
       url: string
       type: 'search.click'
     }
+  | {
+      text: string
+      misspelled: boolean
+      match: number
+      operator: string
+      locale: string
+      url: string
+      type: 'search.query'
+    }
 
 const sendEvent = (options: SearchEvent & { url?: string }) =>
   fetch(`https://sp.vtex.com/event-api/v1/${config.api.storeId}/event`, {
@@ -71,30 +83,50 @@ const isFullTextSearch = (url: URL) =>
   typeof url.searchParams.get('q') === 'string' &&
   /^\/s(\/)?$/g.test(url.pathname)
 
-const handleEvent = (event: AnalyticsEvent | SearchSelectItemEvent) => {
-  if (event.name !== 'search_select_item') {
-    return
-  }
+const handleEvent = (
+  event: AnalyticsEvent | SearchSelectItemEvent | IntelligentSearchQueryEvent
+) => {
+  switch (event.name) {
+    case 'search_select_item': {
+      const url = new URL(event.params.url)
 
-  const url = new URL(event.params.url)
+      if (!isFullTextSearch(url)) {
+        return
+      }
 
-  if (!isFullTextSearch(url)) {
-    return
-  }
+      for (const item of event.params.items ?? []) {
+        const productId = item.item_id ?? item.item_variant
+        const position = item.index
 
-  for (const item of event.params.items ?? []) {
-    const productId = item.item_id ?? item.item_variant
-    const position = item.index
+        if (productId && position) {
+          sendEvent({
+            type: 'search.click',
+            productId,
+            position,
+            url: url.href,
+            text: url.searchParams.get('q') ?? '<empty>',
+          })
+        }
+      }
 
-    if (productId && position) {
-      sendEvent({
-        type: 'search.click',
-        productId,
-        position,
-        url: url.href,
-        text: url.searchParams.get('q') ?? '<empty>',
-      })
+      break
     }
+
+    case 'intelligent_search_query': {
+      sendEvent({
+        type: 'search.query',
+        url: event.params.url,
+        text: event.params.term,
+        misspelled: event.params.isTermMisspelled,
+        match: event.params.totalCount,
+        operator: event.params.logicalOperator,
+        locale: event.params.locale,
+      })
+
+      break
+    }
+
+    default:
   }
 }
 
