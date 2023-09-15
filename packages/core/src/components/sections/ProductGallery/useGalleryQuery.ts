49fea56f07c8dcaf11d934b052279833d5ba0a17
Fanny Chien <fanny.chien@vtex.com.br>
2023-09-15 14:00:22 -0300

    Feat: Adds IS search.query event  (#2023)
    ## What's the purpose of this pull request?
    
    Implements `search.query` event from IS Event API.
    
    ## How it works?
    
    It should send a event every time occur a full-text search.
    
    ## How to test it?
    
    1. Type `pizza` in the search input
    2. Go to Network tab and filter for sp. Look for `search.query` type
    event
    
    <img width="633" alt="image"
    src="https://github.com/vtex/faststore/assets/3356699/6a0e2e6d-c83d-461b-bab3-b8d5b868bbeb">
    
    
    ### Starters Deploy Preview
    
    WIP
    
    ## References
    
    https://github.com/vtex-sites/nextjs.store/pull/373

--- packages/core/src/components/sections/ProductGallery/useGalleryQuery.ts
+++ packages/core/src/components/sections/ProductGallery/useGalleryQuery.ts
@@ -1,19 +1,27 @@
-import { useSearch } from '@faststore/sdk'
+import { sendAnalyticsEvent, useSearch } from '@faststore/sdk'
 import { gql } from '@faststore/graphql-utils'
 
 import { useQuery } from 'src/sdk/graphql/useQuery'
+
 import type {
   ProductGalleryQueryQuery as Query,
   ProductGalleryQueryQueryVariables as Variables,
 } from '@generated/graphql'
+import type { IntelligentSearchQueryEvent } from 'src/sdk/analytics/types'
 
 import { useLocalizedVariables } from '../../../sdk/product/useProductsQuery'
+import { useSession } from 'src/sdk/session'
 
 /**
  * This query is run on the browser and contains
  * the current search state of the user
  */
 export const query = gql`
+  fragment SearchEvent_metadata on SearchMetadata {
+    isTermMisspelled
+    logicalOperator
+  }
+
   query ProductGalleryQuery(
     $first: Int!
     $after: String!
@@ -36,6 +44,9 @@ export const query = gql`
       facets {
         ...Filter_facets
       }
+      metadata {
+        ...SearchEvent_metadata
+      }
     }
   }
 `
@@ -46,6 +57,8 @@ export const useGalleryQuery = () => {
     itemsPerPage,
   } = useSearch()
 
+  const { locale } = useSession()
+
   const localizedVariables = useLocalizedVariables({
     first: itemsPerPage,
     after: '0',
@@ -54,5 +67,21 @@ export const useGalleryQuery = () => {
     selectedFacets,
   })
 
-  return useQuery<Query, Variables>(query, localizedVariables)
+  return useQuery<Query, Variables>(query, localizedVariables, {
+    onSuccess: (data) => {
+      if (data && term) {
+        sendAnalyticsEvent<IntelligentSearchQueryEvent>({
+          name: 'intelligent_search_query',
+          params: {
+            locale,
+            term,
+            url: window.location.href,
+            logicalOperator: data.search.metadata?.logicalOperator ?? 'and',
+            isTermMisspelled: data.search.metadata?.isTermMisspelled ?? false,
+            totalCount: data.search.products.pageInfo.totalCount,
+          },
+        })
+      }
+    },
+  })
 }
