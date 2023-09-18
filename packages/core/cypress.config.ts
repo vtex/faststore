d88a2460cf1bcbb0fb47ce3a1d26c3545ee539c2
Lucas Feijó <lucas.portela@vtex.com>
2023-09-18 12:02:29 -0300

    chore: Removes an auto-generated comment

--- packages/core/cypress.config.ts
+++ packages/core/cypress.config.ts
@@ -8,8 +8,6 @@ export default defineConfig({
   viewportWidth: 1000,
   viewportHeight: 600,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
