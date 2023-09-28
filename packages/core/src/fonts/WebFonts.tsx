145bf1128de37913773ecc63f24fd2171e828d7b
renatamottam <renatamotta.m@gmail.com>
2023-09-28 12:12:25 -0300

    feat: Add `Inter` as proposed font family

--- packages/core/src/fonts/WebFonts.tsx
+++ packages/core/src/fonts/WebFonts.tsx
@@ -6,7 +6,7 @@ function WebFonts() {
       {/* Add a <link> tag for your font-family of choice */}
       {/* <link
         rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
       /> */}
     </>
   )
