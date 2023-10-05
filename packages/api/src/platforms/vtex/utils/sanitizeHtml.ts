92d71c37bc80e34f5b6ed51fe0d974dd48a38134
Ícaro Azevedo <icazevedo10@gmail.com>
2023-10-05 16:38:26 -0300

    adjust solution

--- packages/api/src/platforms/vtex/utils/sanitizeHtml.ts
+++ packages/api/src/platforms/vtex/utils/sanitizeHtml.ts
@@ -1,109 +1,23 @@
 import sanitizeHtmlLib from 'sanitize-html'
 
+
 /**
- * This list was taken from Store Framework's Product Description app.
- * https://github.com/vtex-apps/store-components/blob/master/react/ProductDescription.tsx
+ * For now, we're using sanitize-html's default set
+ * of allowed tags and attributes, which don't even include img elements
  * 
- * This is a time-tested set of options that already works with the whole customer base of VTEX
- * customers using Store Framework.
+ * It is known many client depends on pontentially vulnerable tags, such as script tags
+ * We chose to be restrictive at first, and document those restrictions later.
+ * 
+ * When expanding the set of allowed tags and attributes, please consider performance, privacy and security.
+ * 
+ * This possibily breaks compatibility with Portal and Store Framework,
+ * which both allows an enormous amount of tags and attributes
+ * 
+ * This was a thoughtful decision that can be reviewed in the future given
+ * research was made to back up those changes.
  */
-const allowedTags = [
-  'a',
-  'abbr',
-  'article',
-  'b',
-  'blockquote',
-  'br',
-  'caption',
-  'code',
-  'del',
-  'details',
-  'div',
-  'em',
-  'figure',
-  'h1',
-  'h2',
-  'h3',
-  'h4',
-  'h5',
-  'h6',
-  'hr',
-  'header',
-  'footer',
-  'i',
-  'img',
-  'ins',
-  'iframe',
-  'kbd',
-  'li',
-  'main',
-  'mark',
-  'ol',
-  'p',
-  'picture',
-  'pre',
-  'section',
-  'source',
-  'span',
-  'strike',
-  'strong',
-  'sub',
-  'summary',
-  'sup',
-  'table',
-  'tbody',
-  'td',
-  'th',
-  'thead',
-  'tr',
-  'u',
-  'ul',
-  'link',
-  'body',
-  'html',
-  'style',
-  'link',
-  'script',
-  'head',
-  'meta',
-  'object',
-  'embed',
-]
-
-const allowedAttributes = {
-  '*': [
-    'id',
-    'title',
-    'accesskey',
-    'class',
-    'style',
-    'aria-label',
-    'width',
-    'height',
-    'hidden',
-  ],
-  a: ['href', 'name', 'target'],
-  iframe: ['allow', 'allowfullscreen', 'frameborder', 'src'],
-  img: ['src', 'alt'],
-  link: ['rel', 'type', 'href'],
-  td: ['colspan', 'rowspan', 'headers'],
-  meta: ['charset', 'name', 'content'],
-  object: ['type', 'height', 'width', 'data'],
-  embed: ['height', 'width', 'src'],
-}
-
-const allowedSchemes = ['http', 'https', 'mailto', 'tel']
-
-const allowedClasses = {}
-
 export const sanitizeHtml = (
   dirty: Parameters<typeof sanitizeHtmlLib>[0],
   options?: Parameters<typeof sanitizeHtmlLib>[1]
 ) =>
-  sanitizeHtmlLib(dirty, {
-    ...(options ?? {}),
-    allowedTags,
-    allowedAttributes,
-    allowedClasses,
-    allowedSchemes,
-  })
+  sanitizeHtmlLib(dirty, options)
