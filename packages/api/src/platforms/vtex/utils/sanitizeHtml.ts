13f47498fcc6d43bcd242285e46d74aeff7cec83
Ícaro Azevedo <icaro.azevedo@vtex.com.br>
2023-10-09 12:45:56 -0300

    feat(api): sanitize product description (#2050)
    ## What's the purpose of this pull request?
    
    This PR adds support for HTML in product descriptions.
    
    ## How it works?
    
    FastStore API sanitizes the product description in the backend. This
    ensures no possibly vulnerable tags, such as `script` tags are sent and
    executed on the frontend.
    
    The frontend now inserts the description as innerHTML, which allows it
    to be properly rendered.
    
    ## How to test it?
    
    Browse the two deploy previews below. Note on how one of the product
    descriptions, the HTML renders as text, and, in the other that uses this
    version of the packages it is interpreted as actual HTML.
    
    ### Starters Deploy Preview
    
    
    https://sfj-5d275d1--starter.preview.vtex.app/unbranded-frozen-pizza-1670312/p
    https://starter.vtex.app/unbranded-frozen-pizza-1670312/p
    
    ## References
    
    https://www.npmjs.com/package/sanitize-html

---
+++ packages/api/src/platforms/vtex/utils/sanitizeHtml.ts
@@ -0,0 +1,23 @@
+import sanitizeHtmlLib from 'sanitize-html'
+
+
+/**
+ * For now, we're using sanitize-html's default set
+ * of allowed tags and attributes, which don't even include img elements
+ * 
+ * It is known many client depends on pontentially vulnerable tags, such as script tags
+ * We chose to be restrictive at first, and document those restrictions later.
+ * 
+ * When expanding the set of allowed tags and attributes, please consider performance, privacy and security.
+ * 
+ * This possibily breaks compatibility with Portal and Store Framework,
+ * which both allows an enormous amount of tags and attributes
+ * 
+ * This was a thoughtful decision that can be reviewed in the future given
+ * research was made to back up those changes.
+ */
+export const sanitizeHtml = (
+  dirty: Parameters<typeof sanitizeHtmlLib>[0],
+  options?: Parameters<typeof sanitizeHtmlLib>[1]
+) =>
+  sanitizeHtmlLib(dirty, options)
