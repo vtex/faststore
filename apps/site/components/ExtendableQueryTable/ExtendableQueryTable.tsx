59f909d3f32fb15692f41e90dde311f9c74adc4c
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-10-10 11:57:32 -0300

    chore: updates ExtendableQueryTable prop and format

--- apps/site/components/ExtendableQueryTable/ExtendableQueryTable.tsx
+++ apps/site/components/ExtendableQueryTable/ExtendableQueryTable.tsx
@@ -1,32 +1,24 @@
-import { ReactNode } from "react"
+import { PropsWithChildren } from 'react'
 
-export type ExtendableQueryTableProps = {
-    children: ReactNode
-}
-
-const ExtendableQueryTable = ({ children }: ExtendableQueryTableProps) => {
-    return (
-        <table className="nx-simple-table">
-            <thead>
-                <tr>
-                    <th>Fragment</th>
-                    <th>Side</th>
-                    <th>Query operation</th>
-                    <th>Page</th>
-                    <th>Where is used</th>
-                    <th>Return</th>
-                    <th>Parameters</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    {
-                        children
-                    }
-                </tr>
-            </tbody>
-        </table>
-    )
+const ExtendableQueryTable = ({ children }: PropsWithChildren) => {
+  return (
+    <table className="nx-simple-table">
+      <thead>
+        <tr>
+          <th>Fragment</th>
+          <th>Side</th>
+          <th>Query operation</th>
+          <th>Page</th>
+          <th>Where is used</th>
+          <th>Return</th>
+          <th>Parameters</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>{children}</tr>
+      </tbody>
+    </table>
+  )
 }
 
 export default ExtendableQueryTable
