ada0527af176134b9b99f4db310f5f0a5a60fff5
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2023-11-01 09:15:41 -0300

    No need to copy the node_modules folder

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -2,7 +2,7 @@ import { Command } from '@oclif/core'
 import chalk from 'chalk'
 import { spawnSync } from 'child_process'
 import { existsSync } from 'fs'
-import { moveSync, removeSync } from 'fs-extra'
+import { copySync, moveSync, removeSync } from 'fs-extra'
 import { tmpDir, userDir } from '../utils/directory'
 import { generate } from '../utils/generate'
 
@@ -20,33 +20,49 @@ export default class Build extends Command {
       process.exit(yarnBuildResult.status)
     }
 
-    await copyResource(`${tmpDir}/.next`, `${userDir}/.next`)
-    await copyResource(`${tmpDir}/public`, `${userDir}/public`)
-    await copyResource(
+    await moveResource(`${tmpDir}/.next`, `${userDir}/.next`)
+    await moveResource(`${tmpDir}/public`, `${userDir}/public`)
+    await moveResource(
       `${tmpDir}/lighthouserc.js`,
       `${userDir}/lighthouserc.js`
     )
-    await copyResource(
+    await moveResource(
       `${tmpDir}/cms-webhook-urls.json`,
       `${userDir}/cms-webhook-urls.json`
     )
     
-    if (existsSync(`.next/standalone`)) {
-      await copyResource(
-        `${userDir}/node_modules`,
-        `.next/standalone/node_modules`
-      )
-    }
+    // if (existsSync(`.next/standalone`)) {
+    //   await copyResource(
+    //     `${userDir}/node_modules`,
+    //     `.next/standalone/node_modules`
+    //   )
+    // }
   }
 }
 
-async function copyResource(from: string, to: string) {
+async function moveResource(from: string, to: string) {
   try {
     if (existsSync(to)) {
       removeSync(to)
     }
 
     moveSync(from, to)
+    console.log(
+      `${chalk.green('success')} - ${chalk.dim(from)} copied to ${chalk.dim(
+        to
+      )}`
+    )
+  } catch (err) {
+    console.error(`${chalk.red('error')} - ${err}`)
+  }
+}
+async function copyResource(from: string, to: string) {
+  try {
+    if (existsSync(to)) {
+      removeSync(to)
+    }
+
+    copySync(from, to)
     console.log(
       `${chalk.green('success')} - ${chalk.dim(from)} copied to ${chalk.dim(
         to
