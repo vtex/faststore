b53663d01c7d68dc8f3128d974b2144d533d84e8
Emerson Laurentino <emerson@laurentino.co>
2023-11-14 11:59:35 -0300

    chore: remove storybook from core

--- packages/core/.storybook/mocks/index.ts
+++
@@ -1,5 +0,0 @@
-import './searchHistory'
-
-export { product } from './product'
-export { productGridItems } from './productGridItems'
-export { searchTerms } from './searchTerms'
--- packages/core/.storybook/mocks/searchHistory.ts
+++
@@ -1,8 +0,0 @@
-import { searchHistoryStore } from 'src/sdk/search/useSearchHistory'
-
-searchHistoryStore.set([
-  { term: 'headphone', path: '/' },
-  { term: 'audio & video', path: '/' },
-  { term: 'mh-7000', path: '/' },
-  { term: 'jbl go', path: '/' },
-])
