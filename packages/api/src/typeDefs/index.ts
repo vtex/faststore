3500134703bcacd928f312ed7b2ad0a638be1ff6
Ícaro Azevedo <icazevedo10@gmail.com>
2023-12-06 12:27:02 -0300

    chore: remove tsdx from api, upgrade node to 18 in all packages

--- packages/api/src/typeDefs/index.ts
+++ packages/api/src/typeDefs/index.ts
@@ -1,63 +1,8 @@
 import { print } from 'graphql'
 
-import Address from './address.graphql'
-import AggregateOffer from './aggregateOffer.graphql'
-import AggregateRating from './aggregateRating.graphql'
-import Author from './author.graphql'
-import Brand from './brand.graphql'
-import Breadcrumb from './breadcrumb.graphql'
-import Cart from './cart.graphql'
-import Collection from './collection.graphql'
-import Facet from './facet.graphql'
-import Image from './image.graphql'
-import Mutation from './mutation.graphql'
-import Newsletter from './newsletter.graphql'
-import ObjectOrString from './objectOrString.graphql'
-import Offer from './offer.graphql'
-import Order from './order.graphql'
-import Organization from './organization.graphql'
-import PageInfo from './pageInfo.graphql'
-import Person from './person.graphql'
-import Product from './product.graphql'
-import ProductGroup from './productGroup.graphql'
-import PropertyValue from './propertyValue.graphql'
-import Query from './query.graphql'
-import Review from './review.graphql'
-import Seo from './seo.graphql'
-import Session from './session.graphql'
-import ShippingSimulation from './shipping.graphql'
-import SkuVariants from './skuVariants.graphql'
-import Status from './status.graphql'
+import { loadFilesSync } from '@graphql-tools/load-files'
 
-export const typeDefs = [
-  Query,
-  Mutation,
-  Address,
-  Brand,
-  Breadcrumb,
-  Collection,
-  Facet,
-  Image,
-  PageInfo,
-  Product,
-  Seo,
-  Offer,
-  AggregateRating,
-  Review,
-  Author,
-  ProductGroup,
-  Organization,
-  AggregateOffer,
-  Order,
-  Cart,
-  Status,
-  PropertyValue,
-  Person,
-  ObjectOrString,
-  Session,
-  Newsletter,
-  SkuVariants,
-  ShippingSimulation,
-]
+// Empty string ('') is interpreted as the current dir. Referencing it as './' won't work.
+export const typeDefs = loadFilesSync('', { extensions: ['.graphql'] })
   .map(print)
   .join('\n')
--- packages/api/tsdx.config.js
+++
@@ -1,9 +0,0 @@
-const graphql = require('@rollup/plugin-graphql')
-
-module.exports = {
-  rollup(config) {
-    config.plugins.push(graphql())
-
-    return config
-  },
-}
