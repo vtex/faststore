524c1e55064e9852776950fe1a031aeffad95957
Ícaro Azevedo <icazevedo10@gmail.com>
2023-12-07 13:08:33 -0300

    feat: change cookie domain for faststore api's host

--- packages/api/src/platforms/vtex/utils/cookies.ts
+++ packages/api/src/platforms/vtex/utils/cookies.ts
@@ -1,7 +1,18 @@
 import type { Context } from '../index'
 
+const MATCH_DOMAIN_REGEXP = /(?:^|;\s*)(?:domain=)([^;]+)/i
+
 export const setCookie = (headers: Headers, ctx: Context) => {
-  headers.getSetCookie().forEach((cookie) => ctx.storage.cookies.add(cookie))
+  const faststoreAPIHostname = new URL(`https://${ctx.headers.host}`).hostname
+
+  headers
+    .getSetCookie()
+    .forEach((cookie) =>
+      ctx.storage.cookies.add(
+        // Replaces original cookie domain for FastStore API's domain hostname
+        cookie.replace(MATCH_DOMAIN_REGEXP, `; domain=${faststoreAPIHostname}`)
+      )
+    )
 }
 
 export const getStoreCookie = (ctx: Context) => (headers: Headers) =>
