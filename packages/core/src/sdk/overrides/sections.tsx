30a560546f6230044c1942e7a6398233f9be60a7
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-12-11 20:31:40 -0300

    chore: adds new section to overrides/sections list

--- packages/core/src/sdk/overrides/sections.tsx
+++ packages/core/src/sdk/overrides/sections.tsx
@@ -1,11 +1,14 @@
 import Alert from '../../components/sections/Alert'
+import ProductShelf from '../../components/ui/ProductShelf'
 
 import type { DefaultSectionComponentsDefinitions } from '../../typings/overridesDefinition'
 import type { SectionsOverrides } from '../../typings/overrides'
 import { AlertDefaultComponents } from '../../components/sections/Alert/DefaultComponents'
+import { ProductShelfDefaultComponents } from '../../components/sections/ProductShelf/DefaultComponents'
 
 export const Sections = {
   Alert,
+  ProductShelf,
 }
 
 export const DefaultComponents: Partial<
@@ -15,4 +18,5 @@ export const DefaultComponents: Partial<
   >
 > = {
   Alert: AlertDefaultComponents,
+  ProductShelf: ProductShelfDefaultComponents,
 }
