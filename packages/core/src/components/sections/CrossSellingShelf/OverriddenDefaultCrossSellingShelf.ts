ef46c9fbffa5af0f10b261edf2088634ced8c9c5
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-12-13 18:52:53 -0300

    chore: removes type conversion

--- packages/core/src/components/sections/CrossSellingShelf/OverriddenDefaultCrossSellingShelf.ts
+++ packages/core/src/components/sections/CrossSellingShelf/OverriddenDefaultCrossSellingShelf.ts
@@ -1,6 +1,5 @@
 import { getOverriddenSection } from 'src/sdk/overrides/getOverriddenSection'
-import type { SectionOverrideDefinition } from 'src/typings/overridesDefinition'
 
 export const OverriddenDefaultCrossSellingShelf = getOverriddenSection({
   section: 'CrossSellingShelf',
-} as SectionOverrideDefinition<'CrossSellingShelf'>)
+})
