97d57bbc6e2273059fdc9a5fc93c86b9961a9a3b
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-12-21 19:33:32 -0300

    chore: adds DefaultComponents

---
+++ packages/core/src/components/sections/Navbar/DefaultComponents.ts
@@ -0,0 +1,27 @@
+import {
+  Navbar as UINavbar,
+  NavbarLinks as UINavbarLinks,
+  NavbarLinksList as UINavbarLinksList,
+  NavbarSlider as UINavbarSlider,
+  NavbarSliderHeader as UINavbarSliderHeader,
+  NavbarSliderContent as UINavbarSliderContent,
+  NavbarSliderFooter as UINavbarSliderFooter,
+  NavbarHeader as UINavbarHeader,
+  NavbarRow as UINavbarRow,
+  NavbarButtons as UINavbarButtons,
+  IconButton as UIIconButton,
+} from '@faststore/ui'
+
+export const NavbarDefaultComponents = {
+  Navbar: UINavbar,
+  NavbarLinks: UINavbarLinks,
+  NavbarLinksList: UINavbarLinksList,
+  NavbarSlider: UINavbarSlider,
+  NavbarSliderHeader: UINavbarSliderHeader,
+  NavbarSliderContent: UINavbarSliderContent,
+  NavbarSliderFooter: UINavbarSliderFooter,
+  NavbarHeader: UINavbarHeader,
+  NavbarRow: UINavbarRow,
+  NavbarButtons: UINavbarButtons,
+  IconButton: UIIconButton,
+} as const
