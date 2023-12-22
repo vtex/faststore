4d6bf14e6304b4f39f1464c3da4641834f346407
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-12-22 19:00:57 -0300

    chore: makes EmptyState generic

--- packages/core/src/components/sections/EmptyState/EmptyState.tsx
+++ packages/core/src/components/sections/EmptyState/EmptyState.tsx
@@ -1,5 +1,7 @@
-import { ReactNode } from 'react'
 import type { PropsWithChildren } from 'react'
+import { useRouter } from 'next/router'
+
+import { Icon as UIIcon, Loader as UILoader } from '@faststore/ui'
 
 import { useOverrideComponents } from 'src/sdk/overrides/OverrideContext'
 
@@ -7,25 +9,90 @@ import Section from '../Section'
 import styles from './section.module.scss'
 
 export interface EmptyStateProps {
+  /**
+   * Title for the `EmptyState` component.
+   */
   title: string
-  titleIcon?: ReactNode
+  /**
+   * A React component that will be rendered as an icon.
+   */
+  titleIcon?: {
+    icon: string
+    alt: string
+  }
+  /**
+   * Subtitle for the `EmptyState` component.
+   */
+  subtitle?: string
+  /**
+   * Boolean that makes the loader be shown.
+   */
+  showLoader?: boolean
+  /**
+   * Object that manages the error state descriptions.
+   */
+  errorState?: {
+    errorId?: {
+      show?: boolean
+      description?: string
+    }
+    fromUrl?: {
+      show?: boolean
+      description?: string
+    }
+  }
+}
+
+const useErrorState = () => {
+  const router = useRouter()
+  const { errorId, fromUrl } = router.query
+
+  return {
+    errorId,
+    fromUrl,
+  }
 }
 
 function EmptyState({
   title,
   titleIcon,
   children,
+  subtitle,
+  errorState,
+  showLoader = false,
 }: PropsWithChildren<EmptyStateProps>) {
   const { EmptyState: EmptyStateWrapper } =
     useOverrideComponents<'EmptyState'>()
+  const { errorId, fromUrl } = useErrorState()
+
+  const icon = !!titleIcon?.icon ? (
+    <UIIcon
+      name={titleIcon?.icon}
+      aria-label={titleIcon?.alt}
+      width={56}
+      height={56}
+      weight="thin"
+    />
+  ) : (
+    EmptyStateWrapper.props.titleIcon
+  )
+
   return (
     <Section className={`${styles.section} section-empty-state`}>
       <EmptyStateWrapper.Component
         bkgColor="light"
         {...EmptyStateWrapper.props}
         title={title ?? EmptyStateWrapper.props.title}
-        titleIcon={titleIcon ?? EmptyStateWrapper.props.titleIcon}
+        titleIcon={icon}
       >
+        {!!subtitle && <h2>{subtitle}</h2>}
+        {!!errorState?.errorId?.show && (
+          <p>{`${errorState?.errorId?.description} ${errorId}`}</p>
+        )}
+        {!!errorState?.fromUrl?.show && (
+          <p>{`${errorState?.fromUrl?.description} ${fromUrl}`}</p>
+        )}
+        {showLoader && <UILoader />}
         {children}
       </EmptyStateWrapper.Component>
     </Section>
