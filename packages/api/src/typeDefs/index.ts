b6dfe45088bf37585de2167458c21632b8d03315
Ícaro Azevedo <icazevedo10@gmail.com>
2023-12-28 16:46:01 -0300

    fix: use __dirname to apropriately load graphql files

--- packages/api/src/typeDefs/index.ts
+++ packages/api/src/typeDefs/index.ts
@@ -2,7 +2,6 @@ import { print } from 'graphql'
 
 import { loadFilesSync } from '@graphql-tools/load-files'
 
-// Empty string ('') is interpreted as the current dir. Referencing it as './' won't work.
-export const typeDefs = loadFilesSync('', { extensions: ['.graphql'] })
+export const typeDefs = loadFilesSync(__dirname, { extensions: ['.graphql'] })
   .map(print)
   .join('\n')
