d252370fab04f2f1568a51a1fe5fb2378641a49b
eduardoformiga <eduardo.formiga@gmail.com>
2024-01-08 16:19:05 -0300

    chore: removes node_modules symlink

--- packages/cli/src/utils/generate.ts
+++ packages/cli/src/utils/generate.ts
@@ -6,7 +6,6 @@ import {
   readdirSync,
   readFileSync,
   removeSync,
-  symlinkSync,
   writeFileSync,
   writeJsonSync,
 } from 'fs-extra'
@@ -19,12 +18,10 @@ import {
   tmpCMSDir,
   tmpDir,
   tmpFolderName,
-  tmpNodeModulesDir,
   tmpStoreConfigFileDir,
   tmpThemesCustomizationsFileDir,
   tmpCmsWebhookUrlsFileDir,
   userCMSDir,
-  userNodeModulesDir,
   userSrcDir,
   userStoreConfigFileDir,
   userThemesFileDir,
@@ -253,21 +250,6 @@ function mergeCMSFiles() {
   mergeCMSFile('sections.json')
 }
 
-function createNodeModulesSymbolicLink() {
-  try {
-    symlinkSync(userNodeModulesDir, tmpNodeModulesDir)
-    console.log(
-      `${chalk.green('success')} - Symbolic ${chalk.dim(
-        'node_modules'
-      )} link created from ${chalk.dim(userNodeModulesDir)} to ${chalk.dim(
-        tmpNodeModulesDir
-      )}`
-    )
-  } catch (err) {
-    console.error(`${chalk.red('error')} - ${err}`)
-  }
-}
-
 export async function generate(options?: GenerateOptions) {
   const { setup = false } = options ?? {}
 
@@ -279,7 +261,6 @@ export async function generate(options?: GenerateOptions) {
       copyCoreFiles(),
       copyCypressFiles(),
       copyPublicFiles(),
-      createNodeModulesSymbolicLink(),
     ])
   }
 
