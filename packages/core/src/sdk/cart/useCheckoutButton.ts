60112db57c2b9efbfa5be85f397e024b849fa40c
beatrizmaselli <beatriz.maselli@vtex.com.br>
2024-02-12 16:33:23 +0100

    Revert previously commit

--- packages/core/src/sdk/cart/useCheckoutButton.ts
+++ packages/core/src/sdk/cart/useCheckoutButton.ts
@@ -2,20 +2,13 @@ import storeConfig from '../../../faststore.config'
 import { useCart } from './index'
 
 export const useCheckoutButton = () => {
-  const { isValidating, id } = useCart()
+  const { isValidating } = useCart()
 
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    const isDevEnv =
-      window.location.host.includes('.vtex.app') ||
-      window.location.host.includes('localhost')
 
     if (!isValidating) {
-      if (!isDevEnv) {
-        window.location.href = `${storeConfig.checkoutUrl}`
-      } else if (id) {
-        window.location.href = `${storeConfig.checkoutUrl}?orderFormId=${id}`
-      }
+      window.location.href = `${storeConfig.checkoutUrl}`
     }
   }
 
