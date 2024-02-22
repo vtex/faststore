b1b481f000d88461920676bd7d65fd0cc2ab788e
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-02-22 20:25:42 -0300

    Add support for `--dry-run` to `cms-sync` command

--- packages/cli/src/commands/cms-sync.ts
+++ packages/cli/src/commands/cms-sync.ts
@@ -1,12 +1,22 @@
-import { Command } from '@oclif/core'
+import { Command, Flags } from '@oclif/core'
 import { spawn } from 'child_process'
 import { tmpDir } from '../utils/directory'
 import { generate, generateCMSFiles } from '../utils/generate'
 
 export default class CmsSync extends Command {
+  static flags = {
+    ['dry-run']: Flags.boolean({ char: 'd' }),
+  }
+
   async run() {
+    const { flags } = await this.parse(CmsSync)
+
     await generate({ setup: true })
     await generateCMSFiles()
+
+    if (flags['dry-run']) {
+      return
+    }
 
     return spawn(`vtex cms sync faststore`, {
       shell: true,
