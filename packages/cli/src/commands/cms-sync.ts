2cfbeb01d684f6ab9edb05fd7705d766d7a4909b
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-02-22 17:22:02 -0300

    Use new `generateCMSFiles` function in `cms-sync` command

--- packages/cli/src/commands/cms-sync.ts
+++ packages/cli/src/commands/cms-sync.ts
@@ -1,10 +1,12 @@
 import { Command } from '@oclif/core'
 import { spawn } from 'child_process'
 import { tmpDir } from '../utils/directory'
-import { generate } from '../utils/generate'
+import { generate, generateCMSFiles } from '../utils/generate'
+
 export default class CmsSync extends Command {
   async run() {
     await generate({ setup: true })
+    await generateCMSFiles()
 
     return spawn(`vtex cms sync faststore`, {
       shell: true,
