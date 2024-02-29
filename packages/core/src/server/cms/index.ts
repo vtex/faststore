24c9284d33d071f97d5523fda97c0de47ce9c9b9
Eduardo Formiga <eduardo.formiga@gmail.com>
2024-02-29 14:30:50 -0300

    Update packages/core/src/server/cms/index.ts

--- packages/core/src/server/cms/index.ts
+++ packages/core/src/server/cms/index.ts
@@ -42,7 +42,7 @@ const isLocator = (x: any): x is Locator =>
 
 export const clientCMS = new ClientCMS({
   workspace: config.api.workspace,
-  tenant: 'vendemo',
+  tenant: config.api.storeId,
 })
 
 export const getCMSPage = async (options: Options) => {
