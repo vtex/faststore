6aebfb8d67b51499239e82b20be33c9de9189582
Eduardo Formiga <eduardo.formiga@gmail.com>
2024-03-01 10:46:14 -0300

    Update packages/core/faststore.config.default.js

--- packages/core/faststore.config.default.js
+++ packages/core/faststore.config.default.js
@@ -14,7 +14,7 @@ module.exports = {
 
   // Platform specific configs for API
   api: {
-    storeId: 'lojavivara',
+    storeId: 'storeframework',
     workspace: 'master',
     environment: 'vtexcommercestable',
     hideUnavailableItems: false,
