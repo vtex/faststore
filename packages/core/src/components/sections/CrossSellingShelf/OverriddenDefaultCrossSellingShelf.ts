bac8eb79c45cbd469d2e0bbb2bb95aa95a5beea9
Tiago Freire <freire.tiago@gmail.com>
2024-04-07 22:40:07 -0300

    feat: allows to override __experimentalProductCard of CrossSellingShelf

--- packages/core/src/components/sections/CrossSellingShelf/OverriddenDefaultCrossSellingShelf.ts
+++ packages/core/src/components/sections/CrossSellingShelf/OverriddenDefaultCrossSellingShelf.ts
@@ -1,6 +1,9 @@
+import { override } from 'src/customizations/src/components/overrides/ProductShelf'
 import { getOverriddenSection } from 'src/sdk/overrides/getOverriddenSection'
+import type { SectionOverrideDefinitionV1 } from 'src/typings/overridesDefinition'
 import CrossSellingShelf from '.'
 
 export const OverriddenDefaultCrossSellingShelf = getOverriddenSection({
+  ...(override as SectionOverrideDefinitionV1<'CrossSellingShelf'>),
   Section: CrossSellingShelf,
 })
