d1abc2a7f466f2c56f21647b74f8037a58d4fdd2
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2024-04-15 17:56:46 -0300

    Fix missing Checkbox props on CheckboxField component (#2281)
    ## What's the purpose of this pull request?
    
    Our `CheckboxField` was inconsistent with its documentation. It was not
    accepting the `checked`, `disabled`, and `partial` props from Checkbox.
    This PR fixes this.
    
    As a bonus, I've added Jest and React Testing Library so we can start
    adding those tests to our components. I've first created a failing test
    and then implemented the props.
    
    ## How to test it?
    
    Just run `yarn test` and you should have all success specs.
    
    ### Starters Deploy Preview
    
    For some reason we don't use the `CheckboxField` on our starter.
    
    ---------
    
    Co-authored-by: eduardoformiga <eduardo.formiga@gmail.com>

--- packages/components/src/molecules/CheckboxField/CheckboxField.tsx
+++ packages/components/src/molecules/CheckboxField/CheckboxField.tsx
@@ -1,38 +1,36 @@
 import React, { forwardRef } from 'react'
-import Checkbox from '../../atoms/Checkbox'
+import Checkbox, { CheckboxProps } from '../../atoms/Checkbox'
 import Label from '../../atoms/Label'
 
-export interface CheckboxFieldProps {
-  /**
-   * ID to find this component in testing tools (e.g.: cypress, testing library, and jest).
-   */
-  testId?: string
-  /**
-   * ID to identify input and corresponding label.
-   */
-  id: string
+export interface CheckboxFieldProps extends CheckboxProps {
   /**
    * The text displayed to identify the input checkbox.
    */
   label: string
-  /**
-   * The value to identify the input checkbox.
-   */
-  value?: string
-  /**
-   * Identify checkbox in the same group.
-   */
-  name?: string
 }
 
 const CheckboxField = forwardRef<HTMLDivElement, CheckboxFieldProps>(
   function CheckboxField(
-    { testId = 'fs-checkbox-field', id, label, value, name, ...otherProps },
+    {
+      testId = 'fs-checkbox-field',
+      id,
+      label,
+      value,
+      name,
+      checked,
+      ...otherProps
+    },
     ref
   ) {
     return (
       <div ref={ref} data-fs-checkbox-field data-testid={testId}>
-        <Checkbox id={id} value={value ?? label} name={name} {...otherProps} />
+        <Checkbox
+          id={id}
+          value={value ?? label}
+          name={name}
+          defaultChecked={checked}
+          {...otherProps}
+        />
         <Label htmlFor={id}>{label}</Label>
       </div>
     )
