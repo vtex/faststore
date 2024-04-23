c2182ee73b75a83fe09159e0d9a369a9b88d4db1
eduardoformiga <eduardo.formiga@gmail.com>
2024-04-23 17:55:49 -0300

    feat: tweaks import and adds comments

--- packages/core/src/components/sections/CrossSellingShelf/OverriddenDefaultCrossSellingShelf.ts
+++ packages/core/src/components/sections/CrossSellingShelf/OverriddenDefaultCrossSellingShelf.ts
@@ -1,8 +1,13 @@
-import { override } from 'src/customizations/src/components/overrides/ProductShelf'
+import { override } from 'src/customizations/src/components/overrides/CrossSellingShelf'
 import { getOverriddenSection } from 'src/sdk/overrides/getOverriddenSection'
 import type { SectionOverrideDefinitionV1 } from 'src/typings/overridesDefinition'
 import CrossSellingShelf from '.'
 
+/**
+ * This component exists to support overrides 1.0
+ *
+ * This allows users to override the default CrossSellingShelf section present in the Headless CMS
+ */
 export const OverriddenDefaultCrossSellingShelf = getOverriddenSection({
   ...(override as SectionOverrideDefinitionV1<'CrossSellingShelf'>),
   Section: CrossSellingShelf,
