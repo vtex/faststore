2c7825910ea7f14155cc9dd908e2e7ddb7a1703f
Larícia Mota <laricia.mota@vtex.com.br>
2024-04-25 16:19:55 -0300

    Add loading eager to first product

--- packages/core/src/components/product/ProductGrid/ProductGrid.tsx
+++ packages/core/src/components/product/ProductGrid/ProductGrid.tsx
@@ -49,6 +49,7 @@ function ProductGrid({
                 width: 150,
                 height: 150,
                 sizes: '30vw',
+                loading: idx === 0 ? 'eager' : 'lazy',
               }}
               {...ProductCard.props}
               bordered={bordered ?? ProductCard.props.bordered}
