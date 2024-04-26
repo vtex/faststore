43c1118a5016c9915f23439cd227d23a8eb67fa1
Fanny <fanny.chien@vtex.com.br>
2024-04-26 19:11:02 -0300

    chore: Verifies if fs- prefix already exists

--- packages/components/src/transform.tsx
+++ packages/components/src/transform.tsx
@@ -32,11 +32,12 @@ export default function transformer(file: FileInfo, api: API) {
               attr.value.type === 'StringLiteral'
             ) {
               const iconName = attr.value.value
+              if (!iconName.startsWith('fs-')) {
               const iconDefinition = `fs-${kebabCase(iconName)}`
               return j.jsxAttribute(
                 j.jsxIdentifier('name'),
                 j.literal(iconDefinition)
-              )
+              )}
             }
             return attr
           })
@@ -45,60 +46,41 @@ export default function transformer(file: FileInfo, api: API) {
             (attr) =>
               attr.type !== 'JSXAttribute' || attr.name.name !== 'weight'
           )
-    })
 
-  // Step 3: Convert width and height to size prop
-  root
-    .find(j.JSXElement, {
-      openingElement: {
-        name: {
-          name: 'Icon',
-        },
-      },
-    })
-    .forEach((elementPath) => {
-      const widthAttr = elementPath.node.openingElement.attributes?.find(
-        (attr: any) => attr.name.name === 'width'
-      ) as any
-      const heightAttr = elementPath.node.openingElement.attributes?.find(
-        (attr: any) => attr.name.name === 'height'
-      ) as any
+          // Step 3: Convert width and height to size prop
+          const widthAttr = elementPath.node.openingElement.attributes?.find(
+            (attr: any) => attr.name.name === 'width'
+          ) as any
+          const heightAttr = elementPath.node.openingElement.attributes?.find(
+            (attr: any) => attr.name.name === 'height'
+          ) as any
 
-      if (widthAttr || heightAttr) {
-        const sizeValue = widthAttr
-          ? widthAttr.value?.expression?.value
-          : heightAttr?.value?.expression?.value
-
-        if (sizeValue) {
-          // Size value can only be 20 | 24 | 32 , other size sets default to 20
-
-          let newSizeValue;
-          
-          if ([20, 24, 32].includes(sizeValue)) {
-            newSizeValue = sizeValue;
-          } 
-          else {
-            newSizeValue = 20;
+          if (widthAttr || heightAttr) {
+            const sizeValue = widthAttr
+              ? widthAttr.value?.expression?.value
+              : heightAttr?.value?.expression?.value
+    
+            if (sizeValue) {
+              const newSizeValue = [20, 24, 32].includes(sizeValue) ? sizeValue : 20
+    
+              // Create size prop
+              const sizeProp = j.jsxAttribute(
+                j.jsxIdentifier('size'),
+                // To pass as expression size={20}
+                j.jsxExpressionContainer(j.literal(newSizeValue))
+              )
+    
+              // Replace width/height with size prop
+              elementPath.node.openingElement.attributes = [
+                ...(elementPath.node.openingElement.attributes || []).filter(
+                  (attr: any) =>
+                    attr.name.name !== 'width' && attr.name.name !== 'height'
+                ),
+                sizeProp,
+              ]
+            }
           }
-
-          // Create size prop
-          const sizeProp = j.jsxAttribute(
-            j.jsxIdentifier('size'),
-            // To pass as expression size={20}
-            j.jsxExpressionContainer(j.literal(newSizeValue))
-          )
-
-          // Replace width/height with size prop
-          elementPath.node.openingElement.attributes = [
-            ...(elementPath.node.openingElement.attributes || []).filter(
-              (attr: any) =>
-                attr.name.name !== 'width' && attr.name.name !== 'height'
-            ),
-            sizeProp,
-          ]
-        }
-      }
+          
     })
-
   return root.toSource()
 }
