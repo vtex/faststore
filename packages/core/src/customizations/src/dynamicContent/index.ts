7bf64b1af4c95351c4cb8edb280c0ba837575851
eduardoformiga <eduardo.formiga@gmail.com>
2024-04-30 11:13:00 -0300

    chore: adds aditional instructions in the placeholder dynamicContent

--- packages/core/src/customizations/src/dynamicContent/index.ts
+++ packages/core/src/customizations/src/dynamicContent/index.ts
@@ -1,7 +1,16 @@
-// starter.store code that will be copied to .faststore (core)
-// dynamicContent/index.ts
+// starter.store code that will be copied to .faststore (core) dynamicContent/index.ts
 
-// map the slug to correspondent functions
+// Refer to the Dynamic Content documentation at https://developers.vtex.com/docs/guides/faststore/dynamic-content-overview
+// for mapping the page and the corresponding data-fetching function.
+
+// Example:
+// const dynamicContent = {
+//   home: fetchDataHomepage,
+//   "my-landing-page": fetchDataMyLandingPage,
+// };
+
+// export default dynamicContent;
+
 const dynamicContent = {}
 
 export default dynamicContent
