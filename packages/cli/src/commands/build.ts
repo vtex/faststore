d2bedf0a669e87f630f8f4381dbff46248cb4332
Emerson Laurentino <emerson@laurentino.co>
2024-05-02 10:10:19 -0300

    Update build.ts to include fast checkout component building logic

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -2,8 +2,8 @@ import { Command } from '@oclif/core'
 import chalk from 'chalk'
 import { spawnSync } from 'child_process'
 import { existsSync } from 'fs'
-import { copySync, removeSync, moveSync, readdirSync } from 'fs-extra'
-import { tmpDir, userDir } from '../utils/directory'
+import { copySync, moveSync, readdirSync, removeSync } from 'fs-extra'
+import { tmpCustomizationsSrcDir, tmpDir, userDir } from '../utils/directory'
 import { generate } from '../utils/generate'
 
 export default class Build extends Command {
@@ -22,7 +22,31 @@ export default class Build extends Command {
 
     await normalizeStandaloneBuildDir()
     await copyResources()
+    buildFastCheckoutComponents()
   }
+}
+
+/**
+ * TODO: generate the arctifacts to be deployed by the fast checkout
+ *
+ * here you can run the webpack, but before you'll need to create a allow list
+ * with the components that will be used in the fast checkout
+ * today we have a restriction that the component must has the name configured
+ * in the CMS, but the filename can be different, so we need to look for the
+ * component name that is being exported in the file index.tsx in the components
+ * folder and then we can build the fast checkout components
+ */
+function buildFastCheckoutComponents() {
+  const allowList = ['Summary']
+
+  const components = readdirSync(`${tmpCustomizationsSrcDir}/components`)
+  const allowedComponents = components.filter((component) => {
+    const componentName = component.split('.')[0]
+    return allowList.includes(componentName)
+  })
+
+  console.log('components', components)
+  console.log('allowedComponents', allowedComponents)
 }
 
 async function copyResource(from: string, to: string) {
