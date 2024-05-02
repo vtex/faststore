7902721e4d8a3df2b83346ec196e08dcd8c0fbda
eduardoformiga <eduardo.formiga@gmail.com>
2024-05-02 18:22:35 -0300

    chore: adds RootLayout

--- packages/core/app/layout.tsx
+++ packages/core/app/layout.tsx
@@ -1,11 +1,76 @@
-export default function RootLayout({
+// FastStore UI's base styles
+import 'src/styles/global/index.scss'
+
+import 'src/customizations/src/themes/index.scss'
+
+import ThirdPartyScripts from 'app/components/ThirdPartyScripts'
+import AnalyticsHandler from 'app/sdk/analytics'
+import ErrorBoundary from 'app/sdk/error/ErrorBoundary'
+import UIProvider from 'app/sdk/ui/UIProvider'
+import { WebFonts } from 'src/customizations/src/GlobalOverrides'
+
+import GlobalSections from './components/cms/GlobalSections'
+
+export const GLOBAL_SECTIONS_CONTENT_TYPE = 'globalSections'
+
+import { Section } from '@vtex/client-cms'
+import storeConfig from 'faststore.config'
+import { PageContentType, getPage } from 'src/server/cms'
+
+export type GlobalSectionsData = {
+  sections: Section[]
+}
+
+async function getGlobalSectionsData() {
+  if (storeConfig.cms.data) {
+    const cmsData = JSON.parse(storeConfig.cms.data)
+    const page = cmsData[GLOBAL_SECTIONS_CONTENT_TYPE][0]
+
+    if (page) {
+      const pageData = await getPage<PageContentType>({
+        contentType: GLOBAL_SECTIONS_CONTENT_TYPE,
+        documentId: page.documentId,
+        versionId: page.versionId,
+      })
+
+      return pageData
+    }
+  }
+
+  {
+    /* TODO: we should use DraftMode instead of preview mode in Next 13 */
+  }
+  const { sections } = await getPage<PageContentType>({
+    contentType: GLOBAL_SECTIONS_CONTENT_TYPE,
+  })
+
+  return { sections }
+}
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const globalSections = await getGlobalSectionsData()
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ErrorBoundary>
+      {/* TODO: we should use metadata api from Next 13 */}
+      {/* <DefaultSeo norobots={storeConfig.experimental.noRobots} /> */}
+
+      <AnalyticsHandler />
+
+      <html>
+        <head>
+          {!process.env.DISABLE_3P_SCRIPTS && <ThirdPartyScripts />}
+          <WebFonts />
+        </head>
+        <body className="theme">
+          <UIProvider>
+            <GlobalSections {...globalSections}>{children}</GlobalSections>
+          </UIProvider>
+        </body>
+      </html>
+    </ErrorBoundary>
   )
 }
