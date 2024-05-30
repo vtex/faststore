0fe2e0cab024d6faeb381b92b6331fc606a4a75b
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2024-05-30 17:20:39 -0300

    Create Init command and export as platform

---
+++ packages/cli/src/commands/init.ts
@@ -0,0 +1,5 @@
+import { Command } from '@oclif/core'
+
+export default class Init extends Command {
+  async run() {}
+}
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,9 @@
 export { run } from '@oclif/core'
+
+export { default as Dev } from './commands/dev'
+export { default as Build } from './commands/build'
+export { default as Init } from './commands/init'
+export { default as Serve } from './commands/start'
+export { default as CmsSync } from './commands/cms-sync'
+export { default as GenerateGraphql } from './commands/generate-graphql'
+export { default as Test } from './commands/test'
