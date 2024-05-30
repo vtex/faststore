99b3aebc3c97b2bb1376260d151ed6932a704944
Lucas Feijó <lucas.portela@vtex.com>
2024-05-30 14:32:27 -0300

    feat: Search for redirects with SSR

--- packages/core/src/components/templates/SearchPage/SearchPage.tsx
+++ packages/core/src/components/templates/SearchPage/SearchPage.tsx
@@ -1,6 +1,5 @@
 import { useSearch } from '@faststore/sdk'
 import type { ComponentType } from 'react'
-import router from 'next/router'
 
 import RenderSections from 'src/components/cms/RenderSections'
 import BannerNewsletter from 'src/components/sections/BannerNewsletter/BannerNewsletter'
@@ -58,9 +57,6 @@ function SearchPage({
     selectedFacets,
     itemsPerPage,
   })
-
-  const urlRedirect = pageProductGalleryData?.redirect?.url
-  urlRedirect && router.push(urlRedirect)
 
   const { pages, useGalleryPage } = useCreateUseGalleryPage()
 
