f9d7cc9708ee753c9f23465870a6d999dbec905f
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2024-05-31 11:22:14 -0300

    export inside commands

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,9 +1,19 @@
 export { run } from '@oclif/core'
 
-export { default as Dev } from './commands/dev'
-export { default as Build } from './commands/build'
-export { default as Init } from './commands/init'
-export { default as Serve } from './commands/start'
-export { default as CmsSync } from './commands/cms-sync'
-export { default as GenerateGraphql } from './commands/generate-graphql'
-export { default as Test } from './commands/test'
+import { default as Dev } from './commands/dev'
+import { default as Build } from './commands/build'
+import { default as Init } from './commands/init'
+import { default as Serve } from './commands/start'
+import { default as CmsSync } from './commands/cms-sync'
+import { default as GenerateGraphql } from './commands/generate-graphql'
+import { default as Test } from './commands/test'
+
+export const commands = {
+  Dev,
+  Build,
+  Init,
+  Serve,
+  CmsSync,
+  GenerateGraphql,
+  Test,
+}
