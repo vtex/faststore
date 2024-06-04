ea0414ab005f2e6e3b5f5889c287b05456f276ea
Lucas Feijó <lucas.portela@vtex.com>
2024-06-04 19:11:18 -0300

    fix: Cancel `useProductGalleryQuery`'s on success execution when redirecting

--- packages/core/src/sdk/product/useProductGalleryQuery.ts
+++ packages/core/src/sdk/product/useProductGalleryQuery.ts
@@ -88,6 +88,11 @@ export const useProductGalleryQuery = ({
   return useQuery<Query, Variables>(query, localizedVariables, {
     onSuccess: (data) => {
       if (data) {
+        // Cancel query onSuccess event when redirecting
+        if (data?.redirect?.url) {
+          return
+        }
+
         const fuzzyFacetValue = findFacetValue(selectedFacets, 'fuzzy')
         const operatorFacetValue = findFacetValue(selectedFacets, 'operator')
 
