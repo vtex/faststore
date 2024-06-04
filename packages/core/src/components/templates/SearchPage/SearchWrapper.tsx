880c4a7d5957317518e1bcdb5c8a54c868cb9519
Lucas Feijó <lucas.portela@vtex.com>
2024-06-04 19:09:34 -0300

    feat: Add `SearchWrapper` to handle IS redirects and `SearchPage` rendering

---
+++ packages/core/src/components/templates/SearchPage/SearchWrapper.tsx
@@ -0,0 +1,54 @@
+import { useRouter } from 'next/router'
+import { useSearch } from '@faststore/sdk'
+import { useProductGalleryQuery } from 'src/sdk/product/useProductGalleryQuery'
+
+import type { SearchState } from '@faststore/sdk'
+import type { SearchContentType } from 'src/server/cms'
+import type { SearchPageContextType } from 'src/pages/s'
+
+import SearchPage from './SearchPage'
+
+export type SearchWrapperProps = {
+  itemsPerPage: number
+  searchContentType: SearchContentType
+  serverData: SearchPageContextType
+}
+
+export default function SearchWrapper({
+  itemsPerPage,
+  searchContentType,
+  serverData,
+}: SearchWrapperProps) {
+  const router = useRouter()
+  const {
+    state: { term, sort, selectedFacets },
+  } = useSearch()
+  const { data: pageProductGalleryData, isValidating } = useProductGalleryQuery(
+    {
+      term,
+      sort,
+      itemsPerPage,
+      selectedFacets,
+    }
+  )
+
+  if (isValidating || !pageProductGalleryData) {
+    return null
+  }
+
+  // Redirect when there are registered Intelligent Search redirects on VTEX Admin
+  if (pageProductGalleryData?.redirect?.url) {
+    router.replace(pageProductGalleryData?.redirect?.url, null, {
+      shallow: true,
+    })
+
+    return null
+  }
+
+  return (
+    <SearchPage
+      page={searchContentType}
+      data={{ ...serverData, ...pageProductGalleryData }}
+    />
+  )
+}
--- packages/core/src/components/templates/SearchPage/index.ts
+++ packages/core/src/components/templates/SearchPage/index.ts
@@ -1,2 +1,5 @@
 export { default } from './SearchPage'
 export type { SearchPageProps } from './SearchPage'
+
+export { default as SearchWrapper } from './SearchWrapper'
+export type { SearchWrapperProps } from './SearchWrapper'
