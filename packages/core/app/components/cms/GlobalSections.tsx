546d7f14a13e4ef0379fce3478767b0b71bb91b6
eduardoformiga <eduardo.formiga@gmail.com>
2024-06-11 17:04:41 -0300

    chore: attempts to remove commented imports

--- packages/core/app/components/cms/GlobalSections.tsx
+++ packages/core/app/components/cms/GlobalSections.tsx
@@ -3,16 +3,9 @@ import type { ComponentType } from 'react'
 import { PropsWithChildren } from 'react'
 import CUSTOM_COMPONENTS from 'src/customizations/src/components'
 
-// import Toast from 'src/components/common/Toast'
 import RenderSections from './RenderSections'
 
 import { OverriddenDefaultAlert as Alert } from 'app/components/sections/Alert/OverriddenDefaultAlert'
-// import Footer from 'src/components/sections/Footer'
-// import { OverriddenDefaultNavbar as Navbar } from 'src/components/sections/Navbar/OverriddenDefaultNavbar'
-// import { OverriddenDefaultRegionBar as RegionBar } from 'src/components/sections/RegionBar/OverriddenDefaultRegionBar'
-
-// const RegionModal = lazy(() => import('src/components/region/RegionModal'))
-// const CartSidebar = lazy(() => import('src/components/cart/CartSidebar'))
 
 export const GLOBAL_SECTIONS_CONTENT_TYPE = 'globalSections'
 
