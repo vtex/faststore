d3b8912e6a795ce5c0a3c296e2e9131911ecdf2a
Luiz Nickel <luizaugustonickel@gmail.com>
2024-07-05 15:49:02 -0300

    ref(cli): add sanity check for existing discovery folder
    chore: add package @inquirer/prompts

--- packages/cli/src/commands/discovery/init.ts
+++ packages/cli/src/commands/discovery/init.ts
@@ -1,16 +1,29 @@
-
+import fs from 'node:fs';
 import degit from 'degit';
 import { Command } from '@oclif/core';
+import { confirm } from '@inquirer/prompts'
 
 export default class Init extends Command {
+  static description = "Creates a discovery folder based on the starter.store template."
+
+  static examples = [
+    "$ yarn faststore discovery init"
+  ]
+
   async run() {
-    const discoveryEmitter = degit('vtex-sites/starter.store')
+    const discoveryFolderExists = fs.existsSync('discovery');
+
+    if (discoveryFolderExists) {
+      const confirmOverride = await confirm({ message: 'It looks like you already have a discovery folder in your store. Do you want to override it?' });
+
+      if (!confirmOverride) return this.log('🛑 Interrupted initializing discovery')
+    }
+
+    const discoveryEmitter = degit('vtex-sites/starter.store', {
+      force: true
+    })
 
     this.log('Pulling starter.store template...')
-
-    discoveryEmitter.on('info', info => {
-      this.log(info.message)
-    })
 
     discoveryEmitter.clone('discovery').then(() => {
       this.log('Discovery created successfuly! You can find it at discovery/')
