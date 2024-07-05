c4c6b55ff58c878a1fccb7321b60e207497349b5
Luiz Nickel <luizaugustonickel@gmail.com>
2024-07-05 16:03:28 -0300

    chore: lint

--- packages/cli/src/commands/discovery/init.ts
+++ packages/cli/src/commands/discovery/init.ts
@@ -4,11 +4,9 @@ import { Command } from '@oclif/core';
 import { confirm } from '@inquirer/prompts'
 
 export default class Init extends Command {
-  static description = "Creates a discovery folder based on the starter.store template."
+  static description = "Creates a discovery folder based on the starter.store template.";
 
-  static examples = [
-    "$ yarn faststore discovery init"
-  ]
+  static examples = ["$ yarn faststore discovery init"];
 
   async run() {
     const discoveryFolderExists = fs.existsSync('discovery');
@@ -16,17 +14,17 @@ export default class Init extends Command {
     if (discoveryFolderExists) {
       const confirmOverride = await confirm({ message: 'It looks like you already have a discovery folder in your store. Do you want to override it?' });
 
-      if (!confirmOverride) return this.log('🛑 Interrupted initializing discovery')
+      if (!confirmOverride) return this.log('🛑 Interrupted initializing discovery');
     }
 
     const discoveryEmitter = degit('vtex-sites/starter.store', {
       force: true
-    })
+    });
 
-    this.log('Pulling starter.store template...')
+    this.log('Pulling starter.store template...');
 
     discoveryEmitter.clone('discovery').then(() => {
-      this.log('Discovery created successfuly! You can find it at discovery/')
+      this.log('Discovery created successfuly! You can find it at discovery/');
     })
   }
 }
