5afd5f9e2ecb381042a71184ddfaf7aefb732176
Luiz Nickel <luizaugustonickel@gmail.com>
2024-07-10 12:07:12 -0300

    chore(utils/commands): remove wip banner

--- packages/cli/src/utils/commands.ts
+++ packages/cli/src/utils/commands.ts
@@ -1,8 +1,6 @@
 import { spawnSync } from "node:child_process"
 
-/**
-* WIP: Retrieves the package manager based on the developer lockfile, using `ni`.
-*/
+// Retrieves the package manager based on the developer lockfile, using `ni`.
 export function getPreferredPackageManager() {
   const agent = spawnSync("na", ['\?'], { encoding: 'utf8' }).stdout.trim()
 
