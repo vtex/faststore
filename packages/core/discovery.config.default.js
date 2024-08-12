0834b749e14c22bfa090be955386629e24623322
Everton Ataide <everton.ataide@vtex.com>
2024-08-12 20:05:42 -0300

    Update default config

--- packages/core/discovery.config.default.js
+++ packages/core/discovery.config.default.js
@@ -14,7 +14,7 @@ module.exports = {
 
   // Platform specific configs for API
   api: {
-    storeId: 'storefra1mework',
+    storeId: 'storeframework',
     workspace: 'master',
     subDomainPrefix: ['www'],
     environment: 'vtexcommercestable',
