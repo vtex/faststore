feccb2b935e50677223a0c309dc07bdabc357543
eduardoformiga <eduardo.formiga@gmail.com>
2024-08-18 18:20:37 -0300

    Revert "chore: adds preconnect and dns-prefetch"
    This reverts commit 756dc4a7917f95752066bce755926aa059445a67.

--- packages/core/src/pages/_document.tsx
+++ packages/core/src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-import storeConfig from '../../faststore.config'
 
 import ThirdPartyScripts from 'src/components/ThirdPartyScripts'
 import { WebFonts } from 'src/customizations/src/GlobalOverrides'
@@ -8,14 +7,6 @@ function Document() {
   return (
     <Html>
       <Head>
-        <link
-          rel="preconnect"
-          href={`https://${storeConfig.api.storeId}.vtexassets.com`}
-        />
-        <link
-          rel="dns-prefetch"
-          href={`https://${storeConfig.api.storeId}.vtexassets.com`}
-        />
         {!process.env.DISABLE_3P_SCRIPTS && <ThirdPartyScripts />}
         <WebFonts />
       </Head>
