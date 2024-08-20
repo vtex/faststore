3f70f821859ec8e443ad3c0eb65f56e1facd1f9b
eduardoformiga <eduardo.formiga@gmail.com>
2024-08-20 17:03:46 -0300

    import components from Overrides

--- packages/core/src/components/cms/Components.ts
+++ packages/core/src/components/cms/Components.ts
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 import { ComponentType } from 'react'
 
 import { OverriddenDefaultAlert as Alert } from 'src/components/sections/Alert/OverriddenDefaultAlert'
-import Hero from 'src/components/sections/Hero'
+import { OverriddenDefaultHero as Hero } from 'src/components/sections/Hero/OverriddenDefaultHero'
 import Incentives from 'src/components/sections/Incentives'
 import { OverriddenDefaultNavbar as Navbar } from 'src/components/sections/Navbar/OverriddenDefaultNavbar'
 import { OverriddenDefaultRegionBar as RegionBar } from 'src/components/sections/RegionBar/OverriddenDefaultRegionBar'
@@ -30,22 +30,22 @@ const RegionModal = dynamic(
 const BannerText = dynamic(
   () =>
     import(
-      /* webpackChunkName: "BannerText" */ 'src/components/sections/BannerText'
-    ),
+      /* webpackChunkName: "BannerText" */ 'src/components/sections/BannerText/OverriddenDefaultBannerText'
+    ).then((mod) => ({ default: mod.OverriddenDefaultBannerText })),
   { ssr: false }
 )
 const Newsletter = dynamic(
   () =>
     import(
-      /* webpackChunkName: "Newsletter" */ 'src/components/sections/Newsletter'
-    ),
+      /* webpackChunkName: "Newsletter" */ 'src/components/sections/Newsletter/OverriddenDefaultNewsletter'
+    ).then((mod) => ({ default: mod.OverriddenDefaultNewsletter })),
   { ssr: false }
 )
 const ProductShelf = dynamic(
   () =>
     import(
-      /* webpackChunkName: "ProductShelf" */ 'src/components/sections/ProductShelf'
-    ),
+      /* webpackChunkName: "ProductShelf" */ 'src/components/sections/ProductShelf/OverriddenDefaultProductShelf'
+    ).then((mod) => ({ default: mod.OverriddenDefaultProductShelf })),
   { ssr: false }
 )
 const ProductTiles = dynamic(
