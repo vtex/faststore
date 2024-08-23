ec1facefced3e43f8254717122c0708df5901476
Arthur Andrade <arthurfelandrade@gmail.com>
2024-08-23 10:46:32 -0300

    Update packages/api/src/platforms/vtex/clients/buyerOrg/types/BuyerOrg.ts
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com.br>

--- packages/api/src/platforms/vtex/clients/buyerOrg/types/BuyerOrg.ts
+++ packages/api/src/platforms/vtex/clients/buyerOrg/types/BuyerOrg.ts
@@ -1,21 +1,21 @@
 export type CustomField = {
-    key: string;
-    value: string;
-};
+  key: string
+  value: string
+}
 
 export type BuyerOrg = {
-    id: string;
-    email: string;
-    name: string;
-    tradeName: string;
-    document: string;
-    approved: string;
-    stateRegistration: string;
-    unitIds: string[];
-    priceTables: string[];
-    sellers: string[];
-    categories: string[];
-    tradePolicies: string[];
-    paymentTerms: string[];
-    customFields: CustomField[];
-};
+  id: string
+  email: string
+  name: string
+  tradeName: string
+  document: string
+  approved: string
+  stateRegistration: string
+  unitIds: string[]
+  priceTables: string[]
+  sellers: string[]
+  categories: string[]
+  tradePolicies: string[]
+  paymentTerms: string[]
+  customFields: CustomField[]
+}
