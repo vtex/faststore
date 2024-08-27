8d55ad2481b51b54acb2199e156bfa3a75c35126
Fanny Chien <fannychienn@gmail.com>
2024-08-26 23:02:54 -0300

    fix: Fixes canonical tag

--- packages/core/app/fs-next-update/page.tsx
+++ packages/core/app/fs-next-update/page.tsx
@@ -60,7 +60,9 @@ export async function generateMetadata() {
     title: settings?.seo?.title ?? storeConfig.seo.title,
     description: settings?.seo?.description ?? storeConfig.seo?.description,
     titleTemplate: storeConfig.seo?.titleTemplate ?? storeConfig.seo?.title,
-    canonical: settings?.seo?.canonical ?? storeConfig.storeUrl,
+    alternates: {
+      canonical: settings?.seo?.canonical ?? storeConfig.storeUrl,
+    },
     openGraph: {
       type: 'website',
       url: storeConfig.storeUrl,
