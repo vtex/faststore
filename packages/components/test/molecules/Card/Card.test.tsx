cad5e6045af15b406e6c563bf62e00ed34d1c4bf
Fanny Chien <fannychienn@gmail.com>
2024-09-05 19:11:45 -0300

    chore: Adjusts Card test

--- packages/components/test/molecules/Card/Card.test.tsx
+++ packages/components/test/molecules/Card/Card.test.tsx
@@ -11,26 +11,26 @@ expect.extend(toHaveNoViolations)
 
 describe('Card', () => {
   const mockIconAction = jest.fn()
-  it('renders the card component with title', () => {
+  it('renders the Card component with title', () => {
     render(<Card title="Test Card" />)
-    const cardTitle = screen.getByText('Test Card')
 
+    const cardTitle = screen.getByText('Test Card')
     expect(cardTitle).toBeInTheDocument()
   })
 
-  it('renders the card component with custom max width', () => {
+  it('renders the Card component with custom max width', () => {
     render(<Card title="Test Card" maxWidth="500px" />)
-    const card = screen.getByTestId('fs-card')
 
+    const card = screen.getByTestId('fs-card')
     expect(card).toHaveStyle('max-width: 500px')
   })
 
-  it('renders the card component with icon', () => {
+  it('renders the Card component with icon', () => {
     render(<Card title="Test Card" iconName="star" />)
+
     const iconButton = screen.getByLabelText(
       'Test Card action'
     ) as HTMLButtonElement
-
     expect(iconButton).toBeInTheDocument()
   })
 
@@ -38,11 +38,11 @@ describe('Card', () => {
     render(
       <Card title="Test Card" iconName="star" iconAction={mockIconAction} />
     )
+
     const iconButton = screen.getByLabelText(
       'Test Card action'
     ) as HTMLButtonElement
     iconButton.click()
-
     expect(mockIconAction).toHaveBeenCalled()
   })
 
