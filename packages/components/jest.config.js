c62cd330c8465c012c3fca78e90c28efeb62c4bb
Fanny Chien <fannychienn@gmail.com>
2024-09-05 18:46:56 -0300

    feat: Adds jest-dom and setup

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -2,5 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
+
 
---
+++ packages/components/jest.setup.ts
@@ -0,0 +1,3 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/jest-globals';
+
