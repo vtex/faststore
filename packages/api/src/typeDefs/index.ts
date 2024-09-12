17b009e7b00c760f850fa2a0a905e595998045bc
eduardoformiga <eduardo.formiga@gmail.com>
2024-09-12 17:11:53 -0300

    chore: adds path.join(process.cwd() to loadFilesSync

--- packages/api/src/typeDefs/index.ts
+++ packages/api/src/typeDefs/index.ts
@@ -1,8 +1,8 @@
 import { print } from 'graphql'
+import path from 'path'
 
 import { loadFilesSync } from '@graphql-tools/load-files'
 
-// Empty string ('') is interpreted as the current dir. Referencing it as './' won't work.
-export const typeDefs = loadFilesSync(__dirname, { extensions: ['.graphql'] })
+export const typeDefs = loadFilesSync(path.join(process.cwd()), { extensions: ['.graphql'] })
   .map(print)
   .join('\n')
