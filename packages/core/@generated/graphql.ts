30f6ccaf3d48889d1f6a03a837dcf3a4ffb149cb
Hiago Moreira <hiago.moreira@cubos.io>
2024-09-18 14:58:37 -0300

    feat: add variant product in api

--- packages/core/@generated/graphql.ts
+++ packages/core/@generated/graphql.ts
@@ -576,6 +576,7 @@ export type ShippingSla = {
 export type SkuVariants = {
   /** SKU property values for the current SKU. */
   activeVariations: Maybe<Scalars['ActiveVariations']['output']>
+  allVariantProducts: Maybe<Array<StoreProduct>>
   /** All available options for each SKU variant property, indexed by their name. */
   allVariantsByName: Maybe<Scalars['VariantsByName']['output']>
   /**
@@ -595,6 +596,10 @@ export type SkuVariants = {
    * considered the dominant one.
    */
   slugsMap: Maybe<Scalars['SlugsMap']['output']>
+}
+
+export type SkuVariantsAllVariantProductsArgs = {
+  productID: InputMaybe<Scalars['String']['input']>
 }
 
 export type SkuVariantsAvailableVariationsArgs = {
