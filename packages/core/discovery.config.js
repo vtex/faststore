542ed51f77482309b76a8d3581e5e3b208e1cba6
Emerson Laurentino <emerson@laurentino.co>
2024-10-02 17:48:39 -0300

    Refactor import paths to use discovery.config

--- packages/core/discovery.config.js
+++ packages/core/discovery.config.js
@@ -1,33 +1,19 @@
-const fs = require('fs')
-const path = require('path')
+const defaultConfig = require('./discovery.config.default')
 const deepmerge = require('deepmerge')
 
-const defaultConfig = require('./discovery.config.default')
+let starterConfig
 
-const starterLegacyConfigPath = path.resolve(
-  __dirname,
-  './src/customizations/faststore.config'
-)
-const starterConfigPath = path.resolve(
-  __dirname,
-  './src/customizations/discovery.config'
-)
+try {
+  starterConfig = require('./src/customizations/discovery.config')
+} catch (e) {
+  starterConfig = require('./src/customizations/faststore.config')
+}
 
-const starterLegacyConfig = fs.existsSync(starterLegacyConfigPath)
-  ? require(starterLegacyConfigPath)
-  : {}
-const starterConfig = fs.existsSync(starterConfigPath)
-  ? require(starterConfigPath)
-  : {}
-
-// TODO: Remove starterLegacyConfig in the future when migration to starterConfig is complete
 /**
  * @type {typeof defaultConfig & Record<string, any>}
  * */
-const finalConfig = deepmerge.all([
-  defaultConfig,
-  starterLegacyConfig,
-  starterConfig,
-])
+const finalConfig = deepmerge(defaultConfig, starterConfig)
 
-module.exports = finalConfig
+console.log('finalConfig', finalConfig)
+
+module.exports = starterConfig
