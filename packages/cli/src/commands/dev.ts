4c30ebbf4bc8f86a3b93a2948aa224dbff08b184
Guilherme Carvalho <77246+gvc@users.noreply.github.com>
2024-10-04 17:24:17 -0300

    chore: redo changes without lint

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -1,15 +1,16 @@
-import { Command } from '@oclif/core'
-import { spawn } from 'child_process'
-import chalk from 'chalk'
-import chokidar from 'chokidar'
-import dotenv from 'dotenv'
+import { Command } from '@oclif/core';
+import { spawn } from 'child_process';
+import chalk from 'chalk';
+import chokidar from 'chokidar';
+import dotenv from 'dotenv';
 
-import { readFileSync, cpSync } from 'fs'
-import path from 'path'
-import { withBasePath } from '../utils/directory'
-import { generate } from '../utils/generate'
-import { getPreferredPackageManager } from '../utils/commands'
-import { runCommandSync } from '../utils/runCommandSync'
+import { readFileSync, cpSync } from 'fs';
+import path from 'path';
+import { withBasePath } from '../utils/directory';
+import { generate } from '../utils/generate';
+import { getPreferredPackageManager } from '../utils/commands';
+import { runCommandSync } from '../utils/runCommandSync';
+
 
 /**
  * Taken from toolbelt
@@ -34,12 +35,7 @@ const defaultIgnored = [
 
 const devAbortController = new AbortController()
 
-async function storeDev(
-  rootDir: string,
-  tmpDir: string,
-  coreDir: string,
-  port: number
-) {
+async function storeDev(rootDir: string, tmpDir: string, coreDir: string, port: number) {
   const envVars = dotenv.parse(readFileSync(path.join(rootDir, 'vtex.env')))
 
   const packageManager = getPreferredPackageManager()
@@ -53,18 +49,11 @@ async function storeDev(
     cwd: tmpDir,
   })
 
-  const { success } = copyGenerated(
-    path.join(tmpDir, '@generated'),
-    path.join(coreDir, '@generated')
-  )
+  const { success } = copyGenerated(path.join(tmpDir, '@generated'), path.join(coreDir, '@generated'))
 
   if (!success) {
-    console.log(
-      `${chalk.yellow('warn')} - Failed to copy @generated schema back to node_modules, autocomplete and DX might be impacted.`
-    )
-    console.log(
-      `Attempted to copy from ${path.join(tmpDir, '@generated')} to ${path.join(coreDir, '@generated')}`
-    )
+    console.log(`${chalk.yellow('warn')} - Failed to copy @generated schema back to node_modules, autocomplete and DX might be impacted.`)
+    console.log(`Attempted to copy from ${path.join(tmpDir, '@generated')} to ${path.join(coreDir, '@generated')}`)
   }
 
   const devProcess = spawn(`${packageManager} dev-only --port ${port}`, {
@@ -75,7 +64,7 @@ async function storeDev(
     env: {
       ...process.env,
       ...envVars,
-    },
+    }
   })
 
   devProcess.on('close', () => {
@@ -98,12 +87,12 @@ export default class Dev extends Command {
     {
       name: 'account',
       description:
-        'The account for which the Discovery is running. Currently noop.',
+      'The account for which the Discovery is running. Currently noop.',
     },
     {
       name: 'path',
       description:
-        'The path where the FastStore being run is. Defaults to cwd.',
+      'The path where the FastStore being run is. Defaults to cwd.',
     },
     {
       name: 'port',
