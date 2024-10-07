2b52be30c6f1b8938693b17784ca567e8fc6ad28
Emanuel Ramos <emanuelramos@MacBook-Air-de-Emanuel-3.local>
2024-10-07 10:37:44 -0300

    fix: removing page view event from layout.tsx

--- packages/core/src/Layout.tsx
+++ packages/core/src/Layout.tsx
@@ -1,10 +1,6 @@
 import { type PropsWithChildren } from 'react'
 
-import { usePageViewEvent } from './sdk/analytics/hooks/usePageViewEvent'
-
 function Layout({ children }: PropsWithChildren) {
-  usePageViewEvent()
-
   return <>{children}</>
 }
 
