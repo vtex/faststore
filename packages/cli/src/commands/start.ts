cbff9c30a7b66c1b845ea645ead881d8c632e5ae
Guilherme Carvalho <77246+gvc@users.noreply.github.com>
2024-10-08 16:46:24 -0300

    Update packages/cli/src/commands/start.ts
    Co-authored-by: Larícia Mota <laricia.mota@vtex.com.br>

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -18,7 +18,7 @@ export default class Start extends Command {
     },
     {
       name: 'port',
-      description: 'The port where FastStore should run.',
+      description: 'The port where FastStore should run. Defaults to 3000.',
     },
   ]
 
