754bd48a06d92e743c2da5a827056c048f05d651
Emanuel Ramos <emanuelramos@MacBook-Air-de-Emanuel-3.local>
2024-10-09 08:43:26 -0300

    fix: page view event

--- packages/core/src/sdk/analytics/hooks/usePageViewEvent.ts
+++ packages/core/src/sdk/analytics/hooks/usePageViewEvent.ts
@@ -4,29 +4,25 @@ import { sendAnalyticsEvent } from '@faststore/sdk'
 import type { PageViewEvent } from '@faststore/sdk'
 
 export const usePageViewEvent = () => {
-  const sendPageViewEvent = useCallback(() => {
-    setTimeout(() => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const sendPageViewEvent = () => {
       sendAnalyticsEvent<any>({
         name: 'page_view',
         params: {
-          test: true,
           page_title: document.title,
           page_location: location.href,
           send_page_view: true,
         },
       })
-    }, 0)
-  }, [])
-
-  const router = useRouter()
-
-  useEffect(() => {
+    }
     router.events.on('routeChangeComplete', sendPageViewEvent)
 
     return () => {
       router.events.off('routeChangeComplete', sendPageViewEvent)
     }
-  }, [router, sendPageViewEvent])
+  }, [router])
 
-  return { sendPageViewEvent }
+  return
 }
