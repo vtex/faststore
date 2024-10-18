fe6e7372085dbd4ec30ba2d03d202cf30d4d00b5
Guilherme Carvalho <77246+gvc@users.noreply.github.com>
2024-10-18 10:06:52 -0300

    chore: spread console and override only the `log`

--- packages/cli/src/utils/logger.ts
+++ packages/cli/src/utils/logger.ts
@@ -1,8 +1,7 @@
 export const logger = {
+  ...console,
+
   log: (...args: any[]) => {
     process.env.DISCOVERY_DEBUG === 'true' ? console.log(...args) : undefined
   },
-  error: console.error,
-  info: console.info,
-  warn: console.warn,
 }
