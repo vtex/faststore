c2af4bc428f82747a20c9385b77177809c9283b1
Guilherme Carvalho <77246+gvc@users.noreply.github.com>
2024-10-18 10:41:47 -0300

    Use `Proxy` for type safety.
    Co-authored-by: Emerson Laurentino <emerson@laurentino.co>

--- packages/cli/src/utils/logger.ts
+++ packages/cli/src/utils/logger.ts
@@ -1,7 +1,12 @@
-export const logger = {
-  ...console,
-
-  log: (...args: any[]) => {
-    process.env.DISCOVERY_DEBUG === 'true' ? console.log(...args) : undefined
-  },
-}
+export const logger = new Proxy(console, {
+  get(target, prop: keyof Console) {
+    if (prop === 'log') {
+      return (...args: any[]) => {
+        if (process.env.DISCOVERY_DEBUG === 'true') {
+          target.log(...args);
+        }
+      };
+    }
+    return target[prop];
+  }
+});
