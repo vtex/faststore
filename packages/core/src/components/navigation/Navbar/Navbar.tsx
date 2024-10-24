031cbf8e5895537612b38c2ace4a3fe9f5294b8e
Fanny Chien <fannychienn@gmail.com>
2024-10-24 20:26:47 -0300

    chore: Uses isMobile to avoid duplicated component

--- packages/core/src/components/navigation/Navbar/Navbar.tsx
+++ packages/core/src/components/navigation/Navbar/Navbar.tsx
@@ -175,7 +175,7 @@ function Navbar({
               />
             )}
 
-            <ButtonSignIn.Component {...signInButton} />
+            {!isMobile && <ButtonSignIn.Component {...signInButton} />}
 
             <CartToggle {...cart} />
           </NavbarButtons.Component>
