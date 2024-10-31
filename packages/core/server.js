be06674734d1b95dc3fb4fd9cac5a14a1fb288cb
Bianca Cristina  da Silva <bianca.silva@vtex.com>
2024-10-31 09:59:33 -0300

    faststore-poc(SFS-1709): test custom server

--- packages/core/server.js
+++ packages/core/server.js
@@ -1,34 +1,24 @@
 const http = require('http')
 const url = require('url')
-const NextServer = require('next/dist/server/next-server').default
+const next = require('next')
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
 const { config } = require('./.next/required-server-files.json')
-const { parse } = require('url')
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 process.env.__NEXT_PRIVATE_STANDALONE_CONFIG = JSON.stringify(config)
 
-// eslint-disable-next-line turbo/no-undeclared-env-vars
-const port = parseInt(process.env.PORT || '3000', 10)
-// eslint-disable-next-line turbo/no-undeclared-env-vars
-const hostname = process.env.HOSTNAME || 'localhost'
-const dev = process.env.NODE_ENV !== 'production'
-
-const app = new NextServer({
-  hostname: hostname,
-  port,
-  dir: process.cwd(),
-  dev,
-  conf: config,
-})
-
-const handle = app.getRequestHandler()
-
 app.prepare().then(() => {
   http
     .createServer((req, res) => {
-      let startTime = Date.now()
-      const parsedUrl = parse(req.url, true)
+      const parsedUrl = url.parse(req.url, true)
       handle(req, res, parsedUrl).then(() => {
+        let startTime = Date.now()
         console.log({
           url: req.url,
           method: req.method,
@@ -39,11 +29,11 @@ app.prepare().then(() => {
         })
       })
     })
-    .listen(port, () => {
-      console.log(
-        `> [CUSTOM] Server listening at http://${hostname}:${port} as ${
-          dev ? 'development' : process.env.NODE_ENV
-        }`
-      )
-    })
+    .listen(port)
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
 })
