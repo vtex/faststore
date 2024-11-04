0826ab96a89170154b722429be652d20d48f465f
Fanny Chien <fannychienn@gmail.com>
2024-11-04 19:19:57 -0300

    chore: Uses import dynamic for toast

--- packages/core/src/components/common/Toast/Toast.tsx
+++ packages/core/src/components/common/Toast/Toast.tsx
@@ -1,9 +1,19 @@
 import { useEffect } from 'react'
 
-import { Toast as UIToast, useUI } from '@faststore/ui'
+import dynamic from 'next/dynamic'
+
+import { useUI } from '@faststore/ui'
 import Section from 'src/components/sections/Section/Section'
 import { useCart } from 'src/sdk/cart'
 import styles from './section.module.scss'
+
+const UIToast = dynamic(
+  () =>
+    import(/* webpackChunkName: "UIToast" */ '@faststore/ui').then((module) => {
+      return module.Toast
+    }),
+  { ssr: false }
+)
 
 function Toast() {
   const { toasts, pushToast } = useUI()
