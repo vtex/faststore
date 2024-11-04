ea0f46b1020ea9a4d82e0ed1fe271b35dea3479d
Fanny Chien <fanny.chien@vtex.com>
2024-11-04 15:06:35 -0300

    Chore: Fix CLS issues and Navbar dynamic imports  (#2526)
    ## What's the purpose of this pull request?
    
    - Fix CLS (Cumulative Layout Shift) for `RegionBar` (it was introduced
    when we change to use `isMobile` hook to render the section, although
    using the `display-mobile` won't remove the component from the dom, I
    opted to put it back to avoid the CLS)
    
    |Mobile| Desktop|
    |-|-|
    
    ![CLS-Mobile](https://github.com/user-attachments/assets/d737bcd7-54e4-44cd-a532-33d95471a156)|![CLS-Desk](https://github.com/user-attachments/assets/b669d5aa-b402-49ee-ae16-2d34feed14b6)
    
    - Uses dynamic import to `Navlinks` 
    - Uses `useScreenResize` hook on `Navbar` component to avoid components
    duplication and render depending on the device
    - Uses dynamic import for `NavbarSlider` internal components
    
    ## How it works?
    
    - Mobile
    
    |Starter | CLS fixes + navbar improvements|
    |-|-|
    <img width="1018" alt="mob-starter"
    src="https://github.com/user-attachments/assets/2d9f6d2a-51dc-4341-8aa0-670d6a20281b">|<img
    width="1031" alt="mob-perf"
    src="https://github.com/user-attachments/assets/d7a3e426-39b7-40a4-9871-b90e5c3fb577">|
    
    - Desktop
    
    |Starter | CLS fixes + navbar improvements|
    |-|-|
    |<img width="1008" alt="desk-starter"
    src="https://github.com/user-attachments/assets/68c8da9d-839e-415b-b4c0-39fd4e423019">|<img
    width="1011" alt="desk-perf"
    src="https://github.com/user-attachments/assets/956a19aa-bd92-4acf-a163-348cc94938d1">|
    
    
    ## How to test it?
    
    Use this preview link for tests:
    https://sfj-1842eea--starter.preview.vtex.app
    
    1. Everything should be working as before
    2. Check if the layout shift in the RegionBar is still happening
    3. Try testing the link in https://pagespeed.web.dev/
    
    ### Starters Deploy Preview
    
    https://github.com/vtex-sites/starter.store/pull/587

--- packages/core/src/components/navigation/Navbar/Navbar.tsx
+++ packages/core/src/components/navigation/Navbar/Navbar.tsx
@@ -1,18 +1,36 @@
-import { useRef, useState, useCallback } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
-import { useUI, useScrollDirection, Icon as UIIcon } from '@faststore/ui'
+import { Icon as UIIcon, useScrollDirection, useUI } from '@faststore/ui'
 
 import type { SearchInputRef } from 'src/components/search/SearchInput'
 import SearchInput from 'src/components/search/SearchInput'
-import NavbarLinks from 'src/components/navigation/NavbarLinks'
-import NavbarSlider from 'src/components/navigation/NavbarSlider'
+
 import CartToggle from 'src/components/cart/CartToggle'
-import Logo from 'src/components/ui/Logo'
 import Link from 'src/components/ui/Link'
+import Logo from 'src/components/ui/Logo'
 import { useOverrideComponents } from 'src/sdk/overrides/OverrideContext'
 
-import type { NavbarProps as SectionNavbarProps } from '../../sections/Navbar'
+import dynamic from 'next/dynamic'
 import useScreenResize from 'src/sdk/ui/useScreenResize'
+import type { NavbarProps as SectionNavbarProps } from '../../sections/Navbar'
+
+const NavbarLinks = dynamic(
+  () =>
+    /* webpackChunkName: "NavbarLinks" */ import(
+      'src/components/navigation/NavbarLinks'
+    ),
+  {
+    ssr: false,
+  }
+)
+
+const NavbarSlider = dynamic(
+  () =>
+    /* webpackChunkName: "NavbarSlider" */ import(
+      'src/components/navigation/NavbarSlider'
+    ),
+  { ssr: false }
+)
 
 export interface NavbarProps {
   /**
@@ -79,7 +97,7 @@ function Navbar({
   } = useOverrideComponents<'Navbar'>()
   const scrollDirection = useScrollDirection()
   const { openNavbar, navbar: displayNavbar } = useUI()
-  const { isDesktop } = useScreenResize()
+  const { isMobile } = useScreenResize()
 
   const searchMobileRef = useRef<SearchInputRef>(null)
   const [searchExpanded, setSearchExpanded] = useState(false)
@@ -121,10 +139,12 @@ function Navbar({
             </>
           )}
 
-          <SearchInput
-            placeholder={searchInput?.placeholder}
-            sort={searchInput?.sort}
-          />
+          {!isMobile && (
+            <SearchInput
+              placeholder={searchInput?.placeholder}
+              sort={searchInput?.sort}
+            />
+          )}
 
           <NavbarButtons.Component
             searchExpanded={searchExpanded}
@@ -142,25 +162,27 @@ function Navbar({
               />
             )}
 
-            <SearchInput
-              placeholder=""
-              ref={searchMobileRef}
-              testId="store-input-mobile"
-              buttonTestId="store-input-mobile-button"
-              onSearchClick={handlerExpandSearch}
-              sort={searchInput?.sort}
-              hidden={!searchExpanded}
-              aria-hidden={!searchExpanded}
-            />
+            {isMobile && (
+              <SearchInput
+                placeholder=""
+                ref={searchMobileRef}
+                testId="store-input-mobile"
+                buttonTestId="store-input-mobile-button"
+                onSearchClick={handlerExpandSearch}
+                sort={searchInput?.sort}
+                hidden={!searchExpanded}
+                aria-hidden={!searchExpanded}
+              />
+            )}
 
-            <ButtonSignIn.Component {...signInButton} />
+            {!isMobile && <ButtonSignIn.Component {...signInButton} />}
 
             <CartToggle {...cart} />
           </NavbarButtons.Component>
         </NavbarRow.Component>
       </NavbarHeader.Component>
 
-      {isDesktop && <NavbarLinks links={links} region={region} />}
+      {!isMobile && <NavbarLinks links={links} region={region} />}
 
       {displayNavbar && (
         <NavbarSlider
