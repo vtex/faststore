77738bda4339b17446cbb50aa37121fb65e03db8
Bianca Cristina  da Silva <bianca.silva@vtex.com>
2024-11-04 15:04:04 -0300

    faststore-poc(SFS-1709): custom server

---
+++ packages/core/server.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const url = require('url')
+const pino = require('pino')
+const next = require('next')
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const { config } = require('./.next/required-server-files.json')
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+process.env.__NEXT_PRIVATE_STANDALONE_CONFIG = JSON.stringify(config)
+
+const logger = pino({
+  level: 'debug',
+})
+
+app.prepare().then(() => {
+  http
+    .createServer((req, res) => {
+      const parsedUrl = url.parse(req.url, true)
+      handle(req, res, parsedUrl).then(() => {
+        let startTime = Date.now()
+        logger.info({
+          url: req.url,
+          method: req.method,
+          headers: req.headers,
+          code: res.statusCode,
+          startTime: startTime,
+          endTime: Date.now(),
+        })
+      })
+    })
+    .listen(port)
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
