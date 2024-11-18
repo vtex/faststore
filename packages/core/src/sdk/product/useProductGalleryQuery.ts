5c44306634e08813ec1ee1ed4b322cb9ef3727be
Pedro Soares <pedro.soares@vtex.com>
2024-11-18 09:51:29 -0300

    feat: move loading logic to useProductGalleryQuery hook

--- packages/core/src/sdk/product/useProductGalleryQuery.ts
+++ packages/core/src/sdk/product/useProductGalleryQuery.ts
@@ -92,46 +92,46 @@ export const useProductGalleryQuery = ({
   const fuzzyFacetValue = findFacetValue(selectedFacets, 'fuzzy')
   const operatorFacetValue = findFacetValue(selectedFacets, 'operator')
 
-  const queryResult = useQuery<Query, Variables>(
-    query,
-    localizedVariables,
-    {
-      onSuccess: (data) => {
-        if (data && term) {
-          import('@faststore/sdk').then(({ sendAnalyticsEvent }) => {
-            sendAnalyticsEvent<IntelligentSearchQueryEvent>({
-              name: 'intelligent_search_query',
-              params: {
-                locale,
-                term,
-                url: window.location.href,
-                logicalOperator: data.search.metadata?.logicalOperator ?? 'and',
-                isTermMisspelled:
-                  data.search.metadata?.isTermMisspelled ?? false,
-                totalCount: data.search.products.pageInfo.totalCount,
-              },
-            })
+  const queryResult = useQuery<Query, Variables>(query, localizedVariables, {
+    onSuccess: (data) => {
+      if (data && term) {
+        import('@faststore/sdk').then(({ sendAnalyticsEvent }) => {
+          sendAnalyticsEvent<IntelligentSearchQueryEvent>({
+            name: 'intelligent_search_query',
+            params: {
+              locale,
+              term,
+              url: window.location.href,
+              logicalOperator: data.search.metadata?.logicalOperator ?? 'and',
+              isTermMisspelled: data.search.metadata?.isTermMisspelled ?? false,
+              totalCount: data.search.products.pageInfo.totalCount,
+            },
           })
-        }
-      },
-    }
-  )
+        })
+      }
+    },
+  })
 
   // If there is no fuzzy or operator facet, we need to add them to the selectedFacets and re-fetch the query
-  if (!fuzzyFacetValue && !operatorFacetValue) {
+  const shouldRefetchQuery =
+    !queryResult.error && (!fuzzyFacetValue || !operatorFacetValue)
+  if (shouldRefetchQuery) {
     if (queryResult.data) {
       setState({
         ...state,
         selectedFacets: [
           ...selectedFacets,
           { key: 'fuzzy', value: queryResult.data.search.metadata?.fuzzy },
-          { key: 'operator', value: queryResult.data.search.metadata?.logicalOperator },
+          {
+            key: 'operator',
+            value: queryResult.data.search.metadata?.logicalOperator,
+          },
         ],
       })
     }
 
     // The first result is not relevant, so we return data null to avoid rendering the page while the query is being re-fetched
-    return { ...queryResult, data: null }
+    return { ...queryResult, isValidating: true, data: null }
   }
 
   return queryResult
