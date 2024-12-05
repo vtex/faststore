280df0c2b802b15ddd8e93946677e86f4cd9e90f
Pedro Soares <pedro.soares@vtex.com>
2024-12-05 10:16:07 -0300

    feat: add custom matcher and preprocess

--- packages/core/src/sdk/redirects/index.ts
+++ packages/core/src/sdk/redirects/index.ts
@@ -9,10 +9,39 @@ type GetRedirectResponse = {
   status: number
 }
 
+const searchRegex = /^\/busca\/([^/]+)/
+
+// Custom function to match the pathname
+function matcher(pathname: string) {
+  const searchMatch = pathname.match(searchRegex)
+  if (searchMatch) {
+    const searchTerm = searchMatch[1].replaceAll('-', '+')
+    return `s/?q=${searchTerm}`
+  }
+  return null
+}
+const pdpRegex = /^\/produto\/([^/]+)\/[^/]+$/
+
+// Custom function to preprocess the pathname
+function preprocessPathname(pathname: string) {
+  const pdpMatch = pathname.match(pdpRegex)
+  return pdpMatch ? `/produto/${pdpMatch[1]}` : pathname
+}
+
 export async function getRedirect({
   pathname,
 }: GetRedirectProps): Promise<GetRedirectResponse> {
   try {
+    const preprocessdPathname = preprocessPathname(pathname)
+    const match = matcher(preprocessdPathname)
+
+    if (match) {
+      return {
+        location: match,
+        status: 301,
+      }
+    }
+
     const response = await fetch(
       `https://secure.vtexfaststore.com/api/io/redirect-evaluate?from=${pathname}&workspace=paladino`
     )
