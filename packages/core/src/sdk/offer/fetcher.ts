1ed7859c68ad642d3c265e7100eca9f235f71d4b
Emerson Laurentino <emerson@laurentino.co>
2024-12-06 11:19:54 -0300

    feat: update product search URL to remove unnecessary versioning

--- packages/core/src/sdk/offer/fetcher.ts
+++ packages/core/src/sdk/offer/fetcher.ts
@@ -7,7 +7,7 @@ export function getUrl(skuId: string) {
   const base = IS_PROD
     ? storeUrl
     : `https://${api.storeId}.${api.environment}.com.br`
-  const url = new URL(`${base}/api/io/_v/api/intelligent-search/product_search`)
+  const url = new URL(`${base}/api/intelligent-search/product_search`)
   url.searchParams.append('query', `sku.id:${skuId}`)
   if (IS_PROD) {
     url.searchParams.append('workspace', 'chrs')
