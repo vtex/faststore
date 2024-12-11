86ce7dadf7502b3625ca2281dc068c737efa654f
Pedro Soares <pedro.soares@vtex.com>
2024-12-11 15:23:22 -0300

    feat: change redirect-evaluate url

--- packages/core/src/sdk/redirects/index.ts
+++ packages/core/src/sdk/redirects/index.ts
@@ -22,11 +22,11 @@ export async function getRedirect({
       }
     }
 
-    // TODO: remove paladino workspace when production URL be ready
     const response = await fetch(
-      `${storeConfig.secureSubdomain}/api/io/redirect-evaluate?from=${pathname}&workspace=paladino`
+      `https://${storeConfig.api.storeId}.myvtex.com/redirect-evaluate${pathname}`
     )
     const rewriterData = (await response.json()) as GetRedirectResponse
+
     if (rewriterData.location) {
       return rewriterData
     }
