a4082900744068442c7ae3d7644f203c0ab9ec12
eduardoformiga <eduardo.formiga@gmail.com>
2025-01-15 17:07:50 -0300

    chore: fix indentation

--- packages/components/src/molecules/SearchProducts/SearchProductItemControl.tsx
+++ packages/components/src/molecules/SearchProducts/SearchProductItemControl.tsx
@@ -1,7 +1,7 @@
-import React, {  forwardRef, HTMLAttributes, useCallback, useState } from 'react'
+import React, { forwardRef, HTMLAttributes, useCallback, useState } from 'react'
 import { Badge, Icon, IconButton, Input, Loader, QuantitySelector } from '../..'
 
-import type { ReactNode, MouseEvent } from 'react'
+import type { MouseEvent, ReactNode } from 'react'
 
 type StatusButtonAddToCartType = 'default' | 'inProgress' | 'completed'
 
@@ -9,31 +9,31 @@ export interface SearchProductItemControlProps
   extends Omit<HTMLAttributes<HTMLDivElement>, 'children' | 'onClick'> {
   /**
    * Renders child elements.
-  */	
+   */
   children: ReactNode
   /**
    * Specifies whether the product is available.
-  */
+   */
   availability: boolean
   /**
    * Specifies whether the product has variations.
-  */
+   */
   hasVariants: boolean
   /**
    * Renders the elements of the SKUMatrix.
-  */
+   */
   skuMatrixControl: ReactNode
   /**
    * Specifies the quantity to be added to the cart.
-  */
+   */
   quantity: number
   /**
    * Callback that fires when the add to cart button is clicked.
-  */
+   */
   onClick?(e: MouseEvent<HTMLButtonElement>): void
   /**
    * Callback that fires when the input value changes.
-  */
+   */
   onChangeQuantity(value: number): void
 }
 
@@ -56,10 +56,10 @@ const SearchProductItemControl = forwardRef<
   const [statusAddToCart, setStatusAddToCart] =
     useState<StatusButtonAddToCartType>('default')
 
-	const showSKUMatrixControl = availability && hasVariants;
-	const isMobile = window.innerWidth <= 768	
-  
-	function stopPropagationClick(e: MouseEvent) {
+  const showSKUMatrixControl = availability && hasVariants
+  const isMobile = window.innerWidth <= 768
+
+  function stopPropagationClick(e: MouseEvent) {
     e.preventDefault()
     e.stopPropagation()
   }
@@ -73,12 +73,12 @@ const SearchProductItemControl = forwardRef<
       }, 1000)
 
       setTimeout(() => {
-	setStatusAddToCart('default')
-	onChangeQuantity(1)
+        setStatusAddToCart('default')
+        onChangeQuantity(1)
       }, 2000)
     }
   }
-	
+
   const getIcon = useCallback(() => {
     switch (statusAddToCart) {
       case 'inProgress':
@@ -99,10 +99,7 @@ const SearchProductItemControl = forwardRef<
     >
       <div data-fs-search-product-item-control-content>
         {!availability && (
-          <Badge
-            data-fs-search-product-item-control-badge
-            variant="warning"
-          >
+          <Badge data-fs-search-product-item-control-badge variant="warning">
             Out of Stock
           </Badge>
         )}
@@ -123,14 +120,14 @@ const SearchProductItemControl = forwardRef<
           )}
 
           {isMobile && (
-						<Input
-							data-fs-product-item-control-input
-							type="number"
-							min={1}
-							value={quantity}
-							onChange={(e) => onChangeQuantity(e.target.valueAsNumber)}
-						/>
-					)}
+            <Input
+              data-fs-product-item-control-input
+              type="number"
+              min={1}
+              value={quantity}
+              onChange={(e) => onChangeQuantity(e.target.valueAsNumber)}
+            />
+          )}
 
           <IconButton
             variant="primary"
