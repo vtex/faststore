0690ea86507ff6ad8bd9d647b469d6bf79e4e2a3
Artur Santiago <artursantiago2001@hotmail.com>
2025-01-21 15:24:49 -0300

    feat: add CheckboxField error variant

--- packages/components/src/molecules/CheckboxField/CheckboxField.tsx
+++ packages/components/src/molecules/CheckboxField/CheckboxField.tsx
@@ -7,6 +7,14 @@ export interface CheckboxFieldProps extends CheckboxProps {
    * The text displayed to identify the input checkbox.
    */
   label: string
+  /**
+   * The error message is displayed when an error occurs.
+   */
+  error?: string
+  /**
+   * Control the vertical alignment of the checkbox in relation to the label (center, top, bottom).
+   */
+  alignment?: 'center' | 'top' | 'bottom'
 }
 
 const CheckboxField = forwardRef<HTMLDivElement, CheckboxFieldProps>(
@@ -18,20 +26,38 @@ const CheckboxField = forwardRef<HTMLDivElement, CheckboxFieldProps>(
       value,
       name,
       checked,
+      error,
+      disabled,
+      alignment = 'center',
       ...otherProps
     },
     ref
   ) {
+    const shouldDisplayError = !disabled && error && error !== ''
+
     return (
-      <div ref={ref} data-fs-checkbox-field data-testid={testId}>
+      <div
+        ref={ref}
+        data-testid={testId}
+        data-fs-checkbox-field
+        data-fs-checkbox-field-error={error && error !== ''}
+        data-fs-checkbox-field-alignment={alignment}
+      >
         <Checkbox
           id={id}
           value={value ?? label}
           name={name}
           defaultChecked={checked}
+          disabled={disabled}
           {...otherProps}
         />
-        <Label htmlFor={id}>{label}</Label>
+        <div data-fs-checkbox-field-content>
+          <Label htmlFor={id}>{label}</Label>
+
+          {shouldDisplayError && (
+            <span data-fs-checkbox-field-error-message>{error}</span>
+          )}
+        </div>
       </div>
     )
   }
