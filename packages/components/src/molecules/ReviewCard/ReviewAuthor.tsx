b7737d95e4f7b31ce987a1c640c1c2557011d0d4
Artur Santiago <artursantiago2001@hotmail.com>
2025-01-28 14:28:17 -0300

    feat(ReviewCard): adjust Tooltip placement

--- packages/components/src/molecules/ReviewCard/ReviewAuthor.tsx
+++ packages/components/src/molecules/ReviewCard/ReviewAuthor.tsx
@@ -27,7 +27,7 @@ const ReviewAuthor = forwardRef<HTMLDivElement, ReviewAuthorProps>(
       <div data-fs-review-card-author {...otherProps} ref={ref}>
         <span data-fs-review-card-author-name>{author}</span>
         {isVerified && (
-          <Tooltip content={verifiedText}>
+          <Tooltip content={verifiedText} placement="bottom">
             <Icon
               data-fs-review-card-author-verified
               name="CircleWavyCheck"
