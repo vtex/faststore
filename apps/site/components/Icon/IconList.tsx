0102431db373e2b69697fc350f9897218ffe9738
Matheus P. Silva <cout.matheusps@gmail.com>
2025-02-03 17:19:56 -0300

    chore: remove site (#2666)
    ## What's the purpose of this pull request?
    
    - Remove site since its not being used

--- apps/site/components/Icon/IconList.tsx
+++
@@ -1,8 +0,0 @@
-import React, { type PropsWithChildren } from 'react'
-import styles from './icon.module.css'
-
-const IconList = ({ children }: PropsWithChildren) => {
-  return <ul className={styles.iconList}>{children}</ul>
-}
-
-export default IconList
