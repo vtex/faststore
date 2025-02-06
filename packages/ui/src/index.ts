121de97a7080d206fd9334807d7762e6ae3aa2e0
Artur Santiago <artursantiago2001@hotmail.com>
2025-02-06 16:37:52 -0300

    fix: remove old textarea

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,9 +1,5 @@
 export * from '@faststore/components'
 
-// Atoms
-export { default as TextArea } from './components/atoms/TextArea'
-export type { TextAreaProps } from './components/atoms/TextArea'
-
 export { default as Incentive } from './components/atoms/Incentive'
 export type { IncentiveProps } from './components/atoms/Incentive'
 
