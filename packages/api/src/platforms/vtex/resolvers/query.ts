648bbf8b31361cdf6a94bb8de50b6ccc7d6522c7
Bruno Guilera Gutchenzo <bruno.guilera@cubos.io>
2025-02-14 10:26:14 -0300

    feat: rating resolvers (#2648)
    ## What's the purpose of this pull request?
    
    To add product rating resolvers on graphQL
    
    ## How it works?
    
    It defines a new type called: `StoreProductRating` and increments the
    query resolver for `product` and for `searchResult`
    
    It was necessary to adapt the `EnhancedSku` type and also to adds a
    rating callback to the searchResult's promise resolution
    
    ## How to test it?
    
    run the api graphql server locally with the following command:
    ```bash
    yarn dev:server
    ```
    and make a query call
    
    ## References
    
    [JIRA Task: SFS-2093](https://vtex-dev.atlassian.net/browse/SFS-2093)
    
    ## Checklist
    
    <em>You may erase this after checking them all :wink:</em>
    
    **PR Description**
    
    - [ ] Adds graphQL Rating type
    - [ ] Increments `product` resolver
    - [ ] Increments `searchResult` resolver

--- packages/api/src/platforms/vtex/resolvers/query.ts
+++ packages/api/src/platforms/vtex/resolvers/query.ts
@@ -79,6 +79,10 @@ export const Query = {
         )
       }
 
+      const rating = await commerce.rating(sku.itemId)
+
+      sku.rating = rating
+
       return sku
     } catch (err) {
       if (slug == null) {
@@ -103,9 +107,15 @@ export const Query = {
         throw new NotFoundError(`No product found for id ${route.id}`)
       }
 
+      const rating = await commerce.rating(product.productId)
+
       const sku = pickBestSku(product.items)
 
-      return enhanceSku(sku, product)
+      const enhancedSku = enhanceSku(sku, product)
+
+      enhancedSku.rating = rating
+
+      return enhancedSku
     }
   },
   collection: (_: unknown, { slug }: QueryCollectionArgs, ctx: Context) => {
