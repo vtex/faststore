e60a50ffc1f19ad1caf61d5392d8127fdd9fe87d
Artur Santiago <artursantiago2001@hotmail.com>
2025-02-19 12:00:58 -0300

    feat: create AddReviewModal experimental component in packages/core

--- packages/core/src/components/sections/ReviewsAndRatings/DefaultComponents.ts
+++ packages/core/src/components/sections/ReviewsAndRatings/DefaultComponents.ts
@@ -1,6 +1,21 @@
+import { Button as UIButton } from '@faststore/ui'
+
+import dynamic from 'next/dynamic'
+
+const AddReviewModal = dynamic(
+  () =>
+    /* webpackChunkName: "AddReviewModal" */
+    import('src/components/reviews/AddReviewModal')
+)
+
 export const ReviewsAndRatingsDefaultComponents = {
   // TODO: Update this with the components that will be used in ReviewsAndRatings section
   // Olhar o packages/core/src/components/sections/ProductGallery/DefaultComponents.ts
   // ou o packages/core/src/components/sections/ProductShelf/DefaultComponents.ts
   // para se basear
+
+  // TODO: This button is just for now, to be able to see and
+  // open the modal until the final component is done
+  AddReviewModalButton: UIButton,
+  __experimentalAddReviewModal: AddReviewModal,
 } as const
--- packages/core/src/components/sections/ReviewsAndRatings/ReviewsAndRatings.tsx
+++ packages/core/src/components/sections/ReviewsAndRatings/ReviewsAndRatings.tsx
@@ -1,21 +1,24 @@
 import { useMemo } from 'react'
 
-import ReviewsAndRatings from '../../ui/ReviewsAndRatings'
+import ReviewsAndRatings, {
+  type ReviewsAndRatingsProps,
+} from '../../ui/ReviewsAndRatings'
 import styles from '../ReviewsAndRatings/section.module.scss'
 import Section from '../Section'
 import { ReviewsAndRatingsDefaultComponents } from './DefaultComponents'
 import { getOverridableSection } from '../../../sdk/overrides/getOverriddenSection'
 
 interface Props {
-  title: string
+  title: ReviewsAndRatingsProps['title']
+  addReviewModal: ReviewsAndRatingsProps['addReviewModal']
 }
-const ReviewsAndRatingsSection = ({ title }: Props) => {
+const ReviewsAndRatingsSection = ({ title, addReviewModal }: Props) => {
   return (
     <Section
       id="reviews-and-ratings"
       className={`${styles.section} section-reviews-and-ratings layout__section`}
     >
-      <ReviewsAndRatings title={title} />
+      <ReviewsAndRatings title={title} addReviewModal={addReviewModal} />
     </Section>
   )
 }
