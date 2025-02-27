4eb899572950b3403de44ac0c66ceb774186948a
Artur Santiago <artursantiago2001@hotmail.com>
2025-02-27 15:17:20 -0300

    fix: declare RatingSummary in section components

--- packages/core/src/components/ui/ReviewsAndRatings/ReviewsAndRatings.tsx
+++ packages/core/src/components/ui/ReviewsAndRatings/ReviewsAndRatings.tsx
@@ -1,13 +1,30 @@
 import { RatingSummary } from '@faststore/ui'
 import { usePDP } from 'src/sdk/overrides/PageProvider'
 import useScreenResize from 'src/sdk/ui/useScreenResize'
+import { type RatingSummaryProps, useUI } from '@faststore/components'
+import { useOverrideComponents } from 'src/sdk/overrides/OverrideContext'
 
 export type ReviewsAndRatingsProps = {
   title: string
+  ratingSummary: {
+    ratingCounter: RatingSummaryProps['textLabels']['ratingCounter']
+    createReviewButton: RatingSummaryProps['textLabels']['createReviewButton']
+  }
+  addReviewModal: {
+    title: string
+    closeButtonAriaLabel: string
+    cancelButtonLabel: string
+    submitButtonLabel: string
+  }
 }
 
-function ReviewsAndRatings({ title, ...otherProps }: ReviewsAndRatingsProps) {
+function ReviewsAndRatings({
+  title,
+  ratingSummary,
+  addReviewModal,
+}: ReviewsAndRatingsProps) {
   const context = usePDP()
+  const { RatingSummary } = useOverrideComponents<'ReviewsAndRatings'>()
   const { isDesktop } = useScreenResize()
 
   const rating = context?.data?.product?.rating
@@ -18,7 +35,14 @@ function ReviewsAndRatings({ title, ...otherProps }: ReviewsAndRatingsProps) {
         <h2 className="text__title-section layout__content">{title}</h2>
         <div data-fs-content>
           {(isDesktop || rating?.totalCount > 0) && (
-            <RatingSummary {...rating} {...otherProps} />
+            <RatingSummary.Component
+              {...RatingSummary.props}
+              textLabels={{ ...ratingSummary }}
+              // Dynamic props shouldn't be overridable
+              // This decision can be reviewed later if needed
+              {...rating}
+              onCreateReviewClick={() => alert('Create review')}
+            />
           )}
         </div>
       </>
