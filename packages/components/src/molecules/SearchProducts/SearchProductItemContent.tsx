2065e29177d2387baf1b6719371dcd19c4faea85
Ramon do Rosário <ramon.rosario@cubos.io>
2025-03-10 15:01:41 -0300

    feat: search product control implementations (#2547)
    ## What's the purpose of this pull request?
    
    This PR is about the implementation of the `Quick Order` feature. Two
    other PRs were created, one related to the new SearchProductItemControl
    component and another handling all the typing.
    
    In this PR, the new component, typing, and some logic related to the
    overall implementation of the feature are brought together. A small
    adjustment was also made to the useBuyButton hook to prevent the
    `SliderOver` from opening when an item is added to the cart.
    
    Attention This PR has dependencies on PRs: #2543 and #2546.
    
    ps: Once PR #2543 is merged, the number of changes in this PR will
    decrease.
    
    Link preview: https://starter-b8rhu2zcu-vtex.vercel.app/
    
    ## Printscreens
    
    
    ![image](https://github.com/user-attachments/assets/c0d5cda3-ceec-4947-b794-d5aa8cd502d6)
    
    
    ![image](https://github.com/user-attachments/assets/4a9c3aa5-446f-4199-94bc-b1bc2fcacdf7)
    
    ps: the "Select multiple" button is just a regular button used for
    visualization purposes. Once the SKUMatrix is passed, the SKUMatrix
    trigger will be displayed.
    
    ---------
    
    Co-authored-by: Hiago Moreira <hiago.moreira@cubos.io>
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>

--- packages/components/src/molecules/SearchProducts/SearchProductItemContent.tsx
+++ packages/components/src/molecules/SearchProducts/SearchProductItemContent.tsx
@@ -1,5 +1,6 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useCallback } from 'react'
 import { ProductPrice } from '../..'
+import SearchProductItemControl from './SearchProductItemControl'
 
 import type { PriceDefinition } from '../../typings/PriceDefinition'
 
@@ -12,23 +13,73 @@ export interface SearchProductItemContentProps {
    * Specifies product's prices.
    */
   price: PriceDefinition
+  /**
+   * Quick order settings.
+   */
+  quickOrder?: {
+    enabled: boolean
+    outOfStockLabel: string
+    availability: boolean
+    hasVariants: boolean
+    skuMatrixControl: React.ReactNode
+    quantity: number
+    min?: number
+    max?: number
+    onChangeQuantity(value: number): void
+    buyProps?: {
+      onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+      'data-testid': string
+      'data-sku': string
+      'data-seller': string
+    }
+  }
+  /**
+   * Event emitted when value is out of the min and max bounds
+   */
+  onValidateBlur?: (min: number, maxValue: number, quantity: number) => void
 }
 
 const SearchProductItemContent = forwardRef<
   HTMLElement,
   SearchProductItemContentProps
->(function SearchProductItemContent({ price, title, ...otherProps }, ref) {
+>(function SearchProductItemContent(
+  { price, title, quickOrder, onValidateBlur, ...otherProps },
+  ref
+) {
+  const renderProductItemContent = useCallback(() => {
+    return (
+      <>
+        <p data-fs-search-product-item-title>{title}</p>
+        {price.value !== 0 && (
+          <ProductPrice
+            data-fs-search-product-item-prices
+            listPrice={price.listPrice}
+            value={price.value}
+            formatter={price.formatter}
+          />
+        )}
+      </>
+    )
+  }, [price.formatter, price.listPrice, price.value, title])
+
   return (
     <section ref={ref} data-fs-search-product-item-content {...otherProps}>
-      <p data-fs-search-product-item-title>{title}</p>
+      {!quickOrder?.enabled && renderProductItemContent()}
 
-      {price.value !== 0 && (
-        <ProductPrice
-          data-fs-search-product-item-prices
-          listPrice={price.listPrice}
-          value={price.value}
-          formatter={price.formatter}
-        />
+      {quickOrder?.enabled && (
+        <SearchProductItemControl
+          outOfStockLabel={quickOrder.outOfStockLabel}
+          availability={quickOrder.availability}
+          hasVariants={quickOrder.hasVariants}
+          skuMatrixControl={quickOrder.skuMatrixControl}
+          quantity={quickOrder.quantity}
+          onChangeQuantity={quickOrder.onChangeQuantity}
+          max={quickOrder.max}
+          onValidateBlur={onValidateBlur}
+          {...quickOrder.buyProps}
+        >
+          {renderProductItemContent()}
+        </SearchProductItemControl>
       )}
     </section>
   )
--- packages/core/src/components/search/SearchProductItem/SearchProductItem.tsx
+++ packages/core/src/components/search/SearchProductItem/SearchProductItem.tsx
@@ -1,8 +1,12 @@
+import { useMemo, useState } from 'react'
 import {
+  Button,
+  Icon,
   SearchProductItem as UISearchProductItem,
   SearchProductItemContent as UISearchProductItemContent,
   SearchProductItemImage as UISearchProductItemImage,
   useSearch,
+  useUI,
 } from '@faststore/ui'
 
 import { Image } from 'src/components/ui/Image'
@@ -10,6 +14,8 @@ import { useFormattedPrice } from 'src/sdk/product/useFormattedPrice'
 import { useProductLink } from 'src/sdk/product/useProductLink'
 import { sendAutocompleteClickEvent } from '../SearchDropdown'
 import type { ProductSummary_ProductFragment } from '@generated/graphql'
+import { useBuyButton } from 'src/sdk/cart/useBuyButton'
+
 import { NavbarProps } from 'src/components/sections/Navbar'
 type SearchProductItemProps = {
   /**
@@ -35,6 +41,7 @@ function SearchProductItem({
   const {
     values: { onSearchSelection },
   } = useSearch()
+  const { pushToast } = useUI()
 
   const { href, onClick, ...baseLinkProps } = useProductLink({
     product,
@@ -42,13 +49,32 @@ function SearchProductItem({
     index,
   })
 
+  const [quantity, setQuantity] = useState<number>(1)
+
   const {
+    id,
+    sku,
+    gtin,
+    brand,
+    isVariantOf,
     isVariantOf: { name },
+    unitMultiplier,
     image: [img],
     offers: {
       lowPrice: spotPrice,
-      offers: [{ listPrice }],
+      offers: [
+        {
+          listPrice,
+          availability,
+          price,
+          listPriceWithTaxes,
+          seller,
+          priceWithTaxes,
+          quantity: offersQuantity,
+        },
+      ],
     },
+    additionalProperty,
   } = product
 
   const linkProps = {
@@ -66,6 +92,43 @@ function SearchProductItem({
     ...baseLinkProps,
   }
 
+  const outOfStock = useMemo(
+    () => availability === 'https://schema.org/OutOfStock',
+    [availability]
+  )
+
+  const hasVariants = useMemo(
+    () =>
+      Boolean(
+        Object.keys(product.isVariantOf.skuVariants.allVariantsByName).length
+      ),
+
+    [product]
+  )
+
+  const buyProps = useBuyButton(
+    {
+      id,
+      price,
+      priceWithTaxes,
+      listPrice,
+      listPriceWithTaxes,
+      seller,
+      quantity,
+      itemOffered: {
+        sku,
+        name,
+        gtin,
+        image: [img],
+        brand,
+        isVariantOf,
+        additionalProperty,
+        unitMultiplier,
+      },
+    },
+    false
+  )
+
   return (
     <UISearchProductItem linkProps={linkProps} {...otherProps}>
       <UISearchProductItemImage>
@@ -77,6 +140,28 @@ function SearchProductItem({
           value: spotPrice,
           listPrice: listPrice,
           formatter: useFormattedPrice,
+        }}
+        onValidateBlur={(min, max, quantity) =>
+          pushToast({
+            title: 'Invalid quantity!',
+            message: `The quantity you entered is outside the range of ${min} to ${max}. The quantity was set to ${quantity}.`,
+            status: 'INFO',
+            icon: <Icon name="CircleWavyWarning" width={30} height={30} />,
+          })
+        }
+        quickOrder={{
+          enabled: quickOrderSettings?.quickOrder,
+          outOfStockLabel: 'Out of stock',
+          availability: !outOfStock,
+          hasVariants,
+          buyProps,
+          quantity,
+          max: offersQuantity,
+          onChangeQuantity: setQuantity,
+          // FIXME: Use SKU Matrix component
+          skuMatrixControl: (
+            <Button variant="tertiary">Select Multiples</Button>
+          ),
         }}
       ></UISearchProductItemContent>
     </UISearchProductItem>
