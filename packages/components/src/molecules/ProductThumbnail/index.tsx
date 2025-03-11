31e2194034f7b920b9a19059336a8b60baf902c1
Artur Santiago <artursantiago2001@hotmail.com>
2025-03-11 10:06:34 -0300

    refactor(ProductThumbnail): move it inside the ReviewModal

--- packages/components/src/molecules/ProductThumbnail/index.tsx
+++
@@ -1,8 +0,0 @@
-export { default } from './ProductThumbnail'
-export type { ProductThumbnailProps } from './ProductThumbnail'
-
-export { default as ProductThumbnailImage } from './ProductThumbnailImage'
-export type { ProductThumbnailImageProps } from './ProductThumbnailImage'
-
-export { default as ProductThumbnailTitle } from './ProductThumbnailTitle'
-export type { ProductThumbnailTitleProps } from './ProductThumbnailTitle'
