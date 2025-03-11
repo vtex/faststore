e1e1ef68c2d9e7066437d516a70879e48d1e40eb
Artur Santiago <artursantiago2001@hotmail.com>
2025-03-11 15:10:15 -0300

    styles: remove comment
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com>

--- packages/core/src/components/sections/ReviewsAndRatings/DefaultComponents.ts
+++ packages/core/src/components/sections/ReviewsAndRatings/DefaultComponents.ts
@@ -6,9 +6,5 @@ const UIRatingSummary = dynamic(() =>
 )
 
 export const ReviewsAndRatingsDefaultComponents = {
-  // TODO: Update this with the components that will be used in ReviewsAndRatings section
-  // Olhar o packages/core/src/components/sections/ProductGallery/DefaultComponents.ts
-  // ou o packages/core/src/components/sections/ProductShelf/DefaultComponents.ts
-  // para se basear
   RatingSummary: UIRatingSummary,
 } as const
