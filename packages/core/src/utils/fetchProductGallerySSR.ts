31a67f2c87efed8adc04e0b87d96957417b1a549
eduardoformiga <eduardo.formiga@gmail.com>
2025-03-11 21:56:42 -0300

    feat: add ServerManyProductsQuery and fetchServerManyProducts function

---
+++ packages/core/src/utils/fetchProductGallerySSR.ts
@@ -0,0 +1,120 @@
+import type { SearchState } from '@faststore/sdk'
+import { gql } from '@generated/gql'
+import type {
+  ServerManyProductsQueryQuery,
+  ServerManyProductsQueryQueryVariables,
+} from '@generated/graphql'
+import { ITEMS_PER_PAGE } from 'src/constants'
+import { execute } from 'src/server'
+import storeConfig from '../../discovery.config'
+
+export const query = gql(`
+  query ServerManyProductsQuery(
+    $first: Int!
+    $after: String
+    $sort: StoreSort!
+    $term: String!
+    $selectedFacets: [IStoreSelectedFacet!]!
+    $sponsoredCount: Int
+  ) {
+    ...ClientManyProducts
+    search(
+      first: $first
+      after: $after
+      sort: $sort
+      term: $term
+      selectedFacets: $selectedFacets
+      sponsoredCount: $sponsoredCount
+    ) {
+      products {
+        pageInfo {
+          totalCount
+        }
+        edges {
+          node {
+            ...ProductSummary_product
+          }
+        }
+      }
+      metadata {
+        ...SearchEvent_metadata
+      }
+    }
+  }
+`)
+
+export async function fetchServerManyProducts({
+  itemsPerPage = ITEMS_PER_PAGE,
+  sort = 'score_desc',
+  term = '',
+  selectedFacets = [],
+  sponsoredCount = 3,
+  filter = true,
+}: {
+  itemsPerPage: number
+  sort: SearchState['sort']
+  term: string
+  selectedFacets?: {
+    key: string
+    value: string
+  }[]
+  sponsoredCount?: number
+  filter?: boolean
+}) {
+  let { channel, locale } = storeConfig.session
+
+  if (filter) {
+    const { hasOnlyDefaultSalesChannel, ...filteredChannel } =
+      JSON.parse(channel)
+    channel = JSON.stringify(filteredChannel)
+  }
+
+  try {
+    const variables: ServerManyProductsQueryQueryVariables = {
+      first: itemsPerPage,
+      after: (itemsPerPage * 0).toString(),
+      sort,
+      term,
+      selectedFacets: [
+        ...selectedFacets,
+        { key: 'channel', value: channel ?? '' },
+        { key: 'locale', value: locale },
+      ],
+      sponsoredCount,
+    }
+
+    const result = await execute<
+      ServerManyProductsQueryQueryVariables,
+      ServerManyProductsQueryQuery
+    >({
+      variables,
+      operation: query,
+    })
+
+    // used as cache in useCreateUseGalleryPage hook of frontend
+    const resultVariables = {
+      ...variables,
+      selectedFacets: [
+        ...selectedFacets,
+        {
+          key: 'fuzzy',
+          value: result.data.search.metadata?.fuzzy ?? 'auto',
+        },
+        {
+          key: 'operator',
+          value: result.data.search.metadata?.logicalOperator ?? 'and',
+        },
+        { key: 'channel', value: channel ?? '' },
+        { key: 'locale', value: locale },
+      ],
+      sponsoredCount,
+    }
+
+    return [result.data, resultVariables] as [
+      ServerManyProductsQueryQuery,
+      ServerManyProductsQueryQueryVariables,
+    ]
+  } catch (error) {
+    console.error(error)
+  }
+}
