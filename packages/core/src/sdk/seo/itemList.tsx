5d2dd07db60cea1477cc686232c7cd21e52037ca
eduardoformiga <eduardo.formiga@gmail.com>
2025-03-13 17:06:04 -0300

    feat: adds basic ItemList structure using json ld

---
+++ packages/core/src/sdk/seo/itemList.tsx
@@ -0,0 +1,44 @@
+import { JsonLd, type JsonLdProps } from './jsonLd'
+
+export function setItemListElements(items: ItemListElement[]) {
+  if (items && items.length) {
+    return items.map(({ type, ...rest }) => ({
+      '@type': type ?? 'ListItem',
+      ...rest,
+    }))
+  }
+
+  return undefined
+}
+
+export interface ItemListElement {
+  type?: string
+  [key: string]: any
+}
+
+export interface ItemListJsonLdProps extends JsonLdProps {
+  itemListElements: ItemListElement[]
+}
+
+function ItemListJsonLd({
+  type = 'ItemList',
+  keyOverride,
+  itemListElements,
+  ...rest
+}: ItemListJsonLdProps) {
+  const data = {
+    ...rest,
+    itemListElement: setItemListElements(itemListElements),
+  }
+
+  return (
+    <JsonLd
+      type={type}
+      keyOverride={keyOverride}
+      {...data}
+      scriptKey="ItemList"
+    />
+  )
+}
+
+export default ItemListJsonLd
