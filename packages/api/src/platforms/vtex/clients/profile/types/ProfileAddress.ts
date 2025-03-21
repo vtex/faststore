83542e10a7719ddcb1bf5ee67a7ae9a8bd2101e7
Fanny Chien <fannychienn@gmail.com>
2025-03-20 22:54:07 -0300

    feat: Add profile client and types

---
+++ packages/api/src/platforms/vtex/clients/profile/types/ProfileAddress.ts
@@ -0,0 +1,23 @@
+export interface ProfileInput {
+  userId: string
+}
+
+export interface Profile {
+  addresses: ProfileAddress[]
+}
+
+export interface ProfileAddress {
+  addressName: string
+  addressType: string
+  postalCode: string
+  city: string
+  state: string
+  country: string
+  street: string
+  number: string
+  neighborhood: string
+  complement: string
+  reference: string
+  receiverName: string
+  geoCoordinate: [number][number]
+}
