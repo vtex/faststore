ed2dfe79cd6a265bc91986b16d0a03b39963262a
Fanny Chien <fanny.chien@vtex.com>
2025-03-24 19:04:57 -0300

    feat: Gets user's saved location if logged in and uses in session (#2739)
    ## What's the purpose of this pull request?
    
    This PR implements the scenario where the user is logged in, and when
    the user's saved location (address and geo-coordinates) is available, it
    is saved in the current session.
    
    <img width="400" alt="Screenshot 2025-03-12 at 10 52 35"
    src="https://github.com/user-attachments/assets/f5c345cf-395b-4e48-bd84-c5f280e32d65"
    />
    
    ## How it works?
    
    1. First we check if user is loggedIn
    2. If so, gets it's addresses
    3. Check for saved address (using the first of the list) postalCode and
    geoCoordinate, If available we update the current session with those
    information.
    
    ## How to test it?
    
    First, we will need to simulate user logged. (you can follow this
    [docs](https://docs.google.com/document/d/1DSwrD19dOvzFc650UAlLg4diyxmYTHaBD_4lxWQT8YY/edit?tab=t.0)
    steps with more detail or follow the steps below)
    1. Go to https://www.vtexfaststore.com/ and sign in, then copy the
    cookies: _VtexIdclientAutCookie_storeframework_, _vtex_session_ and
    _vtex_segment_
    2. Running the store locally, paste those cookies.
    3. Update the indexDB you should be able to see `person` object with
    user's information (that was previously `null`)
    4. Now, refresh the page.
    5. if there is an address saved for this user, you should be able to see
    the `postalCode` and `geoCoordinates` in the session
    <img width="823" alt="image"
    src="https://github.com/user-attachments/assets/1ecbb614-aa9f-4a46-bc0c-51b7e756aba6"
    />
    <img width="885" alt="image"
    src="https://github.com/user-attachments/assets/6dc80260-d705-4422-9c01-785a11982f94"
    />
    
    
    ### Starters Deploy Preview
    https://github.com/vtex-sites/starter.store/pull/731
    
    [Jira
    Task](https://vtex-dev.atlassian.net/jira/software/c/projects/SFS/boards/1051?selectedIssue=SFS-2205)

--- packages/core/src/sdk/session/index.ts
+++ packages/core/src/sdk/session/index.ts
@@ -10,6 +10,7 @@ import type {
 import storeConfig from '../../../discovery.config'
 import { cartStore } from '../cart'
 import { request } from '../graphql/request'
+import { getSavedAddress } from '../profile'
 import { createValidationStore, useStore } from '../useStore'
 
 export const mutation = gql(`
@@ -58,12 +59,37 @@ export const mutation = gql(`
 `)
 
 export const validateSession = async (session: Session) => {
+  // If deliveryPromise is enabled and there is no postalCode in the session
   if (storeConfig.deliveryPromise?.enabled && !session.postalCode) {
-    // Use the initial postalCode defined in discovery.config.js when there is no postalCode in the session
-    const initialPostalCode = defaultStore.readInitial().postalCode
+    const isLoggedIn = !!session.person?.id
 
-    if (!!initialPostalCode) {
-      sessionStore.set({ ...session, postalCode: initialPostalCode })
+    // If user is logged try to get the location (postalCode / geoCoordinates / country) from the user's address
+    if (isLoggedIn) {
+      const userId = session.person?.id
+      const address = await getSavedAddress(userId)
+
+      // Save the location in the session
+      if (address) {
+        sessionStore.set({
+          ...session,
+          postalCode: address?.postalCode,
+          geoCoordinates: {
+            // the values come in the reverse expected order
+            latitude: address?.geoCoordinate ? address?.geoCoordinate[1] : null,
+            longitude: address?.geoCoordinate
+              ? address?.geoCoordinate[0]
+              : null,
+          },
+          country: address?.country,
+        })
+      }
+    } else {
+      // Use the initial postalCode defined in discovery.config.js
+      const initialPostalCode = defaultStore.readInitial().postalCode
+
+      if (!!initialPostalCode) {
+        sessionStore.set({ ...session, postalCode: initialPostalCode })
+      }
     }
   }
 
