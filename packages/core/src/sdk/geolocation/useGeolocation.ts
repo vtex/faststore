4f585b14a375c8765139ad4e80063aef29116a4e
Lucas Feijó <lucas.portela@vtex.com>
2025-03-25 09:12:24 -0300

    fix: Ensure current location data won't be overwritten

--- packages/core/src/sdk/geolocation/useGeolocation.ts
+++ packages/core/src/sdk/geolocation/useGeolocation.ts
@@ -4,18 +4,28 @@ import { deliveryPromise } from 'discovery.config'
 import { useSession, validateSession, sessionStore } from 'src/sdk/session'
 
 export default function useGeolocation() {
-  const { isValidating: _, ...session } = useSession()
+  const { postalCode: stalePostalCode, geoCoordinates: staleGeoCoordinates } =
+    useSession()
 
   useEffect(() => {
     if (!deliveryPromise.enabled) {
       return
     }
 
-    if (navigator?.geolocation && !session.geoCoordinates) {
+    if (navigator?.geolocation && (!stalePostalCode || staleGeoCoordinates)) {
       navigator.geolocation.getCurrentPosition(
         async ({ coords: { latitude, longitude } }) => {
+          // We need to revalidate the session because users can set a zip code while granting consent.
+          const revalidatedSession = sessionStore.read()
+          if (
+            revalidatedSession.postalCode ||
+            revalidatedSession.geoCoordinates
+          ) {
+            return
+          }
+
           const newSession = {
-            ...session,
+            ...revalidatedSession,
             geoCoordinates: { latitude, longitude },
           }
           const validatedSession = await validateSession(newSession)
