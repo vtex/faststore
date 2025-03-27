01406a66dfe6e2fd1a669bc0b98d4ede44191473
Pedro Soares <pedro.soares@vtex.com>
2025-03-27 14:42:43 -0300

    fix: call redirect-evaluate only for pages

--- packages/core/src/sdk/redirects/index.ts
+++ packages/core/src/sdk/redirects/index.ts
@@ -17,9 +17,17 @@ type RewriterResponse = {
 
 const PERMANENT_STATUS = 308
 
+const ASSET_FILE_REGEX = /\.(js|css|png|jpg|jpeg|svg|gif|webp|ico|json|map)$/i
+
 export async function getRedirect({
   pathname,
 }: GetRedirectArgs): Promise<GetRedirectReturn> {
+  const isValidPath = !ASSET_FILE_REGEX.test(pathname)
+
+  if (!isValidPath) {
+    return null
+  }
+
   try {
     const redirectMatch = matcher({ pathname })
     if (redirectMatch) {
