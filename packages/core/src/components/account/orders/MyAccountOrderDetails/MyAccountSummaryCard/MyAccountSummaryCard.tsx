2da69b9902e1dd43e781a97bbf27fefaecbda57d
Artur Santiago <artursantiago2001@hotmail.com>
2025-04-01 14:24:55 -0300

    feat(MyAccount): page structure

---
+++ packages/core/src/components/account/orders/MyAccountOrderDetails/MyAccountSummaryCard/MyAccountSummaryCard.tsx
@@ -0,0 +1,9 @@
+import MyAccountCard from '../../../components/MyAccountCard'
+
+function MyAccountSummaryCard() {
+  return (
+    <MyAccountCard title="Summary" data-fs-order-summary-card></MyAccountCard>
+  )
+}
+
+export default MyAccountSummaryCard
