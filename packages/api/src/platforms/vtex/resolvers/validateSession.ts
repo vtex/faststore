fbcf557c402be22cbc1b5f0279886adaf1081b08
Lucas Feijó <lucas.portela@vtex.com>
2025-04-02 14:56:14 -0300

    chore: Handle B2B users

--- packages/api/src/platforms/vtex/resolvers/validateSession.ts
+++ packages/api/src/platforms/vtex/resolvers/validateSession.ts
@@ -145,9 +145,11 @@ export const validateSession = async (
       seller: seller?.id,
       hasOnlyDefaultSalesChannel: !store?.channel?.value,
     }),
-    b2b: {
-      customerId: authentication?.customerId?.value ?? '',
-    },
+    b2b: authentication?.customerId?.value
+      ? {
+          customerId: authentication?.customerId?.value ?? '',
+        }
+      : null,
     marketingData,
     person: profile?.id
       ? {
