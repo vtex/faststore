667c57fdf346bfed5dc3225129fa64ced49ec77f
eduardoformiga <eduardo.formiga@gmail.com>
2025-04-07 19:42:33 -0300

    feat: uses router.pathname as dependency and remove logs

--- packages/core/src/Layout.tsx
+++ packages/core/src/Layout.tsx
@@ -3,7 +3,6 @@ import { useMemo, type PropsWithChildren, type ReactElement } from 'react'
 import { usePageViewEvent } from './sdk/analytics/hooks/usePageViewEvent'
 
 function Layout({ children }: PropsWithChildren) {
-  console.log('🚀 ~ Layout:')
   const props = useMemo(() => (children as ReactElement)?.props, [])
   usePageViewEvent(props)
 
