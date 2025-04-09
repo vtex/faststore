3f01e99652253bf9bc7892b8d6d7a2a15bc269d7
Fanny Chien <fanny.chien@vtex.com>
2025-04-09 10:49:35 -0300

    feat: Displays region modal if no addresses and location is mandatory (#2750)
    ## What's the purpose of this pull request?
    
    This PR implements the scenario where no address is available and the
    location is set as mandatory on `discovery.config`
    
    <img width="400" alt="image"
    src="https://github.com/user-attachments/assets/c37757e3-7d4f-4cbe-9ea6-9fabf781547e"
    />
    
    **note**: The region modal and the flow implemented in this PR differ
    from the previous prototype. But I've already aligned with @renatamottam
    (design team) and she will update the figma file. (The main difference
    is that we won't be using the `Set Location` button, we agreed to use
    the `Apply` from the input field)
    
    <img width="263" alt="image"
    src="https://github.com/user-attachments/assets/827d0a3e-10c9-4628-99b3-3a86f0b6013e"
    />
    
    We also made a fix in the current flow of the default `RegionModal`. 
    - It won't automatically close if the added postalCode is invalid and
    not applied.
    
    Adds `buttonActionText` field in hCMS (the default is `Apply`)
    | hCMS | RegionModal|
    |-|-|
    |<img width="500" alt="image"
    src="https://github.com/user-attachments/assets/64b8aa01-a5e7-48a0-95fe-4d742d8b2f8d"
    />|<img width="589" alt="image"
    src="https://github.com/user-attachments/assets/cbe4c7e3-e6e9-4fad-bfbe-c4a8d10ed683"
    />|
    
    TODO: when merging the feature branch, remind to run cms-sync in the
    default account.
    
    ## How it works?
    
    1. when accessing the website, If there is no address and location is
    set as mandatory, regardless of the page they are accessing, the region
    modal should appear "forcing" the user to add a valid postal code.
    2. while the region modal is displayed the user won't see the modal's
    close button and won't be able to use the escape key to close it.
    3. when a valid postal code is entered, the region modal will close
    automatically.
    <img width="500" alt="image"
    src="https://github.com/user-attachments/assets/662caa3b-3fad-4c45-9f2e-c492dc4776d3"
    />
    
    <img width="500" alt="image"
    src="https://github.com/user-attachments/assets/eb6fcef6-f95c-4b92-aa72-0a5d56c42415"
    />
    
    ## How to test it?
    
    1. Run locally or test via this
    [preview](https://starter-6czvt97g2-vtex.vercel.app)
    
    in the `discovery.config` file, sets `mandatory` as true:
    
     ```
     deliveryPromise: {
        enabled: true,
        mandatory: true,
      },
    ```
    
    2. **Logged in / postalCode available scenario**
    If there is a postalCode already set, you shouldn't see a region modal appearing when accessing the website.
    When clicking to edit the postalCode button (in this example `07008`), you should able to see the `RegionModal` with the close button and be able to close it using the esc key.
    
    <img width="500" alt="image" src="https://github.com/user-attachments/assets/65be27fe-a76a-40cd-81cf-6d13f014fca9" />
    
    3. **postalCode not defined**
    When accessing the website, `RegionModal`  should appear "forcing" the user to add a valid postal code. There won't be a close button and you won't be able to close it using the esc key.
    
    <img width="500" alt="image" src="https://github.com/user-attachments/assets/99f4021a-9166-4c25-87bc-a27c27da5af3" />
    
    
    ### Starters Deploy Preview
    
    https://github.com/vtex-sites/starter.store/pull/741

--- packages/core/src/Layout.tsx
+++ packages/core/src/Layout.tsx
@@ -1,9 +1,10 @@
 import type { PropsWithChildren, ReactElement } from 'react'
+import { usePageViewEvent } from 'src/sdk/analytics/hooks/usePageViewEvent'
+import { useRegionModal } from './components/region/RegionModal/useRegionModal'
 
-import { usePageViewEvent } from './sdk/analytics/hooks/usePageViewEvent'
-
-function Layout({ children }: PropsWithChildren) {
+const Layout = function Layout({ children }: PropsWithChildren) {
   usePageViewEvent((children as ReactElement)?.props)
+  useRegionModal()
 
   return <>{children}</>
 }
