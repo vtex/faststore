792b9b5af14add9206ee72714108226f1a263f8d
eduardoformiga <eduardo.formiga@gmail.com>
2025-04-14 16:17:27 -0300

    feat: turns profile route with own ssr

--- packages/core/src/pages/account/profile.tsx
+++ packages/core/src/pages/account/profile.tsx
@@ -1,16 +1,19 @@
 /* ######################################### */
 /* Mocked Page until development is finished, it will be removed after */
 
+import type { Locator } from '@vtex/client-cms'
+import type { GetServerSideProps } from 'next'
 import { NextSeo } from 'next-seo'
 import type { ComponentType } from 'react'
 import { MyAccountLayout } from 'src/components/account'
 import RenderSections from 'src/components/cms/RenderSections'
 import { default as GLOBAL_COMPONENTS } from 'src/components/cms/global/Components'
 import CUSTOM_COMPONENTS from 'src/customizations/src/components'
-import {
-  getServerSideProps,
-  type MyAccountProps,
-} from 'src/experimental/myAccountSeverSideProps'
+
+import { getGlobalSectionsData } from 'src/components/cms/GlobalSections'
+
+import type { MyAccountProps } from 'src/experimental/myAccountSeverSideProps'
+import { injectGlobalSections } from 'src/server/cms/global'
 
 /* A list of components that can be used in the CMS. */
 const COMPONENTS: Record<string, ComponentType<any>> = {
@@ -45,4 +48,31 @@ export default function Profile({ globalSections }: MyAccountProps) {
   )
 }
 
-export { getServerSideProps }
+export const getServerSideProps: GetServerSideProps<
+  MyAccountProps,
+  Record<string, string>,
+  Locator
+> = async ({ previewData }) => {
+  const [
+    globalSectionsPromise,
+    globalSectionsHeaderPromise,
+    globalSectionsFooterPromise,
+  ] = getGlobalSectionsData(previewData)
+
+  const [globalSections, globalSectionsHeader, globalSectionsFooter] =
+    await Promise.all([
+      globalSectionsPromise,
+      globalSectionsHeaderPromise,
+      globalSectionsFooterPromise,
+    ])
+
+  const globalSectionsResult = injectGlobalSections({
+    globalSections,
+    globalSectionsHeader,
+    globalSectionsFooter,
+  })
+
+  return {
+    props: { globalSections: globalSectionsResult },
+  }
+}
