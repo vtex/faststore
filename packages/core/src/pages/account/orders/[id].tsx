f3e554b00aca96a675ab381422718e8057a02a28
eduardoformiga <eduardo.formiga@gmail.com>
2025-04-14 16:17:51 -0300

    feat: turns order detail route with own ssr

--- packages/core/src/pages/account/orders/[id].tsx
+++ packages/core/src/pages/account/orders/[id].tsx
@@ -1,29 +1,30 @@
+import type { Locator } from '@vtex/client-cms'
+import type { GetServerSideProps } from 'next'
 import { NextSeo } from 'next-seo'
-import { useRouter } from 'next/router'
 import type { ComponentType } from 'react'
 import { MyAccountLayout } from 'src/components/account'
 import MyAccountOrderDetails from 'src/components/account/orders/MyAccountOrderDetails'
 import RenderSections from 'src/components/cms/RenderSections'
 import { default as GLOBAL_COMPONENTS } from 'src/components/cms/global/Components'
 import CUSTOM_COMPONENTS from 'src/customizations/src/components'
-import {
-  getServerSideProps,
-  type MyAccountProps,
-} from 'src/experimental/myAccountSeverSideProps'
+import type { MyAccountProps } from 'src/experimental/myAccountSeverSideProps'
+
+import { getGlobalSectionsData } from 'src/components/cms/GlobalSections'
+import { injectGlobalSections } from 'src/server/cms/global'
 
 const COMPONENTS: Record<string, ComponentType<any>> = {
   ...GLOBAL_COMPONENTS,
   ...CUSTOM_COMPONENTS,
 }
 
-export default function OrderDetailsPage({ globalSections }: MyAccountProps) {
-  // TODO: This is a temporary solution to get the order id from the url.
-  // We should use the getServerSideProps to get the order id from the api
-  const router = useRouter()
-  const id = Array.isArray(router.query.id)
-    ? router.query.id[0]
-    : router.query.id
+type OrderDetailsPageProps = {
+  orderId: string
+} & MyAccountProps
 
+export default function OrderDetailsPage({
+  globalSections,
+  orderId,
+}: OrderDetailsPageProps) {
   return (
     <RenderSections
       globalSections={globalSections.sections}
@@ -32,10 +33,41 @@ export default function OrderDetailsPage({ globalSections }: MyAccountProps) {
       <NextSeo noindex nofollow />
 
       <MyAccountLayout>
-        <MyAccountOrderDetails orderId={id} />
+        <MyAccountOrderDetails orderId={orderId} />
       </MyAccountLayout>
     </RenderSections>
   )
 }
 
-export { getServerSideProps }
+export const getServerSideProps: GetServerSideProps<
+  OrderDetailsPageProps,
+  Record<string, string>,
+  Locator
+> = async (context) => {
+  const {
+    previewData,
+    params: { orderId },
+  } = context
+  const [
+    globalSectionsPromise,
+    globalSectionsHeaderPromise,
+    globalSectionsFooterPromise,
+  ] = getGlobalSectionsData(previewData)
+
+  const [globalSections, globalSectionsHeader, globalSectionsFooter] =
+    await Promise.all([
+      globalSectionsPromise,
+      globalSectionsHeaderPromise,
+      globalSectionsFooterPromise,
+    ])
+
+  const globalSectionsResult = injectGlobalSections({
+    globalSections,
+    globalSectionsHeader,
+    globalSectionsFooter,
+  })
+
+  return {
+    props: { globalSections: globalSectionsResult, orderId },
+  }
+}
