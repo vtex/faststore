8e8645d38d9288991643113a25c4bd9ac862ed0a
Artur Santiago <artursantiago2001@hotmail.com>
2025-04-15 09:39:48 -0300

    feat(MyAccountSummaryCard): use locale to format price

--- packages/core/src/components/account/orders/MyAccountOrderDetails/MyAccountSummaryCard/MyAccountSummaryCard.tsx
+++ packages/core/src/components/account/orders/MyAccountOrderDetails/MyAccountSummaryCard/MyAccountSummaryCard.tsx
@@ -1,4 +1,5 @@
 import MyAccountCard from 'src/components/account/components/MyAccountCard'
+import { useSession } from 'src/sdk/session'
 
 // Interface for order totals (items, shipping, discounts)
 // TODO: Use type from API
@@ -29,6 +30,7 @@ function MyAccountSummaryCard({
   currencyCode,
   transactions,
 }: MyAccountSummaryCardProps) {
+  const { locale } = useSession()
   // Calculate any payment surcharges from active transactions
   const calculatePaymentSurcharge = () => {
     let surchargeAmount = 0
@@ -65,29 +67,30 @@ function MyAccountSummaryCard({
     return totals
   }
 
+  // Format price values according to the specified currency (converts cents to standard units)
+  const formatPrice = (value: number, currencyCode: string, locale: string) => {
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currencyCode,
+      minimumFractionDigits: 2,
+    }).format(value / 100)
+  }
+
   const displayTotals = getDisplayTotals()
 
   const totalAmount = displayTotals.reduce((sum, total) => sum + total.value, 0)
-
-  // Format price values according to the specified currency (converts cents to standard units)
-  const formatPrice = (value: number) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: currencyCode,
-    }).format(value / 100)
-  }
 
   return (
     <MyAccountCard title="Summary" data-fs-order-summary-card>
       {displayTotals.map((total) => (
         <div key={total.id} data-fs-order-summary-item>
           <span>{total.name}</span>
-          <span>{formatPrice(total.value)}</span>
+          <span>{formatPrice(total.value, currencyCode, locale)}</span>
         </div>
       ))}
       <div data-fs-order-summary-item data-fs-order-summary-total>
         <span>Total</span>
-        <span>{formatPrice(totalAmount)}</span>
+        <span>{formatPrice(totalAmount, currencyCode, locale)}</span>
       </div>
     </MyAccountCard>
   )
