b0c870396d42ea7e05f0b19d2327cdb2847be385
Victor Hugo M. Pinto <victor2142@gmail.com>
2025-04-21 18:15:38 +0100

    Add new cp-sync command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,6 +5,7 @@ import { default as Dev } from './commands/dev'
 import { default as Build } from './commands/build'
 import { default as Serve } from './commands/start'
 import { default as CmsSync } from './commands/cms-sync'
+import { default as ContentPlatformSync } from './commands/cp-sync'
 import { default as Test } from './commands/test'
 
 export const commands = {
@@ -13,5 +14,6 @@ export const commands = {
   build: Build,
   serve: Serve,
   'cms-sync': CmsSync,
+  'content-platform-sync': ContentPlatformSync,
   test: Test,
 }
