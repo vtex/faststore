9f869ac82465b7553940bbfa093844a6f402ab44
Matheus P. Silva <cout.matheusps@gmail.com>
2025-04-23 15:32:54 -0300

    add revalidate route

---
+++ packages/core/src/app/api/revalidate/route.ts
@@ -0,0 +1,31 @@
+import { revalidatePath } from 'next/cache'
+import { type NextRequest, NextResponse } from 'next/server'
+
+export const TAGS = {
+  collections: 'collections',
+  products: 'products',
+  cart: 'cart',
+}
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  console.log({
+    called: 'called me',
+    req,
+  })
+
+  const secret = req.nextUrl.searchParams.get('secret')
+  const path = req.nextUrl.searchParams.get('path')
+
+  if (!path) {
+    return NextResponse.json({
+      status: 200,
+      revalidated: false,
+      now: Date.now(),
+      message: 'Missing path to revalidate',
+    })
+  }
+
+  revalidatePath(path, 'page')
+
+  return NextResponse.json({ status: 200, revalidated: true, now: Date.now() })
+}
