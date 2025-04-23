060f800da88da06d3905638d5ce33942b24eaae9
Victor Hugo M. Pinto <victor2142@gmail.com>
2025-04-23 16:54:14 +0100

    Add duplicate definitions check to cp-sync

--- packages/cli/src/utils/contentPlatform.ts
+++ packages/cli/src/utils/contentPlatform.ts
@@ -4,11 +4,12 @@ import { readFileSync, readdir, stat } from 'fs-extra'
 import chalk from 'chalk'
 
 import { formSchema as MockSchema } from './__mockSchema__'
+import { CliUx } from '@oclif/core'
 
 //const SCHEMA_REGISTRY_BASE_URL =
 //  "https://api.vtexcommercebeta.com.br/api/content-platform/schemas";
 
-// const SCHEMA_REGISTRY_BASE_URL = "http://localhost:3002";
+// const SCHEMA_REGISTRY_BASE_URL = 'http://localhost:3002'
 
 const CP_COMPONENT_SCHEMA_REGEX = /(?:^|\/)cp_schema[^/]*\.(json|jsonc)$/
 const CP_CONTENT_TYPE_SCHEMA_REGEX =
@@ -51,7 +52,7 @@ async function fetchFastStoreSchema() {
   return fastStoreSchema
 }
 
-async function groupComponentDefinitions(componentsPath: string) {
+async function groupCustomComponentDefinitions(componentsPath: string) {
   const componentsDir = await listFilesRecursive(
     componentsPath,
     CP_COMPONENT_SCHEMA_REGEX
@@ -85,7 +86,7 @@ async function groupComponentDefinitions(componentsPath: string) {
   return customComponentDefinitions
 }
 
-async function groupContentTypeDefinitions(contentTypesPath: string) {
+async function groupCustomContentTypeDefinitions(contentTypesPath: string) {
   const contentTypesDir = await listFilesRecursive(
     contentTypesPath,
     CP_CONTENT_TYPE_SCHEMA_REGEX
@@ -160,15 +161,35 @@ function addAnyOfToDynamicAreas(schema: Record<string, any>) {
   }
 }
 
-// My idea for finding overrides is using a Set to keep track of previously
-// seen $componentKey's.
-// I first add every key from FastStore's schema then iterate over the custom
-// components doing the same, and just keeping track of the matches.
-// Finally, I can show the user the components they're about to override.
-// function findOverrides(
-//   defaultDefinitions: Record<string, any>,
-//   customDefinitions: Record<string, any>,
-// ) {}
+function findOverrides(
+  defaultDefinitions: Record<string, any>,
+  customDefinitions: Record<string, any>
+) {
+  const defaultKeys = new Set(Object.keys(defaultDefinitions))
+
+  const duplicates = Object.keys(customDefinitions).filter((key) =>
+    defaultKeys.has(key)
+  )
+
+  return duplicates
+}
+
+async function confirmUserChoice(
+  duplicates: string[],
+  definitionType: 'content-types' | 'components'
+) {
+  const goAhead = await CliUx.ux.confirm(
+    `You are about to override default definitions for the following ${definitionType}:
+    \n\n${chalk.yellow(duplicates.join('\n'))}
+    \n\nAre you sure? [yes/no]`
+  )
+
+  if (!goAhead) {
+    throw new Error('Sync operation cancelled by user.')
+  }
+
+  return
+}
 
 export async function generateFullSchema(
   componentsPath: string,
@@ -176,10 +197,28 @@ export async function generateFullSchema(
 ) {
   const [customComponents, customContentTypes, originalSchema] =
     await Promise.all([
-      groupComponentDefinitions(componentsPath),
-      groupContentTypeDefinitions(contentTypesPath),
+      groupCustomComponentDefinitions(componentsPath),
+      groupCustomContentTypeDefinitions(contentTypesPath),
       fetchFastStoreSchema(),
     ])
+
+  const contentTypeDuplicates = findOverrides(
+    originalSchema['content-types'],
+    customContentTypes
+  )
+
+  const componentDuplicates = findOverrides(
+    originalSchema.components,
+    customComponents
+  )
+
+  if (componentDuplicates.length > 0) {
+    await confirmUserChoice(componentDuplicates, 'components')
+  }
+
+  if (contentTypeDuplicates.length > 0) {
+    await confirmUserChoice(contentTypeDuplicates, 'content-types')
+  }
 
   return addAnyOfToDynamicAreas({
     ...originalSchema,
