e72aaca1649929831cc622d60a2d492088c2861d
eduardoformiga <eduardo.formiga@gmail.com>
2025-05-05 23:57:29 -0300

    feat: add FilterFacetDateRange component with date range selection and styling

---
+++ packages/core/src/components/account/components/MyAccountFilterSlider/FilterFacetDateRange/FilterFacetDateRange.tsx
@@ -0,0 +1,93 @@
+import { InputField } from '@faststore/ui'
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react'
+
+export interface FilterFacetDateRangeProps {
+  /**
+   * The from value of the Range Facet
+   */
+  from: string
+  /**
+   * The to value of the Range Facet
+   */
+  to: string
+}
+
+const FilterFacetDateRange = forwardRef<
+  {
+    clear: () => void
+    getDataRangeFacet: () => {
+      key: string
+      value: { from: string; to: string }
+    }
+  },
+  FilterFacetDateRangeProps
+>(({ to, from }, ref) => {
+  const inputFromRef = useRef<HTMLInputElement>(null)
+  const inputToRef = useRef<HTMLInputElement>(null)
+  const [inputFromError, setInputFromError] = useState<string>()
+  const [inputToError, setInputToError] = useState<string>()
+
+  const [dateRange, setDateRange] = useState<{ from: string; to: string }>({
+    from,
+    to,
+  })
+
+  function onChangeInputFrom(value: string) {
+    setInputFromError(undefined)
+
+    // TODO validate date ranges -> from can not be > to
+    setDateRange({ ...dateRange, from: String(value) })
+  }
+
+  function onChangeInputTo(value: string) {
+    setInputToError(undefined)
+
+    // TODO validate date ranges -> To can not be < From
+    setDateRange({ ...dateRange, to: String(value) })
+  }
+
+  useImperativeHandle(ref, () => ({
+    clear: () => {
+      setDateRange({ from: '', to: '' })
+      if (inputFromRef.current) inputFromRef.current.value = ''
+      if (inputToRef.current) inputToRef.current.value = ''
+    },
+    getDataRangeFacet: () => {
+      return {
+        key: 'dateRange',
+        value: dateRange,
+      }
+    },
+  }))
+
+  return (
+    <div data-fs-date-range>
+      <div data-fs-date-range-inputs>
+        <InputField
+          id="date-range-from"
+          label="From"
+          type="date"
+          inputMode="text"
+          error={inputFromError}
+          inputRef={inputFromRef}
+          value={dateRange.from}
+          onChange={(e) => onChangeInputFrom(e.target.value)}
+          // onBlur={() => !inputFromError && onEnd?.(dateRange)}
+        />
+        <InputField
+          id="date-range-to"
+          label="To"
+          type="date"
+          inputMode="text"
+          error={inputToError}
+          inputRef={inputToRef}
+          value={dateRange.to}
+          onChange={(e) => onChangeInputTo(e.target.value)}
+          // onBlur={() => !inputToError && onEnd?.(dateRange)}
+        />
+      </div>
+    </div>
+  )
+})
+
+export default FilterFacetDateRange
---
+++ packages/core/src/components/account/components/MyAccountFilterSlider/MyAccountFilterSlider.tsx
@@ -0,0 +1,207 @@
+import {
+  type SearchInputFieldRef,
+  Filter as UIFilter,
+  FilterFacetBoolean as UIFilterFacetBoolean,
+  FilterFacetBooleanItem as UIFilterFacetBooleanItem,
+  FilterFacets as UIFilterFacets,
+  FilterSlider as UIFilterSlider,
+} from '@faststore/ui'
+
+import router from 'next/router'
+import { type MutableRefObject, useRef } from 'react'
+import type {
+  MyAccountFilter_FacetsFragment,
+  useMyAccountFilter,
+} from 'src/sdk/search/useMyAccountFilter'
+import FilterFacetDateRange from './FilterFacetDateRange/FilterFacetDateRange'
+import styles from './section.module.scss'
+
+export interface FilterSliderProps {
+  /**
+   * ID to find this component in testing tools (e.g.: cypress,
+   * testing-library, and jest).
+   */
+  testId?: string
+  /**
+   * The array that represents the details of every facet.
+   */
+  facets: MyAccountFilter_FacetsFragment[]
+  /**
+   * Title for the `Filter` component.
+   */
+  title?: string
+  /**
+   * Defined label for the clear button component.
+   */
+  clearButtonLabel?: string
+  /**
+   * Defined label for the apply button component.
+   */
+  applyButtonLabel?: string
+  /**
+   * Ref to the search input field.
+   */
+  searchInputRef?: MutableRefObject<SearchInputFieldRef>
+}
+
+function MyAccountFilterSlider({
+  facets,
+  testId,
+  dispatch,
+  expanded,
+  selected,
+  title,
+  clearButtonLabel,
+  applyButtonLabel,
+  searchInputRef,
+}: FilterSliderProps & ReturnType<typeof useMyAccountFilter>) {
+  const dateRangeInputRef = useRef<{
+    clear: () => void
+    getDataRangeFacet: () => {
+      key: string
+      value: { from: string; to: string }
+    }
+  }>(null)
+
+  const handleFilterChange = ({
+    selectedFacets,
+    text,
+  }: {
+    selectedFacets: {
+      key: string
+      value: string
+    }[]
+    text: string
+  }) => {
+    const facets = selectedFacets.reduce(
+      (acc, facet) => {
+        const { key, value } = facet
+        if (key === 'dateInitial') {
+          acc['dateInitial'] = value
+        }
+
+        if (key === 'dateFinal') {
+          acc['dateFinal'] = value
+        }
+
+        if (key === 'status') {
+          acc['status'] = Array.isArray(acc['status'])
+            ? [...acc['status'], value]
+            : [value]
+        }
+
+        return acc
+      },
+      {} as Record<string, string | string[]>
+    )
+
+    router.push({
+      pathname: '/account/orders',
+      query: {
+        ...(text ? { text } : {}),
+        ...facets,
+        page: 1,
+      },
+    })
+  }
+
+  return (
+    <UIFilterSlider
+      overlayProps={{
+        className: `section ${styles.section} section-filter-slider`,
+      }}
+      title={title}
+      size="partial"
+      direction="rightSide"
+      clearBtnProps={{
+        variant: 'secondary',
+        onClick: () => {
+          dateRangeInputRef.current?.clear()
+          searchInputRef.current?.formRef.current?.clear()
+          if (searchInputRef.current?.inputRef.value) {
+            searchInputRef.current.inputRef.value = ''
+          }
+          dispatch({ type: 'selectFacets', payload: [] })
+        },
+        children: clearButtonLabel ?? 'Clear All',
+      }}
+      applyBtnProps={{
+        variant: 'primary',
+        onClick: () => {
+          const dateRangeFacet = dateRangeInputRef.current?.getDataRangeFacet()
+          const selectedFacets =
+            dateRangeFacet.value.from && dateRangeFacet.value.to
+              ? [
+                  ...selected,
+                  {
+                    key: 'dateInitial',
+                    value: dateRangeFacet.value.from,
+                  },
+                  {
+                    key: 'dateFinal',
+                    value: dateRangeFacet.value.to,
+                  },
+                ]
+              : selected
+          handleFilterChange({
+            selectedFacets,
+            text: searchInputRef.current?.inputRef.value,
+          })
+        },
+        children: applyButtonLabel ?? 'Apply',
+      }}
+      onClose={() => {}}
+    >
+      <UIFilter
+        testId={`mobile-${testId}`}
+        indicesExpanded={expanded}
+        onAccordionChange={(index: number) =>
+          dispatch({ type: 'toggleExpanded', payload: index })
+        }
+      >
+        {facets.map((facet, index) => {
+          const { __typename: type, label } = facet
+          const isExpanded = expanded.has(index)
+          return (
+            <UIFilterFacets
+              key={`${testId}-${label}-${index}`}
+              testId={`mobile-${testId}`}
+              index={index}
+              type={type}
+              label={label}
+            >
+              {type === 'StoreFacetBoolean' && isExpanded && (
+                <UIFilterFacetBoolean>
+                  {facet.values.map((item) => (
+                    <UIFilterFacetBooleanItem
+                      key={`${testId}-${facet.label}-${item.label}`}
+                      id={`${testId}-${facet.label}-${item.label}`}
+                      testId={`mobile-${testId}`}
+                      onFacetChange={(facet) =>
+                        dispatch({ type: 'toggleFacet', payload: facet })
+                      }
+                      selected={item.selected}
+                      value={item.value}
+                      quantity={item.quantity}
+                      facetKey={facet.key}
+                      label={item.label}
+                    />
+                  ))}
+                </UIFilterFacetBoolean>
+              )}
+              {type === 'StoreFacetRange' && isExpanded && (
+                <FilterFacetDateRange
+                  ref={dateRangeInputRef}
+                  from={facet.from}
+                  to={facet.to}
+                />
+              )}
+            </UIFilterFacets>
+          )
+        })}
+      </UIFilter>
+    </UIFilterSlider>
+  )
+}
+
+export default MyAccountFilterSlider
