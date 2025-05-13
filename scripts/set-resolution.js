8e8c1af56b745e0f567268604ed39eae4ce57dd2
Otavio Meirelles <otavio.meirelles@vtex.com>
2025-05-13 15:41:55 -0300

    chore: on ci added script to set resolution on starter project before install

---
+++ scripts/set-resolution.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+const [, , basePath, packageToResolve, versionResolution] = process.argv
+
+if (!basePath || !packageToResolve || !versionResolution) {
+  console.error(
+    'Usage: set-resolution <basePath> <packageToResolve> <versionResolution>'
+  )
+  process.exit(1)
+}
+
+const packageJsonPath = path.resolve(process.cwd(), basePath, 'package.json')
+
+if (!fs.existsSync(packageJsonPath)) {
+  console.error(`Error: package.json not found at ${packageJsonPath}`)
+  process.exit(1)
+}
+
+const packageJson = require(packageJsonPath)
+
+packageJson.resolutions = packageJson.resolutions ?? {}
+
+if (packageJson.devDependencies[packageToResolve]) {
+  packageJson.devDependencies[packageToResolve] = versionResolution
+  packageJson.resolutions[packageToResolve] = versionResolution
+} else if (packageJson.dependencies[packageToResolve]) {
+  packageJson.dependencies[packageToResolve] = versionResolution
+  packageJson.resolutions[packageToResolve] = versionResolution
+} else {
+  console.info(
+    `${packageToResolve} is not a dependence of the specified project`
+  )
+  // process.exit(0)
+}
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
