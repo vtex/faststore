333489032d92fcd25081ee6d54ebba80028caa09
Fanny Chien <fanny.chien@vtex.com>
2025-05-14 09:58:30 -0300

    feat: Adds delivery methods in PDP filters (#2826)
    ## What's the purpose of this pull request?
    
    When Delivery Promise enabled:
    - Adds Shipping Filters component in PLP
    - List Shipping options available to filter
    - Enable add custom labels for shipping option via hCMS
    - Adds `Set Location` button when no zip code provided
    
    note: Button actions and facets filters is not fully integrated yet.
    
    ## How it works?
    
    - List Shipping options available to filter (according to the zipcode
    provided)
    
    |Desktop| Mobile|
    |-|-|
    |<img width="559" alt="image"
    src="https://github.com/user-attachments/assets/580c5e0c-9bb2-485a-8746-514f938cf54d"
    />|<img width="300" alt="image"
    src="https://github.com/user-attachments/assets/b4f23fa4-99ab-474e-9484-ab13cd7ff516"
    />|
    
    |Desktop| Mobile|
    |-|-|
    |<img width="691" alt="image"
    src="https://github.com/user-attachments/assets/c1b6a448-5e2e-46e4-9969-742e6a35649b"
    />|<img width="200" alt="image"
    src="https://github.com/user-attachments/assets/2ffe9385-da5f-4417-8d9b-7d1e207ebd1e"
    />|
    
    - Show `Set Location` Button when no zipcode is provided
    
    |Desktop| Mobile|
    |-|-|
    |<img width="518" alt="image"
    src="https://github.com/user-attachments/assets/d66d972b-fa25-4e0e-b07a-51300f5ff787"
    />|<img width="392" alt="image"
    src="https://github.com/user-attachments/assets/2e4bfe9b-412f-4f1f-b3f4-04055834424a"
    />|
    
    
    - CMS
    <img width="500" alt="image"
    src="https://github.com/user-attachments/assets/bb83b8b1-95f0-4bea-b49b-7f750a57b052"
    />
    
    ## How to test it?
    
    if you test using an account that delivery promise is not enabled,
    nothing should be changed.
    
    Run locally pointing to `vendemo` account, ou try this preview.
    
    1. if deliveryPromise.enabled is `false` in `discovery.config, no
    changes are expected.
    
    Enable deliveryPromise in `discovery.config`, and test the scenarios
    below:
    1. Navigate to a PLP, you should be able to see a `Delivery` section in
    the Filters component. (Also try in the mobile resolution)
    2. If no zipCode provided, you should be able to see `Set Location`
    button
    3. If zipCode provided, you should be able to see the list of available
    shipping options for that zipCode
    
    
    ### Starters Deploy Preview
    
    vtex-sites/faststoreqa.store#799
    
    ---------
    
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com>
    Co-authored-by: Larícia Mota <laricia.mota@vtex.com.br>

--- packages/sdk/src/search/facets.ts
+++ packages/sdk/src/search/facets.ts
@@ -43,7 +43,7 @@ export const setFacet = (
   return [...facets, facet]
 }
 
-export const toggleFacet = (facets: Facet[], item: Facet) => {
+export const toggleFacet = (facets: Facet[], item: Facet, unique?: boolean) => {
   const found = facets.find(
     (facet) => facet.key === item.key && facet.value === item.value
   )
@@ -52,7 +52,7 @@ export const toggleFacet = (facets: Facet[], item: Facet) => {
     return removeFacet(facets, item)
   }
 
-  return setFacet(facets, item, false)
+  return setFacet(facets, item, unique)
 }
 
 export const toggleFacets = (facets: Facet[], items: Facet[]) =>
