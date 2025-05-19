b1653c22bdb69b6aca910e5e9bac4e672937bdeb
eduardoformiga <eduardo.formiga@gmail.com>
2025-05-19 17:19:35 -0300

    refactor: simplify OrganizationDrawer component structure by removing unnecessary fragment

--- packages/core/src/components/account/MyAccountDrawer/OrganizationDrawer/OrganizationDrawerBody.tsx
+++ packages/core/src/components/account/MyAccountDrawer/OrganizationDrawer/OrganizationDrawerBody.tsx
@@ -4,20 +4,18 @@ import menuRoutes from 'src/customizations/src/myAccount/navigation'
 
 export const OrganizationDrawerBody = () => {
   return (
-    <>
-      <div data-fs-organization-drawer-body>
-        <div data-fs-organization-drawer-body-contents>
-          {menuRoutes.map((route) => (
-            <Link
-              key={route.route}
-              data-fs-organization-drawer-body-link
-              href={route.route}
-            >
-              {route.title}
-            </Link>
-          ))}
-        </div>
+    <div data-fs-organization-drawer-body>
+      <div data-fs-organization-drawer-body-contents>
+        {menuRoutes.map((route) => (
+          <Link
+            key={route.route}
+            data-fs-organization-drawer-body-link
+            href={route.route}
+          >
+            {route.title}
+          </Link>
+        ))}
       </div>
-    </>
+    </div>
   )
 }
