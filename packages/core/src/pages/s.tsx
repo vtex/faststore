465b4bc569f788b4fb54d4355e17bb83bee4927c
Larícia Mota <laricia.mota@vtex.com.br>
2025-05-19 14:31:41 -0300

    feat: Create Global Sections Settings tab (#2845)
    It creates a new tab inside the Global Sections CMS. In the new Settings
    tab, it will be possible to configure settings related to
    regionalization, which will be used by components such as the Region
    Modal and Region Popover.
    This PR doesn't update all components/sections, it'll be done after. In
    this PR, there is an update to `FilterDesktop` so we can validate the
    change.
    
    The merchant will be able to configure their regionalization-related
    message in a single place (Global Sections Settings tab) instead of
    duplicate messages through sections.
    To avoid introducing breaking changes, I haven't removed the messages
    from the sections yet - this will be done in v4. I've added a
    deprecation notice in every field that was created in the Settings tab.
    
    In v3, the components/sections will use the messages defined in the
    sections CMS; if they're blank, they will use the messages in the Global
    Sections Settings tab.
    <img width="1227" alt="Screenshot 2025-05-15 at 10 30 42"
    src="https://github.com/user-attachments/assets/e1b9b32f-7270-43a7-8016-abb536c08272"
    />
    
    In the CMS of the vendemo store, I've added messages in the Global
    Sections Settings tab using a `[GS]` at the beginning of every message
    so we can identify the ones that are coming from this tab.
    
    - PLP
    I've only removed (left blank) the messages in `Product List Page >
    Product Gallery > Filter > Delivery Settings > **Delivery Section
    description**` and `Product List Page > Product Gallery > Filter >
    Delivery Settings > Delivery Custom labels > **Shipping label**`, those
    are the ones that should use the GS one.
    
    - Search page
    The messages from `Search Page > Product Gallery > Filter > Delivery
    Settings` were all left blank (we hadn't updated this page to add the
    messages -- so this page didn't have messages defined for those cases,
    and we hadn't noticed 😣).
    
    | PLP | Search Page |
    | ---- | ---- |
    | <img width="649" alt="Screenshot 2025-05-15 at 09 59 40"
    src="https://github.com/user-attachments/assets/199d8e0d-a638-4b3e-ade2-95bbeaaf3a86"
    /> | <img width="772" alt="Screenshot 2025-05-15 at 10 09 08"
    src="https://github.com/user-attachments/assets/8edc352e-bf44-46bd-92a2-f9232b407d77"
    /> |
    | <img width="1496" alt="Screenshot 2025-05-15 at 10 15 40"
    src="https://github.com/user-attachments/assets/17284640-fb0e-45d0-b44b-8c7db98fc7be"
    /> | <img width="1499" alt="Screenshot 2025-05-15 at 10 16 30"
    src="https://github.com/user-attachments/assets/97801ddd-3b87-4c1f-a166-f8ef7b8d6fcd"
    /> |
    | <img width="1495" alt="Screenshot 2025-05-15 at 10 17 37"
    src="https://github.com/user-attachments/assets/1eb5023c-b724-41f3-8b4f-8cc57f7c4b4c"
    /> | <img width="1495" alt="Screenshot 2025-05-15 at 10 18 05"
    src="https://github.com/user-attachments/assets/4457b88e-31a7-4aa2-83ed-8038290a1e2c"
    /> |
    
    - Vendemo [[PR](https://github.com/dp-faststore-org/vendemo-dp/pull/17)
    /
    [preview](https://vendemo-cm9sir9v900u7z6llkl62l70j-7sfv6u49l.b.vtex.app/)]
    
    - [Slack discussion
    thread](https://vtex.slack.com/archives/C03L3CRCDC4/p1747248599365189)

--- packages/core/src/pages/s.tsx
+++ packages/core/src/pages/s.tsx
@@ -148,8 +148,13 @@ function Page({
           title: seoData.title,
           searchTerm: searchTerm ?? searchParams.term ?? undefined,
         }}
+<<<<<<< HEAD
         globalSections={globalSections}
         globalSettings={globalSettings}
+=======
+        globalSections={globalSections.sections}
+        globalSectionsSettings={globalSections.settings}
+>>>>>>> ac9905123 (feat: Create Global Sections Settings tab (#2845))
       />
     </SearchProvider>
   )
