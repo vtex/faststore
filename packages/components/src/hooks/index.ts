a4c2beba3668d0fc5528fa97fd6c907c3067e871
Larícia Mota <laricia.mota@vtex.com.br>
2025-05-22 15:29:04 -0300

    feat: Add Region Slider to set/change location (#2851)
    ## What's the purpose of this pull request?
    
    Create the Region Slider component to be used for setting and changing
    the location (postal code).
    
    ## How it works?
    
    It's triggered when the shopper clicks on the "Set location" button in
    the PLP filters and when they click on the postal code link in the PLP
    filters. The first one is the set location type of this Region Slider,
    and the second is the change location type.
    I've added a new state in the UIProvider and functions to handle its
    change for the Region Slider.
    
    ## How to test it?
    
    In the preview, test both the set and change location scenarios:
    #### Set location
    When no postal code is defined yet, the "Set location" button is
    displayed in the PLP filters.
    When the shopper clicks this button, a slideover is displayed to set the
    postal code.
    | Desktop | Mobile |
    | ---- | ---- |
    | <img width="1505" alt="Screenshot 2025-05-19 at 13 42 54"
    src="https://github.com/user-attachments/assets/06abcd1e-f963-4746-ad45-6003c675f49a"
    /> | <img width="218" alt="Screenshot 2025-05-19 at 13 47 14"
    src="https://github.com/user-attachments/assets/75db72a0-7f3b-45a1-ba6d-4c27617c5e55"
    /> |
    | <img width="1507" alt="Screenshot 2025-05-19 at 13 44 04"
    src="https://github.com/user-attachments/assets/3cf4a05d-a58b-47dc-bf19-bac99a53be55"
    /> | <img width="216" alt="Screenshot 2025-05-19 at 13 47 50"
    src="https://github.com/user-attachments/assets/bd87d1ed-9fdd-40f7-b009-e6480d10c89d"
    /> |
    
    #### Change location
    When a postal code was previously set, this postal code is displayed as
    a link in the PLP filters.
    When the shopper clicks this link, a slideover is displayed to change
    the postal code.
    | Desktop | Mobile |
    | ---- | ---- |
    | <img width="1503" alt="Screenshot 2025-05-19 at 13 44 24"
    src="https://github.com/user-attachments/assets/f2f5cbe7-5c11-47b1-9769-f1223a1a3339"
    /> | <img width="216" alt="Screenshot 2025-05-19 at 13 48 14"
    src="https://github.com/user-attachments/assets/9b1b513f-e3be-4f81-8328-7728dec1e876"
    /> |
    | <img width="1506" alt="Screenshot 2025-05-19 at 13 44 45"
    src="https://github.com/user-attachments/assets/06a8e8dc-d7b7-41d0-80f8-2462abd52058"
    /> | <img width="218" alt="Screenshot 2025-05-19 at 13 48 29"
    src="https://github.com/user-attachments/assets/62f1fc05-f24e-4fbb-83de-4c90d2342d63"
    /> |
    
    ### Starters Deploy Preview
    
    
    [Preview](https://vendemo-cm9sir9v900u7z6llkl62l70j-k5r6r35q4.b.vtex.app/)
    [PR](https://github.com/dp-faststore-org/vendemo-dp/pull/19)
    
    ## References
    
    - [Jira task](https://vtex-dev.atlassian.net/browse/SFS-2449)
    
    ---------
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    Co-authored-by: Otavio Moreira Meirelles <otavio.meirelles@vtex.com>
    Co-authored-by: Fanny Chien <fanny.chien@vtex.com>

--- packages/components/src/hooks/index.ts
+++ packages/components/src/hooks/index.ts
@@ -1,15 +1,20 @@
-export { default as UIProvider, Toast as ToastProps, useUI } from './UIProvider'
+export {
+  regionSliderTypes,
+  Toast as ToastProps,
+  default as UIProvider,
+  useUI,
+} from './UIProvider'
 export { useFadeEffect } from './useFadeEffect'
 export { useOnClickOutside } from './useOnClickOutside'
+export { useScrollDirection } from './useScrollDirection'
 export { useSearch } from './useSearch'
 export { useSKUMatrix } from './useSKUMatrix'
-export { useScrollDirection } from './useScrollDirection'
 export { useSlider } from './useSlider'
 export type {
-  UseSliderArgs,
-  SliderState,
-  SliderDispatch,
   SlideDirection,
+  SliderDispatch,
+  SliderState,
+  UseSliderArgs,
 } from './useSlider'
 export { useSlideVisibility } from './useSlideVisibility'
 export { useTrapFocus } from './useTrapFocus'
