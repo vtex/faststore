7c932842468b2c63a961a79b0f84969cef985a08
eduardoformiga <eduardo.formiga@gmail.com>
2025-05-28 18:22:16 -0300

    feat: transform in table

--- packages/core/src/components/account/MyAccountUserDetails/MyAccountUserDetails.tsx
+++ packages/core/src/components/account/MyAccountUserDetails/MyAccountUserDetails.tsx
@@ -17,37 +17,44 @@ export default function MyAccountUserDetails({
     <section className={styles.section}>
       <h1 data-fs-user-details-title>User details</h1>
       <div data-fs-user-details-container>
-        <div data-fs-user-details-row>
-          <span data-fs-user-details-row-label>Name</span>
-          <span data-fs-user-details-row-value>{name}</span>
-        </div>
-
-        <hr data-fs-user-details-divider />
-
-        <div data-fs-user-details-row>
-          <span data-fs-user-details-row-label>Email</span>
-          <span data-fs-user-details-row-value>{email}</span>
-        </div>
-
-        <hr data-fs-user-details-divider />
-
-        <div data-fs-user-details-row>
-          <span data-fs-user-details-row-label>Role</span>
-          <div data-fs-user-details-row-tags>
-            {role.map((r) => (
-              <span key={r} data-fs-user-details-row-value>
-                <Tag>{r}</Tag>
-              </span>
-            ))}
-          </div>
-        </div>
-
-        <hr data-fs-user-details-divider />
-
-        <div data-fs-user-details-row>
-          <span data-fs-user-details-row-label>Organizational unit</span>
-          <span data-fs-user-details-row-value>{orgUnit}</span>
-        </div>
+        <table data-fs-user-details-table>
+          <tbody>
+            <tr data-fs-user-details-row>
+              <th data-fs-user-details-row-label>Name</th>
+              <td data-fs-user-details-row-value>{name}</td>
+            </tr>
+            <tr data-fs-user-details-divider-row>
+              <td data-fs-user-details-divider></td>
+              <td data-fs-user-details-divider></td>
+            </tr>
+            <tr data-fs-user-details-row>
+              <th data-fs-user-details-row-label>Email</th>
+              <td data-fs-user-details-row-value>{email}</td>
+            </tr>
+            <tr data-fs-user-details-divider-row>
+              <td data-fs-user-details-divider></td>
+              <td data-fs-user-details-divider></td>
+            </tr>
+            <tr data-fs-user-details-row>
+              <th data-fs-user-details-row-label>Role</th>
+              <td data-fs-user-details-row-tags>
+                {role.map((r) => (
+                  <span key={r} data-fs-user-details-row-value>
+                    <Tag>{r}</Tag>
+                  </span>
+                ))}
+              </td>
+            </tr>
+            <tr data-fs-user-details-divider-row>
+              <td data-fs-user-details-divider></td>
+              <td data-fs-user-details-divider></td>
+            </tr>
+            <tr data-fs-user-details-row>
+              <th data-fs-user-details-row-label>Organizational unit</th>
+              <td data-fs-user-details-row-value>{orgUnit}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   )
