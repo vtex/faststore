5f7ce91ce7e3633677542550503c40da89f39156
Lucas Feijó <lucas.portela@vtex.com>
2025-06-04 16:37:12 -0300

    feat: Product Comparison `@faststore/api` changes

--- packages/api/src/platforms/vtex/resolvers/query.ts
+++ packages/api/src/platforms/vtex/resolvers/query.ts
@@ -5,6 +5,7 @@ import type {
   QueryListUserOrdersArgs,
   QueryProductArgs,
   QueryProductCountArgs,
+  QueryProductsArgs,
   QueryProfileArgs,
   QueryRedirectArgs,
   QuerySearchArgs,
@@ -217,6 +218,34 @@ export const Query = {
         cursor: (after + index).toString(),
       })),
     }
+  },
+  products: async (
+    _: unknown,
+    { productIds }: QueryProductsArgs,
+    ctx: Context
+  ) => {
+    const {
+      clients: { search },
+    } = ctx
+
+    if (!productIds?.length) {
+      return []
+    }
+
+    const query = `id:${productIds.join(';')}`
+    const products = await search.products({
+      page: 0,
+      count: productIds.length,
+      query,
+    })
+
+    return products.products
+      .flatMap((product) =>
+        product.items.map((sku) => enhanceSku(sku, product))
+      )
+      .filter(
+        (sku) => productIds.includes(sku.itemId) && sku.sellers.length > 0
+      )
   },
   allCollections: async (
     _: unknown,
