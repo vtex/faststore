b6e59446ae09b5a34f496d3d71130509b386e1f1
Artur Santiago <artursantiago2001@hotmail.com>
2025-06-05 13:43:29 -0300

    feat: get b2c name from getUserById

--- packages/api/src/platforms/vtex/resolvers/query.ts
+++ packages/api/src/platforms/vtex/resolvers/query.ts
@@ -32,6 +32,7 @@ import { isValidSkuId, pickBestSku } from '../utils/sku'
 import { SORT_MAP } from '../utils/sort'
 import { FACET_CROSS_SELLING_MAP } from './../utils/facets'
 import { StoreCollection } from './collection'
+import { getAuthCookie, parseJwt } from '../utils/cookies'
 
 export const Query = {
   product: async (_: unknown, { locator }: QueryProductArgs, ctx: Context) => {
@@ -465,9 +466,21 @@ export const Query = {
   accountName: async (_: unknown, __: unknown, ctx: Context) => {
     const {
       clients: { commerce },
+      headers,
+      account,
     } = ctx
 
     const { namespaces } = await commerce.session('')
+
+    const jwt = parseJwt(getAuthCookie(headers?.cookie ?? '', account))
+
+    if (jwt?.userId && !jwt?.isRepresentative) {
+      const user = await commerce.licenseManager.getUserById({
+        userId: jwt.userId,
+      })
+
+      return user?.name
+    }
 
     const { profile } = namespaces
 
