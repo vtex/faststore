4a6b93ee5e0c7cd30c5becd1b30e0af85bdb946a
Fanny Chien <fanny.chien@vtex.com>
2025-06-10 10:34:03 -0300

    feat: Edit pickup points slideover (#2872)
    ## What's the purpose of this pull request?
    
    This PR implements the Editing the store in the PLP delivery filter
    flow.
    The main changes added were:
    - Adds `changePickupPoint`  type to `regionSliderType`
    - List pickup points given a valid zipcode
    - Updates shipping filter with selected pickup point  
    
    reminder: After merging, run `cms sync` to update new section fields.
    (Few new fields were added: `changePickupPoint`,
    `changePickupPointApplyButtonLabel`, `noStoresAvailableInLocation` and
    `errorMessageHelper`)
    
    ## How it works?
    
    1. Buyer can edit the pickup store by clicking in the pickup filter in
    PLP. The SliderOver should appear.
    
    |Desktop|Mobile|
    |-|-|
    |<img width="936" alt="image"
    src="https://github.com/user-attachments/assets/981b704b-ffe5-4ab9-9426-ef5b9b6d8703"
    />|<img width="423" alt="image"
    src="https://github.com/user-attachments/assets/602cc21b-97ab-4dc5-8a63-4ece2325b16a"
    />|
    
    2. If the pickup filter is selected, the slider already opens with the
    field focused and the current location filled in.
    
    |Desktop|Mobile|
    |-|-|
    <img width="936" alt="image"
    src="https://github.com/user-attachments/assets/e5052ab9-88a2-4469-a71b-cabc696f341b"
    />|<img width="423" alt="image"
    src="https://github.com/user-attachments/assets/7fba073e-634a-4189-bb13-45d04cebd597"
    />|
    
    3. If the buyer doesn't change the postal code and selects another
    store, `Update` button should be available.
    - postal code stays the same and pickup point store should be updated in
    the PLP filters
     
    |Desktop|Mobile|
    |-|-|
    |<img width="941" alt="image"
    src="https://github.com/user-attachments/assets/2e1fa6e8-1e85-4c15-b0db-12ee880e5ec1"
    />|<img width="425" alt="image"
    src="https://github.com/user-attachments/assets/8ae9945b-29dd-42e3-89ae-5cc8b22d688e"
    />|
     
    4. If the buyer changes the postal code and selects a new store.
     - after updating the pickup point is updated in the PLP filter
     
    5. If buyer changes the postal code that returns invalid or unavailable
    items for the location, `Update` button should be disabled and a message
    should be displayed.
    
    |Desktop|Mobile|
    |-|-|
    |<img width="936" alt="image"
    src="https://github.com/user-attachments/assets/c8342e1e-b6b9-4833-9b32-67ef123a4129"
    />|<img width="391" alt="image"
    src="https://github.com/user-attachments/assets/c14b19ca-ac02-4af0-bfdb-aa5b9bde368e"
    />|
    
    _this error message layout could be slightly different - discussing with
    design team_
    
    6. If buyer changes the postal code and no pickup stores available for
    the location, `Update` button should be disabled and a message should be
    displayed.
     
    |Desktop|Mobile|
    |-|-|
    |<img width="938" alt="image"
    src="https://github.com/user-attachments/assets/ccdec434-6b47-4681-ad79-472ea9f71f88"
    />|<img width="398" alt="image"
    src="https://github.com/user-attachments/assets/0b2fa62a-b3cd-469b-994e-739d8c17943d"
    />
    
    
    ## How to test it?
    
    - You can run locally and point the account to `vendemo` or try on this
    preview
    [link](https://vendemo-cm9sir9v900u7z6llkl62l70j-kiasokde8.b.vtex.app).
    - Reproduce the scenarios listed above in desktop and mobile.
    
    ### Starters Deploy Preview
    
    - [preview
    link](https://vendemo-cm9sir9v900u7z6llkl62l70j-kiasokde8.b.vtex.app)
    
    ---------
    
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com>
    Co-authored-by: Larícia Mota <laricia.mota@vtex.com.br>

--- packages/core/src/components/search/Filter/FilterDesktop.tsx
+++ packages/core/src/components/search/Filter/FilterDesktop.tsx
@@ -14,13 +14,14 @@ import {
 import { gql } from '@generated/gql'
 import { deliveryPromise } from 'discovery.config'
 import { useFormattedPrice } from 'src/sdk/product/useFormattedPrice'
-import { usePickupPoints } from 'src/sdk/shipping/usePickupPoints'
 import type { useFilter } from 'src/sdk/search/useFilter'
+import { usePickupPoints } from 'src/sdk/shipping/usePickupPoints'
 import type { FilterSliderProps } from './FilterSlider'
 
 import { sessionStore } from 'src/sdk/session'
 import { getRegionalizationSettings } from 'src/utils/globalSettings'
-import RegionSlider from '../../region/RegionSlider/RegionSlider'
+
+import { RegionSlider } from 'src/components/region/RegionSlider'
 import FilterDeliveryOption from './FilterDeliveryOption'
 
 interface FilterDesktopProps
@@ -79,17 +80,28 @@ function FilterDesktop({
   const isDeliveryPromiseEnabled = deliveryPromise.enabled
   const isPickupAllEnabled =
     deliverySettingsData?.deliveryMethods?.pickupAll?.enabled ?? false
-  const defaultPickupPoint = pickupPoints?.[0] ?? undefined
   const shouldDisplayDeliveryButton = isDeliveryPromiseEnabled && !postalCode
+
+  const defaultPickupPoint = pickupPoints?.[0] ?? undefined
+  const selectedPickupPointId = state.selectedFacets.find(
+    ({ key }) => key === 'pickupPoint'
+  )?.value
+
+  // If no pickup point was previously selected, use the first one as default
+  const selectedPickupPoint =
+    pickupPoints?.find(({ id }) => id === selectedPickupPointId) ??
+    defaultPickupPoint
+
   const pickupInPointFacet =
-    isDeliveryPromiseEnabled && defaultPickupPoint
+    isDeliveryPromiseEnabled && selectedPickupPoint
       ? {
           value: 'pickup-in-point',
-          label: defaultPickupPoint?.name ?? defaultPickupPoint?.addressStreet,
+          label:
+            selectedPickupPoint?.name ?? selectedPickupPoint?.address.street,
           selected: !!state.selectedFacets.find(
             ({ value }) => value === 'pickup-in-point'
           ),
-          quantity: defaultPickupPoint?.totalItems ?? 0,
+          quantity: selectedPickupPoint?.totalItems ?? 0,
         }
       : undefined
 
@@ -105,7 +117,7 @@ function FilterDesktop({
         )
 
         // Remove old pickup `pickup in point` facet from list and search state
-        if (pickupInPointFacetIndex !== -1 && !defaultPickupPoint) {
+        if (pickupInPointFacetIndex !== -1 && !selectedPickupPoint) {
           if (state.selectedFacets.some(({ key }) => key === 'shipping')) {
             const selectedShippingFacet = state.selectedFacets.find(
               ({ key }) => key === 'shipping'
@@ -125,7 +137,7 @@ function FilterDesktop({
           )
         }
         // Prevent multiple `pickup in point` facet
-        else if (pickupInPointFacetIndex === -1 && defaultPickupPoint) {
+        else if (pickupInPointFacetIndex === -1 && selectedPickupPoint) {
           facet.values.push(pickupInPointFacet)
         }
         // Replace current `pickup-in-point` facet with the updated one
@@ -143,126 +155,133 @@ function FilterDesktop({
   }
 
   return (
-    <UIFilter
-      testId={`desktop-${testId}`}
-      title={title}
-      indicesExpanded={expanded}
-      onAccordionChange={(idx) =>
-        dispatch({ type: 'toggleExpanded', payload: idx })
-      }
-    >
-      {shouldDisplayDeliveryButton && (
-        <UIFilterFacets
-          key={`${testId}-delivery-unset`}
-          testId={testId}
-          index={0}
-          type=""
-          label={deliveryLabel}
-          description={deliverySettingsData?.description}
-        >
-          <UIButton
-            data-fs-filter-list-delivery-button
-            variant="secondary"
-            onClick={() => {
-              openRegionSlider(regionSliderTypes.setLocation)
-            }}
-            icon={<UIIcon name="MapPin" />}
-          >
-            {deliverySettingsData?.setLocationButtonLabel ?? 'Set Location'}
-          </UIButton>
-        </UIFilterFacets>
-      )}
-      {regionSliderType === regionSliderTypes.setLocation && (
-        <RegionSlider cmsData={regionalizationData} />
-      )}
-      {filteredFacets.map((facet, idx) => {
-        const index = shouldDisplayDeliveryButton ? idx + 1 : idx
-        const { __typename: type, label } = facet
-        const isExpanded = expanded.has(index)
-        const isDeliveryFacet = facet.key === 'shipping'
-
-        return (
+    <>
+      <UIFilter
+        testId={`desktop-${testId}`}
+        title={title}
+        indicesExpanded={expanded}
+        onAccordionChange={(idx) =>
+          dispatch({ type: 'toggleExpanded', payload: idx })
+        }
+      >
+        {shouldDisplayDeliveryButton && (
           <UIFilterFacets
-            key={`${testId}-${label}-${index}`}
+            key={`${testId}-delivery-unset`}
             testId={testId}
-            index={index}
-            type={type}
-            label={isDeliveryFacet ? deliveryLabel : label}
-            description={
-              isDeliveryFacet ? deliverySettingsData?.description : undefined
-            }
+            index={0}
+            type=""
+            label={deliveryLabel}
+            description={deliverySettingsData?.description}
           >
-            {type === 'StoreFacetBoolean' && isExpanded && (
-              <UIFilterFacetBoolean>
-                {facet.values.map(
-                  (item) =>
-                    (item.value !== 'pickup-all' || isPickupAllEnabled) && (
-                      <UIFilterFacetBooleanItem
-                        key={`${testId}-${facet.label}-${item.label}`}
-                        id={`${testId}-${facet.label}-${item.label}`}
-                        testId={testId}
-                        onFacetChange={(facet) => {
-                          if (facet.value === 'pickup-in-point') {
-                            togglePickupInPointFacet([
-                              facet,
-                              {
-                                key: 'pickupPoint',
-                                value: defaultPickupPoint?.id,
-                              },
-                            ])
-                          } else {
-                            toggleFilterFacet(facet)
-                          }
-
-                          resetInfiniteScroll(0)
-                        }}
-                        selected={item.selected}
-                        value={item.value}
-                        quantity={item.quantity}
-                        facetKey={facet.key}
-                        label={
-                          isDeliveryFacet ? (
-                            <FilterDeliveryOption
-                              item={item}
-                              deliveryMethods={
-                                deliverySettingsData?.deliveryMethods
-                              }
-                              cmsData={regionalizationData}
-                            />
-                          ) : (
-                            item.label
-                          )
-                        }
-                        type={isDeliveryFacet ? 'radio' : 'checkbox'}
-                      />
-                    )
-                )}
-              </UIFilterFacetBoolean>
-            )}
-            {type === 'StoreFacetRange' && isExpanded && (
-              <UIFilterFacetRange
-                facetKey={facet.key}
-                min={facet.min}
-                max={facet.max}
-                formatter={
-                  facet.key.toLowerCase() === 'price'
-                    ? useFormattedPrice
-                    : undefined
-                }
-                onFacetChange={(facet) => {
-                  setState({
-                    ...state,
-                    selectedFacets: setFacet(state.selectedFacets, facet, true),
-                    page: 0,
-                  })
-                  resetInfiniteScroll(0)
-                }}
-              />
-            )}
+            <UIButton
+              data-fs-filter-list-delivery-button
+              variant="secondary"
+              onClick={() => {
+                openRegionSlider(regionSliderTypes.setLocation)
+              }}
+              icon={<UIIcon name="MapPin" />}
+            >
+              {deliverySettingsData?.setLocationButtonLabel ?? 'Set Location'}
+            </UIButton>
           </UIFilterFacets>
-        )
-      })}
-    </UIFilter>
+        )}
+
+        {filteredFacets.map((facet, idx) => {
+          const index = shouldDisplayDeliveryButton ? idx + 1 : idx
+          const { __typename: type, label } = facet
+          const isExpanded = expanded.has(index)
+          const isDeliveryFacet = facet.key === 'shipping'
+
+          return (
+            <UIFilterFacets
+              key={`${testId}-${label}-${index}`}
+              testId={testId}
+              index={index}
+              type={type}
+              label={isDeliveryFacet ? deliveryLabel : label}
+              description={
+                isDeliveryFacet ? deliverySettingsData?.description : undefined
+              }
+            >
+              {type === 'StoreFacetBoolean' && isExpanded && (
+                <UIFilterFacetBoolean>
+                  {facet.values.map(
+                    (item) =>
+                      (item.value !== 'pickup-all' || isPickupAllEnabled) && (
+                        <UIFilterFacetBooleanItem
+                          key={`${testId}-${facet.label}-${item.label}`}
+                          id={`${testId}-${facet.label}-${item.label}`}
+                          testId={testId}
+                          onFacetChange={(facet) => {
+                            if (facet.value === 'pickup-in-point') {
+                              togglePickupInPointFacet([
+                                facet,
+                                {
+                                  key: 'pickupPoint',
+                                  value: selectedPickupPoint?.id,
+                                },
+                              ])
+                            } else {
+                              toggleFilterFacet(facet)
+                            }
+
+                            resetInfiniteScroll(0)
+                          }}
+                          selected={item.selected}
+                          value={item.value}
+                          quantity={item.quantity}
+                          facetKey={facet.key}
+                          label={
+                            isDeliveryFacet ? (
+                              <FilterDeliveryOption
+                                item={item}
+                                deliveryMethods={
+                                  deliverySettingsData?.deliveryMethods
+                                }
+                              />
+                            ) : (
+                              item.label
+                            )
+                          }
+                          type={isDeliveryFacet ? 'radio' : 'checkbox'}
+                        />
+                      )
+                  )}
+                </UIFilterFacetBoolean>
+              )}
+              {type === 'StoreFacetRange' && isExpanded && (
+                <UIFilterFacetRange
+                  facetKey={facet.key}
+                  min={facet.min}
+                  max={facet.max}
+                  formatter={
+                    facet.key.toLowerCase() === 'price'
+                      ? useFormattedPrice
+                      : undefined
+                  }
+                  onFacetChange={(facet) => {
+                    setState({
+                      ...state,
+                      selectedFacets: setFacet(
+                        state.selectedFacets,
+                        facet,
+                        true
+                      ),
+                      page: 0,
+                    })
+                    resetInfiniteScroll(0)
+                  }}
+                />
+              )}
+            </UIFilterFacets>
+          )
+        })}
+      </UIFilter>
+      <RegionSlider
+        cmsData={regionalizationData}
+        open={regionSliderType !== 'none'}
+      />
+    </>
   )
 }
 
