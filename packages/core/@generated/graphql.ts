96cce075a3fe3e47982cd17ce2d97ba18ad0700e
Lucas Feijó <lucas.portela@vtex.com>
2025-06-10 19:17:05 -0300

    feat: Create `DeliveryProvider` and `useDelivery` hook to manage Delivery Promise actions and state

--- packages/core/@generated/graphql.ts
+++ packages/core/@generated/graphql.ts
@@ -2649,6 +2649,32 @@ export type CartProductItemFragment = {
   }>
 }
 
+export type ClientPickupPointsQueryQueryVariables = Exact<{
+  geoCoordinates: InputMaybe<IStoreGeoCoordinates>
+  postalCode: InputMaybe<Scalars['String']['input']>
+  country: InputMaybe<Scalars['String']['input']>
+}>
+
+export type ClientPickupPointsQueryQuery = {
+  pickupPoints: {
+    paging: { total: number | null } | null
+    items: Array<{
+      distance: number | null
+      pickupPoint: {
+        id: string | null
+        friendlyName: string | null
+        address: {
+          street: string | null
+          number: string | null
+          postalCode: string | null
+          city: string | null
+          state: string | null
+        } | null
+      } | null
+    } | null> | null
+  } | null
+}
+
 export type SubscribeToNewsletterMutationVariables = Exact<{
   data: IPersonNewsletter
 }>
@@ -3021,32 +3047,6 @@ export type ClientShippingSimulationQueryQuery = {
       neighborhood: string | null
       state: string | null
     } | null
-  } | null
-}
-
-export type ClientPickupPointsQueryQueryVariables = Exact<{
-  geoCoordinates: InputMaybe<IStoreGeoCoordinates>
-  postalCode: InputMaybe<Scalars['String']['input']>
-  country: InputMaybe<Scalars['String']['input']>
-}>
-
-export type ClientPickupPointsQueryQuery = {
-  pickupPoints: {
-    paging: { total: number | null } | null
-    items: Array<{
-      distance: number | null
-      pickupPoint: {
-        id: string | null
-        friendlyName: string | null
-        address: {
-          street: string | null
-          number: string | null
-          postalCode: string | null
-          city: string | null
-          state: string | null
-        } | null
-      } | null
-    } | null> | null
   } | null
 }
 
@@ -3667,6 +3667,15 @@ export const ValidateCartMutationDocument = {
   ValidateCartMutationMutation,
   ValidateCartMutationMutationVariables
 >
+export const ClientPickupPointsQueryDocument = {
+  __meta__: {
+    operationName: 'ClientPickupPointsQuery',
+    operationHash: '66d8a051fb16b0ce0c6177ab85387b8b05518e21',
+  },
+} as unknown as TypedDocumentString<
+  ClientPickupPointsQueryQuery,
+  ClientPickupPointsQueryQueryVariables
+>
 export const SubscribeToNewsletterDocument = {
   __meta__: {
     operationName: 'SubscribeToNewsletter',
@@ -3765,15 +3774,6 @@ export const ClientShippingSimulationQueryDocument = {
 } as unknown as TypedDocumentString<
   ClientShippingSimulationQueryQuery,
   ClientShippingSimulationQueryQueryVariables
->
-export const ClientPickupPointsQueryDocument = {
-  __meta__: {
-    operationName: 'ClientPickupPointsQuery',
-    operationHash: '66d8a051fb16b0ce0c6177ab85387b8b05518e21',
-  },
-} as unknown as TypedDocumentString<
-  ClientPickupPointsQueryQuery,
-  ClientPickupPointsQueryQueryVariables
 >
 export const ServerManyProductsQueryDocument = {
   __meta__: {
