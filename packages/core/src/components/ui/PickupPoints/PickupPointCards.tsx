313e64a6e07c5ae614ef01e185b9845289a83d27
Fanny Chien <fanny.chien@vtex.com>
2025-06-10 10:34:03 -0300

    feat: Edit pickup points slideover (#2872)
    ## What's the purpose of this pull request?
    
    This PR implements the Editing the store in the PLP delivery filter
    flow.
    The main changes added were:
    - Adds `changePickupPoint`  type to `regionSliderType`
    - List pickup points given a valid zipcode
    - Updates shipping filter with selected pickup point  
    
    reminder: After merging, run `cms sync` to update new section fields.
    (Few new fields were added: `changePickupPoint`,
    `changePickupPointApplyButtonLabel`, `noStoresAvailableInLocation` and
    `errorMessageHelper`)
    
    ## How it works?
    
    1. Buyer can edit the pickup store by clicking in the pickup filter in
    PLP. The SliderOver should appear.
    
    |Desktop|Mobile|
    |-|-|
    |<img width="936" alt="image"
    src="https://github.com/user-attachments/assets/981b704b-ffe5-4ab9-9426-ef5b9b6d8703"
    />|<img width="423" alt="image"
    src="https://github.com/user-attachments/assets/602cc21b-97ab-4dc5-8a63-4ece2325b16a"
    />|
    
    2. If the pickup filter is selected, the slider already opens with the
    field focused and the current location filled in.
    
    |Desktop|Mobile|
    |-|-|
    <img width="936" alt="image"
    src="https://github.com/user-attachments/assets/e5052ab9-88a2-4469-a71b-cabc696f341b"
    />|<img width="423" alt="image"
    src="https://github.com/user-attachments/assets/7fba073e-634a-4189-bb13-45d04cebd597"
    />|
    
    3. If the buyer doesn't change the postal code and selects another
    store, `Update` button should be available.
    - postal code stays the same and pickup point store should be updated in
    the PLP filters
     
    |Desktop|Mobile|
    |-|-|
    |<img width="941" alt="image"
    src="https://github.com/user-attachments/assets/2e1fa6e8-1e85-4c15-b0db-12ee880e5ec1"
    />|<img width="425" alt="image"
    src="https://github.com/user-attachments/assets/8ae9945b-29dd-42e3-89ae-5cc8b22d688e"
    />|
     
    4. If the buyer changes the postal code and selects a new store.
     - after updating the pickup point is updated in the PLP filter
     
    5. If buyer changes the postal code that returns invalid or unavailable
    items for the location, `Update` button should be disabled and a message
    should be displayed.
    
    |Desktop|Mobile|
    |-|-|
    |<img width="936" alt="image"
    src="https://github.com/user-attachments/assets/c8342e1e-b6b9-4833-9b32-67ef123a4129"
    />|<img width="391" alt="image"
    src="https://github.com/user-attachments/assets/c14b19ca-ac02-4af0-bfdb-aa5b9bde368e"
    />|
    
    _this error message layout could be slightly different - discussing with
    design team_
    
    6. If buyer changes the postal code and no pickup stores available for
    the location, `Update` button should be disabled and a message should be
    displayed.
     
    |Desktop|Mobile|
    |-|-|
    |<img width="938" alt="image"
    src="https://github.com/user-attachments/assets/ccdec434-6b47-4681-ad79-472ea9f71f88"
    />|<img width="398" alt="image"
    src="https://github.com/user-attachments/assets/0b2fa62a-b3cd-469b-994e-739d8c17943d"
    />
    
    
    ## How to test it?
    
    - You can run locally and point the account to `vendemo` or try on this
    preview
    [link](https://vendemo-cm9sir9v900u7z6llkl62l70j-kiasokde8.b.vtex.app).
    - Reproduce the scenarios listed above in desktop and mobile.
    
    ### Starters Deploy Preview
    
    - [preview
    link](https://vendemo-cm9sir9v900u7z6llkl62l70j-kiasokde8.b.vtex.app)
    
    ---------
    
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com>
    Co-authored-by: Larícia Mota <laricia.mota@vtex.com.br>

---
+++ packages/core/src/components/ui/PickupPoints/PickupPointCards.tsx
@@ -0,0 +1,114 @@
+import {
+  EmptyState as UIEmptyState,
+  Icon as UIIcon,
+  List as UIList,
+  RadioGroup as UIRadioGroup,
+  RadioOption as UIRadioOption,
+} from '@faststore/ui'
+import type { ChangeEventHandler } from 'react'
+import { PickupPointCard } from './PickupPointCard'
+
+export type PickupPoint = {
+  id: string
+  name?: string
+  address?: {
+    street?: string
+    number?: string
+    postalCode?: string
+    city?: string
+    state?: string
+  }
+  distance?: number
+  totalItems?: number
+}
+
+export interface PickupPointCardsProps {
+  /**
+   * Selected option value.
+   */
+  selectedOption?: string
+  /**
+   * Function that is triggered when any option is selected.
+   */
+  onChange?: ChangeEventHandler<HTMLInputElement>
+  /**
+   * Message to be displayed when no pickup points are available.
+   */
+  noPickupPointsAvailableMessage?: string
+  /**
+   * Message to be displayed when there is an error when setting the location.
+   */
+  errorMessage?: {
+    title?: string
+    description?: string
+  }
+  /**
+   * Aria label for the radio group.
+   */
+  choosePickupPointAriaLabel?: string
+  /**
+   * List of pickup points to be displayed.
+   */
+  pickupPoints: PickupPoint[]
+}
+
+function PickupPointCards({
+  pickupPoints,
+  selectedOption,
+  onChange,
+  noPickupPointsAvailableMessage,
+  errorMessage: {
+    title: regionErrorMessage,
+    description: regionErrorHelperMessage,
+  },
+  choosePickupPointAriaLabel = 'Select a store',
+}: PickupPointCardsProps) {
+  if (regionErrorMessage) {
+    return (
+      <UIEmptyState
+        title={regionErrorMessage ?? ''}
+        titleIcon={
+          <UIIcon name="MapPin" width={56} height={56} weight="thin" />
+        }
+        bkgColor="light"
+      >
+        <p>{regionErrorHelperMessage}</p>
+      </UIEmptyState>
+    )
+  }
+
+  if (noPickupPointsAvailableMessage) {
+    return (
+      <UIEmptyState
+        title={
+          noPickupPointsAvailableMessage ?? 'No available stores near location.'
+        }
+        titleIcon={
+          <UIIcon name="Storefront" width={56} height={56} weight="thin" />
+        }
+        bkgColor="light"
+      />
+    )
+  }
+
+  return (
+    <UIList as="ol" data-fs-pickup-point-cards>
+      <UIRadioGroup
+        name="stores-radio-group"
+        onChange={onChange}
+        selectedValue={selectedOption}
+        aria-label={choosePickupPointAriaLabel}
+      >
+        {pickupPoints?.map((item) => (
+          <li data-fs-pickup-point-cards-item key={item.id}>
+            <UIRadioOption value={item.id} label={item.name}>
+              <PickupPointCard store={item} />
+            </UIRadioOption>
+          </li>
+        ))}
+      </UIRadioGroup>
+    </UIList>
+  )
+}
+
+export default PickupPointCards
