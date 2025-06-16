85facba834eb07e92d61d8903da6468e19a02072
Icaro Oliveira <icarovinici@gmail.com>
2025-06-16 19:06:33 -0300

    feat: update error handling in order query and style links in MyAccountListOrdersTable

--- packages/api/src/platforms/vtex/resolvers/query.ts
+++ packages/api/src/platforms/vtex/resolvers/query.ts
@@ -413,17 +413,17 @@ export const Query = {
         clientProfileData: order.clientProfileData,
       }
     } catch (error) {
-      const { message } = JSON.parse((error as Error).message).error as {
+      const result = JSON.parse((error as Error).message).error as {
         code: string
         message: string
         exception: any
       }
 
-      if (message?.toLowerCase()?.includes('order not found')) {
+      if (result?.message?.toLowerCase()?.includes('order not found')) {
         throw new NotFoundError(`No order found for id ${orderId}`)
       }
 
-      if (message?.toLowerCase()?.includes('acesso negado')) {
+      if (result?.message?.toLowerCase()?.includes('acesso negado')) {
         throw new ForbiddenError(
           `You are forbidden to interact with order with id ${orderId}`
         )
