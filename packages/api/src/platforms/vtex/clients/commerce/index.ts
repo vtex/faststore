c95c3fc4c07f1d6bc4489eace2e10fb8921d12e5
Artur Santiago <artur.santiago@cubos.io>
2025-06-19 10:08:42 -0300

    feat: add shopper first name to userName in b2b object in session (#2902)
    ## What's the purpose of this pull request?
    
    Show user name instead of contract name in side menu
    
    ## How it works?
    
    Use `shopper.firstName` from session api in b2b object in
    `validateSession`
    
    ## How to test it?
    
    <!--- Describe the steps with bullet points. Is there any external link
    that can be used to better test it or an example? --->
    
    ### Starters Deploy Preview
    
    <!--- Add a link to a deploy preview from `starter.store` with this
    branch being used. --->
    
    <!--- Tip: You can get an installable version of this branch from the
    CodeSandbox generated when this PR is created. --->
    
    ## References
    
    
    [SFS-2576](https://vtex-dev.atlassian.net/browse/SFS-2576?atlOrigin=eyJpIjoiOGFlMGExZDIyOTYwNDBiYmFiYzdiY2I1NDU5NWRlMzEiLCJwIjoiaiJ9)
    
    
    ![image](https://github.com/user-attachments/assets/9eb063b8-5677-4c25-ad2d-a2a182756d27)
    
    
    
    [SFS-2575]:
    https://vtex-dev.atlassian.net/browse/SFS-2575?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    [SFS-2576]:
    https://vtex-dev.atlassian.net/browse/SFS-2576?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- packages/api/src/platforms/vtex/clients/commerce/index.ts
+++ packages/api/src/platforms/vtex/clients/commerce/index.ts
@@ -395,7 +395,7 @@ export const VtexCommerce = (
 
       params.set(
         'items',
-        'profile.id,profile.email,profile.firstName,profile.lastName,store.channel,store.countryCode,store.cultureInfo,store.currencyCode,store.currencySymbol,authentication.customerId,authentication.storeUserId,authentication.storeUserEmail,authentication.unitId,authentication.unitName,checkout.regionId,'
+        'profile.id,profile.email,profile.firstName,profile.lastName,shopper.firstName,store.channel,store.countryCode,store.cultureInfo,store.currencyCode,store.currencySymbol,authentication.customerId,authentication.storeUserId,authentication.storeUserEmail,authentication.unitId,authentication.unitName,checkout.regionId'
       )
 
       const headers: HeadersInit = withCookie({
