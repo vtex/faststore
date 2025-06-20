651d6cd35da2fff7d2ba031bece0692e01626736
Larícia Mota <laricia.mota@vtex.com.br>
2025-06-20 12:29:45 -0300

    Fix rebase

--- packages/api/src/platforms/vtex/clients/commerce/index.ts
+++ packages/api/src/platforms/vtex/clients/commerce/index.ts
@@ -380,6 +380,7 @@ export const VtexCommerce = (
           'content-type': 'application/json',
           'X-FORWARDED-HOST': forwardedHost,
         })
+
         return fetchAPI(
           `${base}/api/checkout/pub/orders/${orderId}/user-cancel-request`,
           {
