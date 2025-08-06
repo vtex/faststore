8963833df7ea3862245adbc8fce817abe1189c8c
Guilherme Carvalho <77246+gvc@users.noreply.github.com>
2025-08-06 17:25:50 -0300

    Instead of running everything on the discovery path, copy it to cwd

--- packages/cli/src/utils/directory.ts
+++ packages/cli/src/utils/directory.ts
@@ -4,19 +4,8 @@ import fs from 'node:fs'
 export const withBasePath = (basepath: string) => {
   const tmpFolderName = '.faststore'
 
-  const getRoot = () => {
-    if (process.env.OCLIF_COMPILATION) {
-      return ''
-    }
-
-    if (basepath.endsWith(tmpFolderName)) {
-      // if the current working directory is the build folder (tmp folder), return the starter root
-      // this makes sure the semantics of the starter root are consistent with the directories declared below
-      return path.resolve(process.cwd(), path.join(basepath, '..'))
-    }
-
-    return path.resolve(process.cwd(), basepath)
-  }
+  // The basepath is where the discovery code is. It's either . or the path configured on faststore.json
+  const getRoot = () => basepath
 
   /*
    * This will loop from the basepath until the process.cwd() looking for node_modules/@faststore/core
@@ -55,7 +44,11 @@ export const withBasePath = (basepath: string) => {
     throw `Could not find @node_modules on ${basepath} or any of its parents until ${attemptedPath}`
   }
 
-  const tmpDir = path.join(getRoot(), tmpFolderName)
+  // TODO: Create a customizationsDir = getRoot() and stop calling getRoot()
+
+  // Changes from: const tmpDir = path.join(getRoot(), tmpFolderName)
+  // because we want to write the .faststore to the cwd (where the node_modules is)
+  const tmpDir = path.join(process.cwd(), tmpFolderName)
   const userSrcDir = path.join(getRoot(), 'src')
   const getPackagePath = (...packagePath: string[]) =>
     path.join(getRoot(), 'node_modules', ...packagePath)
