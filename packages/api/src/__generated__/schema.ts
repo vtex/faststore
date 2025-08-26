3a08734fce3326ca4d7b6a4b4d93c682f3468e1b
Ramon do Rosário <ramon.rosario@cubos.io>
2025-08-26 16:31:15 -0300

    feat: product comparison package api

--- packages/api/src/__generated__/schema.ts
+++ packages/api/src/__generated__/schema.ts
@@ -552,6 +552,8 @@ export type Query = {
   product: StoreProduct;
   /** Returns the total product count information based on a specific location accessible through the VTEX segment cookie. */
   productCount?: Maybe<ProductCountResult>;
+  /** Returns information about selected products. */
+  products: Array<StoreProduct>;
   /** Returns information about the profile. */
   profile?: Maybe<Profile>;
   /** Returns if there's a redirect for a search. */
@@ -607,6 +609,11 @@ export type QueryProductCountArgs = {
 };
 
 
+export type QueryProductsArgs = {
+  productIds: Array<Scalars['String']>;
+};
+
+
 export type QueryProfileArgs = {
   id: Scalars['String'];
 };
@@ -645,6 +652,21 @@ export type QueryShippingArgs = {
 
 export type QueryUserOrderArgs = {
   orderId: Scalars['String'];
+};
+
+export type SkuSpecificationField = {
+  __typename?: 'SKUSpecificationField';
+  id?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+  originalName?: Maybe<Scalars['String']>;
+};
+
+export type SkuSpecificationValue = {
+  __typename?: 'SKUSpecificationValue';
+  fieldId?: Maybe<Scalars['String']>;
+  id?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+  originalName?: Maybe<Scalars['String']>;
 };
 
 /** Search result. */
@@ -726,11 +748,17 @@ export type ShippingSla = {
   shippingEstimateDate?: Maybe<Scalars['String']>;
 };
 
+export type SkuSpecification = {
+  __typename?: 'SkuSpecification';
+  field: SkuSpecificationField;
+  values: Array<SkuSpecificationValue>;
+};
+
 export type SkuVariants = {
   __typename?: 'SkuVariants';
   /** SKU property values for the current SKU. */
   activeVariations?: Maybe<Scalars['ActiveVariations']>;
-  /** All possible variant combinations of the current product. It also includes the data for each variant. */
+  /** All available options for each SKU variant property, indexed by their name. */
   allVariantProducts?: Maybe<Array<StoreProduct>>;
   /** All available options for each SKU variant property, indexed by their name. */
   allVariantsByName?: Maybe<Scalars['VariantsByName']>;
@@ -761,6 +789,20 @@ export type SkuVariantsAvailableVariationsArgs = {
 
 export type SkuVariantsSlugsMapArgs = {
   dominantVariantName?: Maybe<Scalars['String']>;
+};
+
+export type Specification = {
+  __typename?: 'Specification';
+  name: Scalars['String'];
+  originalName: Scalars['String'];
+  values: Array<Scalars['String']>;
+};
+
+export type SpecificationGroup = {
+  __typename?: 'SpecificationGroup';
+  name: Scalars['String'];
+  originalName: Scalars['String'];
+  specifications: Array<Specification>;
 };
 
 /** Aggregate offer information, for a given SKU that is available to be fulfilled by multiple sellers. */
@@ -1127,6 +1169,8 @@ export type StoreProduct = {
   description: Scalars['String'];
   /** Global Trade Item Number. */
   gtin: Scalars['String'];
+  /** Indicates whether the product has specifications. */
+  hasSpecifications?: Maybe<Scalars['Boolean']>;
   /** Array of images. */
   image: Array<StoreImage>;
   /** Indicates product group related to this product. */
@@ -1145,8 +1189,12 @@ export type StoreProduct = {
   seo: StoreSeo;
   /** Stock Keeping Unit. Merchant-specific ID for the product. */
   sku: Scalars['String'];
+  /** Indicate the specifications of a product. */
+  skuSpecifications: Array<SkuSpecification>;
   /** Corresponding collection URL slug, with which to retrieve this entity. */
   slug: Scalars['String'];
+  /** Indicate the specifications of a group of SKUs. */
+  specificationGroups: Array<SpecificationGroup>;
   /** Sku Unit Multiplier */
   unitMultiplier?: Maybe<Scalars['Float']>;
 };
