4fb1cbe3808d92adcb4b29f3e60c25a0f42f3cb8
fanny <fanny.chien@vtex.com>
2025-09-02 10:28:36 -0300

    chore: Removes thumbor loader from image

--- packages/core/src/components/ui/Image/Image.tsx
+++ packages/core/src/components/ui/Image/Image.tsx
@@ -6,7 +6,7 @@ import loader from './loader'
 export type ImageProps = NextImageProps
 
 // Next loader function does not handle all props as height and options
-// so we use a custom loader to handle images using thumbor server with VTEX CDN
+// so we use a custom loader to handle images using VTEX CDN
 // https://nextjs.org/docs/api-reference/next/image#loader
 function Image({ loading = 'lazy', ...otherProps }: ImageProps) {
   return (
--- packages/core/src/components/ui/Image/loader.ts
+++ packages/core/src/components/ui/Image/loader.ts
@@ -1,29 +1,7 @@
-import storeConfig from 'discovery.config'
 import type { ImageLoaderProps } from 'next/image'
-const THUMBOR_SERVER = `https://${storeConfig.api.storeId}.vtexassets.com`
 
-export default function customImageLoader({
-  src,
-  width,
-  quality,
-}: ImageLoaderProps) {
-  return storeConfig.experimental.enableVtexAssetsLoader
-    ? fileServerLoader({ src, width })
-    : thumborLoader({ src, width, quality })
-}
-
-function thumborLoader({ src, width, quality }: ImageLoaderProps) {
-  const preSizeComponents = [THUMBOR_SERVER, 'unsafe']
-
-  // proportional to the width, enter a height of 0,
-  const height = 0
-  const finalSize = `${width}x${height}`
-
-  const postSizeComponents: string[] = ['center', 'middle']
-  quality && postSizeComponents.push(`filters:quality(${quality || 80})`)
-  postSizeComponents.push(encodeURIComponent(src))
-
-  return [...preSizeComponents, finalSize, ...postSizeComponents].join('/')
+export default function customImageLoader({ src, width }: ImageLoaderProps) {
+  return fileServerLoader({ src, width })
 }
 
 function fileServerLoader({ src, width }: ImageLoaderProps) {
