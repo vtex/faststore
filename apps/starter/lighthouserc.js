086199081ca1cad05601bea56e925a400adaba9b
Guilherme Carvalho <77246+gvc@users.noreply.github.com>
2025-09-03 13:23:51 -0300

    chore: remove cypress dependency (#2940)
    Given our internal discussions about the low value that the Cypress test
    suite brings to ourselves and to our customers, this PR aims to remove
    it Cypress.
    
    **PR Title and Commit Messages**
    
    - [X] PR title and commit messages follow the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) specification
    - Available prefixes: `feat`, `fix`, `chore`, `docs`, `style`,
    `refactor`, `ci` and `test`
    
    **PR Description**
    
    - [X] Added a label according to the PR goal - `breaking change`, `bug`,
    `contributing`, `performance`, `documentation`..
    
    **Dependencies**
    
    - [X] Committed the `pnpm-lock.yaml` file when there were changes to the
    packages
    
    **Documentation**
    
    - [X] PR description
    - [ ] For documentation changes, ping `@Mariana-Caetano` to review and
    update (Or submit a doc request)
    
    ---------
    
    Co-authored-by: Matheus P. Silva <cout.matheusps@gmail.com>

---
+++ apps/starter/lighthouserc.js
@@ -0,0 +1,24 @@
+const lhConfig = require('@faststore/lighthouse').default
+
+const { lighthouse: lh } = require('./discovery.config')
+
+module.exports = lhConfig({
+  urls: Object.values(lh.pages),
+  server: lh.server,
+  assertions: {
+    'csp-xss': 'off',
+
+    // The following rules should be removed one this is fixed:
+    // https://github.com/BuilderIO/partytown/issues/178
+    'categories:best-practices': [
+      'error',
+      {
+        minScore: 0.9,
+      },
+    ],
+    deprecations: 'warn',
+    'resource-summary:stylesheet:count': ['warn', { maxNumericValue: 400 }],
+    'resource-summary:script:count': ['warn', { maxNumericValue: 20 }],
+    'resource-summary:script:size': ['error', { maxNumericValue: 220 * 1024 }],
+  },
+})
