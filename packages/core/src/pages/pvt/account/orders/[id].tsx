fc129af2e56500aabb035af199d6c5fe7609bbd6
Eduardo Formiga <eduardo.formiga@gmail.com>
2025-09-12 13:18:02 -0300

    Update packages/core/src/pages/pvt/account/orders/[id].tsx
    Co-authored-by: Lucas Feijó <lucas.portela@vtex.com>

--- packages/core/src/pages/pvt/account/orders/[id].tsx
+++ packages/core/src/pages/pvt/account/orders/[id].tsx
@@ -24,7 +24,7 @@ import { execute } from 'src/server'
 import { injectGlobalSections } from 'src/server/cms/global'
 import { getMyAccountRedirect } from 'src/utils/myAccountRedirect'
 import { extractStatusFromError } from 'src/utils/utilities'
-import storeConfig from '../../../../../discovery.config'
+import storeConfig from 'discovery.config'
 
 const COMPONENTS: Record<string, ComponentType<any>> = {
   ...GLOBAL_COMPONENTS,
