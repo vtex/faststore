64960bff1d778f85830b9497d2c9374a3a89ebfd
Matheus P. Silva <cout.matheusps@gmail.com>
2025-09-12 08:43:38 -0300

    feat: create script module

---
+++ packages/script/src/index.ts
@@ -0,0 +1,19 @@
+import Script from 'next/script'
+import {
+  GoogleTagManager,
+  GoogleAnalytics,
+  GoogleMapsEmbed,
+  sendGAEvent,
+  sendGTMEvent,
+  YouTubeEmbed,
+} from '@next/third-parties/google'
+
+export {
+  Script,
+  GoogleTagManager,
+  GoogleAnalytics,
+  GoogleMapsEmbed,
+  YouTubeEmbed,
+  sendGAEvent,
+  sendGTMEvent,
+}
