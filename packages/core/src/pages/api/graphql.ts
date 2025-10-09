09e1981098cd93c56e4dbe44c312251dec587795
Otavio Meirelles <otavio.meirelles@vtex.com>
2025-10-09 16:16:02 -0300

    fix: making deps build before generations

--- packages/core/src/pages/api/graphql.ts
+++ packages/core/src/pages/api/graphql.ts
@@ -2,4 +2,5 @@ import discoveryConfig from '../../../discovery.config'
 import { ServerExecuteFunction } from '../../server'
 
 import { FaststoreAPIHandler } from '@vtex/faststore-api'
+
 export default FaststoreAPIHandler(discoveryConfig, ServerExecuteFunction)
