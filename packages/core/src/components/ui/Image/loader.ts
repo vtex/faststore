2f0bd7f299f7dbca904666ffde1a7840286ee0ca
Larícia Mota <laricia.mota@vtex.com.br>
2025-10-23 16:35:36 -0300

    Update image loader

--- packages/core/src/components/ui/Image/loader.ts
+++ packages/core/src/components/ui/Image/loader.ts
@@ -1,32 +1,12 @@
-import storeConfig from 'discovery.config'
 import type { ImageLoaderProps } from 'next/image'
-const THUMBOR_SERVER = `https://${storeConfig.api.storeId}.vtexassets.com`
 
-export default function customImageLoader({
-  src,
-  width,
-  quality,
-}: ImageLoaderProps) {
-  return storeConfig.experimental.enableVtexAssetsLoader
-    ? fileServerLoader({ src, width })
-    : thumborLoader({ src, width, quality })
-}
-
-function thumborLoader({ src, width, quality }: ImageLoaderProps) {
-  const preSizeComponents = [THUMBOR_SERVER, 'unsafe']
-
-  // proportional to the width, enter a height of 0,
-  const height = 0
-  const finalSize = `${width}x${height}`
-
-  const postSizeComponents: string[] = ['center', 'middle']
-  quality && postSizeComponents.push(`filters:quality(${quality || 80})`)
-  postSizeComponents.push(encodeURIComponent(src))
-
-  return [...preSizeComponents, finalSize, ...postSizeComponents].join('/')
-}
-
-function fileServerLoader({ src, width }: ImageLoaderProps) {
+export default function customImageLoader({ src, width }: ImageLoaderProps) {
+  if (src.includes('vtex.file-manager-graphql')) {
+    const url = new URL(src)
+    url.searchParams.set('width', width.toString())
+    url.searchParams.set('aspect', 'true')
+    return url.toString()
+  }
   // Regular expression to match the pattern: /ids/{number}/{filename}.{extension}?{queryParams}
   const regex = /(\/ids\/\d+)\/([^/?]+)(\.[^/?]+)(\?.+)?$/
 
