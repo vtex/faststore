6dd76f3158176a3f5aed8614cca7381e4ad2fc20
fanny <fanny.chien@vtex.com>
2025-12-05 15:39:16 -0300

    feat: Adds i18nButton component

---
+++ packages/core/src/components/ui/I18nButton/I18nButton.tsx
@@ -0,0 +1,32 @@
+import { Icon, Button as UIButton } from '@faststore/ui'
+
+import { useSession } from 'src/sdk/session'
+
+const I18nButton = ({ icon }: { icon: string }) => {
+  const { locale, currency } = useSession()
+
+  // const displayText = `${locale} | ${currency.code}`
+  const displayText = 'IT/EUR'
+
+  return (
+    <UIButton
+      icon={<Icon name={icon} width={18} height={18} weight="bold" />}
+      iconPosition="left"
+      variant="tertiary"
+      data-fs-i18n-button
+      onClick={() => {
+        // TODO: Open the i18nModal to set region/locale/currency/language
+        console.log('I18nButton clicked')
+      }}
+    >
+      <div data-i18n-button-text>
+        <span data-i18n-button-text-locale>US</span>
+        <span data-i18n-button-text-separator>/</span>
+        <span data-i18n-button-text-currency>{currency.code}</span>
+      </div>
+      <Icon name="CaretDown" width={12} height={12} />
+    </UIButton>
+  )
+}
+
+export default I18nButton
