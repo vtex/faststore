0fea6faf812c0f0af3f139b6507b2f342264209c
eduardoformiga <eduardo.formiga@gmail.com>
2025-12-23 19:35:59 -0300

    fix: correct comment for platform specific config in discovery configuration

--- packages/core/discovery.config.default.js
+++ packages/core/discovery.config.default.js
@@ -29,7 +29,7 @@ module.exports = {
   // Ecommerce Platform
   platform: 'vtex',
 
-  // Platform specific configs for API
+  // Platform specific config for API
   api: {
     storeId: 'storeframework',
     workspace: 'master',
