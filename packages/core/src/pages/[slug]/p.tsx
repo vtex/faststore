cd07a3475c3a7febd554cdcbb1808bd63babd241
fanny <fanny.chien@vtex.com>
2026-01-16 22:57:43 -0300

    chore: enables sets formatter for product price metatag

--- packages/core/src/pages/[slug]/p.tsx
+++ packages/core/src/pages/[slug]/p.tsx
@@ -12,7 +12,6 @@ import type {
 } from '@generated/graphql'
 import { default as GLOBAL_COMPONENTS } from 'src/components/cms/global/Components'
 import RenderSections from 'src/components/cms/RenderSections'
-import { getComponentKey } from 'src/utils/cms'
 import BannerNewsletter from 'src/components/sections/BannerNewsletter/BannerNewsletter'
 import { OverriddenDefaultBannerText as BannerText } from 'src/components/sections/BannerText/OverriddenDefaultBannerText'
 import { OverriddenDefaultBreadcrumb as Breadcrumb } from 'src/components/sections/Breadcrumb/OverriddenDefaultBreadcrumb'
@@ -27,6 +26,7 @@ import PLUGINS_COMPONENTS from 'src/plugins'
 import { getRedirect } from 'src/sdk/redirects'
 import { useSession } from 'src/sdk/session'
 import { execute } from 'src/server'
+import { getComponentKey } from 'src/utils/cms'
 
 import storeConfig from 'discovery.config'
 import {
@@ -177,7 +177,12 @@ function Page({
         additionalMetaTags={[
           {
             property: 'product:price:amount',
-            content: product.offers.lowPrice?.toString() ?? undefined,
+            content:
+              product.offers.lowPrice != null
+                ? (storeConfig.seo.pdp?.priceAmountFormatter?.(
+                    product.offers.lowPrice
+                  ) ?? product.offers.lowPrice.toString())
+                : undefined,
           },
           {
             property: 'product:price:currency',
