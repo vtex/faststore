6ca71164163de3125af6feadb2808e3a125d1b7e
fanny <fanny.chien@vtex.com>
2026-02-02 15:20:25 -0300

    chore: Removes redudant locale

--- packages/api/src/platforms/vtex/resolvers/validateSession.ts
+++ packages/api/src/platforms/vtex/resolvers/validateSession.ts
@@ -160,7 +160,6 @@ export const validateSession = async (
 
   const newSession = {
     ...oldSession,
-    locale: oldSession.locale,
     currency: {
       code: store?.currencyCode?.value ?? oldSession.currency.code,
       symbol: store?.currencySymbol?.value ?? oldSession.currency.symbol,
