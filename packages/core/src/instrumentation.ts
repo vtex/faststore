266d23b5ddff6f8a0e7bd417c1ddcc773ab29a03
Otavio Meirelles <otavio.meirelles@vtex.com>
2026-02-19 13:58:55 -0300

    fix: build error importing with-type

--- packages/core/src/instrumentation.ts
+++ packages/core/src/instrumentation.ts
@@ -1,8 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { name, version } = await import('../package.json', {
-      with: { type: 'json' },
-    })
+    const { name, version } = await import('../package.json')
     ;(await import('@faststore/diagnostics')).getTelemetryClient({
       name,
       version,
