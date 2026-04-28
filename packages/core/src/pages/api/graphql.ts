c16cebb533eb4f89b1fda0e116ef50b6c903cffd
eduardoformiga <eduardo.formiga@gmail.com>
2026-04-28 16:58:23 -0300

    fix: Enhance session validation logic to skip token refresh in local environments
    - Introduced a check to bypass token refresh logic when running in local environments (localhost or 127.0.0.1).
    - Updated GraphQL API handler and session validation functions to ensure consistent behavior across environments.
    - This change prevents unnecessary refresh attempts during local development, improving the developer experience.

--- packages/core/src/pages/api/graphql.ts
+++ packages/core/src/pages/api/graphql.ts
@@ -161,7 +161,12 @@ const handler: NextApiHandler = async (request, response) => {
     // value is used to cache bust the request if there is a VtexIdclientAutCookie
     const { operation, variables, query, v: value } = parseRequest(request)
 
+    const isLocal =
+      request.headers.host?.startsWith('localhost') ||
+      request.headers.host?.startsWith('127.0.0.1')
+
     if (
+      !isLocal &&
       operation.__meta__.operationName === 'ValidateSession' &&
       discoveryConfig.experimental?.refreshToken
     ) {
