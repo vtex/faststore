054d0861befde8e2a84454a07d43c5d741869fbd
eduardoformiga <eduardo.formiga@gmail.com>
2026-05-01 21:28:09 -0300

    refactor: Simplify runtime checks in register function for improved clarity
    - Introduced boolean variables for runtime checks: isNodeRuntime, isProduction, and isVercel.
    - Updated the conditional logic in the register function to enhance readability and maintainability.

--- packages/core/src/instrumentation.ts
+++ packages/core/src/instrumentation.ts
@@ -1,5 +1,9 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  const isNodeRuntime = process.env.NEXT_RUNTIME === 'nodejs'
+  const isProduction = process.env.NODE_ENV === 'production'
+  const isVercel = !!process.env.VERCEL
+
+  if (isNodeRuntime && isProduction && !isVercel) {
     await require('pino')
     await require('next-logger')
   }
