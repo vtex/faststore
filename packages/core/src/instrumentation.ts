1c7a007435345c812de98a3b50c3f14c8dd5eb61
eduardoformiga <eduardo.formiga@gmail.com>
2026-05-01 21:45:54 -0300

    refactor: streamline conditional logic in register function
    - Simplified the runtime checks by removing unnecessary boolean variables.
    - Enhanced readability by directly using environment variables in the conditional statements.

--- packages/core/src/instrumentation.ts
+++ packages/core/src/instrumentation.ts
@@ -1,10 +1,11 @@
 export async function register() {
-  const isNodeRuntime = process.env.NEXT_RUNTIME === 'nodejs'
-  const isProduction = process.env.NODE_ENV === 'production'
-  const isVercel = !!process.env.VERCEL
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const isProduction = process.env.NODE_ENV === 'production'
+    const isVercel = !!process.env.VERCEL
 
-  if (isNodeRuntime && isProduction && !isVercel) {
-    await require('pino')
-    await require('next-logger')
+    if (isProduction && !isVercel) {
+      await require('pino')
+      await require('next-logger')
+    }
   }
 }
