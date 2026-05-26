c5ab38b326cb418b1448b88d7c240abf3642827f
fanny <fanny.chien@vtex.com>
2026-05-25 22:59:31 -0300

    chore: Extracts/Creates localization product context

--- packages/core/src/sdk/localization/LocalizedProductContext.tsx
+++ packages/core/src/sdk/localization/LocalizedProductContext.tsx
@@ -30,7 +30,7 @@ interface LocalizedProductProviderProps extends PropsWithChildren {
 export function LocalizedProductProvider({
   otherLocales,
   children,
-}: Readonly<LocalizedProductProviderProps>) {
+}: LocalizedProductProviderProps) {
   const value = useMemo(
     () => ({ otherLocales: otherLocales ?? null }),
     [otherLocales]
